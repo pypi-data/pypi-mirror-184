76dfa378f98026df70810fccb805b1c10e1792de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:53 +0000

    Add package proxynt/0.0.3/proxynt-0.0.3.tar.gz

---
+++ packages/proxynt/proxynt-0.0.3.tar.gz/proxynt-0.0.3/setup.py
@@ -0,0 +1,39 @@
+import glob
+import os.path
+
+from constant.system_constant import SystemConstant
+
+package_name = SystemConstant.PACKAGE_NAME
+version='0.0.3'
+import setuptools
+l = setuptools.find_packages(where='.')
+l = [package_name + '.'+ x for x in l]
+l.append(package_name)
+
+html_file = glob.glob(os.path.join('server/template', '**.html'))
+js_file = glob.glob(os.path.join('server/template/js', '**.js'))
+css_file = glob.glob(os.path.join('server/template/css', '**.css'))
+font_file = glob.glob(os.path.join('server/template/css/fonts', '**.woff'))
+
+setuptools.setup(
+    name=package_name,
+    version=version,
+    package_dir={
+        package_name: '.',
+    },
+    # data_files=[
+    #     (f'{package_name}/server/template', html_file),
+    #     (f'{package_name}/server/template/js', js_file),
+    #     (f'{package_name}/server/template/css', css_file),
+    #     (f'{package_name}/server/template/css/fonts', font_file),
+    # ],
+    include_package_data=True,
+
+    entry_points=f"""
+    [console_scripts]
+    nt_client = {package_name}.run_client:main
+    nt_server = {package_name}.run_server:main
+    """,
+    packages=l,
+    install_requires=['tornado',  'typing_extensions']
+)
