f4f881f737e59eaf6a57a0cd99579ad086ef8d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:46 +0000

    Add package tsma/1.0.2/tsma-1.0.2.tar.gz

---
+++ packages/tsma/tsma-1.0.2.tar.gz/tsma-1.0.2/tsma/analyses/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+tsma.analyses
+
+"""
+import statistics, vandin, metrics, clustering
+
+__all__ = ['statistics', 'vandin', 'metrics', 'clustering']
+
---
+++ packages/tsma/tsma-1.0.2.tar.gz/tsma-1.0.2/tsma/collect/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+tsma.collect
+"""
+import output_management, iterators, data_collect
+
+__all__ = ['output_management', 'iterators', 'data_collect']
+
