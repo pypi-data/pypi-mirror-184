616cb8825907696cde8160e11db6fa49d0c91574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:40 +0000

    Add package SigProfilerAssignment/0.0.24/SigProfilerAssignment-0.0.24.tar.gz

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.24.tar.gz/SigProfilerAssignment-0.0.24/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.24'
+version = '0.0.24'
+Update = 'v0.0.24: Upgrade decomposition plot dependencies, rename signature subgroups'
+
+    
+    
