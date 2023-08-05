abe9758ce068ee146a1a355b5c2543f2d601ed05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:08 +0000

    Add package multiwidth/0.1.1/multiwidth-0.1.1-py3-none-any.whl

---
+++ packages/multiwidth/multiwidth-0.1.1-py3-none-any.whl/multiwidth/__init__.py
@@ -0,0 +1,5 @@
+"""A Python module for reading/writing fixed-width files with variable column widths"""
+
+from .multiwidth import split, get_letter_locations, process_line, load, loads, dumps, dump
+
+__version__ = "0.1.0"
