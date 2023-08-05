bb12240b884045827721642705916e5054fbf515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:24 +0000

    Add package aio-geojson-planefinderlocal/0.2/aio_geojson_planefinderlocal-0.2-py3-none-any.whl

---
+++ packages/aio-geojson-planefinderlocal/aio_geojson_planefinderlocal-0.2-py3-none-any.whl/aio_geojson_planefinderlocal/feed_manager.py
@@ -0,0 +1,21 @@
+"""Feed Manager for Flight Air Map Incidents feed."""
+from aio_geojson_client.feed_manager import FeedManagerBase
+from aiohttp import ClientSession
+from .feed import PlanefinderLocalFeed
+
+class PlanefinderLocalFeedManager(FeedManagerBase):
+    """Feed Manager for Flight Air Map feed."""
+
+    def __init__(
+        self,
+        websession: ClientSession,
+        generate_callback,
+        update_callback,
+        remove_callback,
+        coordinates=None,
+        feed_url=None,
+        filter_radius=None,
+    ):
+        """Initialize the Flight Air Map Manager."""
+        feed = PlanefinderLocalFeed(websession, coordinates, feed_url, filter_radius)
+        super().__init__(feed, generate_callback, update_callback, remove_callback)
