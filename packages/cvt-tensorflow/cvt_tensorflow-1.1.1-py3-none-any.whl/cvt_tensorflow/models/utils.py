f98f7f3621eb1ac7875f21bc2e06c9379f56788c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:04 +0000

    Add package cvt-tensorflow/1.1.1/cvt_tensorflow-1.1.1-py3-none-any.whl

---
+++ packages/cvt-tensorflow/cvt_tensorflow-1.1.1-py3-none-any.whl/cvt_tensorflow/models/utils.py
@@ -0,0 +1,71 @@
+import collections.abc as container_abcs
+from itertools import repeat
+from typing import List
+
+import tensorflow as tf
+
+
+def _to_channel_last(x: tf.Tensor) -> tf.Tensor:
+    """
+    Parameters
+    ----------
+    x : tf.Tensor
+        Tensor of shape: (B, C, H, W).
+
+    Returns
+    -------
+    tf.Tensor
+        Tensor of shape: (B, H, W, C).
+    """
+    return tf.transpose(x, perm=[0, 2, 3, 1])
+
+
+def _to_channel_first(x: tf.Tensor) -> tf.Tensor:
+    """
+    Parameters
+    ----------
+    x : tf.Tensor
+        Tensor of shape: (B, H, W, C).
+
+    Returns
+    -------
+    tf.Tensor
+        Tensor of shape: (B, C, H, W).
+    """
+    return tf.transpose(x, perm=[0, 3, 1, 2])
+
+
+def run_layers_list(input_: tf.Tensor, list_: List[tf.keras.layers.Layer]) -> tf.Tensor:
+    """
+    Parameters
+    ----------
+    input_ : tf.Tensor
+        Input Tensor.
+    list_ : List[tf.keras.layers.Layer]
+        List of tf.keras.layers.Layer.
+
+    Returns
+    -------
+    tf.Tensor
+        Output Tensor.
+    """
+    x = input_
+    for layer in list_:
+        x = layer(x)
+    return x
+
+
+def _ntuple(n):
+    def parse(x):
+        if isinstance(x, container_abcs.Iterable):
+            return x
+        return tuple(repeat(x, n))
+
+    return parse
+
+
+to_1tuple = _ntuple(1)
+to_2tuple = _ntuple(2)
+to_3tuple = _ntuple(3)
+to_4tuple = _ntuple(4)
+to_ntuple = _ntuple
