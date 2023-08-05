1e556b6b8bb7a87f7e24ceb61e8400a04542166d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:21 +0000

    Add package cloudtik/1.0.1/cloudtik-1.0.1-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/cloudtik/cloudtik-1.0.1-cp39-cp39-manylinux2014_x86_64.whl/cloudtik/__init__.py
@@ -0,0 +1,4 @@
+
+# Replaced with the current commit when building the wheels.
+__commit__ = "1b3f29769ab3ef3668423ec1f2fcff10f9c98685"
+__version__ = "1.0.1"
---
+++ packages/cloudtik/cloudtik-1.0.1-cp39-cp39-manylinux2014_x86_64.whl/cloudtik/runtime/spark/scripts/services.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ ! -n "${HADOOP_HOME}" ]; then
+    echo "HADOOP_HOME environment variable is not set."
+    exit 1
+fi
+
+if [ ! -n "${SPARK_HOME}" ]; then
+    echo "SPARK_HOME environment variable is not set."
+    exit 1
+fi
+
+case "$1" in
+start-head)
+    echo "Starting Resource Manager..."
+    $HADOOP_HOME/bin/yarn --daemon start resourcemanager
+    echo "Starting Spark History Server..."
+    export SPARK_LOCAL_IP=${CLOUDTIK_NODE_IP}; $SPARK_HOME/sbin/start-history-server.sh > /dev/null
+    echo "Starting Jupyter..."
+    nohup jupyter lab --no-browser > /tmp/logs/jupyterlab.log 2>&1 &
+    ;;
+stop-head)
+    $HADOOP_HOME/bin/yarn --daemon stop resourcemanager
+    $SPARK_HOME/sbin/stop-history-server.sh
+    # workaround for stopping jupyter when password being set
+    JUPYTER_PID=$(pgrep jupyter)
+    if [ -n "$JUPYTER_PID" ]; then
+      echo "Stopping Jupyter..."
+      kill $JUPYTER_PID >/dev/null 2>&1
+    fi
+    ;;
+start-worker)
+    $HADOOP_HOME/bin/yarn --daemon start nodemanager
+    ;;
+stop-worker)
+    $HADOOP_HOME/bin/yarn --daemon stop nodemanager
+    ;;
+-h|--help)
+    echo "Usage: $0 start-head|stop-head|start-worker|stop-worker" >&2
+    ;;
+*)
+    echo "Usage: $0 start-head|stop-head|start-worker|stop-worker" >&2
+    ;;
+esac
+
+exit 0
