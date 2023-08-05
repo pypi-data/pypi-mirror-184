62518c32a172b2667be9c4f9bb798de4ffda28e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:40 +0000

    Add package umshini/0.0.2/umshini-0.0.2.tar.gz

---
+++ packages/umshini/umshini-0.0.2.tar.gz/umshini-0.0.2/Umshini/example_client.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import time
+import traceback
+from .tournament_client import TournamentConnection
+from colorama import Fore, Style
+
+
+class ColosseumTournamentAgent:
+    def __init__(self, policy, latency=0,  games=["__all__"], port=12345, direct=False, host="localhost", maximum_rounds=10000):
+        self.host = host
+        self.policy = policy
+        self.port = port
+        self.botname = ''
+        self.key = ''
+        self.latency = latency
+        self.direct = direct
+        self.games = games
+        self.tournament = None
+        self.maximum_rounds = maximum_rounds
+
+    def connect(self, botname, key):
+        self.botname = botname
+        self.key = key
+        try:
+            # TODO: Use policy for test
+            # Test that policy runs without errors in local environments
+            self.tournament = TournamentConnection(
+                self.host, self.port, self.botname, self.key, available_games=self.games
+            )
+            print(Fore.GREEN + "Bot: {}'s policy has passed environment verifications".format(self.botname))
+            print(Style.RESET_ALL)
+        except Exception as e:
+            print(Fore.RED + "Bot: {}'s policy has failed verification testing in environment: ".format(self.botname))
+            print(Style.RESET_ALL)
+            quit()
+
+    def run(self):
+        # Connect to tournament server each round, until the end signal is received.
+        try:
+            env = self.tournament.next_match()
+        except Exception as e:
+            print(Fore.RED + str(e))
+            print(traceback.format_exc())
+            print(Style.RESET_ALL)
+            quit()
+        current_round = 1
+        while env is not None:
+            surprises = []
+            term = False
+            trunc = False
+            timestep = 0
+            obs = rew = info = None
+            while not (term or trunc):
+                #if timestep % 100 == 0:
+                #    print(f"{self.botname}: Timestep {timestep}\n")
+                time.sleep(self.latency / 1000)  # Used to simulate network latency
+                (action, surprise) = self.policy(obs, rew, term, trunc, info)  # receive action and surprise from user
+                obs, rew, term, trunc, info = env.step(action)  # Send action to game server
+                surprises.append(surprise)  # Collect surprise for later use when game server supports
+                timestep += 1
+            current_round += 1
+            if current_round > self.maximum_rounds:
+                env = None
+            else:
+                env = self.tournament.next_match()
---
+++ packages/umshini/umshini-0.0.2.tar.gz/umshini-0.0.2/setup.py
@@ -0,0 +1,81 @@
+from setuptools import find_packages, setup
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from Cython.Build import cythonize
+import numpy as np
+import atexit
+from subprocess import check_call
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        print("pre develop")
+        develop.run(self)
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+        check_call("AutoROM --accept-license".split())
+        print("post develop")
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        print("pre install")
+        install.run(self)
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+        check_call("AutoROM --accept-license".split())
+        print("post install")
+
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Umshini Client API'
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
+    name='umshini',
+    version=VERSION,
+    author='umshini team',
+    author_email="j.k.terry@swarmlabs.com",
+    description=DESCRIPTION,
+    url='https://github.com/umshini/umshini',
+    long_description=long_description,
+    #long_description_content_type="text/markdown",
+    #keywords=["Reinforcement Learning", "game", "RL", "AI", "gym"],
+    #python_requires=">=3.6, <3.10",
+    #data_files=[("", ["LICENSE.txt"])],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Cython',
+        'colorama',
+        'pettingzoo[atari,classic]==1.22.3',
+        'supersuit==3.7.1',
+        'autorom[accept-rom-license]',
+        'gymnasium',
+        'numpy',
+        'halo',
+    ],
+    setup_requires=[
+        'Cython',
+        'colorama',
+        'pettingzoo[atari,classic]==1.22.3',
+        'supersuit==3.7.1',
+        'autorom[accept-rom-license]',
+        'gymnasium',
+        'numpy',
+        'halo',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
