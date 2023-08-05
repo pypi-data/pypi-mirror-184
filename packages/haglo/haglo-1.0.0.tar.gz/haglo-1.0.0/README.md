a9c0843443619ece8f14b4a2f2f5f683bf9a2fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:32 +0000

    Add package haglo/1.0.0/haglo-1.0.0.tar.gz

---
+++ packages/haglo/haglo-1.0.0.tar.gz/haglo-1.0.0/README.md
@@ -0,0 +1,4 @@
+from haglo import tiktok_timestamp
+
+create_time = 1234567890 # your timestamp
+print(tiktok_timestamp(create_time))
---
+++ packages/haglo/haglo-1.0.0.tar.gz/haglo-1.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Tiktok'
+LONG_DESCRIPTION = 'Tiktok'
+
+# Setting up
+setup(
+    name="haglo",
+    version=VERSION,
+    author="haglo",
+    author_email="haglohd007@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests', 'datetime'],
+    keywords=['python', 'tiktok', 'tiktokapi'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
