937238a4be9c3fb7d8094430d5ff63b40f52c0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:09 +0000

    Add package nonebot-plugin-ayaka/1.0.1b1/nonebot_plugin_ayaka-1.0.1b1.tar.gz

---
+++ packages/nonebot-plugin-ayaka/nonebot_plugin_ayaka-1.0.1b1.tar.gz/nonebot_plugin_ayaka-1.0.1b1/ayaka/master.py
@@ -0,0 +1,28 @@
+from .box import AyakaBox, get_box, box_list, get_group
+from .lazy import GroupMessageEvent
+
+box = AyakaBox("盒子管理器")
+'''盒子管理器'''
+
+
+@box.on_cmd(cmds="盒子帮助", always=True)
+async def show_help():
+    '''展示盒子帮助'''
+    if box.arg:
+        b = get_box(str(box.arg))
+        if b:
+            await box.send(b.all_help)
+    else:
+        infos = [b.all_help for b in box_list]
+        await box.send_many(infos)
+
+
+@box.on_cmd(cmds="盒子状态", always=True)
+async def show_state(event: GroupMessageEvent):
+    '''展示盒子状态'''
+    group = get_group(event.group_id)
+    if group.current_box_name:
+        info = f"正在运行应用[{group.current_box_name}]\n当前状态[{group.state}]"
+    else:
+        info = "当前没有任何应用在运行"
+    await box.send(info)
