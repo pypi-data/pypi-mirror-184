35b4942a131f4705ec75249c1dd69087f91b0862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:33 +0000

    Add package Mensajes/2.0/Mensajes-2.0.tar.gz

---
+++ packages/Mensajes/Mensajes-2.0.tar.gz/Mensajes-2.0/Mensajes/hola/Saludos.py
@@ -0,0 +1,13 @@
+def saludar():
+  print("Hola, te saludo desde saludos.saludar():")  
+
+def prueba():
+  print("Esto es una de la nueva versión")
+
+class Saludo:
+    def __init__(self):
+       print("Te saludo desde Saludo.__init__()")
+
+
+if __name__ == '__main__':
+    saludar()
---
+++ packages/Mensajes/Mensajes-2.0.tar.gz/Mensajes-2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Mensajes',
+    version='2.0',
+    description='un paquete para saludar y despedir',
+    author='jhonatan Rincon',
+    author_email='jeramix92@gmail.com',
+    url='https://www.hektor.dev',
+    packages=['Mensajes','Mensajes.hola','Mensajes.adios'],
+    scripts=['Test.py']
+)
