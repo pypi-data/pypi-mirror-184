905ffb73ca517ec803f422310cd6b90822232322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:19 +0000

    Add package as-common/0.9.1/as_common-0.9.1.tar.gz

---
+++ packages/as-common/as_common-0.9.1.tar.gz/as_common-0.9.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='as_common',
+    version='0.9.1',
+    description='common modules used in Arthasangraha suite of projects',
+    author='Arthasangraha',
+    author_email='roopesh@arthasangraha.com',
+    packages=[],
+    install_requires=[
+        'ta-lib', 'pandas', 'numpy'
+    ],
+)
