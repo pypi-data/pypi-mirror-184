27d16d466aa1aa59638bad2d0c66ba925410b657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:05 +0000

    Add package fe507/0.1.18a0/fe507-0.1.18a0.tar.gz

---
+++ packages/fe507/fe507-0.1.18a0.tar.gz/fe507-0.1.18a0/fe507/__init__.py
@@ -0,0 +1,12 @@
+# src / __init__.py
+# Created by azat at 1.01.2023
+
+"""FE507 is a simple yet very powerful, 'batteries included' intuitive package for data analysing."""
+__version__ = "0.1.18-alpha"
+
+from fe507.settings import settings
+from fe507.fe507 import *
+
+'''
+https://flit.pypa.io/en/latest/index.html
+'''
