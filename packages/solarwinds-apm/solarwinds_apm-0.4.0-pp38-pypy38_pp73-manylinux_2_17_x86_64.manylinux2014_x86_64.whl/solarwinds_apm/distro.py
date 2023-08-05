a6e475ce5b1bb0f50fbb404d7354296ab11c51d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:19 +0000

    Add package solarwinds-apm/0.4.0/solarwinds_apm-0.4.0-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/solarwinds-apm/solarwinds_apm-0.4.0-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/solarwinds_apm/distro.py
@@ -0,0 +1,27 @@
+"""Module to configure OpenTelemetry to work with SolarWinds backend"""
+
+from os import environ
+
+from opentelemetry.environment_variables import (
+    OTEL_PROPAGATORS,
+    OTEL_TRACES_EXPORTER,
+)
+from opentelemetry.instrumentation.distro import BaseDistro
+
+from solarwinds_apm.apm_constants import (
+    INTL_SWO_DEFAULT_PROPAGATORS,
+    INTL_SWO_DEFAULT_TRACES_EXPORTER,
+)
+
+
+class SolarWindsDistro(BaseDistro):
+    """OpenTelemetry Distro for SolarWinds reporting environment"""
+
+    def _configure(self, **kwargs):
+        """Configure default OTel exporter and propagators"""
+        environ.setdefault(
+            OTEL_TRACES_EXPORTER, INTL_SWO_DEFAULT_TRACES_EXPORTER
+        )
+        environ.setdefault(
+            OTEL_PROPAGATORS, ",".join(INTL_SWO_DEFAULT_PROPAGATORS)
+        )
