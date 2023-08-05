14fe66f1b7284bc36f90bbbfd5b38c0fa9c30340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:02 +0000

    Add package jmcfunction/1.2.10b1/jmcfunction-1.2.10b1-py3-none-any.whl

---
+++ packages/jmcfunction/jmcfunction-1.2.10b1-py3-none-any.whl/jmc/__main__.py
@@ -0,0 +1,33 @@
+"""Main module"""
+import atexit
+from .compile import Logger
+from .terminal.utils import RestartException, handle_exception
+from .terminal import GlobalData, Colors, start
+
+VERSION = "v1.2.10-beta.1"
+CONFIG_FILE_NAME = "jmc_config.json"
+GlobalData().init(VERSION, CONFIG_FILE_NAME)
+
+from . import terminal_commands  # noqa
+
+logger = Logger(__name__)
+
+
+def main():
+    """Main function"""
+    atexit.register(lambda: print(Colors.EXIT.value))
+    logger.info("Starting session")
+    global_data = GlobalData()
+    while True:
+        try:
+            start()
+        except Exception as error:
+            handle_exception(error, global_data.EVENT, is_ok=True)
+        except RestartException:
+            pass
+        except KeyboardInterrupt:
+            break
+
+
+if __name__ == "__main__":
+    main()
