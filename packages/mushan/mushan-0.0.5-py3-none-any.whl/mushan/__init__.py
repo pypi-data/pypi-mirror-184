5137d41a9d9a218341bd249ce2ed546920a067f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:46 +0000

    Add package mushan/0.0.5/mushan-0.0.5-py3-none-any.whl

---
+++ packages/mushan/mushan-0.0.5-py3-none-any.whl/mushan/__init__.py
@@ -0,0 +1,14 @@
+
+
+from mushan import shell
+from mushan import audio
+from mushan import nb
+from mushan import ml
+from mushan import dl
+from mushan import tool
+from mushan import log
+
+__all__ = [
+    "__version__",
+    "shell",
+]
---
+++ packages/mushan/mushan-0.0.5-py3-none-any.whl/mushan/audio/audio.py
@@ -0,0 +1,4 @@
+from IPython.display import Audio
+import librosa
+
+
