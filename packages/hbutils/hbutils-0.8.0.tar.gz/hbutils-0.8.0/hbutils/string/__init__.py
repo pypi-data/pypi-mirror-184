fbec950d44da1011fe19e811ab6068ae0babc827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:28 +0000

    Add package hbutils/0.8.0/hbutils-0.8.0.tar.gz

---
+++ packages/hbutils/hbutils-0.8.0.tar.gz/hbutils-0.8.0/hbutils/string/__init__.py
@@ -0,0 +1,8 @@
+"""
+Overview:
+    String process module, include some useful utilities for string.
+"""
+from .ordinal import *
+from .plural import *
+from .template import *
+from .trunc import *
