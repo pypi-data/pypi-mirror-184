f366f0989856a897d48990bcfd83a381f7e3e522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:07 +0000

    Add package django-serve/0.3/django-serve-0.3.tar.gz

---
+++ packages/django-serve/django-serve-0.3.tar.gz/django-serve-0.3/README.md
@@ -0,0 +1,77 @@
+# django-serve
+
+A gunicorn based django runserver command.
+
+
+## Install ##
+
+`python3 -m pip install django-serve`
+
+and add to `INSTALLED_APPS`
+
+```!python
+#!python
+INSTALLED_APPS = [
+    ...
+    "django_serve.apps.ServeConfig",
+    ...
+]
+```
+
+## Usage ##
+
+```!shell
+./manage.py serve
+```
+
+### options
+
+* `--addr` The socket address to bind [default=127.0.0.1]
+* `--port` The socket port to bind [default=8000]
+* `--workers` The number of worker processes for handling requests [default max(1, (cpu + 1) / 2)]
+* `--name` A base to use with setproctitle for process naming [default=django]
+* `--wsgi` Dotted path to wsgi application [default=settings.WSGI_APPLICATION]
+* `--config` The Gunicorn config file [default=None]
+* `--log-level` The granularity of Error log outputs (debug, info, warning, error, critical) [default=info]
+* `--logformat` The access log format (default='"%(m)s %(U)s%(q)s %(H)s" %(s)s %(B)s' same as default django runserver)
+* `--logger-class` The logger you want to use to log events [default=gunicorn_color.Logger if installed else gunicorn default one]
+* `--reload-engine` Which reload engine to use between polling or inotify (or not at all) [default=auto]
+
+
+## CHANGES ##
+
+### 0.3.0
+
+* add LOG_LEVEL env support
+* add WORKERS env support
+* drop Django < 3.2 support
+* add Django 4.0 and 4.1 support
+* drop Python < 3.8 support
+* add Python 3.11 support
+* update isort and flake8 config
+* add rule for `__future__` annotations
+
+### 0.2.0
+
+* use black
+* drop Python 3.5 support
+* add Django 3.2 support
+* add Python 3.9 and 3.10 support
+* add django-serve command
+* add support for python -m django_serve
+* install inotify only on linux platform
+* add --reload-engine and DJANGO_DEFAULT_RELOAD_ENGINE option
+* update default workers count
+
+### 0.1.1
+
+* add gunicorn-color as required
+
+### 0.1.0
+
+* initial implementation
+* add support for gunicorn config file
+* add custom django reloader
+* add log-level option
+* add logformat configuration
+* add logger-class option
---
+++ packages/django-serve/django-serve-0.3.tar.gz/django-serve-0.3/django_serve/main.py
@@ -0,0 +1,64 @@
+# Copyright (C) Raffaele Salmaso <raffaele@salmaso.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from __future__ import annotations
+
+import os
+
+try:
+    from gunicorn import reloader
+    from gunicorn.app.wsgiapp import WSGIApplication
+except ImportError:
+    raise Exception("You need gunicorn to be installed")
+
+
+class InotifyReloader(reloader.InotifyReloader):
+    """Patch InotifyReloader to process only py, .env and translation files."""
+
+    def __init__(self, *args, **kwargs):
+        import inotify.constants
+
+        # extends original gunicorn event mask to monitor metadata changes
+        self.event_mask = self.event_mask | inotify.constants.IN_ATTRIB
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        self._dirs = self.get_dirs()
+
+        for dirname in self._dirs:
+            if os.path.isdir(dirname):
+                self._watcher.add_watch(dirname, mask=self.event_mask)
+
+        for event in self._watcher.event_gen():
+            if event is None:
+                continue
+
+            filename = event[3]
+            if filename.endswith((".py", ".env", ".mo")):
+                self._callback(filename)
+
+
+reloader.preferred_reloader = InotifyReloader if reloader.has_inotify else reloader.Reloader
+reloader.reloader_engines["auto"] = reloader.preferred_reloader
+reloader.reloader_engines["inotify"] = InotifyReloader
+
+
+def run():
+    WSGIApplication("").run()
