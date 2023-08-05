6d8441b27018e1414b8ebfe31c6cc20d0d6878e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:52 +0000

    Add package nonebot-plugin-ayaka/1.0.1b0/nonebot_plugin_ayaka-1.0.1b0-py3-none-any.whl

---
+++ packages/nonebot-plugin-ayaka/nonebot_plugin_ayaka-1.0.1b0-py3-none-any.whl/ayaka/__init__.py
@@ -0,0 +1,27 @@
+'''1.0.1b0
+
+https://bridgel.github.io/ayaka_doc/1.0.1/
+'''
+# ---- 方便生成api文档 ----
+try:
+    from nonebot import get_driver
+    get_driver()
+except:
+    import nonebot
+    nonebot.init()
+# ----  ----
+
+# ---- ayaka box ----
+from .box import AyakaBox
+from .config import AyakaConfig, load_data_from_file
+from .helpers import Timer, get_user, do_nothing, singleton, run_in_startup, slow_load_config
+from .orm import AyakaDB, AyakaGroupDB, AyakaUserDB
+
+# ---- 方便使用 ----
+from .lazy import *
+
+# ---- master ----
+from . import master
+
+# ---- log ----
+from . import log
---
+++ packages/nonebot-plugin-ayaka/nonebot_plugin_ayaka-1.0.1b0-py3-none-any.whl/ayaka/master.py
@@ -0,0 +1,29 @@
+from .box import AyakaBox, get_box, box_list, get_group
+from .lazy import on_command, GroupMessageEvent
+
+box = AyakaBox("盒子管理器")
+HELP = on_command(
+    "盒子帮助", aliases={"box help", "box_help", "box-help", "box帮助"})
+STATE = on_command(
+    "盒子状态", aliases={"box state", "box_state", "box-state", "box状态"})
+
+
+@HELP.handle()
+async def show_help():
+    if box.arg:
+        b = get_box(str(box.arg))
+        if b:
+            await box.send(b.all_help)
+    else:
+        infos = [b.all_help for b in box_list]
+        await box.send_many(infos)
+
+
+@STATE.handle()
+async def show_state(event: GroupMessageEvent):
+    group = get_group(event.group_id)
+    if group.current_box_name:
+        info = f"正在运行应用[{group.current_box_name}]\n当前状态[{group.state}]"
+    else:
+        info = "当前没有任何应用在运行"
+    await box.send(info)
