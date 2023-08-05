6abfd6a67603b20385ab3ae45df445a6a59a9140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:15 +0000

    Add package maus/0.3.0a0/maus-0.3.0a0.tar.gz

---
+++ packages/maus/maus-0.3.0a0.tar.gz/maus-0.3.0a0/tests/integration_tests/test_beginn_msb_maus.py
@@ -0,0 +1,8 @@
+import pytest  # type:ignore[import]
+from helpers import create_maus_and_assert, write_to_file_or_assert_equality  # type:ignore[import]
+
+
+class TestBeginnMsbMaus:
+    """
+    A unit test that ensures that the 11042/43/44 MAUS.json are created.
+    """
