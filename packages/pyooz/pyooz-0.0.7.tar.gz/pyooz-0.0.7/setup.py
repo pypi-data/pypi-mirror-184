10f337bf0ae951f0a59ffeac2709cd7351153614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:56 +0000

    Add package pyooz/0.0.7/pyooz-0.0.7.tar.gz

---
+++ packages/pyooz/pyooz-0.0.7.tar.gz/pyooz-0.0.7/setup.py
@@ -0,0 +1,39 @@
+import os
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup, Extension
+
+ooz_sources = [
+    os.path.join('ooz/dep/ooz/', x) for x in [
+        "bitknit.cpp",
+        "kraken.cpp",
+        "lzna.cpp",
+    ]
+]
+
+ext_modules = [
+    Pybind11Extension(
+        'ooz',
+        sources=['ooz/ooz_bindings.cpp'] + ooz_sources,
+        define_macros=[('OOZ_BUILD_DLL', 1)],
+        include_dirs=['ooz/dep/ooz/simde'],
+    ),
+]
+
+from pathlib import Path
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name='pyooz',
+    version='0.0.7',
+    description="ooz decompression bindings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    cmdclass={"build_ext": build_ext},
+    ext_modules=ext_modules,
+)
