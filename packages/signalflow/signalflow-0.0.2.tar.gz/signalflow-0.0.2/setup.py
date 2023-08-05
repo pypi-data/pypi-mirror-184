9d12777fcb4bb6060b6ac8e0ebfc7f1489fb4246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:49 +0000

    Add package signalflow/0.0.2/signalflow-0.0.2.tar.gz

---
+++ packages/signalflow/signalflow-0.0.2.tar.gz/signalflow-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='signalflow',
+    version='0.0.2',
+    description='',
+    long_description='',
+    url='https://github.com/ideoforms/signalflow',
+    author='Daniel Jones',
+    author_email='daniel@jones.org.uk',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
