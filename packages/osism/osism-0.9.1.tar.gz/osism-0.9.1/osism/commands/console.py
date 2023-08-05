d65940ccdedd64452105c26a3c0df4c7813f8b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:39 +0000

    Add package osism/0.9.1/osism-0.9.1.tar.gz

---
+++ packages/osism/osism-0.9.1.tar.gz/osism-0.9.1/osism/commands/console.py
@@ -0,0 +1,49 @@
+import subprocess
+
+from cliff.command import Command
+
+
+class Run(Command):
+    def get_parser(self, prog_name):
+        parser = super(Run, self).get_parser(prog_name)
+        parser.add_argument(
+            "--type",
+            default="ssh",
+            choices=["ansible", "container", "ssh"],
+            help="Type of the console (default: %(default)s)",
+        )
+        parser.add_argument(
+            "host",
+            nargs=1,
+            type=str,
+            help="Hostname or address of the console to connect",
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        type_console = parsed_args.type
+        host = parsed_args.host[0]
+
+        if type_console == "ansible":
+            subprocess.call(f"/run-ansible-console.sh {host}", shell=True)
+        elif type_console == "ssh":
+            # FIXME: use paramiko or something else more Pythonic + make operator user + key configurable
+            subprocess.call(
+                f"/usr/bin/ssh -i /ansible/secrets/id_rsa.operator -o StrictHostKeyChecking=no -o LogLevel=ERROR dragon@{host}",
+                shell=True,
+            )
+        elif type_console == "container":
+            target_containername = host.split("/")[1]
+            target_host = host.split("/")[0]
+            target_command = "bash"
+
+            ssh_command = f"docker exec -it {target_containername} {target_command}"
+            ssh_options = (
+                "-o RequestTTY=force -o StrictHostKeyChecking=no -o LogLevel=ERROR"
+            )
+
+            # FIXME: use paramiko or something else more Pythonic + make operator user + key configurable
+            subprocess.call(
+                f"/usr/bin/ssh -i /ansible/secrets/id_rsa.operator {ssh_options} dragon@{target_host} {ssh_command}",
+                shell=True,
+            )
---
+++ packages/osism/osism-0.9.1.tar.gz/osism-0.9.1/osism/commands/container.py
@@ -0,0 +1,30 @@
+import argparse
+import subprocess
+
+from cliff.command import Command
+
+
+class Run(Command):
+    def get_parser(self, prog_name):
+        parser = super(Run, self).get_parser(prog_name)
+        parser.add_argument("host", nargs=1, type=str, help="Hostname or address")
+        parser.add_argument(
+            "command",
+            nargs=argparse.REMAINDER,
+            type=str,
+            help="Command to run",
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        host = parsed_args.host[0]
+        command = " ".join(parsed_args.command)
+
+        ssh_command = f"docker {command}"
+        ssh_options = "-o StrictHostKeyChecking=no -o LogLevel=ERROR"
+
+        # FIXME: use paramiko or something else more Pythonic + make operator user + key configurable
+        subprocess.call(
+            f"/usr/bin/ssh -i /ansible/secrets/id_rsa.operator {ssh_options} dragon@{host} {ssh_command}",
+            shell=True,
+        )
