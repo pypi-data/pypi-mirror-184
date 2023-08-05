7423d0135e48919659b3449bed4b711349470358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:15 +0000

    Add package pyswitchbee/1.7.18/pyswitchbee-1.7.18.tar.gz

---
+++ packages/pyswitchbee/pyswitchbee-1.7.18.tar.gz/pyswitchbee-1.7.18/src/switchbee/api/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from packaging.version import parse as parse_version
+
+__all__ = ["polling", "wsrpc", "central_unit"]
+
+from .polling import CentralUnitPolling
+from .wsrpc import (
+    CentralUnitWsRPC,
+    DeviceConnectionError,
+    InvalidMessage,
+    ConnectionClosed,
+)
+
+from .central_unit import (
+    SwitchBeeError,
+    SwitchBeeTokenError,
+    SwitchBeeDeviceOfflineError,
+    CUVersion,
+)
+
+
+def is_wsrpc_api(api: CentralUnitPolling | CentralUnitWsRPC) -> bool:
+    assert isinstance(api.version, CUVersion)
+    if parse_version(str(api.version)) >= parse_version("1.4.6.1"):
+        return True
+    return False
