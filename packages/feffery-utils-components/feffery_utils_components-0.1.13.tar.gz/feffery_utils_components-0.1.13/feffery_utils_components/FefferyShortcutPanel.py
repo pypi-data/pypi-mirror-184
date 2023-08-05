a33e2560c94e9193b15837101315d65c566267d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:56 +0000

    Add package feffery-utils-components/0.1.13/feffery_utils_components-0.1.13.tar.gz

---
+++ packages/feffery-utils-components/feffery_utils_components-0.1.13.tar.gz/feffery_utils_components-0.1.13/feffery_utils_components/FefferyShortcutPanel.py
@@ -0,0 +1,75 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class FefferyShortcutPanel(Component):
+    """A FefferyShortcutPanel component.
+
+
+Keyword arguments:
+
+- id (string; optional)
+
+- data (list of dicts; optional)
+
+    `data` is a list of dicts with keys:
+
+    - children (list of strings; optional)
+
+    - handler (string; optional)
+
+    - hotkey (string; optional)
+
+    - id (string; required)
+
+    - keywords (string; optional)
+
+    - parent (string; optional)
+
+    - section (string; optional)
+
+    - title (string; required)
+
+- disableHotkeys (boolean; optional)
+
+- loading_state (dict; optional)
+
+    `loading_state` is a dict with keys:
+
+    - component_name (string; optional):
+        Holds the name of the component that is loading.
+
+    - is_loading (boolean; optional):
+        Determines if the component is loading or not.
+
+    - prop_name (string; optional):
+        Holds which property is loading.
+
+- locale (a value equal to: 'en', 'zh'; default 'zh')
+
+- openHotkey (string; optional)
+
+- placeholder (string; optional)
+
+- style (dict; optional)
+
+- theme (a value equal to: 'light', 'dark'; default 'light')
+
+- triggeredHotkey (string; optional)"""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'feffery_utils_components'
+    _type = 'FefferyShortcutPanel'
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, style=Component.UNDEFINED, locale=Component.UNDEFINED, data=Component.UNDEFINED, triggeredHotkey=Component.UNDEFINED, placeholder=Component.UNDEFINED, disableHotkeys=Component.UNDEFINED, openHotkey=Component.UNDEFINED, theme=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'data', 'disableHotkeys', 'loading_state', 'locale', 'openHotkey', 'placeholder', 'style', 'theme', 'triggeredHotkey']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'data', 'disableHotkeys', 'loading_state', 'locale', 'openHotkey', 'placeholder', 'style', 'theme', 'triggeredHotkey']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        super(FefferyShortcutPanel, self).__init__(**args)
