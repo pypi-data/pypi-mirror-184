f5bb3f3369eae25780fd0f165f3e16c1323e8e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:38 +0000

    Add package promptlayer/0.1.3/promptlayer-0.1.3.tar.gz

---
+++ packages/promptlayer/promptlayer-0.1.3.tar.gz/promptlayer-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="promptlayer",
+    description="PromptLayer is a package to keep track of your GPT models training",
+    author_email="hello@magniv.io",
+    url="https://www.magniv.io",
+    project_urls={"Documentation": "https://magniv.notion.site/Prompt-Layer-Docs-db0e6f50cacf4564a6d09824ba17a629",},
+    version="0.1.3",
+    py_modules=["promptlayer"],
+    packages=find_packages(),
+    install_requires=["requests", "openai"],
+)
