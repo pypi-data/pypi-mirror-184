0607e8318a32d7529332ce38eac5dce1b31b6480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:54 +0000

    Add package jupyter-duckdb/0.3.2/jupyter-duckdb-0.3.2.tar.gz

---
+++ packages/jupyter-duckdb/jupyter-duckdb-0.3.2.tar.gz/jupyter-duckdb-0.3.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name='jupyter-duckdb',
+    version='0.3.2',
+    author='Eric Tröbs',
+    author_email='eric.troebs@tu-ilmenau.de',
+    description='a basic wrapper kernel for DuckDB',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/erictroebs/jupyter-duckdb',
+    project_urls={
+        'Bug Tracker': 'https://github.com/erictroebs/jupyter-duckdb/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=3.6',
+    install_requires=[
+        'jupyter',
+        'duckdb==0.6.1'
+    ],
+    package_data={
+        'duckdb_kernel': [
+            'kernel.json'
+        ]
+    },
+    include_package_data=True,
+)
