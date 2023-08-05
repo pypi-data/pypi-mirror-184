65a2ba8ff3ed14f18ef0e6b9eb48cf66862eedae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:22 +0000

    Add package tsma/1.0.5/tsma-1.0.5-py3-none-any.whl

---
+++ packages/tsma/tsma-1.0.5-py3-none-any.whl/tsma/analyses/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+tsma.analyses
+
+"""
+import statistics, vandin, metrics, clustering
+from statistics import *
+from vandin import *
+from metrics import *
+from clustering import *
+
+__all__ = ['statistics', 'vandin', 'metrics', 'clustering']
+
