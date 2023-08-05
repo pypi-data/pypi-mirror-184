29a27e48b3c4d6b915e2d030368296b828ed9ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:51 +0000

    Add package edge-tts/6.0.0/edge-tts-6.0.0.tar.gz

---
+++ packages/edge-tts/edge-tts-6.0.0.tar.gz/edge-tts-6.0.0/examples/dynamic_voice_selection.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Example of dynamic voice selection using VoicesManager.
+"""
+
+import asyncio
+import random
+
+import edge_tts
+from edge_tts import VoicesManager
+
+
+async def main() -> None:
+    voices = await VoicesManager.create()
+    voice = voices.find(Gender="Male", Language="es")
+    # Also supports Locales
+    # voice = voices.find(Gender="Female", Locale="es-AR")
+    VOICE = random.choice(voice)["ShortName"]
+    TEXT = "Hoy es un buen día."
+    OUTPUT_FILE = "spanish.mp3"
+
+    communicate = edge_tts.Communicate(TEXT, VOICE)
+    await communicate.save(OUTPUT_FILE)
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
