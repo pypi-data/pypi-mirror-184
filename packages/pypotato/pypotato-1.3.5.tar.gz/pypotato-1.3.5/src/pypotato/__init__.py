44b6388cf97cddd35a0b3ca1b1c3ab8f1394e548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:23 +0000

    Add package pypotato/1.3.5/pypotato-1.3.5.tar.gz

---
+++ packages/pypotato/pypotato-1.3.5.tar.gz/pypotato-1.3.5/src/pypotato/__init__.py
@@ -0,0 +1,13 @@
+"""
+pypotato library
+
+"""
+
+from . import potentiostat
+from . import load_data
+
+__version__ = "1.3.5"
+__author__ = 'Oliver Rodriguez'
+
+# modules to import when user does 'from pypotato import *':
+__all__ = ['potentiostat', 'load_data']
