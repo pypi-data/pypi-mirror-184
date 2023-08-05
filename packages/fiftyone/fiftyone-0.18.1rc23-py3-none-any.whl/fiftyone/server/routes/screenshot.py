3ae035c036928c4a72db36a07530a94d20b0984a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:57 +0000

    Add package fiftyone/0.18.1rc23/fiftyone-0.18.1rc23-py3-none-any.whl

---
+++ packages/fiftyone/fiftyone-0.18.1rc23-py3-none-any.whl/fiftyone/server/routes/screenshot.py
@@ -0,0 +1,48 @@
+"""
+FiftyOne Server /screenshot route
+
+| Copyright 2017-2022, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+import typing as t
+
+from starlette.endpoints import HTTPEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+
+import fiftyone.core.context as focx
+from fiftyone.core.session.events import get_screenshot
+from fiftyone.core.session.templates import SCREENSHOT_DATABRICKS
+
+from fiftyone.server.decorators import route
+from fiftyone.server.events import get_port
+
+
+class Screenshot(HTTPEndpoint):
+    @route
+    async def get(self, request: Request, data: t.Dict) -> Response:
+        img = request.path_params["img"]
+        proxy = request.query_params["proxy"]
+        subscription, ext = img.split(".")
+        screenshot = get_screenshot(subscription, False)
+        if ext == "html":
+            content = SCREENSHOT_DATABRICKS.render(
+                subscription=subscription,
+                image=f"{proxy}screenshot/{subscription}.jpeg",
+                proxy=proxy,
+                max_width=screenshot.max_width,
+            )
+            media_type = "text/html"
+        else:
+            content = screenshot.bytes
+            media_type = "image/png"
+
+        return Response(
+            content,
+            media_type=media_type,
+            headers={
+                "Cache-Control": "max-age=31536000",
+                "Pragma": "public",
+            },
+        )
