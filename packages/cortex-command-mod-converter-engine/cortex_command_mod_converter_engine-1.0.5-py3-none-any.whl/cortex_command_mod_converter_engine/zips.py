96489c94ec77a7525fb26325e252db3671d60ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:28 +0000

    Add package cortex-command-mod-converter-engine/1.0.5/cortex_command_mod_converter_engine-1.0.5-py3-none-any.whl

---
+++ packages/cortex-command-mod-converter-engine/cortex_command_mod_converter_engine-1.0.5-py3-none-any.whl/cortex_command_mod_converter_engine/zips.py
@@ -0,0 +1,27 @@
+import os, shutil, zipfile
+
+import cortex_command_mod_converter_engine.shared_globals as cfg
+
+
+def unzip(input_mod_path, input_folder_path):
+    if zipfile.is_zipfile(input_mod_path):
+        with zipfile.ZipFile(input_mod_path) as item:
+            item.extractall(input_folder_path)
+        os.remove(input_mod_path)
+
+
+def zip(input_mod_name, output_folder_path):
+    print("Zipping '{}'".format(input_mod_name))
+
+    output_mod_zip_name = input_mod_name.replace(
+        ".rte", f"-{cfg.GAME_VERSION}-v1.0.rte"
+    )
+    shutil.make_archive(
+        output_folder_path / output_mod_zip_name,
+        "zip",
+        root_dir=output_folder_path,
+        base_dir=input_mod_name,
+    )
+
+    output_mod_path = output_folder_path / input_mod_name
+    shutil.rmtree(output_mod_path)
