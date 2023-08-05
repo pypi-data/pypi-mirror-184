21d955ef96be3c019e561ac90f72fb6fc6e0ee6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:21:21 +0000

    Add package hsutilities/0.1.4/hsutilities-0.1.4-py3-none-any.whl

---
+++ packages/hsutilities/hsutilities-0.1.4-py3-none-any.whl/hsutilities/domains.py
@@ -0,0 +1,35 @@
+import os, subprocess
+import hsutilities.users as hsusers
+
+def get_subdomains_of_domain(user, domain):
+
+    path = f"~/doms/{domain}/subs-ssl"
+    stream = os.popen(f'sudo -u {user} -s /bin/bash -c "if [ -d {path} ]; then ls -1 {path}; fi"')
+    subdomains = stream.read()
+    if subdomains:
+        return subdomains.split('\n')
+    return None
+
+
+def get_domains_of_user(user):
+
+    result = {}
+    cmd = "if [ -d ~/doms ]; then ls ~/doms -1; fi"
+    output = subprocess.getoutput(f'sudo -u {user} -s /bin/bash -c "{cmd}"').split('\n')
+    for line in output:
+        if line:
+            result[line] = {'domain': line, 'user': user}
+    return result
+
+
+def get_domains_of_pac(pac):
+
+    result = {}
+
+    result = get_domains_of_user(pac)
+    users = hsusers.get_users_of_pac(pac)
+    for user in (users or []):
+        result.update(get_domains_of_user(f"{pac}-{user}"))
+
+    return result
+
