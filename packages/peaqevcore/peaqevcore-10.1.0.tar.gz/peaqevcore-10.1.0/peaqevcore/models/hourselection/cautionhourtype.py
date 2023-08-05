1be5fcb7beae5ed43dbbf83929e4467e7313a850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:55 +0000

    Add package peaqevcore/10.1.0/peaqevcore-10.1.0.tar.gz

---
+++ packages/peaqevcore/peaqevcore-10.1.0.tar.gz/peaqevcore-10.1.0/peaqevcore/models/hourselection/cautionhourtype.py
@@ -0,0 +1,14 @@
+from enum import Enum
+from const import (
+    CAUTIONHOURTYPE_SUAVE,
+    CAUTIONHOURTYPE_INTERMEDIATE,
+    CAUTIONHOURTYPE_AGGRESSIVE
+)
+
+class CautionHourType(Enum):
+    Suave = CAUTIONHOURTYPE_SUAVE
+    Intermediate = CAUTIONHOURTYPE_INTERMEDIATE
+    Aggressive = CAUTIONHOURTYPE_AGGRESSIVE
+
+
+ttt = CautionHourType(CAUTIONHOURTYPE_SUAVE)
