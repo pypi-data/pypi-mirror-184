8ebb204be4cbd8dc7ae467868d980ae552321136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:16 +0000

    Add package arize/6.0.0rc3/arize-6.0.0rc3.tar.gz

---
+++ packages/arize/arize-6.0.0rc3.tar.gz/arize-6.0.0rc3/arize/pandas/embeddings/usecases.py
@@ -0,0 +1,23 @@
+from enum import Enum, auto, unique
+
+
+@unique
+class NLPUseCases(Enum):
+    SEQUENCE_CLASSIFICATION = auto()
+    # TOKEN_CLASSIFICATION = auto()
+
+
+@unique
+class CVUseCases(Enum):
+    IMAGE_CLASSIFICATION = auto()
+
+
+@unique
+class TabularUseCases(Enum):
+    FEATURE_EMBEDDING = auto()
+
+
+class UseCases:
+    NLP = NLPUseCases
+    CV = CVUseCases
+    # TABULAR = TabularUseCases
