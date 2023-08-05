dfc6e75d11d4f096dd9a0253c2db8f5c542f6d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:17:07 +0000

    Add package hatch-msgfmt/0.0.8/hatch-msgfmt-0.0.8.tar.gz

---
+++ packages/hatch-msgfmt/hatch-msgfmt-0.0.8.tar.gz/hatch-msgfmt-0.0.8/src/hatch_msgfmt/hooks.py
@@ -0,0 +1,29 @@
+from typing import Any
+
+from hatchling.plugin import hookimpl
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+from .msgfmt import make
+
+class MsgfmtBuildHook(BuildHookInterface):
+    PLUGIN_NAME = 'msgfmt'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print('INITIALIZING')
+
+    def initialize(self, version: str, build_data: dict[str, Any]) -> None:
+        if self.target_name != 'wheel':
+            return
+        print('initialize')
+        print(f'{version=}')
+        print(f'{build_data=}')
+        print(f'{self.directory=}')
+
+    def finalize(self, *args, **kwargs) -> None:
+        if self.target_name != 'wheel':
+            return
+        print('finalize')
+
+@hookimpl
+def hatch_register_build_hook():
+    return MsgfmtBuildHook
