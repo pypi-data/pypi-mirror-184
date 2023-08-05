5739a7cede71367c3267c7f348faf16817588bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:06 +0000

    Add package flake8-iw/0.0.14/flake8_iw-0.0.14-py2.py3-none-any.whl

---
+++ packages/flake8-iw/flake8_iw-0.0.14-py2.py3-none-any.whl/flake8_iw/plugin.py
@@ -0,0 +1,35 @@
+import sys
+
+from .freeze_time import ForbiddenTimePatchFinder
+from .logging import ErrorLoggerFinder
+from .patch_call import ForbiddenPatchCallFinder
+from .datetime_now import DatetimeNowFinder
+from .datetime_replace_tzinfo import DatetimeReplaceTzinfoFinder
+
+if sys.version_info < (3, 8):
+    import importlib_metadata
+else:
+    import importlib.metadata as importlib_metadata
+
+
+class Plugin(object):
+    name = "flake8_iw"
+    version = importlib_metadata.version("flake8_iw")
+    rules = (
+        ForbiddenPatchCallFinder,
+        ForbiddenTimePatchFinder,
+        ErrorLoggerFinder,
+        DatetimeNowFinder,
+        DatetimeReplaceTzinfoFinder,
+    )
+
+    def __init__(self, tree):
+        self.tree = tree
+
+    def run(self):
+        for rule_class in self.rules:
+            parser = rule_class(self.tree)
+            parser.visit(self.tree)
+
+            for lineno, column, msg in parser.issues:
+                yield (lineno, column, msg, Plugin)
