2ffbdf9fbd761cfc667127d6cbc6ca0dd2063daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:11 +0000

    Add package garmin-daily/1.1.1/garmin_daily-1.1.1-py3-none-any.whl

---
+++ packages/garmin-daily/garmin_daily-1.1.1-py3-none-any.whl/garmin_daily/__init__.py
@@ -0,0 +1,9 @@
+"""Garmin data aggregated daily."""
+from garmin_daily.garmin_aggregations import (
+    SPORT_STEP_LENGTH_KM,
+    WALKING_SPORT,
+    Activity,
+    ActivityField,
+    GarminDaily,
+    GarminDay,
+)
