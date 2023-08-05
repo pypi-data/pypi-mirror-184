4f3443dce4da6e5347dbe5a5602bbdc08c9cfb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:52 +0000

    Add package steam-interfaces/0.1.8/steam_interfaces-0.1.8.tar.gz

---
+++ packages/steam-interfaces/steam_interfaces-0.1.8.tar.gz/steam_interfaces-0.1.8/steam_interfaces/__init__.py
@@ -0,0 +1,13 @@
+"""
+:authors: Tarodictrl
+:license: MIT License, see LICENSE file
+:copyright: (c) 2022 Tarodictrl
+"""
+
+from .__main__ import *
+
+
+__author__ = "Tarodictrl"
+__version__ = "0.0.1"
+__license__ = "MIT License"
+__email__ = "vudi600@gmail.com"
