82b2080012864846f5a75ec2825450ad64cf762e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:23 +0000

    Add package pinyin-rhymer/0.1.7/pinyin-rhymer-0.1.7.tar.gz

---
+++ packages/pinyin-rhymer/pinyin-rhymer-0.1.7.tar.gz/pinyin-rhymer-0.1.7/pinyin_rhymer/error.py
@@ -0,0 +1,22 @@
+class NotAPinYinError(Exception):
+    def __init__(self, string):
+        message = f'"{string}" is not a valid pinyin.'
+        super().__init__(message)
+
+
+class NotAConsonantError(Exception):
+    def __init__(self, string):
+        message = f'"{string}" is not a valid consonant.'
+        super().__init__(message)
+
+
+class NotAVowelError(Exception):
+    def __init__(self, string):
+        message = f'"{string}" is not a valid vowel.'
+        super().__init__(message)
+
+
+class NotARhymeSchemeError(Exception):
+    def __init__(self, string, scheme_name):
+        message = f'"{string}" is not a valid {scheme_name}.'
+        super().__init__(message)
