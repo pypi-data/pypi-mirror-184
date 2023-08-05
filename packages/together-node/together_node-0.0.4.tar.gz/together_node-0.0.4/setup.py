d415d7c9badb786127151e3519419250aebaaac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:37 +0000

    Add package together-node/0.0.4/together_node-0.0.4.tar.gz

---
+++ packages/together-node/together_node-0.0.4.tar.gz/together_node-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='together_node',
+    author="Xiaozhe Yao",
+    author_email="askxzyao@gmail.com",
+    description="Toma Cluster Manager",
+    version='0.0.4',
+    scripts=["together_node/bin/together_node"],
+    package_dir={'together_node': 'together_node'},
+    packages=find_packages(),
+    install_requires=[
+        "typer",
+        "requests",
+        "rich",
+        "loguru",
+
+    ]
+)
