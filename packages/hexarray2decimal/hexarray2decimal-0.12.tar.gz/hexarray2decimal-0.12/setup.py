5a0016df7acf0fd15b509d9d6b36e20cce82c812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:40 +0000

    Add package hexarray2decimal/0.12/hexarray2decimal-0.12.tar.gz

---
+++ packages/hexarray2decimal/hexarray2decimal-0.12.tar.gz/hexarray2decimal-0.12/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+#change to dict
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)),'README.md'), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.12'
+DESCRIPTION = "Converts a numpy string array with hex values to int"
+
+# Setting up
+setup(
+    name="hexarray2decimal",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/hexarray2decimal',
+    author="Johannes Fischer",
+    author_email="<aulasparticularesdealemaosp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    #packages=['numpy'],
+    keywords=['numpy', 'hex', 'hex2int'],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only', 'Programming Language :: Python :: 3.9', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Text Editors :: Text Processing', 'Topic :: Text Processing :: General', 'Topic :: Text Processing :: Indexing', 'Topic :: Text Processing :: Filters', 'Topic :: Utilities'],
+    install_requires=['numpy'],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
