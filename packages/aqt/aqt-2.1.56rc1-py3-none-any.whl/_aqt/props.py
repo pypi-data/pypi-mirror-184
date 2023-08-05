983a1d6aa6dd14dd1c992994032ec376820f7cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:41 +0000

    Add package aqt/2.1.56rc1/aqt-2.1.56rc1-py3-none-any.whl

---
+++ packages/aqt/aqt-2.1.56rc1-py3-none-any.whl/_aqt/props.py
@@ -0,0 +1,19 @@
+# Copyright: Ankitects Pty Ltd and contributors
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+# This file was automatically generated from _root-vars.scss
+FONT_SIZE = {
+    "comment": "",
+    "light": "15px",
+    "dark": "15px",
+}
+BORDER_RADIUS = {
+    "comment": "Used to round corners of various UI elements",
+    "light": "5px",
+    "dark": "5px",
+}
+BORDER_RADIUS_LARGE = {
+    "comment": "Used for big centered buttons",
+    "light": "15px",
+    "dark": "15px",
+}
