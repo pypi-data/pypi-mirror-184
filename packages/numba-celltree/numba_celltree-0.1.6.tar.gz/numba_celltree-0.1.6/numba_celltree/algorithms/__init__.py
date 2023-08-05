c68ce43dd5b7bc0395f460f40d0d305512caed44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:26 +0000

    Add package numba-celltree/0.1.6/numba_celltree-0.1.6.tar.gz

---
+++ packages/numba-celltree/numba_celltree-0.1.6.tar.gz/numba_celltree-0.1.6/numba_celltree/algorithms/__init__.py
@@ -0,0 +1,7 @@
+from .barycentric_triangle import barycentric_triangle_weights
+from .barycentric_wachspress import barycentric_wachspress_weights
+from .cohen_sutherland import cohen_sutherland_line_box_clip
+from .cyrus_beck import cyrus_beck_line_polygon_clip
+from .liang_barsky import liang_barsky_line_box_clip
+from .separating_axis import polygons_intersect
+from .sutherland_hodgman import area_of_intersection, box_area_of_intersection
