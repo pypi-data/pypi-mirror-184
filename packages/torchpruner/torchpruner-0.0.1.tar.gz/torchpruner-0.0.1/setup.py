1d38c75405562b991aea2f13ec32089906dd0372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:33 +0000

    Add package torchpruner/0.0.1/torchpruner-0.0.1.tar.gz

---
+++ packages/torchpruner/torchpruner-0.0.1.tar.gz/torchpruner-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+	name="torchpruner",
+	version="0.0.1",
+	author="Ashhadul Islam, Samir Brahim Belhaouari",
+	description="pruning pytorch neural networks",
+	packages=["torchpruner"]
+
+	)
