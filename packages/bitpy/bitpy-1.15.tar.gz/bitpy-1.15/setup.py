d173ffc42eea7510483a3b5d8fbb6617df707b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:27 +0000

    Add package bitpy/1.15/bitpy-1.15.tar.gz

---
+++ packages/bitpy/bitpy-1.15.tar.gz/bitpy-1.15/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bitpy',
+    version='1.15',
+    description='BitPy',
+    author='Philippe Remy',
+    install_requires=[
+        'numpy',
+        'websocket-client',
+        'requests',
+        'urllib3'
+    ],
+    packages=find_packages()
+)
