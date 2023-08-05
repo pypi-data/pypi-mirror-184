d0ec5dd72c1c85601bcc9efb5bdc37bf1a1c2805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:24 +0000

    Add package akira/0.1.1/akira-0.1.1.tar.gz

---
+++ packages/akira/akira-0.1.1.tar.gz/akira-0.1.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['akira']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.5.2,<2.0.0',
+ 'pytest-cov>=4.0.0,<5.0.0',
+ 'pytest>=7.2.0,<8.0.0',
+ 'pyyaml>=6.0,<7.0']
+
+setup_kwargs = {
+    'name': 'akira',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '',
+    'author': 'Bruno-Felix',
+    'author_email': 'auxbfelix@gmail.com',
+    'maintainer': 'Bruno-Felix',
+    'maintainer_email': 'auxbfelix@gmail.com',
+    'url': 'https://github.com/PDA-FGA/Playground',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
