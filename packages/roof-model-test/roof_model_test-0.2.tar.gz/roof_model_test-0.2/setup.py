247dd4494fc4d01bbfdbeecf1595751147b59414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:10 +0000

    Add package roof-model-test/0.2/roof_model_test-0.2.tar.gz

---
+++ packages/roof-model-test/roof_model_test-0.2.tar.gz/roof_model_test-0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="roof_model_test",
+    version="0.2",
+    author="Ruixu",
+    author_email="lrxjason@gmail.com",
+    description="test_code",
+    long_description="upload_test",
+    long_description_content_type="text/markdown",
+    url="https://github.com/lrxjason/roof_model_test/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
