1cd7f89166f543832680d8a6ba316d5bd5c267fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:07 +0000

    Add package srsinst.sr860/0.0.2/srsinst.sr860-0.0.2.tar.gz

---
+++ packages/srsinst.sr860/srsinst.sr860-0.0.2.tar.gz/srsinst.sr860-0.0.2/srsinst/sr860/__init__.py
@@ -0,0 +1,6 @@
+
+from .instruments.sr865.vxi11interface import Vxi11Interface
+from .instruments.sr865.sr865 import SR865
+from .instruments.get_instruments import get_sr865
+
+__version__ = "0.0.2"
