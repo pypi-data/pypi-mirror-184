8c67efd35c3251a3288452da4c4b865e59ff4153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:43 +0000

    Add package GooblerPKG/2.0.30/GooblerPKG-2.0.30.tar.gz

---
+++ packages/GooblerPKG/GooblerPKG-2.0.30.tar.gz/GooblerPKG-2.0.30/gooblerpkg/Config/__init__.py
@@ -0,0 +1,15 @@
+###################
+### Goobler 2022 ##
+###################
+# Scripts made by Eric #
+
+class colors:
+  HEADER = '\033[95m'
+  OKBLUE = '\033[94m'
+  OKCYAN = '\033[96m'
+  OKGREEN = '\033[92m'
+  WARNING = '\033[93m'
+  FAIL = '\033[91m'
+  ENDC = '\033[0m'
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
