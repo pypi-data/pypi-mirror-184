5b456a9f42295d24681f8b5aafbe097ada29bd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:25 +0000

    Add package pasta-eln/2.1.7/pasta_eln-2.1.7-py3-none-any.whl

---
+++ packages/pasta-eln/pasta_eln-2.1.7-py3-none-any.whl/pasta_eln/widgetBody.py
@@ -0,0 +1,35 @@
+""" Central widget: everything that is not sidebar """
+from PySide6.QtWidgets import QWidget, QVBoxLayout   # pylint: disable=no-name-in-module
+from PySide6.QtCore import Slot   # pylint: disable=no-name-in-module
+
+from .widgetDocTypes import DocTypes
+from .widgetProject import Project
+
+class Body(QWidget):
+  """ Central widget: everything that is not sidebar """
+  def __init__(self, comm):
+    super().__init__()
+    self.comm = comm
+    comm.chooseDocType.connect(self.changeDoctype)
+    self.docTypes = DocTypes(comm)
+    self.project  = Project(comm)
+    self.project.hide()
+    mainL = QVBoxLayout()
+    mainL.addWidget(self.docTypes)
+    mainL.addWidget(self.project)
+    self.setLayout(mainL)
+
+  @Slot(str)
+  def changeDoctype(self, docType):
+    """
+    What happens when user clicks to change doc-type
+
+    Args:
+      docType (str): document type
+    """
+    if docType=='x0':
+      self.docTypes.hide()
+      self.project.show()
+    else:
+      self.project.hide()
+      self.docTypes.show()
