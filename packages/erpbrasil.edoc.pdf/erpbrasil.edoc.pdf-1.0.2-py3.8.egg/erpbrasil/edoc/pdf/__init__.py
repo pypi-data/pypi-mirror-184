6769c5ee2eeb0c6cded68967fae22cbc8db3b013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:56 +0000

    Add package erpbrasil.edoc.pdf/1.0.2/erpbrasil.edoc.pdf-1.0.2-py3.8.egg

---
+++ packages/erpbrasil.edoc.pdf/erpbrasil.edoc.pdf-1.0.2-py3.8.egg/erpbrasil/edoc/pdf/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '1.0.2'
+
+from lxml import etree
+from lxml import objectify
+
+lookup = etree.ElementNamespaceClassLookup(
+    objectify.ObjectifyElementClassLookup())
+parser = etree.XMLParser()
+parser.set_element_class_lookup(lookup)
+
+# Não troque essa ordem ou tire isso daqui que vai dar merda!
+from erpbrasil.edoc.pdf import nfe  # noqa: F401,F403,E402
+from erpbrasil.edoc.pdf.base import ImprimirXml  # noqa: F401,F403,E402
