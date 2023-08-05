78a2e3d1feda3bee1140db58378f7216ce099180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:49 +0000

    Add package pandora-storage-server-client/0.1.1/pandora-storage-server-client-0.1.1.tar.gz

---
+++ packages/pandora-storage-server-client/pandora-storage-server-client-0.1.1.tar.gz/pandora-storage-server-client-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# *************************************
+# Pandora Storage Server Python Client
+# Client
+# 'setup.py'
+# Author: Juan Carlos Juárez
+# Contact: jc.juarezgarcia@outlook.com
+# *************************************
+
+from setuptools import setup
+from pathlib import Path
+
+VERSION = '0.1.1'
+DESCRIPTION = 'Pandora Storage Server Client.'
+
+# Read Content of README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+# Package setup
+setup(
+    name="pandora-storage-server-client",
+    version=VERSION,
+    author="Juan Carlos Juárez",
+    author_email="<jc.juarezgarcia@outlook.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    install_requires=["requests"],
+    keywords=['Storage', 'Key-Value', 'Server', 'NoSQL', 'Schemaless', 'GNU/Linux'],
+)
