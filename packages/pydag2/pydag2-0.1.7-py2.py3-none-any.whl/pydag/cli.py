20a18780990cb5643c251bbccb1df7131960756d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:06 +0000

    Add package pydag2/0.1.7/pydag2-0.1.7-py2.py3-none-any.whl

---
+++ packages/pydag2/pydag2-0.1.7-py2.py3-none-any.whl/pydag/cli.py
@@ -0,0 +1,164 @@
+from pygocron.pygocron import PyGoCron
+from plan import Plan
+from argparse import ArgumentParser
+from .utils import compose_command_for_job, prepare_rich_logger, list_pipelines
+from .job import GoCronJob
+from .log import BasicJobLogger
+
+logger = prepare_rich_logger("Pydag")
+
+
+def submit(args):
+    """
+    plan job run by cron via `plan`
+    """
+    file = args.file
+    job_name = args.name
+
+    if not job_name:
+        raise ValueError(
+            "No `job` name provided, please define one via `--name=[job name]` flag"
+        )
+
+    command = compose_command_for_job(
+        file, to_run_new=False
+    )  # always run a existing job
+    cron = Plan(name=job_name)
+
+    cron_expression = args.cron
+    if not cron_expression:
+        raise ValueError(
+            "No cron expresion provides, please define one via `--cron=[cron expression]` flag"
+        )
+    cron.command(command, every=cron_expression)
+    try:
+        cron.run("update")  # this problem is due to python plan module, for a none-root user, first time trigger a `update` will lead to a error
+    except:
+        cron.run("write")
+
+
+def delete(args):
+    pyg = PyGoCron()
+    job_name = args.job_name
+
+    # delete all task instance
+    GoCronJob.delete_job_tasks(pyg, job_name)
+
+    # delete associated cron-job
+    cron = Plan(name=job_name)
+    cron.run("clear")
+
+    # delete associate logs
+    bl = BasicJobLogger()
+    bl.clear_log(job_name)
+
+
+def log(args):
+    bl = BasicJobLogger()
+    
+
+    style = args.style
+
+    if args.task_name:
+        bl.log_task(args.job_name, args.task_name, args.n)
+    else:
+        bl.log_job(args.job_name, args.n, style)
+
+    
+def list(args):
+    job_name = args.job_name
+    _ = list_pipelines(job_name)
+
+class Submit:
+    @staticmethod
+    def register_subcommand(subparser):
+        submit_parser = subparser.add_parser(
+            "submit",
+            help="CLI tool to submit a `pydag` job;Before running this command, you have to make sure you had submitted the job before",
+        )
+        submit_parser.add_argument(
+            "file",
+            type=str,
+            help="Executable file to run, be aware that the `file` must exist in the current directory",
+        )
+        submit_parser.add_argument(
+            "--cron",
+            type=str,
+            help="Cron expression for the job, then job will run according to that expression",
+        )
+        submit_parser.add_argument(
+            "--name",
+            type=str,
+            help="Job name, this is optional, if not given, the `file` will treates as a job name; Not this job name can differ from the job name you defined in the file",
+        )
+        submit_parser.set_defaults(func=submit)
+
+
+class Log:
+    @staticmethod
+    def register_subcommand(subparser):
+        log_parser = subparser.add_parser(
+            "log", help="CLI tool to get `pydag` job & task logs"
+        )
+        log_parser.add_argument("job_name", type=str, help="Job name")
+        log_parser.add_argument(
+            "--task_name",
+            type=str,
+            help="Task name, optional; If it's given, then the log for specific task will be shown",
+        )
+        log_parser.add_argument(
+            "-n",
+            type=int,
+            default=3,
+            help="The number of recent job logs, default is `3`",
+        )
+        log_parser.add_argument(
+            "--style",
+            type=str,
+            default="flat",
+            choices=["flat", "tree"],
+            help="Print the log in `tree` format or `flat` format"
+        )
+        log_parser.set_defaults(func=log)
+
+
+class Delete:
+    @staticmethod
+    def register_subcommand(subparser):
+        delete_parser = subparser.add_parser(
+            "delete", help="CLI tool to delete `pydag` job"
+        )
+        delete_parser.add_argument(
+            "job_name",
+            type=str,
+            help="Job name, all tasks associate with the job name then will be deleted",
+        )
+        delete_parser.set_defaults(func=delete)
+
+class List:
+    @staticmethod
+    def register_subcommand(subparser):
+        list_parser = subparser.add_parser(
+            "list", help="CLI tool to list all the `pydag` job and tasks"
+        )
+        list_parser.add_argument(
+            "--job_name",
+            type=str,
+            help="Job name, all tasks associate with the job name then will be list",
+        )
+        list_parser.set_defaults(func=list)
+
+def main():
+    parser = ArgumentParser()
+    subparser = parser.add_subparsers()
+    Submit.register_subcommand(subparser)
+    Log.register_subcommand(subparser)
+    Delete.register_subcommand(subparser)
+    List.register_subcommand(subparser)
+
+    args = parser.parse_args()
+    if not hasattr(args, "func"):
+        parser.print_help()
+        exit(1)
+
+    args.func(args)
---
+++ packages/pydag2/pydag2-0.1.7-py2.py3-none-any.whl/pydag/task.py
@@ -0,0 +1,130 @@
+from pygocron.pygocron import RunStatus
+from .utils import compose_task_name, TaskStatus
+from .log import BasicJobLogger
+from .environments import TO_RUN_NEW, TASK_TIMEOUT
+from .exceptions import PyDagException
+
+
+class Task:
+    def __init__(self, name: str, command: str):
+        self.id = self.name = name
+        self.command = command
+        self.upstreams = []
+        self.downstreams = []
+
+    def set_downstream(self, task: "Task"):
+        if task not in self.downstreams:
+            self.downstreams.append(task)
+
+    def set_upstream(self, task: "Task"):
+        if task not in self.upstreams:
+            self.upstreams.append(task)
+
+    def _get_node_presentation(self):
+        return (self.id, self._get_infos())
+
+    def _get_infos(self):
+        return {"task_name": self.name, "command": self.command}
+
+    def _get_edges(self):
+        edges = []
+        for successor in self.downstreams:
+            edges.append((self.id, successor.id))
+        for predecessor in self.upstreams:
+            edges.append((predecessor.id, self.id))
+        return edges
+
+    def __repr__(self) -> str:
+        return f"Task<{self.id}>"
+
+
+class GoCronTask(Task):
+    def __init__(self, name: str, command: str, node_id:int=None):
+        super().__init__(name, command)
+        self._task_logger = BasicJobLogger()
+        self._node_id = node_id if node_id else 1
+
+    def submit(self, add_job_name=False, job_name="", tag="", task_manager=None):
+        """
+        submit ad task
+        """
+        if task_manager == None:
+            raise ValueError(
+                "It seems you didn't provide a `task_manger` when definnig a `job`"
+            )
+
+        if add_job_name:
+            if job_name:
+                task_name = compose_task_name(job_name, self.name)
+            else:
+                raise ValueError(
+                    "if `add_job_name` is `True`, then you must set a `job_name`"
+                )
+        else:
+            task_name = self.name
+
+        # check if is to run a brand new job or existing job
+        if TO_RUN_NEW == "yes":
+            task_id = task_manager.create_task(
+                name=task_name,
+                spec=None,
+                tag=tag,
+                command=self.command,
+                level=2,
+                timeout=TASK_TIMEOUT,
+                host_id=self._node_id 
+
+            )
+        elif TO_RUN_NEW == "no":
+            task_id = task_manager.get_task_id_by_name(task_name)
+        else:
+            raise PyDagException(
+                f"Wrong `PYDAG_RUN_NEW` value, must be one of [`yes`, `no`] (case insensitive)"
+            )
+
+        if task_id:
+            self.task_id = task_id
+        else:
+            raise PyDagException("Task submittion failed")
+
+    def run(self, task_manager):
+        """
+        run a task, and a run id of the task will be set
+        """
+
+        run_id = task_manager.run_task(self.task_id)
+        if run_id:
+            self.run_id = run_id
+        else:
+            raise PyDagException("Task run failed")
+
+    def check_run_status(self, task_manager) -> str:
+        """
+        get a runnning task status, either one of ["sucess", "failed", "pendding", "running"]
+        """
+        if not hasattr(self, "run_id"):
+            # the run_id is set by run method, if task doesn't has a run id, meanning task didn't ran yet
+            return TaskStatus.PENDING
+
+        status = task_manager.check_run_status(self.task_id, self.run_id)
+
+        if status == RunStatus.SUCCESS:
+            return TaskStatus.SUCCESS
+        elif status == RunStatus.FAILED:
+            return TaskStatus.FAILED
+        elif status == RunStatus.RUNNING:
+            return TaskStatus.RUNNING
+        elif status == RunStatus.PENDING:
+            return TaskStatus.PENDING
+        raise PyDagException(f"Wrong status: {status}")
+
+    def record(self, job_name, job_run_at, status: TaskStatus, task_record_at):
+        self._task_logger.record_task_info(
+            job_name,
+            job_run_at,
+            self.name,
+            self.run_id,
+            self.task_id,
+            status.value,
+            task_record_at,
+        )
