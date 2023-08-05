ced341d2485a94a2b572144a76ae39b4462d4382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:58 +0000

    Add package parm-geo/0.0.1/parm_geo-0.0.1.tar.gz

---
+++ packages/parm-geo/parm_geo-0.0.1.tar.gz/parm_geo-0.0.1/setup.py
@@ -0,0 +1,20 @@
+## test upload
+import setuptools
+
+setuptools.setup(
+    name="parm_geo",
+    version='0.0.1',
+    author="Ruixu",
+    license="Mit",
+    description="Roof detection",
+    long_description_content_type="text/markdown",
+    url="https://github.com/lrxjason",
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+
+    ],
+)
