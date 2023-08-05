6689aadc9835bbb61f4f96698ae554fcc72dc7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:19 +0000

    Add package testcompose/0.1.8/testcompose-0.1.8.tar.gz

---
+++ packages/testcompose/testcompose-0.1.8.tar.gz/testcompose-0.1.8/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['testcompose',
+ 'testcompose.client',
+ 'testcompose.configs',
+ 'testcompose.containers',
+ 'testcompose.models',
+ 'testcompose.models.bootstrap',
+ 'testcompose.models.client',
+ 'testcompose.models.container',
+ 'testcompose.models.network',
+ 'testcompose.waiters']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=5.4.1,<6.0.0',
+ 'docker==6.0.1',
+ 'pydantic==1.10.2',
+ 'requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'testcompose',
+    'version': '0.1.8',
+    'description': 'Provide an easy way to perform integration testing with docker and python',
+    'long_description': '<h1 align="center" style="font-size: 3rem; margin: -15px 0">\nTestcompose\n</h1>\n\n\n\n<p align="center" style="margin: 30px 0 10px">\n  <img width="350" height="208" src="docs/images/testcompose.png" alt=\'Testcompose\'>\n</p>\n\n<p align="center"><strong>Testcompose</strong> <em>- A clean and better way to test your Python containerized applications.</em></p>\n\n\n\n![PyPI - Python Version](https://img.shields.io/pypi/pyversions/testcompose)\n![PyPI - Implementation](https://img.shields.io/pypi/implementation/testcompose)\n![PyPI](https://img.shields.io/pypi/v/testcompose)\n![PyPI - Downloads](https://img.shields.io/pypi/dm/testcompose)\n[![Tests](https://github.com/rugging24/python-testcompose/workflows/RunningTests/badge.svg)](https://github.com/rugging24/python-testcompose/blob/main/.github/workflows/tests.yaml)\n\n\n---\n**Testcompose** provides an easy way of using docker containers for functional and integration testing. It allows for combination of more than one containers and allows for interactions with these containers from your test code without having to write extra scripts for such interactions. I.e providing a docker compose kind of functionality with the extra benefit of being able to fully control the containers from test codes.\n\nThis is inspired by the [testcontainers-python](https://testcontainers-python.readthedocs.io/en/latest/index.html#) project and goes further to add a few additional functionalities to improve software integration testing while allowing the engineer to control every aspect of the test.\n\n---\n\nInstall testcompose using pip:\n\n```shell\n$ pip install testcompose\n```\n\ntestcompose requires Python 3.7+.\n\nUsing a config file. See the [Quickstart](https://github.com/rugging24/python-testcompose/blob/main/docs/quickstart.md) for other options\n\n```yaml\nservices:\n  - name: database\n    image: "postgres:13"\n    command: ""\n    environment:\n      POSTGRES_USER: postgres\n      POSTGRES_DB: postgres\n      POSTGRES_PASSWORD: password\n    exposed_ports:\n      - 5432\n    log_wait_parameters:\n      log_line_regex: "database system is ready to accept connections"\n      wait_timeout_ms: 30000\n      poll_interval_ms: 2000\n  - name: application\n    image: "python:3.9"\n    command: "/bin/bash -x /run_app.sh"\n    environment:\n      DB_URL: "${database.postgres_user}:${database.postgres_password}@${database.container_hostname}:5432/${database.postgres_db}"\n    volumes:\n      - host: "docker-test-files/run_app.sh"\n        container: "/run_app.sh"\n        mode: "ro"\n        source: "filesystem"\n      - host: "docker-test-files/app.py"\n        container: "/app.py"\n        mode: "ro"\n        source: "filesystem"\n    exposed_ports:\n      - "8000"\n    log_wait_parameters:\n      log_line_regex: ".*Application startup complete.*"\n      wait_timeout_ms: 45000\n      poll_interval_ms: 2000\n    http_wait_parameters:\n      http_port: 8000\n      response_status_code: 200\n      end_point: "/ping"\n      startup_delay_time_ms: 30000\n      use_https": false\n    depends_on:\n      - database\n```\n\nVerify it as follows:\n\n```python\nimport json\nfrom typing import Any, Dict\nfrom requests import Response, get\nfrom testcompose.configs.service_config import Config\nfrom testcompose.models.bootstrap.container_service import ContainerServices\nfrom testcompose.models.container.running_container import RunningContainer\nfrom testcompose.run_containers import RunContainers\n\nconfig_services: ContainerServices = TestConfigParser.parse_config(file_name=\'some-config.yaml\')\nrunning_config: Config = Config(test_services=config_services)\n\nwith RunContainers(\n        config_services=config_services,\n        ranked_services=running_config.ranked_config_services,\n) as runner:\n    assert runner\n    app_container_srv_name = "application"\n    app_service: RunningContainer = runner.running_containers[app_container_srv_name]\n    app_env_vars: Dict[str, Any] = app_service.config_environment_variables\n    mapped_port = app_service.generic_container.get_exposed_port("8000")\n    print(app_env_vars)\n    app_host = app_service.generic_container.get_container_host_ip()\n    assert app_env_vars\n    assert mapped_port\n    assert app_host\n    response: Response = get(url=f"http://{app_host}:{int(mapped_port)}/version")\n    assert response\n    assert response.status_code == 200\n    assert response.text\n    assert isinstance(json.loads(response.text), dict)\n```\n\n\n## Documentation\n\n[Quickstart](https://github.com/rugging24/python-testcompose/blob/main/docs/quickstart.md)\n\n[Special-Variables](https://github.com/rugging24/python-testcompose/blob/main/docs/environment_variables.md)\n\n[Full-Doc](https://rugging24.github.io/python-testcompose/)\n',
+    'author': 'Olakunle Olaniyi',
+    'author_email': 'rugging24@gmail.com',
+    'maintainer': 'Olakunle Olaniyi',
+    'maintainer_email': 'rugging24@gmail.com',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/testcompose/testcompose-0.1.8.tar.gz/testcompose-0.1.8/testcompose/run_containers.py
@@ -0,0 +1,130 @@
+from logging import Logger
+import time
+from uuid import uuid4
+from typing import Dict, Optional
+from testcompose.client.base_docker_client import BaseDockerClient
+from testcompose.containers.generic_container import GenericContainer
+from testcompose.containers.container_network import ContainerNetwork
+from testcompose.log_setup import stream_logger
+from testcompose.models.client.client_login import ClientFromEnv, ClientFromUrl
+from testcompose.models.client.registry_parameters import Login
+from testcompose.models.bootstrap.container_service import (
+    ContainerServices,
+    RankedContainerServices,
+    ContainerService,
+)
+from testcompose.models.container.running_container import RunningContainer, RunningContainers
+from docker.errors import APIError
+
+
+logger: Logger = stream_logger(__name__)
+
+
+class RunContainers(BaseDockerClient):
+    def __init_subclass__(cls, **kwargs) -> None:
+        if cls is not RunContainers:
+            raise TypeError("The class RunContainers can not be extended")
+        return super().__init_subclass__()
+
+    def __init__(
+        self,
+        config_services: ContainerServices,
+        ranked_services: RankedContainerServices,
+        wait_time_between_container_start: float = 20.0,
+        registry_login_param=Login(),
+        env_param: ClientFromEnv = ClientFromEnv(),
+        url_param: ClientFromUrl = ClientFromUrl(),
+    ) -> None:
+        super(RunContainers, self).__init__(client_env_param=env_param, client_url_param=url_param)
+        self._config_services: ContainerServices = config_services
+        self._ranked_config_services: RankedContainerServices = ranked_services
+        self.running_containers = RunningContainers()
+        self._wait_time_between_container_start: float = wait_time_between_container_start
+        self.registry_login(login_credentials=registry_login_param)
+
+    @property
+    def running_containers(self) -> RunningContainers:
+        return self._running_containers
+
+    @running_containers.setter
+    def running_containers(self, containers: RunningContainers) -> None:
+        self._running_containers: RunningContainers = containers
+
+    @property
+    def unique_container_label(self) -> str:
+        return self._unique_container_label
+
+    @unique_container_label.setter
+    def unique_container_label(self, label: str) -> None:
+        self._unique_container_label: str = label
+
+    def __enter__(self) -> RunningContainers:
+        return self.run_containers()
+
+    def __exit__(self, exc_type, exc_value, exc_tb) -> None:
+        self.stop_running_containers()
+        if exc_tb and exc_type:
+            logger.info("%s[%s]: %s", exc_type, exc_value, exc_tb)
+
+    def run_containers(self) -> RunningContainers:
+        self.unique_container_label = uuid4().hex
+        network_name: str = f"{self.unique_container_label}_network"
+        processed_containers_services: Dict[str, RunningContainer] = dict()
+
+        for rank in sorted(self._ranked_config_services.ranked_services.keys()):
+            service: ContainerService = self._config_services.services[
+                self._ranked_config_services.ranked_services[rank]
+            ]
+            self.pull_docker_image(service.image)
+            generic_container: GenericContainer = GenericContainer()
+            generic_container.container_network = ContainerNetwork(self.docker_client, network_name)
+            generic_container.with_service(
+                service,
+                processed_containers_services,
+                generic_container.container_network.name,  # type: ignore
+            )
+            try:
+                generic_container.container = generic_container.start(self.docker_client)
+                generic_container.check_container_health(self.docker_client)
+                running_container: RunningContainer = RunningContainer(
+                    service_name=service.name,
+                    config_environment_variables=generic_container.container_environment_variables,
+                    generic_container=generic_container,
+                )
+                processed_containers_services.update({service.name: running_container})
+                time.sleep(self._wait_time_between_container_start)
+            except Exception as exc:
+                logger.error(exc)
+                self.stop_running_containers(
+                    RunningContainers(running_containers=processed_containers_services)
+                )
+                raise APIError(exc)
+        logger.info("The following containers were started: %s", list(processed_containers_services.keys()))
+        self.running_containers = RunningContainers(running_containers=processed_containers_services)
+        return self.running_containers
+
+    def stop_running_containers(self, running_containers: RunningContainers = RunningContainers()) -> None:
+        container_network: Optional[ContainerNetwork] = None
+        try:
+            _running_containers: RunningContainers = (
+                running_containers if running_containers.running_containers else self.running_containers
+            )
+            for rank in sorted(self._ranked_config_services.ranked_services.keys(), reverse=True):
+                service_name: str = self._ranked_config_services.ranked_services[rank]
+                if _running_containers.running_containers.get(service_name):
+                    container: GenericContainer = _running_containers.running_containers[
+                        service_name
+                    ].generic_container
+                    container.stop()
+                    if not container_network:
+                        container_network = container.container_network
+                    logger.info(
+                        "Successfully stopped container: %s(%s): %s",
+                        service_name,
+                        str(rank),
+                        container.get_container_id(),
+                    )
+                    time.sleep(5)
+        finally:
+            if container_network:
+                container_network.remove_network()
