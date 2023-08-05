e19f725c6f78c077636d14a383308e9d0088c28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:20 +0000

    Add package tspq0/0.1.16/tspq0-0.1.16.tar.gz

---
+++ packages/tspq0/tspq0-0.1.16.tar.gz/tspq0-0.1.16/tspq0.py
@@ -0,0 +1,278 @@
+# useful additional packages
+import matplotlib.pyplot as plt
+import matplotlib.axes as axes
+import numpy as np
+import networkx as nx
+
+from qiskit import Aer
+from qiskit.tools.visualization import plot_histogram
+from qiskit.circuit.library import TwoLocal
+from qiskit_optimization.applications import Maxcut, Tsp
+from qiskit.algorithms import VQE, NumPyMinimumEigensolver
+from qiskit.algorithms.optimizers import SPSA
+from qiskit.utils import algorithm_globals, QuantumInstance
+from qiskit_optimization.algorithms import MinimumEigenOptimizer
+from qiskit_optimization.problems import QuadraticProgram
+
+# Input the number of nodes
+
+while True:
+    try:
+        
+        n = int(input("Type '5' to create a graph of 5 nodes: "))
+        
+    except ValueError:
+        print( "This is not a valid input")
+        continue
+    else:
+        break
+
+#Generating a graph of 4 nodes
+
+G = nx.Graph()
+G.add_nodes_from(np.arange(0, n, 1))
+elist = [(0, 1, 1.0), (0, 2, 1.0), (0, 3, 1.0), (1, 2, 1.0), (2, 3, 1.0), (0, 4, 1.0), (3, 4, 1.0)]
+# tuple is (i,j,weight) where (i,j) is the edge
+G.add_weighted_edges_from(elist)
+
+colors = ["r" for node in G.nodes()]
+pos = nx.spring_layout(G)
+
+
+def draw_graph(G, colors, pos):
+    default_axes = plt.axes(frameon=True)
+    nx.draw_networkx(G, node_color=colors, node_size=600, alpha=0.8, ax=default_axes, pos=pos)
+    edge_labels = nx.get_edge_attributes(G, "weight")
+    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_labels)
+
+
+draw_graph(G, colors, pos)
+
+# Computing the weight matrix from the random graph
+w = np.zeros([n, n])
+for i in range(n):
+    for j in range(n):
+        temp = G.get_edge_data(i, j, default=0)
+        if temp != 0:
+            w[i, j] = temp["weight"]
+print(w)
+
+best_cost_brute = 0
+for b in range(2**n):
+    x = [int(t) for t in reversed(list(bin(b)[2:].zfill(n)))]
+    cost = 0
+    for i in range(n):
+        for j in range(n):
+            cost = cost + w[i, j] * x[i] * (1 - x[j])
+    if best_cost_brute < cost:
+        best_cost_brute = cost
+        xbest_brute = x
+    print("case = " + str(x) + " cost = " + str(cost))
+
+colors = ["r" if xbest_brute[i] == 0 else "c" for i in range(n)]
+draw_graph(G, colors, pos)
+print("\nBest solution = " + str(xbest_brute) + " cost = " + str(best_cost_brute))
+
+max_cut = Maxcut(w)
+qp = max_cut.to_quadratic_program()
+print(qp.prettyprint())
+
+qubitOp, offset = qp.to_ising()
+print("Offset:", offset)
+print("Ising Hamiltonian:")
+print(str(qubitOp))
+
+# solving Quadratic Program using exact classical eigensolver
+exact = MinimumEigenOptimizer(NumPyMinimumEigensolver())
+result = exact.solve(qp)
+print(result.prettyprint())
+
+# Making the Hamiltonian in its full form and getting the lowest eigenvalue and eigenvector
+ee = NumPyMinimumEigensolver()
+result = ee.compute_minimum_eigenvalue(qubitOp)
+
+x = max_cut.sample_most_likely(result.eigenstate)
+print("energy:", result.eigenvalue.real)
+print("max-cut objective:", result.eigenvalue.real + offset)
+print("solution:", x)
+print("solution objective:", qp.objective.evaluate(x))
+
+colors = ["r" if x[i] == 0 else "c" for i in range(n)]
+draw_graph(G, colors, pos)
+
+algorithm_globals.random_seed = 123
+seed = 10598
+backend = Aer.get_backend("aer_simulator_statevector")
+quantum_instance = QuantumInstance(backend, seed_simulator=seed, seed_transpiler=seed)
+
+# construct VQE
+spsa = SPSA(maxiter=300)
+ry = TwoLocal(qubitOp.num_qubits, "ry", "cz", reps=5, entanglement="linear")
+vqe = VQE(ry, optimizer=spsa, quantum_instance=quantum_instance)
+
+# run VQE
+result = vqe.compute_minimum_eigenvalue(qubitOp)
+
+# print results
+x = max_cut.sample_most_likely(result.eigenstate)
+print("energy:", result.eigenvalue.real)
+print("time:", result.optimizer_time)
+print("max-cut objective:", result.eigenvalue.real + offset)
+print("solution:", x)
+print("solution objective:", qp.objective.evaluate(x))
+
+# plot results
+colors = ["r" if x[i] == 0 else "c" for i in range(n)]
+draw_graph(G, colors, pos)
+
+# create minimum eigen optimizer based on VQE
+vqe_optimizer = MinimumEigenOptimizer(vqe)
+
+# solve quadratic program
+result = vqe_optimizer.solve(qp)
+print(result.prettyprint())
+
+colors = ["r" if result.x[i] == 0 else "c" for i in range(n)]
+draw_graph(G, colors, pos)
+
+# Ask to input the number of nodes
+while True:
+    try:
+        
+        n = int(input("Type '3' to create a graph of 3 nodes: "))
+        
+    except ValueError:
+        print( "This is not a valid input")
+        continue
+    else:
+        break
+
+# Generating a graph of 3 nodes
+num_qubits = n**2
+tsp = Tsp.create_random_instance(n, seed=123)
+adj_matrix = nx.to_numpy_matrix(tsp.graph)
+print("distance\n", adj_matrix)
+
+colors = ["r" for node in tsp.graph.nodes]
+pos = [tsp.graph.nodes[node]["pos"] for node in tsp.graph.nodes]
+draw_graph(tsp.graph, colors, pos)
+
+from itertools import permutations
+
+
+def brute_force_tsp(w, N):
+    a = list(permutations(range(1, N)))
+    last_best_distance = 1e10
+    for i in a:
+        distance = 0
+        pre_j = 0
+        for j in i:
+            distance = distance + w[j, pre_j]
+            pre_j = j
+        distance = distance + w[pre_j, 0]
+        order = (0,) + i
+        if distance < last_best_distance:
+            best_order = order
+            last_best_distance = distance
+            print("order = " + str(order) + " Distance = " + str(distance))
+    return last_best_distance, best_order
+
+
+best_distance, best_order = brute_force_tsp(adj_matrix, n)
+print(
+    "Best order from brute force = "
+    + str(best_order)
+    + " with total distance = "
+    + str(best_distance)
+)
+
+
+def draw_tsp_solution(G, order, colors, pos):
+    G2 = nx.DiGraph()
+    G2.add_nodes_from(G)
+    n = len(order)
+    for i in range(n):
+        j = (i + 1) % n
+        G2.add_edge(order[i], order[j], weight=G[order[i]][order[j]]["weight"])
+    default_axes = plt.axes(frameon=True)
+    nx.draw_networkx(
+        G2, node_color=colors, edge_color="b", node_size=600, alpha=0.8, ax=default_axes, pos=pos
+    )
+    edge_labels = nx.get_edge_attributes(G2, "weight")
+    nx.draw_networkx_edge_labels(G2, pos, font_color="b", edge_labels=edge_labels)
+
+
+draw_tsp_solution(tsp.graph, best_order, colors, pos)
+
+qp = tsp.to_quadratic_program()
+print(qp.prettyprint())
+
+from qiskit_optimization.converters import QuadraticProgramToQubo
+
+qp2qubo = QuadraticProgramToQubo()
+qubo = qp2qubo.convert(qp)
+qubitOp, offset = qubo.to_ising()
+print("Offset:", offset)
+print("Ising Hamiltonian:")
+print(str(qubitOp))
+
+result = exact.solve(qubo)
+print(result.prettyprint())
+
+# Making the Hamiltonian in its full form and getting the lowest eigenvalue and eigenvector
+ee = NumPyMinimumEigensolver()
+result = ee.compute_minimum_eigenvalue(qubitOp)
+
+print("energy:", result.eigenvalue.real)
+print("tsp objective:", result.eigenvalue.real + offset)
+x = tsp.sample_most_likely(result.eigenstate)
+print("feasible:", qubo.is_feasible(x))
+z = tsp.interpret(x)
+print("solution:", z)
+print("solution objective:", tsp.tsp_value(z, adj_matrix))
+draw_tsp_solution(tsp.graph, z, colors, pos)
+
+algorithm_globals.random_seed = 123
+seed = 10598
+backend = Aer.get_backend("aer_simulator_statevector")
+quantum_instance = QuantumInstance(backend, seed_simulator=seed, seed_transpiler=seed)
+
+spsa = SPSA(maxiter=300)
+ry = TwoLocal(qubitOp.num_qubits, "ry", "cz", reps=5, entanglement="linear")
+vqe = VQE(ry, optimizer=spsa, quantum_instance=quantum_instance)
+
+result = vqe.compute_minimum_eigenvalue(qubitOp)
+
+print("energy:", result.eigenvalue.real)
+print("time:", result.optimizer_time)
+x = tsp.sample_most_likely(result.eigenstate)
+print("feasible:", qubo.is_feasible(x))
+z = tsp.interpret(x)
+print("solution:", z)
+print("solution objective:", tsp.tsp_value(z, adj_matrix))
+draw_tsp_solution(tsp.graph, z, colors, pos)
+
+algorithm_globals.random_seed = 123
+seed = 10598
+backend = Aer.get_backend("aer_simulator_statevector")
+quantum_instance = QuantumInstance(backend, seed_simulator=seed, seed_transpiler=seed)
+
+# create minimum eigen optimizer based on VQE
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning)
+vqe_optimizer = MinimumEigenOptimizer(vqe)
+
+# solve quadratic program
+result = vqe_optimizer.solve(qp)
+print(result.prettyprint())
+
+z = tsp.interpret(x)
+print("solution:", z)
+print("solution objective:", tsp.tsp_value(z, adj_matrix))
+draw_tsp_solution(tsp.graph, z, colors, pos)
+
+import qiskit.tools.jupyter
+
+#%qiskit_version_table
+#%qiskit_copyright
