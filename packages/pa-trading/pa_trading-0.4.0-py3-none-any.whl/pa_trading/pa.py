279f25c0430e9ae41b8c6aba7b52851b2917e2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:46 +0000

    Add package pa-trading/0.4.0/pa_trading-0.4.0-py3-none-any.whl

---
+++ packages/pa-trading/pa_trading-0.4.0-py3-none-any.whl/pa_trading/pa.py
@@ -0,0 +1,34 @@
+# pa-trading
+# Copyright 2022 Valmir França da Silva
+# http://github.com/vfranca
+import click
+from pa_trading import __version__
+
+
+# Importa os comandos
+from pa_trading.l import l
+from pa_trading.pb import pb
+from pa_trading.tr import tr
+from pa_trading.p2 import p2
+from pa_trading.b import b
+
+
+# Cria o grupo de comandos pa
+@click.group(invoke_without_command=True)
+@click.option("--version", is_flag=True, help="Show the version and exit.")
+def pa(version):
+    """grupo de comandos pa (price action)."""
+    if version: click.echo("pa-trading %s" % __version__)
+    else: click.echo("Digite pa --help para mais informações")
+
+
+# Adiciona os comandos
+pa.add_command(l)
+pa.add_command(pb)
+pa.add_command(tr)
+pa.add_command(p2)
+pa.add_command(b)
+
+
+if __name__ == "__main__":
+    pa()
