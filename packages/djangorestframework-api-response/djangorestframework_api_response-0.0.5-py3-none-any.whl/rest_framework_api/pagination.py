5c019ff94540ae9b78ea36df561433e66aaa3a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:23 +0000

    Add package djangorestframework-api-response/0.0.5/djangorestframework_api_response-0.0.5-py3-none-any.whl

---
+++ packages/djangorestframework-api-response/djangorestframework_api_response-0.0.5-py3-none-any.whl/rest_framework_api/pagination.py
@@ -0,0 +1,14 @@
+from rest_framework.pagination import PageNumberPagination
+
+class CustomPagination(PageNumberPagination):
+    page_query_param = 'p'
+    page_size_query_param = 'page_size'
+
+    def __init__(self, page_size=None, max_page_size=None):
+        self.page_size = page_size
+        self.max_page_size = max_page_size
+
+    def paginate_data(self, data, request):
+        self.page = request.query_params.get(self.page_query_param, 1)
+        self.page_size = self._get_page_size(request)
+        return self.paginate_queryset(data, request)
