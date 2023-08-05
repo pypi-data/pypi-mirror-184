91868df618845bea5fecc325917bafe9a1470c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:02 +0000

    Add package ga-utils/0.0.3/ga-utils-0.0.3.tar.gz

---
+++ packages/ga-utils/ga-utils-0.0.3.tar.gz/ga-utils-0.0.3/ga_utils/client.py
@@ -0,0 +1,63 @@
+import socket
+import json
+import struct
+import helper
+
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+CMD_DUMP_TREE = 300
+CMD_GET_POSITION = 103
+
+cmd_tree = json.dumps({
+    'cmd': CMD_DUMP_TREE,
+    'value': ''
+})
+def dump_tree(client):
+    body_buffer, head_buffer = helper.get_buffer(cmd_tree)
+    client.send(head_buffer)
+    client.send(body_buffer)
+
+def init_client(port: int, cmd: int):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, port))
+
+    dump_tree(client)
+    data = client.recv(4)
+    body_len = struct.unpack('<I', data)[0] 
+    msg = client.recv(body_len)
+    helper.log(msg)
+    print(msg)
+
+    # 通过@id或者@name获取元素的位置信息
+    if (cmd == CMD_GET_POSITION):
+        tree_data = json.loads(msg.decode('UTF-8'))
+        xml = json.loads(helper.xmltojson(tree_data['data']['xml']))
+        root = xml['AbstractRoot']
+        child = 'GameObject'
+        attr = '@id'
+        isNumber = True
+        if 'UWidget' in root:
+            child = 'UWidget'
+            attr = '@name'
+            isNumber = False
+
+        ids = helper.getIds(root, child, attr)
+        cmd_position = json.dumps({
+            'cmd': CMD_GET_POSITION,
+            'value': helper.handleIds(ids, isNumber)
+        })
+        body_buffer, head_buffer = helper.get_buffer(cmd_position)
+        client.send(head_buffer)
+        client.send(body_buffer)
+        data = client.recv(4)
+        body_len = struct.unpack('<I', data)[0] 
+        msg = client.recv(body_len)
+        helper.log(msg)
+        print(msg)
+    
+
+
+
+
+
+
---
+++ packages/ga-utils/ga-utils-0.0.3.tar.gz/ga-utils-0.0.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    name="ga-utils",                 
+    version="0.0.3",                               
+    author="janeluck",  
+    license='MIT',
+    author_email="janeluck158@outlook.com",
+    url="https://git.woa.com/janeajian/ga-util",
+    description="通过GA协议获取数据，用于调试GA控件树",
+    long_description="通过GA协议获取数据，用于调试GA控件树，示例：ga-utils get 300 --port 2004",
+    install_requires=[
+    'xmltodict>=0.13.0'
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    entry_points = {
+        'console_scripts': [
+            'ga-utils=ga_utils.main:main',
+        ]
+    }
+)
