3a33148df6e65c15b916e48466d2b5db8e4ec524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:14 +0000

    Add package greek-language-tools/1.1.0/greek_language_tools-1.1.0-py3-none-any.whl

---
+++ packages/greek-language-tools/greek_language_tools-1.1.0-py3-none-any.whl/greeklt/__init__.py
@@ -0,0 +1,4 @@
+from .capitalize import capitalize
+from .convert_final_s import convert_final_s
+from .greek_transliteration import greek_transliteration
+from .remove_accentuation import remove_accentuation
