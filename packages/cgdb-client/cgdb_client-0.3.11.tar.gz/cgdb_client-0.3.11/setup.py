dd2e5a483af09007a8109dde314e23cc9a55c1ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:12 +0000

    Add package cgdb-client/0.3.11/cgdb_client-0.3.11.tar.gz

---
+++ packages/cgdb-client/cgdb_client-0.3.11.tar.gz/cgdb_client-0.3.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cgdb_client',
+    packages=find_packages(include=["cgdb", "cgdb.exceptions", "cgdb.managers", "cgdb.resources", "cgdb.utils"]),
+    version='0.3.11',
+    install_requires=["pandas==1.1.5", "requests==2.27.0"],
+    description='CGDB Client',
+    author='CzechGlobe',
+    license='MIT',
+)
