43b63d0ca55d39a6ef7c4792ae4aeac72c514fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:36 +0000

    Add package jevan-library/0.0.21/jevan-library-0.0.21.tar.gz

---
+++ packages/jevan-library/jevan-library-0.0.21.tar.gz/jevan-library-0.0.21/users/urls.py
@@ -0,0 +1,38 @@
+# from rest_framework import routers
+from .views import InvitationViewSet, accept_invitation, UserViewSet, GroupViewSet, PermissionViewSet, ContenttypeViewSet, GroupOptionsViewSet, CUserViewSet, CGroupViewSet, signup
+from django.conf.urls import include, url
+from xtrm_drest.routers import DynamicRouter
+
+# router = routers.SimpleRouter()
+router = DynamicRouter()
+router.register('users', UserViewSet)
+router.register('edit/user', CUserViewSet)
+router.register('groups', GroupViewSet)
+router.register('edit/group', CGroupViewSet)
+router.register('permissions', PermissionViewSet)
+router.register('contenttypes', ContenttypeViewSet)
+router.register('groupoptions', GroupOptionsViewSet)
+router.register('invitations', InvitationViewSet)
+invitations_patterns = (
+    [
+        url(
+            r'^{0}/{1}/(?P<key>\w+)/?$'.format(
+                'invitations', 'verify'
+            ),
+            accept_invitation,
+            name='accept-invite'
+        ),
+        url(
+            r'^{0}/{1}/(?P<key>\w+)/?$'.format(
+                'invitations', 'signup'
+            ),
+            signup,
+            name='signup-invite'
+        ),
+    ],
+    'invitations'
+)
+
+urlpatterns = router.urls + [
+    url(r'^', include(invitations_patterns)),
+]
