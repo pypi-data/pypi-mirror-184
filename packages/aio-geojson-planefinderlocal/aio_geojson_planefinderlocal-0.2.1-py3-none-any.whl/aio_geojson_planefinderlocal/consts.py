760f020ce7338469813c088f1fe272f9d256294d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:29 +0000

    Add package aio-geojson-planefinderlocal/0.2.1/aio_geojson_planefinderlocal-0.2.1-py3-none-any.whl

---
+++ packages/aio-geojson-planefinderlocal/aio_geojson_planefinderlocal-0.2.1-py3-none-any.whl/aio_geojson_planefinderlocal/consts.py
@@ -0,0 +1,12 @@
+"""Flight Air Map constants."""
+
+ATTRIBUTION = "Planefinder Local Data"
+
+CUSTOM_ATTRIBUTE = 'custom_attribute'
+
+ATTR_FLIGHT_CODE = 'flightno'
+ATTR_AIRCRAFT_REGISTRATION = 'reg'
+ATTR_AIRCRAFT_TYPE = 'type'
+ATTR_DEPARTURE_AIRPORT = 'airport_dep'
+ATTR_ARRIVAL_AIRPORT = 'airport_final'
+ATTR_ID = 'id'
