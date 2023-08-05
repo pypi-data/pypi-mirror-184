9d689a51bd0ffe6cf2ef4e4381af00efbc0cfaea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:49 +0000

    Add package GooblerPKG/2.0.32/GooblerPKG-2.0.32.tar.gz

---
+++ packages/GooblerPKG/GooblerPKG-2.0.32.tar.gz/GooblerPKG-2.0.32/gooblerpkg/Storage/__init__.py
@@ -0,0 +1,17 @@
+###################
+### Goobler 2022 ##
+###################
+# Scripts made by Eric #
+
+commandsRegistered = {} #all commands that have been registered
+
+def registerCommand(name, commandLogic):
+    commandsRegistered[name] = commandLogic
+
+def getCommand(name):
+    return commandsRegistered[name]
+
+def executeCommand(name, args):
+    command = getCommand(name)
+    if command:
+        command(args)
---
+++ packages/GooblerPKG/GooblerPKG-2.0.32.tar.gz/GooblerPKG-2.0.32/gooblerpkg/__init__.py
@@ -0,0 +1,45 @@
+# IMPORTS:
+import os
+import sys
+import re
+import time
+import random
+import json
+import requests
+from .Config import colors
+from .Storage import commandsRegistered, getCommand, executeCommand, registerCommand
+
+###################
+### Goobler 2022 ##
+###################
+# Scripts made by Eric #
+
+# PURE CLASSES:
+
+#class colors:
+  #HEADER = '\033[95m'
+  #OKBLUE = '\033[94m'
+  #OKCYAN = '\033[96m'
+  #OKGREEN = '\033[92m'
+  #WARNING = '\033[93m'
+  #FAIL = '\033[91m'
+  #ENDC = '\033[0m'
+  #BOLD = '\033[1m'
+  #UNDERLINE = '\033[4m'
+
+# PURE FUNCS:
+
+def start(tkn):
+  resp = requests.get("https://goobl2.ericplayzyt.repl.co/api/get_name_using_auth", {"auth": f"{tkn}"}).text
+  print(f"Starting bot({resp})...")
+  resp2 = requests.post("https://goobl2.ericplayzyt.repl.co/api/login/bot_account", {"auth": f"{tkn}"})
+  print(colors.BOLD + resp + " has logged in." + colors.ENDC)
+  registerCommand("help", help)
+
+def Application(client_id=None, token=None):
+  if token == None:
+    print(colors._FAIL + colors._BOLD + colors._UNDERLINE + "[GooblerAPI] You cannot execute 'MakeApplication' without the positional arguments, token. Remember, the token is the admin password to your application.")
+
+
+while True:
+  getCommand("help")
