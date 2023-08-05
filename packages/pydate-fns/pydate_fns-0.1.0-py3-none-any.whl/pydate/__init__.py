e65734ff7b02d6fa0a9c8ace5e285a893933c91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:23 +0000

    Add package pydate-fns/0.1.0/pydate_fns-0.1.0-py3-none-any.whl

---
+++ packages/pydate-fns/pydate_fns-0.1.0-py3-none-any.whl/pydate/__init__.py
@@ -0,0 +1,17 @@
+# Version of the realpython-reader package
+__version__ = "0.0.1"
+
+from pydate.add import *
+from pydate.add_days import *
+from pydate.add_hours import *
+from pydate.add_milliseconds import *
+from pydate.add_months import *
+from pydate.add_weeks import *
+from pydate.add_years import *
+from pydate.closest_to import *
+from pydate.is_after import *
+from pydate.is_before import *
+from pydate.start_of_day import *
+from pydate.sub_days import *
+from pydate.sub_months import *
+from pydate.to_date import *
---
+++ packages/pydate-fns/pydate_fns-0.1.0-py3-none-any.whl/pydate/add_months/test.py
@@ -0,0 +1,25 @@
+import unittest
+from audioop import add
+from datetime import datetime
+
+from pydate import add_months
+
+
+class TestAddMonths(unittest.TestCase):
+    def test_add_months_1(self):
+        """
+        adds the given number of months
+        :return:
+        """
+
+        self.assertEqual(add_months(datetime(2014, 8, 1), 5), datetime(2015, 1, 1))
+
+    def test_add_months_negative(self):
+        """
+        adds negative amount of months
+        """
+        raise NotImplementedError("TODO: implement add negative amount of months")
+
+
+if __name__ == "__main__":
+    unittest.main()
