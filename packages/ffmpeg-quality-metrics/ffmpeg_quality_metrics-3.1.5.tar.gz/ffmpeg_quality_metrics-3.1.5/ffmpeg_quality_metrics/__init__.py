fd64507c7775a0ca3ae23295aa454b022f3b55ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:18 +0000

    Add package ffmpeg-quality-metrics/3.1.5/ffmpeg_quality_metrics-3.1.5.tar.gz

---
+++ packages/ffmpeg-quality-metrics/ffmpeg_quality_metrics-3.1.5.tar.gz/ffmpeg_quality_metrics-3.1.5/ffmpeg_quality_metrics/__init__.py
@@ -0,0 +1,13 @@
+from .ffmpeg_quality_metrics import (
+    FfmpegQualityMetrics,
+    FfmpegQualityMetricsError,
+    VmafOptions,
+)
+
+__version__ = "3.1.5"
+__all__ = [
+    "FfmpegQualityMetrics",
+    "FfmpegQualityMetricsError",
+    "VmafOptions",
+    "__version__",
+]
