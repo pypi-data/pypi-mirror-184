39986e951e7994758a384818233b46ecbc320d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:28 +0000

    Add package pychromaprint/1.2.2/pychromaprint-1.2.2.tar.gz

---
+++ packages/pychromaprint/pychromaprint-1.2.2.tar.gz/pychromaprint-1.2.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='pychromaprint',
+    version='1.2.2',
+    description='Python bindings for Chromaprint acoustic fingerprinting library',
+    packages=['chromaprint'],
+)
