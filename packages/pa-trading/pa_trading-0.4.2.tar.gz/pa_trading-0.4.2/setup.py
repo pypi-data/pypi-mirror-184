6cfe4a06314d183b4fcce0543f4db54073cfbb66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:14 +0000

    Add package pa-trading/0.4.2/pa_trading-0.4.2.tar.gz

---
+++ packages/pa-trading/pa_trading-0.4.2.tar.gz/pa_trading-0.4.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pa_trading', 'pa_trading._pb']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.1.3,<9.0.0']
+
+entry_points = \
+{'console_scripts': ['pa = pa_trading.pa:pa']}
+
+setup_kwargs = {
+    'name': 'pa-trading',
+    'version': '0.4.2',
+    'description': 'Comandos para cálculos de price action',
+    'long_description': '',
+    'author': 'Valmir Franca',
+    'author_email': 'vfranca3@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/vfranca/pa-trading',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
