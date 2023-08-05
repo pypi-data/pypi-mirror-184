d0afe2e82e9273de86b779a29a71a76ae6429001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:55 +0000

    Add package jade-config/0.0.1/jade_config-0.0.1-py3-none-any.whl

---
+++ packages/jade-config/jade_config-0.0.1-py3-none-any.whl/jade_config/__main__.py
@@ -0,0 +1,13 @@
+# Standard library imports
+import sys
+
+# Reader imports
+import config
+from config import test
+
+def main() -> None:
+    #Do all the logic stuff
+    test()
+
+if __name__ == "__main__":
+    main()
