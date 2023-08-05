720d9b931afc2d32f6ffe3d5d4397395136b044b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:11 +0000

    Add package selva/0.6.6/selva-0.6.6.tar.gz

---
+++ packages/selva/selva-0.6.6.tar.gz/selva-0.6.6/src/selva/di/container.py
@@ -0,0 +1,245 @@
+import inspect
+import logging
+from collections.abc import Awaitable, Iterable
+from types import FunctionType, ModuleType
+from typing import Any, Type, TypeVar
+
+from selva._util.maybe_async import maybe_async
+from selva._util.package_scan import scan_packages
+from selva.di.decorator import DI_SERVICE_ATTRIBUTE
+from selva.di.error import (
+    DependencyLoopError,
+    ServiceNotFoundError,
+    ServiceWithoutDecoratorError,
+)
+from selva.di.interceptor import Interceptor
+from selva.di.service.model import InjectableType, ServiceDependency, ServiceSpec
+from selva.di.service.parse import get_dependencies, parse_service_spec
+from selva.di.service.registry import ServiceRegistry
+
+TService = TypeVar("TService")
+
+
+logger = logging.getLogger(__name__)
+
+
+class Container:
+    def __init__(self):
+        self.registry = ServiceRegistry()
+        self.store: dict[tuple[type, str | None], Any] = {}
+        self.finalizers: list[Awaitable] = []
+        self.interceptors: list[Type[Interceptor]] = []
+
+    def register(
+        self,
+        service: InjectableType,
+        *,
+        provides: type = None,
+        name: str = None,
+    ):
+        self._register_service_spec(service, provides, name)
+
+    def service(self, service: type):
+        service_info = getattr(service, DI_SERVICE_ATTRIBUTE, None)
+
+        if not service_info:
+            raise ServiceWithoutDecoratorError(service)
+
+        provides, name = service_info
+        self._register_service_spec(service, provides, name)
+
+    def _register_service_spec(
+        self, service: type, provides: type | None, name: str | None
+    ):
+        service_spec = parse_service_spec(service, provides, name)
+        provided_service = service_spec.provides
+
+        self.registry[provided_service, name] = service_spec
+
+        logger.debug(
+            "service registered with name '%s': %s (provided by %s)",
+            name,
+            provided_service,
+            service.__qualname__,
+        )
+
+    def define(self, service_type: type, instance: Any, *, name: str = None):
+        self.store[service_type, name] = instance
+        logger.debug(
+            "service defined with name %s: %s", name, service_type.__qualname__
+        )
+
+    def interceptor(self, interceptor: Type[Interceptor]):
+        self.register(
+            interceptor,
+            provides=Interceptor,
+            name=f"{interceptor.__module__}.{interceptor.__qualname__}",
+        )
+        self.interceptors.append(interceptor)
+
+        logger.debug("interceptor registered: %s", interceptor.__qualname__)
+
+    def scan(self, *packages: str | ModuleType):
+        def predicate(item: Any):
+            return hasattr(item, DI_SERVICE_ATTRIBUTE)
+
+        for service in scan_packages(packages, predicate):
+            provides, name = getattr(service, DI_SERVICE_ATTRIBUTE)
+            self.register(service, provides=provides, name=name)
+
+    def has(self, service: type) -> bool:
+        definition = self.registry.get(service)
+        return definition is not None
+
+    def iter_service(
+        self, key: type
+    ) -> Iterable[tuple[type | FunctionType, str | None]]:
+        record = self.registry.services.get(key)
+        if not record:
+            raise ServiceNotFoundError(key)
+
+        for name, definition in record.providers.items():
+            yield definition.service, name
+
+    def iter_all_services(
+        self,
+    ) -> Iterable[tuple[type, type | FunctionType | None, str | None]]:
+        for interface, record in self.registry.services.items():
+            for name, definition in record.providers.items():
+                yield interface, definition.service, name
+
+    async def get(
+        self,
+        service_type: TService,
+        *,
+        name: str = None,
+        optional=False,
+    ) -> TService:
+        dependency = ServiceDependency(service_type, name=name, optional=optional)
+        return await self._get(dependency)
+
+    async def create(self, service: type) -> Any:
+        instance = service()
+        for name, dep_spec in get_dependencies(service):
+            dependency = await self._get(dep_spec)
+            setattr(instance, name, dependency)
+
+        return instance
+
+    def _get_from_cache(self, service_type: type, name: str | None) -> Any | None:
+        # try getting service from store
+        if instance := self.store.get((service_type, name)):
+            return instance
+
+        return None
+
+    async def _get(
+        self,
+        dependency: ServiceDependency,
+        stack: list = None,
+    ) -> Any | None:
+        service_type, name = dependency.service, dependency.name
+
+        if instance := self._get_from_cache(service_type, name):
+            return instance
+
+        try:
+            service_spec = self.registry[service_type, name]
+        except ServiceNotFoundError:
+            if dependency.optional:
+                return None
+            raise
+
+        stack = stack or []
+
+        if service_type in stack:
+            raise DependencyLoopError(stack + [service_type])
+
+        stack.append(service_type)
+        instance = await self._create_service(service_spec, stack)
+        stack.pop()
+
+        return instance
+
+    async def _create_service(
+        self,
+        service_spec: ServiceSpec,
+        stack: list[type],
+    ) -> Any:
+        # check if service exists in cache
+        name = service_spec.name
+        if instance := self._get_from_cache(service_spec.provides, name):
+            return instance
+
+        if factory := service_spec.factory:
+            dependencies = {
+                name: await self._get(dep, stack)
+                for name, dep in service_spec.dependencies
+            }
+
+            instance = await maybe_async(factory, **dependencies)
+            if inspect.isgenerator(instance):
+                gen_factory = instance.__next__
+                instance = await maybe_async(gen_factory)
+                self._setup_generator_finalizer(gen_factory)
+            elif inspect.isasyncgen(instance):
+                gen_factory = instance.__anext__
+                instance = await gen_factory()
+                self._setup_asyncgen_finalizer(gen_factory)
+
+            self.store[service_spec.provides, name] = instance
+        else:
+            instance = service_spec.service()
+            self.store[service_spec.provides, name] = instance
+
+            for name, dep_service in service_spec.dependencies:
+                dependency = await self._get(dep_service, stack)
+                setattr(instance, name, dependency)
+
+            await self._run_initializer(service_spec, instance)
+            self._setup_finalizer(service_spec, instance)
+
+        if service_spec.provides is not Interceptor:
+            await self._run_interceptors(instance, service_spec.provides)
+
+        return instance
+
+    @staticmethod
+    async def _run_initializer(service_spec: ServiceSpec, instance: Any):
+        if initializer := service_spec.initializer:
+            await maybe_async(initializer, instance)
+
+    def _setup_finalizer(self, service_spec: ServiceSpec, instance: Any):
+        if finalizer := service_spec.finalizer:
+            self.finalizers.append(maybe_async(finalizer, instance))
+
+    def _setup_generator_finalizer(self, finalizer):
+        def _finalizer():
+            try:
+                finalizer()
+            except StopIteration:
+                pass
+
+        self.finalizers.append(maybe_async(_finalizer))
+
+    def _setup_asyncgen_finalizer(self, finalizer):
+        async def _finalizer():
+            try:
+                await finalizer()
+            except StopAsyncIteration:
+                pass
+
+        self.finalizers.append(_finalizer())
+
+    async def _run_interceptors(self, instance: Any, service_type: type):
+        for cls in self.interceptors:
+            interceptor = await self.get(
+                Interceptor, name=f"{cls.__module__}.{cls.__qualname__}"
+            )
+            await maybe_async(interceptor.intercept, instance, service_type)
+
+    async def run_finalizers(self):
+        for finalizer in reversed(self.finalizers):
+            await finalizer
+
+        self.finalizers.clear()
---
+++ packages/selva/selva-0.6.6.tar.gz/selva-0.6.6/src/selva/web/application.py
@@ -0,0 +1,263 @@
+import importlib
+import inspect
+import logging
+from http import HTTPStatus
+from types import FunctionType, ModuleType
+from typing import Any
+
+from starlette.exceptions import HTTPException, WebSocketException
+from starlette.responses import Response
+from starlette.types import Receive, Scope, Send
+from starlette.websockets import WebSocketClose, WebSocketDisconnect, WebSocketState
+
+from selva._util.base_types import get_base_types
+from selva._util.maybe_async import maybe_async
+from selva.configuration.logging import setup_logging
+from selva.configuration.settings import Settings
+from selva.di.container import Container
+from selva.di.decorator import DI_SERVICE_ATTRIBUTE
+from selva.web.context import RequestContext
+from selva.web.converter import (
+    from_request_impl,
+    into_response_impl,
+    path_converter_impl,
+)
+from selva.web.converter.from_request import FromRequest
+from selva.web.converter.into_response import IntoResponse
+from selva.web.converter.path_converter import PathConverter
+from selva.web.error import HTTPNotFoundError
+from selva.web.middleware import Middleware
+from selva.web.routing.decorator import CONTROLLER_ATTRIBUTE
+from selva.web.routing.router import Router
+
+logger = logging.getLogger(__name__)
+
+
+def _is_controller(arg) -> bool:
+    return inspect.isclass(arg) and hasattr(arg, CONTROLLER_ATTRIBUTE)
+
+
+def _is_service(arg) -> bool:
+    return hasattr(arg, DI_SERVICE_ATTRIBUTE)
+
+
+def _is_module(arg) -> bool:
+    return inspect.ismodule(arg) or isinstance(arg, str)
+
+
+class Selva:
+    """Entrypoint class for a Selva Application
+
+    Will try to automatically import and register a module called "application".
+    Other modules and classes can be registered using the "register" method
+    """
+
+    def __init__(self):
+        self.di = Container()
+        self.router = Router()
+        self.handler = self._process_request
+
+        self.settings = Settings()
+        setup_logging(self.settings)
+
+        self.di.define(Settings, self.settings)
+        self.di.define(Router, self.router)
+
+        self.di.scan(path_converter_impl, from_request_impl, into_response_impl)
+        self.di.scan(self.settings.COMPONENTS)
+
+        components = self.settings.COMPONENTS
+        self._register_components(components)
+
+    async def __call__(self, scope, receive, send):
+        match scope["type"]:
+            case "http" | "websocket":
+                await self._handle_request(scope, receive, send)
+            case "lifespan":
+                await self._handle_lifespan(scope, receive, send)
+            case _:
+                raise RuntimeError(f"unknown scope '{scope['type']}'")
+
+    def _register_components(
+        self, components: list[str | ModuleType | type | FunctionType]
+    ):
+        try:
+            app = importlib.import_module("application")
+
+            if app not in components or app.__name__ not in components:
+                components.append(app)
+        except ImportError:
+            pass
+
+        services = []
+        packages = []
+
+        for component in components:
+            if _is_service(component):
+                services.append(component)
+            elif _is_module(component):
+                packages.append(component)
+            else:
+                raise TypeError(f"Invalid component: {component}")
+
+        self.di.scan(*packages)
+
+        for service in services:
+            self.di.service(service)
+
+        for _iface, impl, _name in self.di.iter_all_services():
+            if _is_controller(impl):
+                self.router.route(impl)
+
+    async def _initialize_middleware(self):
+        middleware = self.settings.MIDDLEWARE
+        if len(middleware) == 0:
+            return
+
+        if middleware_errors := [
+            m for m in middleware if not issubclass(m, Middleware)
+        ]:
+            mid_classes = [
+                f"{m.__module__}.{m.__qualname__}" for m in middleware_errors
+            ]
+            mid_class_name = f"{Middleware.__module__}.{Middleware.__qualname__}"
+            raise TypeError(
+                f"Middleware classes must inherit from '{mid_class_name}': {mid_classes}"
+            )
+
+        for cls in reversed(self.settings.MIDDLEWARE):
+            mid = await self.di.create(cls)
+            mid.set_app(self.handler)
+            self.handler = mid
+
+    async def _initialize(self):
+        await self._initialize_middleware()
+
+    async def _finalize(self):
+        await self.di.run_finalizers()
+
+    async def _handle_lifespan(self, _scope, receive, send):
+        while True:
+            message = await receive()
+            if message["type"] == "lifespan.startup":
+                try:
+                    await self._initialize()
+                    await send({"type": "lifespan.startup.complete"})
+                except Exception as err:
+                    await send({"type": "lifespan.startup.failed", "message": str(err)})
+            elif message["type"] == "lifespan.shutdown":
+                try:
+                    await self._finalize()
+                    await send({"type": "lifespan.shutdown.complete"})
+                except Exception as err:
+                    await send(
+                        {"type": "lifespan.shutdown.failed", "message": str(err)}
+                    )
+                break
+
+    async def _handle_request(self, scope: Scope, receive: Receive, send: Send):
+        context = RequestContext(scope, receive, send)
+
+        try:
+            if response := await self.handler(context):
+                await response(scope, receive, send)
+        except (WebSocketException, WebSocketDisconnect) as err:
+            await WebSocketClose(code=err.code, reason=err.reason)(scope, receive, send)
+        except HTTPException as err:
+            await Response(status_code=err.status_code)(scope, receive, send)
+        except Exception as err:
+            logger.exception("Error processing request", exc_info=err)
+            await Response(status_code=HTTPStatus.INTERNAL_SERVER_ERROR)(
+                scope, receive, send
+            )
+
+    async def _process_request(
+        self, context: RequestContext
+    ) -> Response | WebSocketClose:
+        method = context.method
+        path = context.path
+
+        match = self.router.match(method, path)
+
+        if not match:
+            raise HTTPNotFoundError()
+
+        controller = match.route.controller
+        action = match.route.action
+        path_params = match.params
+
+        path_params = await self._params_from_path(path_params, match.route.path_params)
+        request_params = await self._params_from_request(
+            context, match.route.request_params
+        )
+
+        all_params = path_params | request_params
+        instance = await self.di.get(controller)
+        response = await maybe_async(action, instance, **all_params)
+
+        if context.is_websocket:
+            if (
+                not response
+                and context.websocket._inner.client_state != WebSocketState.DISCONNECTED
+            ):
+                response = WebSocketClose()
+        else:
+            response = await self._into_response(response)
+
+        return response
+
+    async def _params_from_path(
+        self,
+        values: dict[str, str],
+        params: dict[str, type],
+    ) -> dict[str, Any]:
+        path_params = {}
+
+        for name, item_type in params.items():
+            for base_type in get_base_types(item_type):
+                if converter := await self.di.get(
+                    PathConverter[base_type], optional=True
+                ):
+                    value = await maybe_async(converter.from_path, values[name])
+                    path_params[name] = value
+                    break
+            else:
+                raise RuntimeError(
+                    f"no implementation of '{PathConverter.__name__}' found for type {item_type}"
+                )
+
+        return path_params
+
+    async def _params_from_request(
+        self,
+        context: RequestContext,
+        params: dict[str, type],
+    ) -> dict[str, Any]:
+        request_params = {}
+
+        for name, item_type in params.items():
+            for base_type in get_base_types(item_type):
+                if converter := await self.di.get(
+                    FromRequest[base_type], optional=True
+                ):
+                    value = await maybe_async(converter.from_request, context)
+                    request_params[name] = value
+                    break
+            else:
+                raise RuntimeError(
+                    f"no implementation of '{FromRequest.__name__}' found for type {item_type}"
+                )
+
+        return request_params
+
+    async def _into_response(self, value: Any | None) -> Response | None:
+        if isinstance(value, Response):
+            return value
+
+        for base in get_base_types(type(value)):
+            if converter := await self.di.get(IntoResponse[base], optional=True):
+                return await maybe_async(converter.into_response, value)
+
+        raise RuntimeError(
+            f"no implementation of '{IntoResponse.__name__}' found for type '{type(value)}'"
+        )
