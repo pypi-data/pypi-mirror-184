1f1db20750538f52f37de0a6bdc23b2a58e1d8cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:47 +0000

    Add package PyMieSim/0.9.5/PyMieSim-0.9.5-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/PyMieSim/PyMieSim-0.9.5-cp310-cp310-macosx_11_0_arm64.whl/PyMieSim/Examples/experiment/coreshell/plot_coreshell_Qback_vs_corediameter.py
@@ -0,0 +1,33 @@
+"""
+============================
+Qsca, Qback vs core diameter
+============================
+
+"""
+
+
+def run():
+    import numpy
+    from PyMieSim.Experiment import SourceSet, Setup, CoreShellSet
+    from PyMieSim.Materials import BK7, Silver
+    from PyMieSim import Measure
+
+    scatSet = CoreShellSet(core_diameter=numpy.geomspace(100e-09, 600e-9, 400),
+                           shell_diameter=800e-9,
+                           core_material=Silver,
+                           shell_material=BK7,
+                           n_medium=1)
+
+    sourceSet = SourceSet(wavelength=[800e-9, 900e-9, 1000e-9],
+                          polarization=0,
+                          amplitude=1)
+
+    experiment = Setup(scatterer_set=scatSet, source_set=sourceSet)
+
+    data = experiment.Get([Measure.Qsca, Measure.Qback])
+
+    data.plot(y=Measure.Qback, x=scatSet.core_diameter, y_scale='log').show()
+
+
+if __name__ == '__main__':
+    run()
