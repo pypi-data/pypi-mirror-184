3b2177d155c4d09f002226e4b038934e3976f1f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:03 +0000

    Add package erdpy/3.0.2/erdpy-3.0.2.tar.gz

---
+++ packages/erdpy/erdpy-3.0.2.tar.gz/erdpy-3.0.2/README.md
@@ -0,0 +1,14 @@
+# Description
+Python Command Line Tools for interacting with Multivers<sup>X</sup>.
+
+## Documentation
+[docs.elrond.com](https://docs.elrond.com/sdk-and-tools/erdpy/erdpy/)
+
+## CLI
+[CLI](erdpy/CLI.md)
+
+## Distribution
+[erdpy-up](https://docs.elrond.com/sdk-and-tools/erdpy/installing-erdpy/) and [PyPi](https://pypi.org/project/erdpy/#history)
+
+## CHANGELOG
+[CHANGELOG](erdpy/CHANGELOG.md)
---
+++ packages/erdpy/erdpy-3.0.2.tar.gz/erdpy-3.0.2/erdpy/projects/templates_config.py
@@ -0,0 +1,44 @@
+import time
+
+from erdpy import config
+from erdpy.projects.templates_repository import TemplatesRepository
+from erdpy.utils import query_latest_release_tag
+
+
+def get_templates_repositories():
+    timestamp = int(time.time())
+    examples_rs_tag = config.get_dependency_tag('elrond_wasm_rs')
+
+    if examples_rs_tag == 'latest':
+        examples_rs_tag = query_latest_release_tag('ElrondNetwork/elrond-wasm-rs')
+
+    examples_rs_tag_no_v = remove_initial_v_from_version(examples_rs_tag)
+
+    return [
+        TemplatesRepository(
+            key="sc-examples",
+            url=f"https://github.com/ElrondNetwork/sc-examples/archive/master.zip?t={timestamp}",
+            github="ElrondNetwork/sc-examples",
+            relative_path="sc-examples-master"
+        ),
+
+        TemplatesRepository(
+            key="elrond-wasm-rs",
+            url=f"https://github.com/ElrondNetwork/elrond-wasm-rs/archive/{examples_rs_tag}.zip?t={timestamp}",
+            github="ElrondNetwork/elrond-wasm-rs",
+            relative_path=f"mx-sdk-rs-{examples_rs_tag_no_v}/contracts/examples"
+        )
+    ]
+
+
+def remove_initial_v_from_version(version: str) -> str:
+    """Remove the initial 'v' from semver strings 'vX.XX.XX', but leave branch
+    names or non-semver tags unchanged"""
+    if version[0] != 'v':
+        return version
+
+    version_no_v = version[1:]
+    if not version_no_v[0].isnumeric():
+        return version
+
+    return version_no_v
