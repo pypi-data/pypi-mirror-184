a05199ba3139121c02e633c0e7c61fe934ff68fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:29 +0000

    Add package openlineage-dbt/0.19.2/openlineage-dbt-0.19.2.tar.gz

---
+++ packages/openlineage-dbt/openlineage-dbt-0.19.2.tar.gz/openlineage-dbt-0.19.2/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#
+# Copyright 2018-2022 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+#
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open("README.md") as readme_file:
+     readme = readme_file.read()
+
+__version__ = "0.19.2"
+
+requirements = [
+    f"tqdm>=4.62.0",
+    f"openlineage-integration-common[dbt]=={__version__}",
+]
+
+
+extras_require = {
+    "tests": [
+        "pytest",
+        "pytest-cov",
+        "mock",
+        "flake8",
+        "mypy>=0.9.6",
+        "python-dateutil"
+    ],
+}
+extras_require["dev"] = set(sum(extras_require.values(), []))
+
+setup(
+    name="openlineage-dbt",
+    version=__version__,
+    description="OpenLineage integration with dbt",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="OpenLineage",
+    scripts=['scripts/dbt-ol'],
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require=extras_require,
+    python_requires=">=3.7",
+    zip_safe=False,
+    keywords="openlineage",
+)
