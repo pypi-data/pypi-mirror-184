e29df21e0ae53e7925f443652b84571759e37bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:45 +0000

    Add package mines-data-engineering/0.1.2a1/mines_data_engineering-0.1.2a1.tar.gz

---
+++ packages/mines-data-engineering/mines_data_engineering-0.1.2a1.tar.gz/mines_data_engineering-0.1.2a1/mines_data_engineering/__init__.py
@@ -0,0 +1,29 @@
+import logging
+import time
+from mines_data_engineering.container import MongoDB, TimescaleDB, dbdir
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+_mongo_instance = None
+_pg_instance = None
+
+def start_mongo():
+    """
+    Starts MongoDB and returns the connection string
+    """
+    global _mongo_instance
+    _mongo_instance = MongoDB.run()
+    logging.info("Sleeping for 2 seconds to let MongoDB start")
+    time.sleep(2)
+    return "mongodb://" + f"{dbdir.name}/mongodb-27017.sock".replace('/', '%2F')
+
+
+def start_postgres():
+    """
+    Starts MongoDB and returns the connection string
+    """
+    global _pg_instance
+    _pg_instance = TimescaleDB.run()
+    logging.info("Sleeping for 20 seconds to let Postgres start")
+    time.sleep(20)
+    return f"user=postgres password=password host={dbdir.name}"
