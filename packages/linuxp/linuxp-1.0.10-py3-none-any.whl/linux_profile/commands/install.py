9372f4104ac24a54b3857434aa49744e7595a38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:27 +0000

    Add package linuxp/1.0.10/linuxp-1.0.10-py3-none-any.whl

---
+++ packages/linuxp/linuxp-1.0.10-py3-none-any.whl/linux_profile/commands/install.py
@@ -0,0 +1,50 @@
+from linux_profile.base.action import Action
+from linux_profile.base.config import Config
+from linux_profile.handlers.alias import HandlerAlias
+from linux_profile.handlers.script import HandlerScript
+from linux_profile.handlers.package import HandlerPackage
+
+
+class Install(Config):
+
+    def setup(self):
+        """Defines the functions that are executed each
+        time the class is instantiated.
+        """
+        self.command = self.__class__.__name__.lower()
+        self.action = Action(
+            self.join([self.linuxp_path_config, self.linuxp_file_profile]))
+
+        func = self.join(value=[self.command, self.module], separator="_")
+        getattr(self, func, self)()
+
+    def install_package(self):
+        data = self.action.deep_search(
+            module=self.module,
+            tag=self.tag,
+            key='name',
+            value=self.item
+        )
+        for item in data:
+            item["command"] = self.command
+            HandlerPackage(sudo=self.sudo, debug=self.debug, **item)
+
+    def install_alias(self):
+        data = self.action.deep_search(
+            module=self.module,
+            tag=self.tag,
+            key='command',
+            value=self.item
+        )
+        for item in data:
+            HandlerAlias(sudo=self.sudo, debug=self.debug, **item)
+
+    def install_script(self):
+        data = self.action.deep_search(
+            module=self.module,
+            tag=self.tag,
+            key='name',
+            value=self.item
+        )
+        for item in data:
+            HandlerScript(sudo=self.sudo, debug=self.debug, **item, **dict(temp=self.linuxp_path_temp))
