48235d3fc5ce520f2c03ee52f70d66fbf711b599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:25 +0000

    Add package torchyolo/0.1.2/torchyolo-0.1.2.tar.gz

---
+++ packages/torchyolo/torchyolo-0.1.2.tar.gz/torchyolo-0.1.2/torchyolo/modelhub/yolox.py
@@ -0,0 +1,21 @@
+from torchyolo.modelhub.basemodel import YoloDetectionModel
+from yoloxdetect import YoloxDetector
+
+
+
+class YoloxDetectionModel(YoloDetectionModel):
+    def load_model(self):
+        model = YoloxDetector(self.model_path, config_path=self.config_path, device=self.device, hf_model=False)
+        model.conf = self.confidence_threshold
+        model.iou = self.iou_threshold
+        model.save = self.save
+        model.show = self.show
+        self.model = model
+        
+    def predict(self, image):
+        self.model.predict(image_path=image, image_size=self.image_size)
+    
+        
+        
+    
+        
