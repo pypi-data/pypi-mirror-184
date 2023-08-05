b98417abd0510483768a2a89ff44069a597e5c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:31 +0000

    Add package miniofs/0.0.4b0/miniofs-0.0.4b0-py3-none-any.whl

---
+++ packages/miniofs/miniofs-0.0.4b0-py3-none-any.whl/miniofs/__init__.py
@@ -0,0 +1,11 @@
+from miniofs.functional import *
+from minio import Minio
+
+__version__ = "0.0.4b"
+cfg = load_config()
+client = Minio(
+    cfg.host,
+    secure=False,
+    access_key=cfg.username,
+    secret_key=cfg.password,
+)
