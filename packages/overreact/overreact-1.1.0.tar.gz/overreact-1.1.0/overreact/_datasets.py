1d8f416d54b731968e819c97e8d78afbde0457f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:44 +0000

    Add package overreact/1.1.0/overreact-1.1.0.tar.gz

---
+++ packages/overreact/overreact-1.1.0.tar.gz/overreact-1.1.0/overreact/_datasets.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Small toy datasets for tests and benchmark."""
+
+import os
+
+import overreact as rx
+
+data_path = os.path.normpath(os.path.join(os.path.dirname(__file__), "../data/"))
+
+
+logfiles = {}
+for name in os.listdir(data_path):
+    walk_dir = os.path.join(data_path, name)
+    if os.path.isdir(walk_dir):
+        logfiles[name] = rx.io._LazyDict()
+        logfiles[name]._function = rx.io.read_logfile
+        for root, _, files in os.walk(walk_dir):
+            for filename in files:
+                if filename.endswith(".out"):
+                    logfiles[name][
+                        f"{filename[:-4]}@{os.path.relpath(root, walk_dir)}".replace(
+                            "@.", ""
+                        )
+                    ] = os.path.join(root, filename)
+
+
+if __name__ == "__main__":
+    for name in logfiles:
+        for compound in logfiles[name]:
+            print(name, compound, logfiles[name][compound].logfile)  # noqa: T201
