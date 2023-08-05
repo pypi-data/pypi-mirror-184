378f9ae96d13d7e111011f9eaf0363450532605c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:05 +0000

    Add package toydata/1.1.0/toydata-1.1.0-py3-none-any.whl

---
+++ packages/toydata/toydata-1.1.0-py3-none-any.whl/toydata/__init__.py
@@ -0,0 +1,30 @@
+from .Graph import Graph
+from .LinkedLists import Doublellist, Singlellist
+from .Maps import ChainHashMap, ProbeHashMap, SortedTableMap
+from .PositionalList import PositionalList
+from .Queue import ArrayDeque, ArrayQueue, LinkedDeque, LinkedQueue
+from .SearchTree import AVLTreeMap, RedBlackTreeMap, SplayTreeMap
+from .Stack import ArrayStack, LinkedStack
+from .Tree import LinkedBinaryTree
+
+__version__ = "1.0"
+
+__all__ = [
+    "Graph",
+    "Doublellist",
+    "Singlellist",
+    "ChainHashMap",
+    "ProbeHashMap",
+    "SortedTableMap",
+    "PositionalList",
+    "ArrayDeque",
+    "ArrayQueue",
+    "LinkedDeque",
+    "LinkedQueue",
+    "AVLTreeMap",
+    "RedBlackTreeMap",
+    "SplayTreeMap",
+    "LinkedStack",
+    "ArrayStack",
+    "LinkedBinaryTree",
+]
