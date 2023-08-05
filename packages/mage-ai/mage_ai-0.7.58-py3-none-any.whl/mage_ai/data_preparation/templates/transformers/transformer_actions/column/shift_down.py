6fa32f2069491701c70e6f8f67eb61e33a87d6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:48 +0000

    Add package mage-ai/0.7.58/mage_ai-0.7.58-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.7.58-py3-none-any.whl/mage_ai/data_preparation/templates/transformers/transformer_actions/column/shift_down.py
@@ -0,0 +1,18 @@
+{% extends "transformers/transformer_actions/action.jinja" %}
+{% block action %}
+    """
+    Execute Transformer Action: ActionType.SHIFT_DOWN
+
+    Shifts value in the selected column down by specified number periods.
+
+    Docs: https://github.com/mage-ai/mage-ai/blob/master/docs/actions/transformer_actions/README.md#shift-down
+    """
+    action = build_transformer_action(
+        df,
+        action_type=ActionType.SHIFT_DOWN,
+        arguments=[],  # Specify one column to perform shift on
+        axis=Axis.COLUMN,
+        options={'periods': 1},
+        outputs=[{'uuid': 'down_shifted_column_1', 'type': 'category'}],
+    )
+{% endblock %}
