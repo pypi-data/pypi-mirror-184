52eaddbfe0cb9128c9e3c4f69075b388cc78e5f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:33 +0000

    Add package pygadm/0.0.0/pygadm-0.0.0.tar.gz

---
+++ packages/pygadm/pygadm-0.0.0.tar.gz/pygadm-0.0.0/README.rst
@@ -0,0 +1,54 @@
+PyGADM
+======
+
+.. image:: https://readthedocs.org/projects/pygadm/badge/?version=latest
+    :target: https://pygadm.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+    
+.. image:: https://img.shields.io/badge/License-MIT-yellow.svg
+    :target: https://opensource.org/licenses/MIT
+    :alt: License: MIT
+
+.. image:: https://github.com/12rambau/pygadm/actions/workflows/unit.yml/badge.svg
+    :target: https://github.com/12rambau/pygadm/actions/workflows/unit.yml
+    :alt: Build
+
+.. image:: https://api.codeclimate.com/v1/badges/0b52bd4ca56ded02c96f/maintainability
+   :target: https://codeclimate.com/github/12rambau/pygadm/maintainability
+   :alt: Maintainability
+
+.. image:: https://codecov.io/gh/12rambau/pygadm/branch/main/graph/badge.svg?token=O6ksUUazr4 
+   :target: https://codecov.io/gh/12rambau/pygadm
+   :alt: Coverage
+    
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+   :target: https://github.com/psf/black
+   :alt: Black badge
+   
+.. image:: https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg
+   :target: https://conventionalcommits.org
+   :alt: conventional commit
+
+Easy access to administrative boundary defined by GADM from a Python scripts.
+
+Thi lib provides access to GADM datasets from a Python script without downloading the file from their server. We provide access to The current version (4.1.) which delimits 400,276 administrative areas. 
+
+The data are freely available for academic use and other non-commercial use. Redistribution, or commercial use is not allowed without prior permission. See the `license <https://gadm.org/license.html>`__ of the GADM project for more details.
+
+.. note:: 
+
+   the dataset are generated in the GADM (Global Administrative Areas) project from Berkeley University. Any request relative to the geometries should be redirected to them. 
+
+install it using either ``pip`` or ``conda``: 
+
+.. code-block:: console
+
+   pip install pygadm 
+
+and then request area of interest from their name or GADM Id: 
+
+.. code-block:: python
+
+   import pygadm 
+
+   gdf = pygadm.get_items(name="Singapore", content_level=1)
---
+++ packages/pygadm/pygadm-0.0.0.tar.gz/pygadm-0.0.0/pygadm/bin/refresh_database.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+"""
+Script to manually update the database of GADM
+
+When GADM is releasing a new version the table should be updated to make sure that the names are still available in list and that new one are included. It is only meant to be executed by maintainer, Any PR included unwanted modifications to the database will be refused.
+"""
+
+import argparse
+import tempfile
+from pathlib import Path
+from urllib.parse import urlparse
+from urllib.request import urlopen
+
+import geopandas as gpd
+from tqdm import tqdm
+
+from pygadm import __gadm_version__
+
+parser = argparse.ArgumentParser(description=__doc__, usage="refresh_database")
+
+if __name__ == "__main__":
+
+    # read arguments
+    parser.add_argument(
+        "-f",
+        dest="gadm_src",
+        metavar="source.gpkg",
+        help="(str) : path to the GADM file",
+        required=False,
+        type=Path,
+    )
+
+    # parse agruments
+    args = parser.parse_args()
+    # raise Exception(args)
+
+    # url of the gadm files
+    url = f"https://geodata.ucdavis.edu/gadm/gadm4.1/gadm_{__gadm_version__}-gpkg.zip"
+
+    # read the all the geodata available in the server at once
+    with tempfile.TemporaryDirectory() as tmp_dir:
+
+        # check if a download is required
+        if vars(args)["gadm_src"] is not None:
+            file = Path(vars(args)["gadm_src"])
+
+        else:
+            # get the file as a simple dataframe
+            file = Path(tmp_dir) / urlparse(url).path.split("/")[-1]
+            response = urlopen(url)
+            pbar = tqdm(total=response.length, unit="iB", unit_scale=True)
+            size = 16 * 1024
+            with open(file, "wb") as f:
+                while True:
+                    chunk = response.read(size)
+                    pbar.update(size)
+                    if not chunk:
+                        break
+                    f.write(chunk)
+
+        # read the file
+        gadm_df = gpd.read_file(
+            file, layer=f"gadm_{__gadm_version__}", ignore_geometry=True
+        )
+
+    # replace the country column by NAME_0 for coherence
+    gadm_df.rename(columns={"COUNTRY": "NAME_0"}, inplace=True)
+
+    # filter all columns but the GID and the NAME
+    # we are not including the VARNAME to keep the file size under 3Mo
+    columns = ["UID"]
+    columns += [f"GID_{i}" for i in range(6)]
+    columns += [f"NAME_{i}" for i in range(6)]
+    gadm_df_filtered = gadm_df.filter(items=columns)
+
+    # save it in the data folder
+    filename = Path(__file__).parents[1] / "data" / "gadm_database.parquet"
+
+    # specifying the protocol for compatibility with Python 3.7
+    gadm_df_filtered.to_parquet(filename)
