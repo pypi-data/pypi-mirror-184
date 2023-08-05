a996fc7e54e17b2094b1610835e390a8b66b83ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:36 +0000

    Add package odap/0.1.1.dev2/odap-0.1.1.dev2-py3-none-any.whl

---
+++ packages/odap/odap-0.1.1.dev2-py3-none-any.whl/odap/common/exceptions.py
@@ -0,0 +1,20 @@
+class ConfigAttributeMissingException(Exception):
+    pass
+
+
+class InvalidConfigAttributException(Exception):
+    pass
+
+
+class WidgetValueException(Exception):
+    pass
+
+
+class NotebookException(Exception):
+    def __init__(self, message, path):
+        message = f"{message} At: {path}"
+        super().__init__(message)
+
+
+class InvalidNotebookLanguageException(Exception):
+    pass
