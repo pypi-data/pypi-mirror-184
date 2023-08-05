36d1d6d6c825a56f8721a8a20e93f471496b5284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:53 +0000

    Add package tensor-theorem-prover/0.11.2/tensor_theorem_prover-0.11.2-py3-none-any.whl

---
+++ packages/tensor-theorem-prover/tensor_theorem_prover-0.11.2-py3-none-any.whl/tensor_theorem_prover/prover/ProofStep.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Dict, Optional
+
+from tensor_theorem_prover.types import Variable, Term
+from tensor_theorem_prover.normalize import CNFDisjunction, CNFLiteral
+
+
+SubstitutionsMap = Dict[Variable, Term]
+
+
+def subsitutions_to_str(substitutions: SubstitutionsMap) -> str:
+    inner_str = ", ".join(f"{var} -> {term}" for var, term in substitutions.items())
+    return "{" + inner_str + "}"
+
+
+@dataclass
+class ProofStep:
+    """A single step in a proof of a goal"""
+
+    source: CNFDisjunction
+    target: CNFDisjunction
+    source_unification_literal: CNFLiteral
+    target_unification_literal: CNFLiteral
+    source_substitutions: SubstitutionsMap
+    target_substitutions: SubstitutionsMap
+    resolvent: CNFDisjunction
+    similarity: float
+    # this refers to the overall similarity of this step and all of its parents
+    running_similarity: float
+    depth: int
+    parent: Optional[ProofStep] = None
+
+    def __str__(self) -> str:
+        output = f"Similarity: {self.similarity}\n"
+        output += f"Source: {self.source}\n"
+        output += f"Target: {self.target}\n"
+        output += f"Unify: {self.source_unification_literal.atom} = {self.target_unification_literal.atom}\n"
+        output += f"Subsitutions: {subsitutions_to_str(self.source_substitutions)}, {subsitutions_to_str(self.target_substitutions)}\n"
+        output += f"Resolvent: {self.resolvent}"
+        return output
---
+++ packages/tensor-theorem-prover/tensor_theorem_prover-0.11.2-py3-none-any.whl/tensor_theorem_prover/types/Atom.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Union
+
+from .Variable import Variable
+from .Constant import Constant
+from .Predicate import Predicate
+from .Function import BoundFunction
+
+Term = Union[Constant, Variable, BoundFunction]
+
+
+@dataclass(frozen=True)
+class Atom:
+    """
+    Logical Atom (predicate with terms)
+    """
+
+    predicate: Predicate
+    terms: tuple[Term, ...]
+
+    def __str__(self) -> str:
+        terms_str = ",".join([str(term) for term in self.terms])
+        return f"{self.predicate.symbol}({terms_str})"
