b150f45105f08fbfe35d6c0b350ba787afa84bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:49 +0000

    Add package aioecopanel/0.0.4/aioecopanel-0.0.4.tar.gz

---
+++ packages/aioecopanel/aioecopanel-0.0.4.tar.gz/aioecopanel-0.0.4/src/aioecopanel/exceptions.py
@@ -0,0 +1,16 @@
+"""Exceptions for EcoPanel"""
+
+class EcoPanelError(Exception):
+    """Generic EcoPanel exception."""
+
+class EcoPanelEmptyResponseError(Exception):
+    """"EcoPanel API response is empty exception"""
+
+class EcoPanelConnectionError(EcoPanelError):
+    """EcoPanel connection exception."""
+
+class EcoPanelConnectionTimeoutError(EcoPanelConnectionError):
+    """EcoPanel connection timeout exception."""
+
+class EcoPanelConnectionClosed(EcoPanelConnectionError):
+    """EcoPanel connection closed."""
