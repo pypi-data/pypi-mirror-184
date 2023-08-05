39a86b274a2c1ed4f6e757bf83a36b76684f33a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:29 +0000

    Add package openlineage-python/0.19.2/openlineage-python-0.19.2.tar.gz

---
+++ packages/openlineage-python/openlineage-python-0.19.2.tar.gz/openlineage-python-0.19.2/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#
+# Copyright 2018-2022 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+#
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_namespace_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "attrs>=19.3.0",
+    "requests>=2.20.0",
+    "pyyaml>=5.1.0",
+    "python-dateutil"
+]
+
+extras_require = {
+    "tests": [
+        "pytest",
+        "pytest-cov",
+        "mock",
+        "flake8",
+        "requests",
+        "pyyaml",
+        "mypy>=0.9.6",
+        "types-requests",
+        "types-PyYAML"
+    ],
+    "kafka": ["confluent-kafka"],
+}
+extras_require["dev"] = set(sum(extras_require.values(), []))
+
+setup(
+    name="openlineage-python",
+    version="0.19.2",
+    description="OpenLineage Python Client",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="OpenLineage",
+    packages=find_namespace_packages(include=['openlineage.*']),
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require=extras_require,
+    python_requires=">=3.7",
+    zip_safe=False,
+    keywords="openlineage",
+)
