3485cc0a6c0f0827406bf7577833ee67208e29d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:40 +0000

    Add package cubelib/1.0.2/cubelib-1.0.2-py3-none-any.whl

---
+++ packages/cubelib/cubelib-1.0.2-py3-none-any.whl/cubelib/__init__.py
@@ -0,0 +1,11 @@
+from cubelib.enums import state, bound
+from cubelib.types import NextState
+from cubelib.p import readPacketsStream
+from . import proto
+
+version = '1.0.2'
+
+supported_versions = [
+    47,
+    340
+]
---
+++ packages/cubelib/cubelib-1.0.2-py3-none-any.whl/cubelib/proto/version_independent/ServerBound.py
@@ -0,0 +1,42 @@
+from cubelib.types import VarInt, String, UnsignedShort, NextState, Long, ByteArray
+from cubelib.p import Night
+
+class Handshaking:
+
+    class Handshake(Night):
+
+        ProtoVer: VarInt
+        ServerName: String[255]
+        ServerPort: UnsignedShort
+        NextState: NextState
+
+    map = {0: Handshake}
+    inv_map = {Handshake: 0}
+
+class Status:
+
+    class Request(Night):
+        pass
+
+    class Ping(Night):
+
+        Uniq: Long
+
+    map = {0: Request, 1: Ping}
+    inv_map = {Request: 0, Ping: 1}
+
+class ClassicLogin:
+
+    class LoginStart(Night):
+
+        Username: String[16]
+
+    class EncryptionResponse(Night):
+
+        SharedSecretLength: VarInt
+        SharedSecret: ByteArray
+        VerifyTokenLength: VarInt
+        VerifyToken: ByteArray
+
+    map = {0: LoginStart, 1: EncryptionResponse}
+    inv_map = {LoginStart: 0, EncryptionResponse: 1}
