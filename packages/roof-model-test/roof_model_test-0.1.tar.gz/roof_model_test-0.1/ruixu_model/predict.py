88bbd2130296a7eef07042c7414ee26223c4fa58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:57 +0000

    Add package roof-model-test/0.1/roof_model_test-0.1.tar.gz

---
+++ packages/roof-model-test/roof_model_test-0.1.tar.gz/roof_model_test-0.1/ruixu_model/predict.py
@@ -0,0 +1,10 @@
+# test pip
+
+def mask():
+    print('Ruixu is working on it.')
+    
+def run():
+    print('Qiang is working on it.')
+
+def help():
+    print('Delin, Aaron and Hao-wei are working on it.')
