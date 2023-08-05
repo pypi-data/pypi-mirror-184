d0a8b7cfc078dc50cd6f5f3771dd6892542e0639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:11 +0000

    Add package alterize/0.0.3/alterize-0.0.3-py3-none-any.whl

---
+++ packages/alterize/alterize-0.0.3-py3-none-any.whl/alterize/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.0.3'
+
+from alterize.alter import rename_table, rename_column, add_column, drop_column
+from alterize.alter import create_primary_key, replace_primary_key, copy_table
