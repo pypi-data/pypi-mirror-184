1c0224dbb3477726c71d81765bb8bcdb7ad7594e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:23 +0000

    Add package beebeesheep/1.3/beebeesheep-1.3.tar.gz

---
+++ packages/beebeesheep/beebeesheep-1.3.tar.gz/beebeesheep-1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='beebeesheep',
+    version='v1.3',
+    description='this module can print bee bee im sheep',
+    py_modules=['beebeesheep'],
+    author='cleversheep2011', 
+    author_email='cleversheep2011@outlook.com',
+    url='https://afdian.net/a/cleversheep',
+    license='MIT'
+)
