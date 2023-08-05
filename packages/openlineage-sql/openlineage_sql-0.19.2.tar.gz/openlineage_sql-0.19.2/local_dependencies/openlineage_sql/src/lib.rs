873e0036b3cb7ae14c5a5d85bd8c6eaff456dd65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:30 +0000

    Add package openlineage-sql/0.19.2/openlineage_sql-0.19.2.tar.gz

---
+++ packages/openlineage-sql/openlineage_sql-0.19.2.tar.gz/openlineage_sql-0.19.2/local_dependencies/openlineage_sql/src/lib.rs
@@ -0,0 +1,84 @@
+// Copyright 2018-2022 contributors to the OpenLineage project
+// SPDX-License-Identifier: Apache-2.0
+
+mod bigquery;
+mod context;
+mod dialect;
+mod lineage;
+mod visitor;
+
+use std::collections::HashSet;
+
+pub use bigquery::BigQueryDialect;
+use context::Context;
+pub use dialect::*;
+pub use lineage::*;
+use visitor::Visit;
+
+use anyhow::{anyhow, Result};
+use sqlparser::parser::Parser;
+
+pub fn parse_multiple_statements(
+    sql: Vec<&str>,
+    dialect: &dyn CanonicalDialect,
+    default_schema: Option<String>,
+) -> Result<SqlMeta> {
+    let mut inputs: HashSet<DbTableMeta> = HashSet::new();
+    let mut outputs: HashSet<DbTableMeta> = HashSet::new();
+    let mut column_lineage: Vec<ColumnLineage> = vec![];
+
+    for statement in sql {
+        let ast = Parser::parse_sql(dialect.as_base(), statement)?;
+
+        if ast.is_empty() {
+            return Err(anyhow!("Empty statement list"));
+        }
+
+        for stmt in ast {
+            let mut context = Context::new(dialect.clone(), default_schema.clone());
+            stmt.visit(&mut context)?;
+            column_lineage.extend(context.mut_columns().drain().map(|(descendant, lineage)| {
+                ColumnLineage {
+                    descendant,
+                    lineage: Vec::from_iter(lineage),
+                }
+            }));
+            inputs.extend(context.inputs);
+            outputs.extend(context.outputs);
+        }
+    }
+    Ok(SqlMeta::new(
+        inputs.into_iter().collect(),
+        outputs.into_iter().collect(),
+        column_lineage,
+    ))
+}
+
+pub fn parse_sql(
+    sql: &str,
+    dialect: &dyn CanonicalDialect,
+    default_schema: Option<String>,
+) -> Result<SqlMeta> {
+    parse_multiple_statements(vec![sql], dialect, default_schema)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::DbTableMeta;
+
+    #[test]
+    fn compare_db_meta() {
+        assert_ne!(
+            DbTableMeta {
+                database: None,
+                schema: None,
+                name: "discount".to_string()
+            },
+            DbTableMeta {
+                database: None,
+                schema: Some("public".to_string()),
+                name: "discount".to_string()
+            }
+        );
+    }
+}
