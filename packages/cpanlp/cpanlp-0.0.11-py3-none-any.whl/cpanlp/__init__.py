f064a8553316dd6ed22e2739cb72263ff323db2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:56 +0000

    Add package cpanlp/0.0.11/cpanlp-0.0.11-py3-none-any.whl

---
+++ packages/cpanlp/cpanlp-0.0.11-py3-none-any.whl/cpanlp/__init__.py
@@ -0,0 +1,12 @@
+__author__ = 'Bfsu Intelligent Accounting Team'
+__copyright__ = 'Copyright (C) 2023 DracoDeng'
+__license__ = 'MIT'
+__version__ = '0.0.11'
+
+from cpanlp.hello import sayhello
+from cpanlp.asset import *
+from cpanlp.calculate import *
+from cpanlp.liability import *
+from cpanlp.stocktrade import *
+from equity import *
+from nlp import *
