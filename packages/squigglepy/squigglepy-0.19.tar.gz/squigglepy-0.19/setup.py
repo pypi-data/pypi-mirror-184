fc4aa4d46e5dd2c99a0b88109039461339187027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:53 +0000

    Add package squigglepy/0.19/squigglepy-0.19.tar.gz

---
+++ packages/squigglepy/squigglepy-0.19.tar.gz/squigglepy-0.19/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='squigglepy',
+     version='0.19',
+     author='Peter Wildeford',
+     author_email='peter@peterhurford.com',
+     description=('Squiggle programming language for intuitive probabilistic' +
+                  ' estimation features in Python'),
+     long_description=long_description,
+     long_description_content_type='text/markdown',
+     url='https://github.com/rethinkpriorities/squigglepy',
+     packages=setuptools.find_packages(),
+     classifiers=[
+         'Development Status :: 3 - Alpha',
+         'Programming Language :: Python :: 3',
+         'License :: OSI Approved :: MIT License',
+         'Operating System :: OS Independent',
+     ],
+ )
