39afec11774c0c3e04710273a5e8fae134c4c964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:49 +0000

    Add package parm-geo/0.0.0/parm_geo-0.0.0.tar.gz

---
+++ packages/parm-geo/parm_geo-0.0.0.tar.gz/parm_geo-0.0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="parm_geo",
+    version="0.0.0",
+    author="Ruixu",
+    author_email="lrxjason@gmail.com",
+    description="test_code",
+    long_description="upload_test",
+    long_description_content_type="text/markdown",
+    url="https://github.com/lrxjason/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
