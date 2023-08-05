a6a70b2491cc61ff88be8f039c43dcd1f0f71be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:26 +0000

    Add package HelloAlgo/0.0.1/HelloAlgo-0.0.1.tar.gz

---
+++ packages/HelloAlgo/HelloAlgo-0.0.1.tar.gz/HelloAlgo-0.0.1/README.md
@@ -0,0 +1,4 @@
+# HelloAlgo
+This an implementation of the HelloAlgo behavior and optimisation alorithm in Python. this tool is a part of Luciole project
+
+
---
+++ packages/HelloAlgo/HelloAlgo-0.0.1.tar.gz/HelloAlgo-0.0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="HelloAlgo",
+    version="0.0.1",
+    descreption="This an implementation of the HelloAlgo behavior and optimisation alorithm in Python. this tool is a part of Luciole project",
+) 
