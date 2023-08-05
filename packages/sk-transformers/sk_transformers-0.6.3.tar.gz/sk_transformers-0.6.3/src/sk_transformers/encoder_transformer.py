45407c980de1a37abe0ad5e150d4dd4fbd491249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:27 +0000

    Add package sk-transformers/0.6.3/sk_transformers-0.6.3.tar.gz

---
+++ packages/sk-transformers/sk_transformers-0.6.3.tar.gz/sk_transformers-0.6.3/src/sk_transformers/encoder_transformer.py
@@ -0,0 +1,45 @@
+from typing import Union
+
+import pandas as pd
+from feature_engine.encoding import MeanEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class MeanEncoderTransformer(BaseEstimator, TransformerMixin):
+    """
+    Scikit-learn API for the [feature-engine MeanEncoder](https://feature-engine.readthedocs.io/en/latest/api_doc/encoding/MeanEncoder.html).
+
+    Args:
+        fill_na_value (Union[int, float]): Value to fill NaN values with.
+            Those may appear if a category is not present in the set the encoder was not fitted on.
+    """
+
+    def __init__(self, fill_na_value: Union[int, float] = -999) -> None:
+        self.encoder = MeanEncoder(ignore_format=False)
+        self.fill_na_value = fill_na_value
+
+    def fit(self, X: pd.DataFrame, y: pd.Series) -> "MeanEncoderTransformer":
+        """
+        Fit the MeanEncoder to the data.
+
+        Args:
+            X (pandas.DataFrame): DataFrame to fit the MeanEncoder to.
+            y (pandas.Series): Target variable.
+
+        Returns:
+            MeanEncoder: Fitted MeanEncoder.
+        """
+        self.encoder.fit(X, y)
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Transform the data using the fitted MeanEncoder.
+
+        Args:
+            X (pandas.DataFrame): DataFrame to transform.
+
+        Returns:
+            pandas.DataFrame: Transformed DataFrame.
+        """
+        return self.encoder.transform(X).fillna(self.fill_na_value)
