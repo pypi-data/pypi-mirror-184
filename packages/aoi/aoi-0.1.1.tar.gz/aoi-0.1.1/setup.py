8c40532087f867d2060eab039705282eb8c55962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:54 +0000

    Add package aoi/0.1.1/aoi-0.1.1.tar.gz

---
+++ packages/aoi/aoi-0.1.1.tar.gz/aoi-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aoi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama>=0.4.6,<0.5.0']
+
+setup_kwargs = {
+    'name': 'aoi',
+    'version': '0.1.1',
+    'description': 'SQLITE3 CLI wrapper build on python.',
+    'long_description': '# 🍥 aoi\nA simple python based sqlite3 CLI.\n\n---\n\n## Usage \n```sh\n$python -m aoi [-c "path to connect"]\n```\nusing the -c/--connect option will connect the application to the provided db file path.\n\nIf no option is provided, `:memory:` ( in memory database ) will be used.\n\n## Additional Features\n\nApart from normal sqlite quries you can run the following commands within the CLI:\n\n`:h/:help`: Get help for commands.\n\n`:q/:quit`: Exit the CLI.\n\n`:r/:recent [amount=5]`: Show last [amount=5] queries.\n\n---\n## Requirements\n* Python (3.8 or later)\n\nInstalling aoi in your environment using pip, poetry or any favourable package manager\n```sh\n# pip\n$python -m pip install git+https://github.com/sarthhh/aoi.git\n# poetry\n$poetry add git+https://github.com/sarthhh/aoi.git\n```',
+    'author': 'sarthhh',
+    'author_email': 'shiva02939@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
