ecaa1570980d5a55cdb06c7ddb68568f90122971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:33 +0000

    Add package Mensajes/1.0/Mensajes-1.0.tar.gz

---
+++ packages/Mensajes/Mensajes-1.0.tar.gz/Mensajes-1.0/Mensajes/hola/Saludos.py
@@ -0,0 +1,11 @@
+def saludar():
+  print("Hola, te saludo desde saludos.saludar():")  
+
+
+class Saludo:
+    def __init__(self):
+       print("Te saludo desde Saludo.__init__()")
+
+
+if __name__ == '__main__':
+    saludar()
---
+++ packages/Mensajes/Mensajes-1.0.tar.gz/Mensajes-1.0/Test.py
@@ -0,0 +1,13 @@
+# import Saludos este es para importar todo el script
+# from Saludos import saludar si tenemos más definiciones podemos agregarlas con comas
+# from Saludos import saludar, Saludo
+# from Saludos import * para importar todas las funciones o clases del módulo
+from Mensajes.hola.Saludos import *
+from Mensajes.adios.despedida import *
+
+# Saludos.saludar()
+saludar()
+Saludo()
+
+despedir()
+Despedida()
