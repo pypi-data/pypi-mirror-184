7f6d5b38ad8208bb71b7463ebb172e069f67f201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:12 +0000

    Add package vodex/1.0.4/vodex-1.0.4-py3-none-any.whl

---
+++ packages/vodex/vodex-1.0.4-py3-none-any.whl/vodex/__init__.py
@@ -0,0 +1,16 @@
+"""Volumetric Data and Experiment manager.
+
+Modules exported by this package:
+
+- `loaders`: The classes to read the image data and metadate from files.
+- `core`: The core classes to organise the information about the experiment.
+- `dbmethods`: The classes to create, write to and query the data base.
+- `utils`: Some helper functions.
+"""
+from .loaders import *
+from .core import *
+from .dbmethods import *
+from .experiment import *
+
+# Version of the vodex package
+__version__ = "1.0.4"
