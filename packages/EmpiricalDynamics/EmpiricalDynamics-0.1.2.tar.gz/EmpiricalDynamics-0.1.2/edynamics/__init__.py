8f49da09007a44f9696d3a7edddcda521eb4beba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:42 +0000

    Add package EmpiricalDynamics/0.1.2/EmpiricalDynamics-0.1.2.tar.gz

---
+++ packages/EmpiricalDynamics/EmpiricalDynamics-0.1.2.tar.gz/EmpiricalDynamics-0.1.2/edynamics/__init__.py
@@ -0,0 +1,3 @@
+from .modelling_tools import data_types
+from .modelling_tools import models
+from .modelling_tools import blocks
---
+++ packages/EmpiricalDynamics/EmpiricalDynamics-0.1.2.tar.gz/EmpiricalDynamics-0.1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='EmpiricalDynamics',
+      version='0.1.2',
+      description='Modelling tools for nonlinear state space reconstruction analysis',
+      packages=['edynamics'],
+      author='Patrick Mahon',
+      author_email='pmahon3@uwo.ca',
+      zip_safe=False)
