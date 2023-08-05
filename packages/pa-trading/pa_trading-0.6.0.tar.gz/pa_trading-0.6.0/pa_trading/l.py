08c69a5b408c9693d8d19e1890bbe98c0867fcd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:54 +0000

    Add package pa-trading/0.6.0/pa_trading-0.6.0.tar.gz

---
+++ packages/pa-trading/pa_trading-0.6.0.tar.gz/pa_trading-0.6.0/pa_trading/l.py
@@ -0,0 +1,19 @@
+# pa-trading
+# Copyright 2022 Valmir França da Silva
+# http://github.com/vfranca
+import click
+
+
+# Cria o comando l - linha de canal
+@click.command()
+@click.argument("a", type=float)
+@click.argument("b", type=float)
+def l(a, b):
+    """Calcula proximo ponto da linha de canal."""
+    gap = a - b
+    c = b - gap
+    click.echo("%.2f" % c)
+
+
+if __name__ == "__main__":
+    l()
