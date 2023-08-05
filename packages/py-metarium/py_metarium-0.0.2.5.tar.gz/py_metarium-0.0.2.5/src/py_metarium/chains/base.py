e7aff85b6783fcb3c9d71c9d3b0f5a41d307bd19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:14 +0000

    Add package py-metarium/0.0.2.5/py_metarium-0.0.2.5.tar.gz

---
+++ packages/py-metarium/py_metarium-0.0.2.5.tar.gz/py_metarium-0.0.2.5/src/py_metarium/chains/base.py
@@ -0,0 +1,20 @@
+class Chain(object):
+
+    RECONNECTION_WAIT_DURATION_SECONDS = 5
+    MAX_RECONNECTION_ATTEMPTS = 10
+    POINT_PREFIX = "chain"
+
+    def prefix(self):
+        raise NotImplementedError
+    
+    def info(self):
+        raise NotImplementedError
+    
+    def get_blocks(self):
+        raise NotImplementedError
+    
+    def get_block_hash_from_block_number(self, block_number):
+        raise NotImplementedError
+    
+    def get_tip_number(self, finalized_only=False):
+        raise NotImplementedError
