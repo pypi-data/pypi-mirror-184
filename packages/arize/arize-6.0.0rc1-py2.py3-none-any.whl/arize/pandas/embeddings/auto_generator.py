9dc66ed0f97b3eb7accef279e636ee0e0221fc58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:20 +0000

    Add package arize/6.0.0rc1/arize-6.0.0rc1-py2.py3-none-any.whl

---
+++ packages/arize/arize-6.0.0rc1-py2.py3-none-any.whl/arize/pandas/embeddings/auto_generator.py
@@ -0,0 +1,20 @@
+from .cv_generators import EmbeddingGeneratorForCVImageClassification
+from .nlp_generators import EmbeddingGeneratorForNLPSequenceClassification
+from .usecases import UseCases
+
+
+class AutoEmbeddingGenerator:
+    def __init__(self, **kwargs):
+        raise EnvironmentError(
+            f"{self.__class__.__name__} is designed to be instantiated "
+            f"using the `{self.__class__.__name__}.from_use_case(use_case, **kwargs)`."
+        )
+
+    @staticmethod
+    def from_use_case(use_case, **kwargs):
+        if use_case == UseCases.NLP.SEQUENCE_CLASSIFICATION:
+            return EmbeddingGeneratorForNLPSequenceClassification(**kwargs)
+        elif use_case == UseCases.CV.IMAGE_CLASSIFICATION:
+            return EmbeddingGeneratorForCVImageClassification(**kwargs)
+        else:
+            raise ValueError(f"Invalid use case {use_case}")
