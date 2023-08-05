e3e1f0aa8ec9272e7a23c0e690280dcbc4cb52a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:08 +0000

    Add package together-node/0.0.5/together_node-0.0.5-py3-none-any.whl

---
+++ packages/together-node/together_node-0.0.5-py3-none-any.whl/together_node/src/constants.py
@@ -0,0 +1,35 @@
+MODEL_CONFIG = {
+    'OPT-6.7B': {
+        "singularity_url": "https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/ft_base_ampere.sif",
+        "weights_url":"https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/opt-6.7b-tp1.tar.gz",
+        "worker_model":"opt-6.7b-tp1",
+        "docker_id": "togethercomputer/fastertransformer",
+        "docker_startup_script": "/usr/local/bin/together start --config /home/user/cfg.yaml --worker.model_type gpt"
+    },
+    'OPT-1.3B': {
+        "singularity_url": "https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/ft_base_ampere.sif",
+        "weights_url":"https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/opt-1.3b-tp1.tar.gz",
+        "worker_model":"opt-1.3b-tp1",
+        "docker_id": "togethercomputer/fastertransformer",
+        "docker_startup_script": "/usr/local/bin/together start --config /home/user/cfg.yaml --worker.model_type gpt"
+    }
+}
+
+BINARY_CONFIG = {
+    "GO-TOGETHER": {
+        "url":"https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/together",
+        "config_url":"",
+    },
+    "TCM": {
+        "url":"https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/tcm",
+    }
+}
+
+SLURM_TEMPLATES_DOCKER = """#!/bin/bash
+{{SLURM_HEAD}}
+
+docker run --rm --gpus '"device={{CUDA_VISIBLE_DEVICES}}"' --ipc=host \
+-e NUM_WORKERS=auto \
+-v {{TOGETHER_PATH}}:/home/user/.together \
+{{DOCKER_ID}} {{DOCKER_STARTUP_SCRIPT}}
+"""
