93f3a5cdd73981cec6e4f4296b02264d73208e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:25 +0000

    Add package rejected-peps/1.0.0.dev1/rejected-peps-1.0.0.dev1.tar.gz

---
+++ packages/rejected-peps/rejected-peps-1.0.0.dev1.tar.gz/rejected-peps-1.0.0.dev1/rejected_peps/pep211.py
@@ -0,0 +1,37 @@
+"""\
+PEP INFO
+
+PEP 211 -- Adding A New Outer Product Operator
+Status: Rejected
+Created: 2000-07-15
+
+MODULE INFO
+
+This module implements a wrapper that enables the @ operator
+to mean itertools.product. Call the wrapper to get the original object.
+
+REFERENCES
+
+PEP 211: <https://peps.python.org/pep-0211/>
+"""
+PEP = 211
+
+from itertools import product
+class wrapper:
+    """Wrap an object to let it allow @ => product. Call it to get the original object.
+    For example:
+    >>> list(wrapper([2, 3]) @ (5, 6))
+    [(2, 5), (2, 6), (3, 5), (3, 6)]
+    >>> list([1] @ wrapper({4, 5}))
+    [(1, 4), (1, 5)]
+    >>> wrapper([1, 2])()
+    [1, 2]
+    """
+    def __init__(self, obj):
+        self._obj = obj
+    def __matmul__(self, other) -> product:
+        return product(self(), getattr(other, '_obj', other))
+    def __rmatmul__(self, other) -> product:
+        return product(getattr(other, '_obj', other), self())
+    def __call__(self):
+        return self._obj
