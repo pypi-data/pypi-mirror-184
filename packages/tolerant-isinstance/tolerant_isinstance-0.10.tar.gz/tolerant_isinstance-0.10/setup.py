f73b577838a2160a9fdb4efba869e58708470c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:36 +0000

    Add package tolerant-isinstance/0.10/tolerant_isinstance-0.10.tar.gz

---
+++ packages/tolerant-isinstance/tolerant_isinstance-0.10.tar.gz/tolerant_isinstance-0.10/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+#change to dict
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)),'README.md'), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.10'
+DESCRIPTION = "isinstance - more tolerant"
+
+# Setting up
+setup(
+    name="tolerant_isinstance",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/tolerant_isinstance',
+    author="Johannes Fischer",
+    author_email="<aulasparticularesdealemaosp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    #packages=['isiter'],
+    keywords=['isinstance', 'None', 'pd.NA'],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only', 'Programming Language :: Python :: 3.9', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Text Editors :: Text Processing', 'Topic :: Text Processing :: General', 'Topic :: Text Processing :: Indexing', 'Topic :: Text Processing :: Filters', 'Topic :: Utilities'],
+    install_requires=['isiter'],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
