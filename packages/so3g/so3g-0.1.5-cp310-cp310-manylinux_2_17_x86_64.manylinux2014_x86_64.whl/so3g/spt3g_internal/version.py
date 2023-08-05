5488058e9cc1ce6760dea2016457cee2293f2d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:28 +0000

    Add package so3g/0.1.5/so3g-0.1.5-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/so3g/so3g-0.1.5-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/so3g/spt3g_internal/version.py
@@ -0,0 +1,12 @@
+# AUTO-GENERATED FILE: DO NOT EDIT
+
+# Contains version control information (revision IDs, paths, etc.)
+# as of the last time make was run.
+
+upstream_url="UNKNOWN VCS"
+upstream_branch="UNKNOWN VCS"
+revision="a75e311ee9c6c8bfd7b6e96d26b712c6654a1130"
+gitrevision="a75e311ee9c6c8bfd7b6e96d26b712c6654a1130"
+localdiffs=True
+versionname=""
+fullversion="0.3-111-ga75e311-dirty"
