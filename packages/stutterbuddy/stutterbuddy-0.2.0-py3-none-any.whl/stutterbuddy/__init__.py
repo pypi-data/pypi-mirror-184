fd3406c7037c9d74b48d74fff8c2b7d3166fc488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:24 +0000

    Add package stutterbuddy/0.2.0/stutterbuddy-0.2.0-py3-none-any.whl

---
+++ packages/stutterbuddy/stutterbuddy-0.2.0-py3-none-any.whl/stutterbuddy/__init__.py
@@ -0,0 +1,14 @@
+"""
+    Author: Jonas Briguet
+    Date: 06.12.2021
+    Last Change: 3.01.2023
+    version: 0.2.0
+    This is a library with functions to interact with the stutterbuddy.ch api 
+"""
+
+from .lib.SubmissionSettings import SubmissionSettings
+from .lib.Stutterbuddy import Stutterbuddy
+from .lib.Job import Job
+from .lib.Asset import Asset
+
+
