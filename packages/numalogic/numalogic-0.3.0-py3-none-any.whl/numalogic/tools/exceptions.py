7796e742b567f3fceb3611aeed8d049e518ed706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:59 +0000

    Add package numalogic/0.3.0/numalogic-0.3.0-py3-none-any.whl

---
+++ packages/numalogic/numalogic-0.3.0-py3-none-any.whl/numalogic/tools/exceptions.py
@@ -0,0 +1,18 @@
+class ModelInitializationError(Exception):
+    pass
+
+
+class ModelNotFoundError(Exception):
+    pass
+
+
+class InvalidRangeParameter(Exception):
+    pass
+
+
+class LayerSizeMismatchError(Exception):
+    pass
+
+
+class DataModuleError(Exception):
+    pass
