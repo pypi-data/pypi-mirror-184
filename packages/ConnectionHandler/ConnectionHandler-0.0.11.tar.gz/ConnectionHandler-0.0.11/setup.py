926ec9479db64cb912f375cc553629d434ba93e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:34 +0000

    Add package ConnectionHandler/0.0.11/ConnectionHandler-0.0.11.tar.gz

---
+++ packages/ConnectionHandler/ConnectionHandler-0.0.11.tar.gz/ConnectionHandler-0.0.11/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION= '0.0.11'
+DESCREIPTION='AWS Database Connection Handler'
+LONG_DESCRIPTION='Handles connecting to databases within the AWS environment'
+NAME='Connection Handler'
+AUTHOR='Tyler Arnold'
+AUTHOR_EMAIL='tarnold@databankimx.com'
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCREIPTION,
+    packages=find_packages(),
+    install_requires=[
+        'aws-psycopg2'
+    ],
+    keywords=['Python', 'AWS', 'Database']
+)
