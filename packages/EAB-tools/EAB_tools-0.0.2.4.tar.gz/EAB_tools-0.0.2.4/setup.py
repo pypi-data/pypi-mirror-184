c3e7243555e39143e9d37a603151ec09084c64ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:32 +0000

    Add package EAB-tools/0.0.2.4/EAB_tools-0.0.2.4.tar.gz

---
+++ packages/EAB-tools/EAB_tools-0.0.2.4.tar.gz/EAB_tools-0.0.2.4/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="EAB_tools",
+    version="0.0.2.4",
+    author="Moshe Rubin",
+    author_email="mosherubin137@gmail.com",
+    description="Tools for data exported from EAB",
+    license="MIT",
+    install_requires=[
+        "pandas",
+        "ipython",
+        "dataframe_image",
+    ],
+)
