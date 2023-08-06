04eb87b9806aefc18c25ac5af068e9d2d70246ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:39 +0000

    Add package deskAssistant/0.0.5/deskAssistant-0.0.5.tar.gz

---
+++ packages/deskAssistant/deskAssistant-0.0.5.tar.gz/deskAssistant-0.0.5/deskAssistant/__init__.py
@@ -0,0 +1,5 @@
+from deskAssistant.deskAssistant import speak
+from deskAssistant.deskAssistant import tellTime
+from deskAssistant.deskAssistant import openMail
+from deskAssistant.deskAssistant import tellDay
+from deskAssistant.deskAssistant import takeCommand
