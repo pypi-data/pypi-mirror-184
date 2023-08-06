373106cff970d5afd73c6875265aeb622a4bd8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:51 +0000

    Add package april-vision/1.0.2/april_vision-1.0.2-py3-none-any.whl

---
+++ packages/april-vision/april_vision-1.0.2-py3-none-any.whl/april_vision/__init__.py
@@ -0,0 +1,22 @@
+"""An AprilTags wrapper with camera discovery and axis conversion."""
+from ._version import __version__
+from .calibrations import calibrations
+from .detect_cameras import find_cameras
+from .frame_sources import FrameSource, USBCamera
+from .marker import (CartesianCoordinates, Marker, Orientation,
+                     PixelCoordinates, SphericalCoordinate)
+from .vision import Processor
+
+__all__ = [
+    '__version__',
+    'CartesianCoordinates',
+    'FrameSource',
+    'Marker',
+    'Orientation',
+    'PixelCoordinates',
+    'Processor',
+    'SphericalCoordinate',
+    'USBCamera',
+    'calibrations',
+    'find_cameras',
+]
---
+++ packages/april-vision/april_vision-1.0.2-py3-none-any.whl/april_vision/j5/__init__.py
@@ -0,0 +1,7 @@
+"""j5 integration for april_vision."""
+from .april_camera import AprilCameraBoard, AprilTagHardwareBackend
+
+__all__ = [
+    'AprilCameraBoard',
+    'AprilTagHardwareBackend',
+]
