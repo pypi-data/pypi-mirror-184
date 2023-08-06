40d6b9159eb3a750326201b7c77e74d4437aabac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:18 +0000

    Add package evogression/0.7.2/evogression-0.7.2-cp310-cp310-win_amd64.whl

---
+++ packages/evogression/evogression-0.7.2-cp310-cp310-win_amd64.whl/tests/surface_3d_population_regression_test.py
@@ -0,0 +1,33 @@
+import unittest
+import sys
+sys.path.insert(1, '..')
+import evogression
+from test_data import surface_3d_data
+import matplotlib.pyplot as plt
+
+
+class Test3DSurfaceRegression(unittest.TestCase):
+    def test_best_creature_3d(self):
+        evolutions = evogression.random_population('z', surface_3d_data, num_creatures=25000, num_cycles=7, group_size=5)
+        z_test = [sum(e.predict(d, 'pred')['pred'] for e in evolutions) / len(evolutions) for d in surface_3d_data]
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        x = [point_dict['x'] for point_dict in surface_3d_data]
+        y = [point_dict['y'] for point_dict in surface_3d_data]
+        z = [point_dict['z'] for point_dict in surface_3d_data]
+
+        ax.scatter3D(x, y, z)
+        ax.scatter3D(x, y, z_test)
+
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        plt.title('Surface Regression - Random Population Test')
+        plt.show()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
