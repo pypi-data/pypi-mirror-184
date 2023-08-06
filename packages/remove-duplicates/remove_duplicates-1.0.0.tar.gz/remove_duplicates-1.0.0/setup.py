a974330f86b3b6291471824d13af23b6e12c3fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:27:32 +0000

    Add package remove-duplicates/1.0.0/remove_duplicates-1.0.0.tar.gz

---
+++ packages/remove-duplicates/remove_duplicates-1.0.0.tar.gz/remove_duplicates-1.0.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import setuptools
+
+with open("README.md") as file:
+    read_me_description = file.read()
+
+setuptools.setup(
+    name="remove_duplicates",
+    version="1.0.0",
+    author="Roman Shevchik",
+    author_email="goctaprog@gmail.com",
+    description="Utility to recursive search and move/delete duplicate files of the same size and context in specified folder.",
+    long_description=read_me_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/octaprog7/remove_duplicates",
+    py_modules = ['my_utils', 'remove_duplicates'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+    ],
+    python_requires='>=3.7.2,<3.12',
+    entry_points={
+        'console_scripts': [
+            'rmdup = remove_duplicates:main',
+        ],
+    }
+)
