9c05775fe30c59a60e191986d0276c2ff4e3fdfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:20 +0000

    Add package robotpy-apriltag/0.0.0/robotpy-apriltag-0.0.0.tar.gz

---
+++ packages/robotpy-apriltag/robotpy-apriltag-0.0.0.tar.gz/robotpy-apriltag-0.0.0/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup
+setup(name="robotpy-apriltag", version="0.0.0", author="Dustin Spicuzza", url="https://github.com/robotpy/robotpy-apriltag", packages=["robotpy_apriltag"])
