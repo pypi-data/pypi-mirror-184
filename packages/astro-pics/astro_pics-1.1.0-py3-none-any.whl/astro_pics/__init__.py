6cbef0877b8c7664ccf6a447cdfa692fbc1f8c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:46 +0000

    Add package astro-pics/1.1.0/astro_pics-1.1.0-py3-none-any.whl

---
+++ packages/astro-pics/astro_pics-1.1.0-py3-none-any.whl/astro_pics/__init__.py
@@ -0,0 +1,8 @@
+import subprocess
+subprocess.run(["pip", "install", "astropy", "-q"])
+subprocess.run(["pip", "install", "requests", "-q"])
+subprocess.run(['pip', 'install', 'pyquery', "-q"])
+subprocess.run(["pip", "install", "pillow", "-q"])
+subprocess.run(["pip", "install", "matplotlib", "-q"])
+subprocess.run(["pip", "install", "numpy", "-q"])
+subprocess.run(["pip", "install", "scipy", "-q"])
