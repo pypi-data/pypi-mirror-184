863200409809ea380aa0152ff6720c0de7683a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:54 +0000

    Add package snakecdysis/0.0.1rc0/snakecdysis-0.0.1rc0-py3-none-any.whl

---
+++ packages/snakecdysis/snakecdysis-0.0.1rc0-py3-none-any.whl/snakecdysis/global_variable.py
@@ -0,0 +1,5 @@
+
+
+__dict_context_settings = dict(ignore_unknown_options=True, max_content_width=800, help_option_names=('-h', '--help'))
+
+GIT_URL =  "https://forge.ird.fr/phim/sravel/snakecdysis"
