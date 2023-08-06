f129b55118d0137f7a4503113b825ac539256720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:33 +0000

    Add package theprometheus/1.0.19/theprometheus-1.0.19.tar.gz

---
+++ packages/theprometheus/theprometheus-1.0.19.tar.gz/theprometheus-1.0.19/prometheus/__init__.py
@@ -0,0 +1,9 @@
+__all__ = ["models","getpsf","synth","groupfit","leastsquares","allfit",
+           "ccddata","detection","aperture","sky","prometheus","utils","galfit"]
+__version__ = '1.0.19'
+
+from .ccddata import CCDData
+from prometheus import prometheus as pm
+
+read = CCDData.read
+run = pm.run
