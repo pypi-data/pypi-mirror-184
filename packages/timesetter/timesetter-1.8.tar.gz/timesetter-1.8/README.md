55d570d43f9e5da28e639454bdd3ef679f742d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:56 +0000

    Add package timesetter/1.8/timesetter-1.8.tar.gz

---
+++ packages/timesetter/timesetter-1.8.tar.gz/timesetter-1.8/README.md
@@ -0,0 +1,59 @@
+# User Guide
+
+Timesetter is a Python package to set system time regardless of operating system
+
+## Supported Platforms
+
+- Windows
+- Linux
+- macOS
+
+## Tips
+
+- This package aims to set system time in the precision of below milliseconds.
+- You need to pass a datetime object to the `set` function. If the timezone information was not included in the time object(naive), the package will assume that it's identical to the system.
+- You need to run your Python code with administration privileges for this package to work properly. Otherwise, `PermissionError` will be raised. This applies to all platforms.
+
+## How to Use
+
+Install the package from PyPI
+
+```
+pip install timesetter
+```
+
+Then import it in your Python code
+
+```
+from datetime import datetime
+import timesetter
+
+target_time = datetime(year=2021, month=3, day=29, hour=15, minute=38, second=12)
+timesetter.set(target_time)
+```
+
+# For Maintainers
+
+## Rules
+
+Type hints as well as stub files for Python should be provided for the maintainability of the code. Turn on strict type checking in whatever IDE you are using. If some third party packages doesn't support type checking very well, then you can write `# type: ignore` to suppress the warning.
+
+## Command Line Scripts
+
+Install packages written in `requirements.txt` from PyPI
+
+```
+pip install -r ./requirements.txt
+```
+
+Generate the distribution archive to `/dist`
+
+```
+python -m build
+```
+
+Upload the package to PYPI
+
+```
+python -m twine upload dist/*
+```
---
+++ packages/timesetter/timesetter-1.8.tar.gz/timesetter-1.8/module/timesetter/__init__.py
@@ -0,0 +1,79 @@
+import os
+import platform
+import ctypes
+import time
+from datetime import datetime, timezone, timedelta
+
+
+def set(time_object: datetime):
+
+    # ■■■■■ check the datetime object ■■■■■
+
+    if type(time_object) != datetime:
+        raise TypeError("Argument needs to be a datetime object")
+
+    if time_object.tzinfo is None:
+        # if the object is timezone naive
+        # apply the system timezone to the object
+        system_timezone = timezone(timedelta(seconds=-time.timezone))
+        time_object = time_object.replace(tzinfo=system_timezone)
+
+    # now the time is represented in pure UTC timezone
+    time_object = time_object.astimezone(timezone.utc)
+
+    # ■■■■■ check administration privileges ■■■■■
+
+    if platform.system() == "Windows":
+        # on windows
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0  # type: ignore
+    elif platform.system() == "Linux" or platform.system() == "Darwin":
+        # on unix
+        is_admin = os.getuid() == 0  # type: ignore
+    else:
+        return
+
+    if not is_admin:
+        raise PermissionError(
+            "Administrator privileges are needed to set the system time. Make sure"
+            " you've run your Python code with administrator privileges."
+        )
+
+    # ■■■■■ change the system time ■■■■■
+
+    if platform.system() == "Windows":
+
+        this_folder, _ = os.path.split(__file__)
+        dll_filepath = os.path.join(this_folder, "on_windows.dll")
+        loaded_dll = ctypes.windll.LoadLibrary(dll_filepath)  # type: ignore
+
+        time_setter = loaded_dll["set_windows_time"]
+        time_setter.argtypes = (
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_int,
+        )
+        time_setter.restype = ctypes.c_int
+
+        time_setter(
+            time_object.year,
+            time_object.month,
+            time_object.day,
+            time_object.hour,
+            time_object.minute,
+            time_object.second,
+            int(time_object.microsecond / 1000),  # convert to milliseconds
+        )
+
+    elif platform.system() == "Linux" or platform.system() == "Darwin":
+        # unix: linux or macOS
+        # https://docs.python.org/ko/3.10/library/time.html#time.clock_settime
+
+        # system-wide real-time clock
+        realtime_clock_id = time.CLOCK_REALTIME  # type: ignore
+
+        timestamp = time_object.timestamp()
+        time.clock_settime(realtime_clock_id, timestamp)  # type: ignore
