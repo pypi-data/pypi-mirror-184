a6c41958245ae197630d19dccaad22eeada4619b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:54 +0000

    Add package bronx/2.0.0b3/bronx-2.0.0b3-py3-none-any.whl

---
+++ packages/bronx/bronx-2.0.0b3-py3-none-any.whl/bronx/net/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classes and functions to deal with network connections.
+"""
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+#: No automatic export
+__all__ = []
---
+++ packages/bronx/bronx-2.0.0b3-py3-none-any.whl/bronx/system/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+System related tools.
+"""
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+#: No automatic export
+__all__ = []
