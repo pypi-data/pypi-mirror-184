b944ccf183b54b6a63f33ad3526c727685c0aa92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:54 +0000

    Add package bronx/2.0.0b3/bronx-2.0.0b3.tar.gz

---
+++ packages/bronx/bronx-2.0.0b3.tar.gz/bronx-2.0.0b3/src/bronx/graphics/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+Graphical tools (mostly related to matplotlib).
+"""
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+#: No automatic export
+__all__ = []
---
+++ packages/bronx/bronx-2.0.0b3.tar.gz/bronx-2.0.0b3/src/bronx/meteo/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+Atmospheric physics related constants, functions or classes.
+"""
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+#: No automatic export
+__all__ = []
