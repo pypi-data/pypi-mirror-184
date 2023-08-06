b80add2a9aa0feabb7d49bff437f1668bcd06cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:50:27 +0000

    Add package python-simple-menu/1.0.2/python_simple_menu-1.0.2.tar.gz

---
+++ packages/python-simple-menu/python_simple_menu-1.0.2.tar.gz/python_simple_menu-1.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages
+from setuptools import setup
+
+VERSION = "1.0.2"
+
+setup(
+    name="python_simple_menu",
+    version=VERSION,
+    author="Chris Vann",
+    author_email="chrisvann01@gmail.com",
+    description="A simple console menu",
+    url="https://github.com/ripvannwinkler/python-simple-menu",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+    keywords=["python", "menu", "simple menu", "console menu"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+    ],
+)
