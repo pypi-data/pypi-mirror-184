b784084a86c97ea9db2d1fc0ebdc122e838d62f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:54 +0000

    Add package FLAML/1.1.1/FLAML-1.1.1.tar.gz

---
+++ packages/FLAML/FLAML-1.1.1.tar.gz/FLAML-1.1.1/flaml/__init__.py
@@ -0,0 +1,9 @@
+from flaml.automl import AutoML, logger_formatter
+from flaml.tune.searcher import CFO, BlendSearch, FLOW2, BlendSearchTuner, RandomSearch
+from flaml.onlineml.autovw import AutoVW
+from flaml.version import __version__
+import logging
+
+# Set the root logger.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
