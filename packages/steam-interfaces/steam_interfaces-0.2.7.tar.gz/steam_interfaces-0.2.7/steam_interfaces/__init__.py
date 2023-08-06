a70a373eba50c8c988d2241e6c4225dd34cf01e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:11 +0000

    Add package steam-interfaces/0.2.7/steam_interfaces-0.2.7.tar.gz

---
+++ packages/steam-interfaces/steam_interfaces-0.2.7.tar.gz/steam_interfaces-0.2.7/steam_interfaces/__init__.py
@@ -0,0 +1,13 @@
+"""
+:authors: Tarodictrl
+:license: MIT License, see LICENSE file
+:copyright: (c) 2023 Tarodictrl
+"""
+
+from .__main__ import *
+
+
+__author__ = "Tarodictrl"
+__version__ = "0.2.7"
+__license__ = "MIT License"
+__email__ = "vudi600@gmail.com"
