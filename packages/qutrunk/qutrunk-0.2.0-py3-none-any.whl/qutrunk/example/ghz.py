1cd8ddef260c14f63095ad62a99915a594f99439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:32 +0000

    Add package qutrunk/0.2.0/qutrunk-0.2.0-py3-none-any.whl

---
+++ packages/qutrunk/qutrunk-0.2.0-py3-none-any.whl/qutrunk/example/ghz.py
@@ -0,0 +1,39 @@
+"""GHZ state example."""
+
+from qutrunk.circuit import QCircuit
+from qutrunk.circuit.gates import CX, Measure, H, Barrier, All
+
+
+def run_ghz(backend=None):
+    # Create quantum circuit
+    qc = QCircuit(name="ghz", backend=backend)
+
+    # Allocate quantum qubits
+    qr = qc.allocate(3)
+
+    # Create a GHZ state
+    H * qr[0]
+    CX * (qr[0], qr[1])
+    CX * (qr[0], qr[2])
+
+    Barrier * qr
+
+    # Measure all the qubits
+    All(Measure) * qr
+
+    # Run quantum circuit with 1024 times
+    res = qc.run(shots=1024)
+
+    # Print measure results like:
+    # [{"000": 527}, {"111": 497}]
+    print(res.get_counts())
+    return qc
+
+
+if __name__ == "__main__":
+    # Run locally
+    circuit = run_ghz()
+
+    # Dram quantum circuit
+    circuit.draw()
+
