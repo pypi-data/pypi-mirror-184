b7a27116ab8860e1fae27f012f5655312c826936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:32 +0000

    Add package fastapi-class/2.0.0/fastapi_class-2.0.0-py3-none-any.whl

---
+++ packages/fastapi-class/fastapi_class-2.0.0-py3-none-any.whl/fastapi_class/__init__.py
@@ -0,0 +1,7 @@
+"""contains classes and decorators to use FastAPI with "class based routing"."""
+
+__version__ = "2.0.0"
+
+from fastapi_class import args, decorators, routable
+
+__all__ = ["args", "routable", "decorators"]
