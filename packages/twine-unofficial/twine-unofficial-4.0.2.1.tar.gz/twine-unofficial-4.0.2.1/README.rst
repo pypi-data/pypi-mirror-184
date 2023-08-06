1bc32e694f2381a8269b577e295f42c1a0d7ecdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:24 +0000

    Add package twine-unofficial/4.0.2.1/twine-unofficial-4.0.2.1.tar.gz

---
+++ packages/twine-unofficial/twine-unofficial-4.0.2.1.tar.gz/twine-unofficial-4.0.2.1/README.rst
@@ -0,0 +1,83 @@
+.. image:: https://img.shields.io/pypi/v/twine-unofficial.svg
+   :target: https://pypi.org/project/twine-unofficial
+
+.. image:: https://img.shields.io/pypi/pyversions/twine-unofficial.svg
+   :target: https://pypi.org/project/twine-unofficial
+
+.. image:: https://img.shields.io/pypi/dm/twine-unofficial
+   :target: https://pypi.org/project/twine-unofficial
+
+.. image:: https://img.shields.io/readthedocs/twine
+   :target: https://twine.readthedocs.io
+
+twine-unofficial
+================
+
+This is a fork of the `official twine`_ project, with the following
+changes:
+
+- Upload command now supports ``--skip-tls-check`` option to disable
+  TLS certificate verification. This is useful for uploading to
+  repositories with self-signed certificates.
+
+  Closed issue: `#463 <https://github.com/pypa/twine/pull/463>`_,
+  rejected PR: `#387 <https://github.com/pypa/twine/issues/387>`_.
+
+- Upload ``--skip-existing`` option now support pre-upload check
+  against non-default PyPI repositories.
+
+  Similar issue: `#919 <https://github.com/pypa/twine/issues/919>`_
+
+- Upload command support ``--fail-on-existing`` option to fail if
+  the package already exists on the repository.
+  This is useful for avoid overwriting existing packages, even
+  if PyPI is configured to allow overwriting.
+
+Installation
+------------
+
+.. code:: shell
+
+   $ pip install twine-unofficial
+
+Versioning
+----------
+
+Fork versioning is based on the official version, with the following
+format: ``{official-version}.{fork-version}``.
+
+twine
+=====
+
+Twine is a utility for `publishing`_ Python packages on `PyPI`_.
+
+It provides build system independent uploads of source and binary
+`distribution artifacts <distributions_>`_ for both new and existing
+`projects`_.
+
+See our `documentation`_ for a description of features, installation
+and usage instructions, and links to additional resources.
+
+Contributing
+------------
+
+See our `developer documentation`_ for how to get started, an
+architectural overview, and our future development plans.
+
+Code of Conduct
+---------------
+
+Everyone interacting in the Twine project's codebases, issue
+trackers, chat rooms, and mailing lists is expected to follow the
+`PSF Code of Conduct`_.
+
+.. _`official twine`: https://github.com/pypa/twine
+.. _`publishing`: https://packaging.python.org/tutorials/packaging-projects/
+.. _`PyPI`: https://pypi.org
+.. _`distributions`:
+   https://packaging.python.org/glossary/#term-Distribution-Package
+.. _`projects`: https://packaging.python.org/glossary/#term-Project
+.. _`documentation`: https://twine.readthedocs.io/
+.. _`developer documentation`:
+   https://twine.readthedocs.io/en/latest/contributing.html
+.. _`PSF Code of Conduct`: https://github.com/pypa/.github/blob/main/CODE_OF_CONDUCT.md
---
+++ packages/twine-unofficial/twine-unofficial-4.0.2.1.tar.gz/twine-unofficial-4.0.2.1/tests/test_main.py
@@ -0,0 +1,84 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+
+import pretend
+import requests
+
+from twine import __main__ as dunder_main
+from twine.commands import upload
+
+
+def test_exception_handling(monkeypatch, capsys):
+    monkeypatch.setattr(sys, "argv", ["twine", "upload", "missing.whl"])
+
+    error = dunder_main.main()
+    assert error
+
+    captured = capsys.readouterr()
+
+    # Hard-coding control characters for red text; couldn't find a succinct alternative.
+    # Removing trailing whitespace on wrapped lines; trying to test it was ugly.
+    level = "\x1b[31mERROR   \x1b[0m"
+    assert [line.rstrip() for line in captured.out.splitlines()] == [
+        f"{level} InvalidDistribution: Cannot find file (or expand pattern):",
+        "         'missing.whl'",
+    ]
+
+
+def test_http_exception_handling(monkeypatch, capsys):
+    monkeypatch.setattr(sys, "argv", ["twine", "upload", "test.whl"])
+    monkeypatch.setattr(
+        upload,
+        "upload",
+        pretend.raiser(
+            requests.HTTPError(
+                response=pretend.stub(
+                    url="https://example.org",
+                    status_code=400,
+                    reason="Error reason",
+                )
+            )
+        ),
+    )
+
+    error = dunder_main.main()
+    assert error
+
+    captured = capsys.readouterr()
+
+    # Hard-coding control characters for red text; couldn't find a succinct alternative.
+    # Removing trailing whitespace on wrapped lines; trying to test it was ugly.
+    level = "\x1b[31mERROR   \x1b[0m"
+    assert [line.rstrip() for line in captured.out.splitlines()] == [
+        f"{level} HTTPError: 400 Bad Request from https://example.org",
+        "         Error reason",
+    ]
+
+
+def test_no_color_exception(monkeypatch, capsys):
+    monkeypatch.setattr(sys, "argv", ["twine", "--no-color", "upload", "missing.whl"])
+
+    error = dunder_main.main()
+    assert error
+
+    captured = capsys.readouterr()
+
+    # Removing trailing whitespace on wrapped lines; trying to test it was ugly.
+    assert [line.rstrip() for line in captured.out.splitlines()] == [
+        "ERROR    InvalidDistribution: Cannot find file (or expand pattern):",
+        "         'missing.whl'",
+    ]
+
+
+# TODO: Test verbose output formatting
