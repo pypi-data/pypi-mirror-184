a7309817f89d8b281b47ed5622a9c8c5f2081d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:16 +0000

    Add package selenium-shortcuts/2.6/selenium-shortcuts-2.6.tar.gz

---
+++ packages/selenium-shortcuts/selenium-shortcuts-2.6.tar.gz/selenium-shortcuts-2.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'selenium-shortcuts',
+    packages = ['selenium_shortcuts'],
+    version = '2.6',
+    description = 'Selenium shortcut functions',
+    url = 'https://github.com/xjxckk/selenium-shortcuts/',
+    download_url = 'https://github.com/xjxckk/selenium-shortcuts/archive/refs/tags/v1.2.tar.gz',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+    )
