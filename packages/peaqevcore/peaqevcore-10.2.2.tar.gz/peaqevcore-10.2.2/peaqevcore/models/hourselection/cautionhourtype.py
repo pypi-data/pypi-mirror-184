22fbbbbd54beedcfb571872082e862de03f053b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:14 +0000

    Add package peaqevcore/10.2.2/peaqevcore-10.2.2.tar.gz

---
+++ packages/peaqevcore/peaqevcore-10.2.2.tar.gz/peaqevcore-10.2.2/peaqevcore/models/hourselection/cautionhourtype.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class CautionHourType(Enum):
+    SUAVE = "Suave"
+    INTERMEDIATE = "Intermediate"
+    AGGRESSIVE = "Aggressive"
+
+    @staticmethod
+    def get_num_value(type_state):
+        values = {
+            CautionHourType.SUAVE.value: 0.75,
+            CautionHourType.INTERMEDIATE.value: 0.5,
+            CautionHourType.AGGRESSIVE.value: 0.4
+            }
+
+        try:
+            if type_state in CautionHourType:
+                tt = type_state.value
+            else:
+                tt = type_state
+        except:
+            tt = type_state
+            
+        return values[tt]
+
+
+
+        
