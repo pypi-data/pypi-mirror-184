c3d9553d4811d9ece707afd2576ddd8997644c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:20 +0000

    Add package pbat/0.0.13/pbat-0.0.13.tar.gz

---
+++ packages/pbat/pbat-0.0.13.tar.gz/pbat-0.0.13/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('readme.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    packages = find_packages(),
+    name = 'pbat',
+    version = '0.0.13',
+    author = "Stanislav Doronin",
+    author_email = "mugisbrows@gmail.com",
+    url = 'https://github.com/mugiseyebrows/pbat',
+    description = 'Batch file preprocessor',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    install_requires = ['lark'],
+    package_data = {
+        'pbat': ['examples/*.pbat', 'examples/*.bat', 'pbat.lark']
+    },
+    entry_points = {
+        'console_scripts': [
+            'pbat = pbat.compile:main',
+            'pbat.compile = pbat.compile:main',
+            'pbat.watch = pbat.watch:main'
+        ]
+    }
+)
