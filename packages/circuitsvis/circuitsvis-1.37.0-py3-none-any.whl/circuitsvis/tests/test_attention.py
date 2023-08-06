911a76b6b21eee60257e23d4625ce9335da7bf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:52 +0000

    Add package circuitsvis/1.37.0/circuitsvis-1.37.0-py3-none-any.whl

---
+++ packages/circuitsvis/circuitsvis-1.37.0-py3-none-any.whl/circuitsvis/tests/test_attention.py
@@ -0,0 +1,16 @@
+import circuitsvis
+import circuitsvis.utils.render
+import numpy as np
+from circuitsvis.attention import attention_patterns
+
+
+class TestAttention:
+    def test_matches_snapshot(self, snapshot, monkeypatch):
+        monkeypatch.setattr(circuitsvis.utils.render, "uuid4", lambda: "mock")
+        monkeypatch.setattr(circuitsvis, "__version__", "1.0.0")
+
+        res = attention_patterns(
+            tokens=["a", "b"],
+            attention=np.array([[[0, 1], [0, 1]]])
+        )
+        snapshot.assert_match(str(res))
