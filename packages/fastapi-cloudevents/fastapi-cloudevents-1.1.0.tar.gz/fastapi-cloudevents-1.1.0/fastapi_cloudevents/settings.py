5ec12bf31ea5aa0c1c5367a83a36d1a488521e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:51 +0000

    Add package fastapi-cloudevents/1.1.0/fastapi-cloudevents-1.1.0.tar.gz

---
+++ packages/fastapi-cloudevents/fastapi-cloudevents-1.1.0.tar.gz/fastapi-cloudevents-1.1.0/fastapi_cloudevents/settings.py
@@ -0,0 +1,19 @@
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseSettings, Field
+from pydantic.types import constr
+
+
+class ContentMode(Enum):
+    binary = "binary"
+    structured = "structured"
+
+
+class CloudEventSettings(BaseSettings):
+    default_source: Optional[constr(min_length=1)]
+    default_response_mode: ContentMode = ContentMode.binary
+    allow_non_cloudevent_models: bool = Field(
+        default=True,
+        description="When allowed, will not fail on non-CloudEvent objects",
+    )
