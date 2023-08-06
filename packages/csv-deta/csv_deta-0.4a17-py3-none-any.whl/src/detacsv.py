942838325bb15161d361e9eaec1a04d6d478e012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:17 +0000

    Add package csv-deta/0.4a17/csv_deta-0.4a17-py3-none-any.whl

---
+++ packages/csv-deta/csv_deta-0.4a17-py3-none-any.whl/src/detacsv.py
@@ -0,0 +1,47 @@
+####
+##
+#
+#
+
+import os
+import sys
+import csv
+import deta
+import fire
+import src.cli as this
+
+
+class DetaCsv:
+    def __init__(self, version=None):
+        if version:
+            print(
+                os.path.basename(sys.argv[0]).rsplit('.', 1)[0],
+                'version',
+                this.__version__,
+            )
+            sys.exit()
+        pass
+
+    def upload(self):
+        pass
+
+
+def csv_to_deta(deta_id: str, path: str, db: str):
+    """
+    Reads in a CSV file and uploads its contents to a Deta Base
+    :param str deta_id: The ID of your target Deta Base instance
+    :param str path: The absolute path to the target CSV file
+    :param str db: The name of your Deta Base
+    :return: None
+    """
+    deta = deta.Deta(deta_id)
+    datafile = deta.Base(db)
+    print(f"Reading CSV from {path}")
+    with open(path) as csv_file:
+        count = 0
+        csv_reader = csv.DictReader(csv_file)
+        for csv_row in csv_reader:
+            datafile.insert(csv_row)
+            print(f"Uploaded {csv_row}")
+            count += 1
+        print(f"Succesfully uploaded {count} rows to {db} Deta Base")
