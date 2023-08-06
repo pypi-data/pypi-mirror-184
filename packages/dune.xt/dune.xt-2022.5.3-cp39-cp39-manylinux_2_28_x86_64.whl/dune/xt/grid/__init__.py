2681cda55c9ba97fb8deaf3163ca8e77104b27b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:42 +0000

    Add package dune.xt/2022.5.3/dune.xt-2022.5.3-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/dune.xt/dune.xt-2022.5.3-cp39-cp39-manylinux_2_28_x86_64.whl/dune/xt/grid/__init__.py
@@ -0,0 +1,80 @@
+# ~~~
+# This file is part of the dune-xt project:
+#   https://zivgitlab.uni-muenster.de/ag-ohlberger/dune-community/dune-xt
+# Copyright 2009-2021 dune-xt developers and contributors. All rights reserved.
+# License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+#      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+#          with "runtime exception" (http://www.dune-project.org/license.html)
+# Authors:
+#   Felix Schindler (2017, 2019 - 2021)
+#   René Fritze     (2018 - 2019)
+#   Tobias Leibner  (2019 - 2021)
+# ~~~
+import numpy as np
+from numbers import Number
+
+from dune.xt import guarded_import
+from dune.xt.common.config import config
+
+for mod_name in (
+        '_grid_boundaryinfo_alldirichlet',
+        '_grid_boundaryinfo_allneumann',
+        '_grid_boundaryinfo_allreflecting',
+        '_grid_boundaryinfo_interfaces',
+        '_grid_boundaryinfo_functionbased',
+        '_grid_boundaryinfo_normalbased',
+        '_grid_boundaryinfo_types',
+        '_grid_element',
+        '_grid_filters_base',
+        '_grid_filters_element',
+        '_grid_functors_boundary_detector',
+        # '_grid_functors_bounding_box',
+        '_grid_functors_interfaces',
+        # '_grid_functors_refinement',
+        '_grid_gridprovider_cube',
+        '_grid_gridprovider_gmsh',
+        '_grid_gridprovider_provider',
+        '_grid_dd_glued_gridprovider_cube',
+        '_grid_dd_glued_gridprovider_provider',
+        '_grid_intersection',
+        '_grid_traits',
+        '_grid_walker',
+):
+    guarded_import(globals(), 'dune.xt.grid', mod_name)
+
+
+def Dim(d):
+    assert isinstance(d, Number)
+    if f'Dimension{d}' not in globals():
+        raise RuntimeError(f'Dimension {d} not available, extend <python/dune/xt/grid/traits.cc>!')
+    return globals()[f'Dimension{d}']()
+
+
+def visualize_grid(grid):
+    if grid.dimension == 1:
+        from matplotlib import pyplot as plt
+
+        centers = np.array(grid.centers(1), copy=False)[:, 0]
+
+        points = np.zeros(2 * grid.size(0))
+        points[1::2] = centers[1:]
+        points[2::2] = centers[1:(len(centers) - 1)]
+
+        indices = np.zeros(2 * grid.size(0))
+        indices[::2] = np.arange(grid.size(0))
+        indices[1::2] = np.arange(grid.size(0))
+
+        plt.figure()
+        plt.title(f'grid with {grid.size(0)} elements')
+        plt.plot(points, indices)
+
+        return plt.gca()
+
+    elif config.HAVE_K3D:
+        from tempfile import NamedTemporaryFile
+        from dune.xt.common.vtk.plot import plot
+
+        tmpfile = NamedTemporaryFile(mode='wb', delete=False, suffix='.vtu').name
+        grid.visualize(tmpfile[:-4])
+        return plot(tmpfile,
+                    color_attribute_name='Element index')  # see visualize in python/dune/xt/grid/gridprovider.hh
---
+++ packages/dune.xt/dune.xt-2022.5.3-cp39-cp39-manylinux_2_28_x86_64.whl/scripts/generate_compare_functions.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+#
+# ~~~
+# This file is part of the dune-xt project:
+#   https://zivgitlab.uni-muenster.de/ag-ohlberger/dune-community/dune-xt
+# Copyright 2009-2021 dune-xt developers and contributors. All rights reserved.
+# License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+#      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+#          with "runtime exception" (http://www.dune-project.org/license.html)
+# Authors:
+#   Felix Schindler (2017)
+#   René Fritze     (2016, 2018 - 2019)
+#   Tobias Leibner  (2020)
+# ~~~
+
+import os
+from string import Template
+
+common_tpl = """
+template <Style style,
+            class FirstType,
+            class SecondType,
+            class ToleranceType = typename MT<FirstType>::Eps>
+  typename std::enable_if<internal::cmp_type_check<FirstType, SecondType, ToleranceType>::value, bool>::type ${id}(
+      const FirstType& first,
+      const SecondType& second,
+      const typename Dune::XT::Common::FloatCmp::MT<FirstType>::Eps& rtol =
+          Dune::XT::Common::FloatCmp::DefaultEpsilon<ToleranceType, style>::value(),
+      const typename Dune::XT::Common::FloatCmp::MT<FirstType>::Eps& atol =
+          Dune::XT::Common::FloatCmp::DefaultEpsilon<ToleranceType, style>::value())
+  {
+    return internal::Call<FirstType, SecondType, typename Dune::FloatCmp::EpsilonType<ToleranceType>::Type, style>::
+        ${id}(first, second, rtol, atol);
+  }
+
+  template <class FirstType, class SecondType, class ToleranceType = typename MT<FirstType>::Eps>
+  typename std::enable_if<internal::cmp_type_check<FirstType, SecondType, ToleranceType>::value, bool>::type ${id}(
+      const FirstType& first,
+      const SecondType& second,
+      const typename Dune::XT::Common::FloatCmp::MT<FirstType>::Eps& rtol =
+          Dune::XT::Common::FloatCmp::DefaultEpsilon<ToleranceType>::value(),
+      const typename Dune::XT::Common::FloatCmp::MT<FirstType>::Eps& atol =
+          Dune::XT::Common::FloatCmp::DefaultEpsilon<ToleranceType>::value())
+  {
+    return ${id}<Style::defaultStyle>(first, second, rtol, atol);
+  }
+"""
+
+test_tpl = """
+  template <Dune::XT::Common::FloatCmp::Style style,
+            class FirstType,
+            class SecondType,
+            class ToleranceType = typename Dune::XT::Common::FloatCmp::MT<FirstType>::Eps>
+  typename std::
+      enable_if<Dune::XT::Common::FloatCmp::internal::cmp_type_check<FirstType, SecondType, ToleranceType>::value,
+                void>::type
+          DXTC_EXPECT_FLOAT_${ID}(const FirstType& expected,
+                                 const SecondType& actual,
+                                 const typename Dune::XT::Common::FloatCmp::MT<FirstType>::Eps& rtol =
+                                     Dune::XT::Common::FloatCmp::DefaultEpsilon<ToleranceType, style>::value(),
+                                 const typename Dune::XT::Common::FloatCmp::MT<FirstType>::Eps& atol =
+                                     Dune::XT::Common::FloatCmp::DefaultEpsilon<ToleranceType, style>::value())
+  {
+    const auto ${id} = Dune::XT::Common::FloatCmp::internal::
+        Call<FirstType, SecondType, typename Dune::XT::Common::FloatCmp::MT<FirstType>::Eps, style>::${id};
+    EXPECT_PRED4(${id}, expected, actual, rtol, atol);
+  }
+
+  template <class FirstType, class SecondType, class ToleranceType = typename Dune::XT::Common::FloatCmp::MT<FirstType>::Eps>
+  typename std::enable_if<Dune::XT::Common::FloatCmp::internal::cmp_type_check<FirstType, SecondType, ToleranceType>::
+                              value,
+                          void>::type
+      DXTC_EXPECT_FLOAT_${ID}(
+          const FirstType& first,
+          const SecondType& second,
+          const typename Dune::XT::Common::FloatCmp::MT<FirstType>::Eps& rtol =
+              Dune::XT::Common::FloatCmp::DefaultEpsilon<ToleranceType,
+                                                         Dune::XT::Common::FloatCmp::Style::defaultStyle>::value(),
+          const typename Dune::XT::Common::FloatCmp::MT<FirstType>::Eps& atol =
+              Dune::XT::Common::FloatCmp::DefaultEpsilon<ToleranceType,
+                                                         Dune::XT::Common::FloatCmp::Style::defaultStyle>::value())
+  {
+    DXTC_EXPECT_FLOAT_${ID}<Dune::XT::Common::FloatCmp::Style::defaultStyle>(first, second, rtol, atol);
+  }
+"""  # noqa: E501
+
+bindir = os.path.dirname(os.path.abspath(__file__))
+fn_test = os.path.join(bindir, "..", "dune", "xt", "common", "test", "float_cmp_generated.hxx")
+fn_common = os.path.join(bindir, "..", "dune", "xt", "common", "float_cmp_generated.hxx")
+cmps = ["eq", "ne", "gt", "lt", "ge", "le"]
+with open(fn_test, "w") as test_header, open(fn_common, "w") as common_header:
+    for name in cmps:
+        common_header.write(Template(common_tpl).substitute(id=name))
+        test_header.write(Template(test_tpl).substitute(id=name, ID=name.upper()))
