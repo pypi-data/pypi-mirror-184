c84453ab576bb5f7d07bee662ce312abd9cf275c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:47 +0000

    Add package minio-handler/0.0.3/minio_handler-0.0.3.tar.gz

---
+++ packages/minio-handler/minio_handler-0.0.3.tar.gz/minio_handler-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='minio_handler',
+    version='0.0.3',
+    description='an abstraction on top of the minio package to make basic functions easier',
+    url='https://github.com/ReinierNel/opstools/tree/main/libraries/python3/minio_handler',
+    author='Reinier Nel',
+    author_email='hi@reinier.co.za',
+    license='MIT License',
+    packages=['minio_handler'],
+    install_requires=['minio>=7.1.12'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
