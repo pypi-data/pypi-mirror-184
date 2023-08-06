c808d6e54b136067628eb87db4e4411a214bdfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:13 +0000

    Add package pygame-chart/1.0.0/pygame_chart-1.0.0.tar.gz

---
+++ packages/pygame-chart/pygame_chart-1.0.0.tar.gz/pygame_chart-1.0.0/src/pygame_chart/settings.py
@@ -0,0 +1,40 @@
+
+BG_COLOR = (255,255,255)
+FONT_SIZE = 10
+TITLE_FONT_SIZE = 14
+TEXT_COLOR = (0,0,0)
+PADDING = 5
+MAX_TICK = 10
+GRID_COLOR = (224,224,224)
+CHART_MARGIN = 10
+MIN_LEGEND_LINE_HEIGHT = 10
+LEGEND_ITEM_WIDTH = 10
+
+
+COLORS = [
+    (0.0, 0.0, 255.0), #blue
+    (255.0, 165.0, 0.0), #orange
+    (0.0, 128.0, 0.0), #green
+    (255.0, 0.0, 0.0), # red
+    (128.0, 0.0, 128.0), #purple
+    (165.0, 42.0, 42.0), #brown
+    (255.0, 192.0, 203.0), #pink
+    (128.0, 128.0, 128.0), #gray
+    (128.0, 128.0, 0.0), #olive
+    (0.0, 255.0, 255.0), #cyan
+]
+
+
+
+# COLORS = [
+# # 'tomato',palegreen,'gold','mediumaquamarine','teal','royalblue','darkviolet','pink'
+# [255, 99, 71],
+# [152, 251, 152],
+# [255, 215, 0],
+# [102, 205, 170],
+# [0, 128, 128],
+# [65, 105, 225],
+# [148, 0, 211],
+# [255, 192, 203]
+#  ]
+
