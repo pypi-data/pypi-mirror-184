e4c442da9224a83a65d6dd07d707b5101de6ff9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:49 +0000

    Add package MIG-Profiler/0.0.2/MIG_Profiler-0.0.2-py3-none-any.whl

---
+++ packages/MIG-Profiler/MIG_Profiler-0.0.2-py3-none-any.whl/mig_perf/profiler/utils/model_hub.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Author: Li Yuanming
+Email: yli056@e.ntu.edu.sg
+Date: 7/8/2020
+Obtain the pre-trained PyTorch model object.
+"""
+
+import torch
+import torch.hub
+
+# Supported model architecture
+_MODEL_CV_REPOSITORY = ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']
+_MODEL_NLP_REPOSITORY = [
+    'distilbert-base-cased',
+    'bert-base-cased',
+    'bert-large-cased',
+    'xlnet-base-cased',
+    'roberta-base'
+]
+
+
+def load_pytorch_model(model_name: str, task: str = 'model', **kwargs):
+    """
+    Load model by model name (and task name).
+    Supported model names:
+    CV:
+        -   'resnet18'
+        -   'resnet34'
+        -   'resnet50'
+        -   'resnet101'
+        -   'resnet152'
+    NLP:
+        -   'distilbert-base-cased'
+        -   'bert-base-cased'
+        -   'bert-large-cased'
+        -   'xlnet-base-cased'
+        -   'roberta-base'
+    Currently supported task (for NLP only):
+    -   'model'
+    -   'modelForSequenceClassification'
+    Args:
+        model_name (str): Model name.
+        task (str): Model task.
+    """
+
+    if model_name in _MODEL_CV_REPOSITORY:
+        import torchvision
+        return getattr(torchvision.models, model_name)(**kwargs)
+    elif model_name in _MODEL_NLP_REPOSITORY:
+        from transformers import AutoModelForSequenceClassification, AutoConfig
+
+        transformer_config = AutoConfig.from_pretrained(model_name)
+        if kwargs.get('num_labels'):
+            transformer_config.num_labels = kwargs['num_labels']
+        transformer_config.problem_type = task
+        return AutoModelForSequenceClassification.from_config(transformer_config)
+    else:
+        raise ValueError(f'model name={model_name} not supported.')
