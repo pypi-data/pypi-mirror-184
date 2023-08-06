04eeaf3eb5697931381e20e9324687bce29fc58c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:50:19 +0000

    Add package pykeops/2.1.1/pykeops-2.1.1.tar.gz

---
+++ packages/pykeops/pykeops-2.1.1.tar.gz/pykeops-2.1.1/pykeops/numpy/__init__.py
@@ -0,0 +1,28 @@
+##########################################################
+# Import pyKeOps routines
+
+
+from .generic.generic_red import Genred
+from .operations import KernelSolve
+from .generic.generic_ops import (
+    generic_sum,
+    generic_logsumexp,
+    generic_argmin,
+    generic_argkmin,
+)
+from .lazytensor.LazyTensor import LazyTensor, ComplexLazyTensor, Vi, Vj, Pm
+
+__all__ = sorted(
+    [
+        "Genred",
+        "generic_sum",
+        "generic_logsumexp",
+        "generic_argmin",
+        "generic_argkmin",
+        "KernelSolve",
+        "LazyTensor",
+        "Vi",
+        "Vj",
+        "Pm",
+    ]
+)
