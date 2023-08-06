819c03a6ab1b93b6a48d94acb7b8de28d7cbb406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:50:50 +0000

    Add package netbox-disk/0.0.7.6.5.23/netbox_disk-0.0.7.6.5.23.tar.gz

---
+++ packages/netbox-disk/netbox_disk-0.0.7.6.5.23.tar.gz/netbox_disk-0.0.7.6.5.23/netbox_disk/models.py
@@ -0,0 +1,65 @@
+from django.db import models, transaction
+from django.urls import reverse
+
+from netbox.models import NetBoxModel
+from netbox.search import SearchIndex, register_search
+
+
+class Disk(NetBoxModel):
+    vg_name = models.CharField(
+        unique=False,
+        max_length=255,
+    )
+    lv_name = models.CharField(
+        unique=False,
+        max_length=255,
+    )
+    size = models.PositiveIntegerField(
+        verbose_name="Size (GB)",
+        null=True,
+        blank=True,
+    )
+    path = models.CharField(
+        unique=False,
+        max_length=255,
+    )
+    cluster = models.ForeignKey(
+        to='virtualization.Cluster',
+        on_delete=models.PROTECT,
+        related_name='disks',
+        blank=True,
+        null=True
+    )
+    virtual_machine = models.ForeignKey(
+        to='virtualization.VirtualMachine',
+        on_delete=models.CASCADE,
+        related_name='disks',
+        null=True,
+        blank=True
+    )
+    description = models.CharField(
+        max_length=200,
+        blank=True,
+    )
+
+    clone_fields = ["vg_name", "lv_name", "size", "path", "virtual_machine", "description"]
+
+    def get_absolute_url(self):
+        return reverse("plugins:netbox_disk:disk", kwargs={"pk": self.id})
+
+    def __str__(self):
+        return f"VM: test-vm vg_{self.vg_name}-lv_{self.lv_name}"
+
+    class Meta:
+        ordering = ("vg_name", "lv_name", "size", "path")
+
+
+@register_search
+class DiskIndex(SearchIndex):
+    model = Disk
+    fields = (
+        ("vg_name", 100),
+        ("lv_name", 150),
+        ("size", 200),
+        ("path", 200),
+    )
