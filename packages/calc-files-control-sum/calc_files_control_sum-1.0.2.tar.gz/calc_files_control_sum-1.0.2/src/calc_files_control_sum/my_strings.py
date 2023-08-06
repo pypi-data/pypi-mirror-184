0d4378fc8741a7f1b21fae880c64156f2ef2f522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:23 +0000

    Add package calc-files-control-sum/1.0.2/calc_files_control_sum-1.0.2.tar.gz

---
+++ packages/calc-files-control-sum/calc_files_control_sum-1.0.2.tar.gz/calc_files_control_sum-1.0.2/src/calc_files_control_sum/my_strings.py
@@ -0,0 +1,27 @@
+"""any strings for util"""
+
+strFolderHandling = "Folder handling"
+strFEF = "File extension filter"
+strCCA = "Checksum calculation algorithm"
+
+head_header = "#"
+str_start_files_header = f"FILES_AND_CONTROL_SUM"
+str_settings_header = "SETTINGS"
+str_info_section = "INFO AND STATISTICS"
+strCS_filename_splitter = "\t"
+strKeyValueSeparator = ":"
+
+strInvalidSrcFld = "Invalid source folder!"
+strInvalidCheckFn = "Invalid check file name!"
+strOsError = "Operational System Error!"
+
+strFileNotModified = "File is not modified."
+strFileModified = "Attention! The file has been modified"
+strFileNotFound = "File not found!"
+
+strCheckingStarted = "Checking started!"
+strCheckingEnded = "Checking ended!"
+
+strParseFileError = "Error parsing file!"
+strCRClabel = "hash_val"
+default_cfg_crc_alg = "md5"
