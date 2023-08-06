134c1506e8872961d00cf42f267d5f2b55085ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:28:31 +0000

    Add package nessai/0.7.1/nessai-0.7.1-py3-none-any.whl

---
+++ packages/nessai/nessai-0.7.1-py3-none-any.whl/nessai/config.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Global configuration for nessai.
+"""
+import numpy as np
+
+# Settings for live points
+LOGL_DTYPE = "f8"
+IT_DTYPE = "i4"
+DEFAULT_FLOAT_DTYPE = "f8"
+DEFAULT_FLOAT_VALUE = np.nan
+CORE_PARAMETERS = ["logP", "logL", "it"]
+CORE_PARAMETERS_DEFAULTS = [np.nan, np.nan, 0]
+CORE_PARAMETERS_DTYPE = [DEFAULT_FLOAT_DTYPE, LOGL_DTYPE, IT_DTYPE]
+EXTRA_PARAMETERS = []
+EXTRA_PARAMETERS_DEFAULTS = []
+EXTRA_PARAMETERS_DTYPE = []
+NON_SAMPLING_PARAMETERS = CORE_PARAMETERS + EXTRA_PARAMETERS
+NON_SAMPLING_DEFAULTS = CORE_PARAMETERS_DEFAULTS + EXTRA_PARAMETERS_DEFAULTS
+NON_SAMPLING_DEFAULT_DTYPE = CORE_PARAMETERS_DTYPE + EXTRA_PARAMETERS_DTYPE
+# Plotting config
+DISABLE_STYLE = False
+"""Disable nessai's custom plotting style globally.
+
+Useful since all plotting functions use the
+:py:func:`~nessai.plot.nessai_style` decorator by default.
+"""
+SNS_STYLE = "ticks"
+"""Default seaborn style."""
+BASE_COLOUR = "#02979d"
+"""Base colour for plots."""
+HIGHLIGHT_COLOUR = "#f5b754"
+"""Highlight colour for plots."""
+LINE_COLOURS = ["#4575b4", "#d73027", "#fad117", "#ff8c00"]
+"""Default line colours."""
+LINE_STYLES = ["-", "--", ":", "-."]
+"""Default line styles."""
---
+++ packages/nessai/nessai-0.7.1-py3-none-any.whl/nessai/posterior.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+Functions related to computing the posterior samples.
+"""
+import logging
+
+import numpy as np
+from scipy.special import logsumexp
+
+from .evidence import logsubexp, log_integrate_log_trap
+from .utils.stats import effective_sample_size
+
+logger = logging.getLogger(__name__)
+
+
+def compute_weights(samples, nlive):
+    """
+    Returns the log-evidence and log-weights for the log-likelihood samples
+    assumed to the result of nested sampling with nlive live points
+
+    Parameters
+    ----------
+    samples : array_like
+        Log-likelihood samples.
+    nlive : int
+        Number of live points used in nested sampling.
+
+    Returns
+    -------
+    float
+        The computed log-evidence.
+    array_like
+        Array of computed weights (already normalised by the log-evidence).
+    """
+    samples = np.asarray(samples)
+    start_data = np.concatenate(([float("-inf")], samples[:-nlive]))
+    end_data = samples[-nlive:]
+
+    log_wts = np.zeros(samples.shape[0])
+
+    log_vols_start = np.cumsum(
+        np.ones(len(start_data) + 1) * np.log1p(-1.0 / nlive)
+    ) - np.log1p(-1 / nlive)
+    log_vols_end = np.zeros(len(end_data))
+    log_vols_end[-1] = np.NINF
+    log_vols_end[0] = log_vols_start[-1] + np.log1p(-1.0 / nlive)
+    for i in range(len(end_data) - 1):
+        log_vols_end[i + 1] = log_vols_end[i] + np.log1p(-1.0 / (nlive - i))
+
+    log_likes = np.concatenate((start_data, end_data, [end_data[-1]]))
+
+    log_vols = np.concatenate((log_vols_start, log_vols_end))
+    log_ev = log_integrate_log_trap(log_likes, log_vols)
+
+    log_dXs = logsubexp(log_vols[:-1], log_vols[1:])
+    log_wts = log_likes[1:-1] + log_dXs[:-1]
+
+    log_wts -= log_ev
+
+    return log_ev, log_wts
+
+
+def draw_posterior_samples(
+    nested_samples,
+    nlive=None,
+    n=None,
+    log_w=None,
+    method="rejection_sampling",
+    return_indices=False,
+):
+    """Draw posterior samples given the nested samples.
+
+    Requires either the posterior weights or the number of live points.
+
+    Parameters
+    ----------
+    nested_samples : structured array
+        Array of nested samples.
+    nlive : int, optional
+        Number of live points used during nested sampling. Either this
+        arguments or log_w must be specified.
+    n : int, optional
+        Number of samples to draw. Only used for importance sampling. If not
+        specified, the effective sample size is used instead.
+    log_w : array_like, optional
+        Array of posterior weights. If specified the weights are not computed
+        and these weights are used instead.
+    method : str
+        Method for drawing the posterior samples. Choose from
+
+            - :code:`'rejection_sampling'`
+            - :code:`'multinomial_resampling'`
+            - :code:`'importance_sampling'` (same as multinomial)
+    return_indices : bool
+        If true return the indices of the accepted samples.
+
+    Returns
+    -------
+    posterior_samples : numpy.ndarray
+        Samples from the posterior distribution.
+    indices : numpy.ndarray
+        Indices of the accepted samples in the original nested samples.
+        Only returned if :code:`return_indices` is True.
+
+    Raises
+    ------
+    ValueError
+        If the chosen method is not a valid method.
+    """
+    nested_samples = np.asarray(nested_samples)
+    if log_w is None:
+        _, log_w = compute_weights(nested_samples["logL"], nlive)
+    else:
+        log_w = np.asarray(log_w)
+    ess = effective_sample_size(log_w)
+    logger.info(f"Effective sample size: {ess:.1f}")
+    if method == "rejection_sampling":
+        logger.info("Producing posterior samples using rejection sampling")
+        if n is not None:
+            logger.warning(
+                "Number of samples cannot be specified for rejection sampling"
+            )
+        log_w = log_w - np.max(log_w)
+        log_u = np.log(np.random.rand(nested_samples.size))
+        indices = np.where(log_w > log_u)[0]
+        samples = nested_samples[indices]
+    elif method in ["importance_sampling", "multinomial_resampling"]:
+        logger.info("Producing posterior samples using multinomial resampling")
+        if n is None:
+            n = int(ess)
+        log_w = log_w - logsumexp(log_w)
+        indices = np.random.choice(
+            nested_samples.size, size=n, p=np.exp(log_w), replace=True
+        )
+        samples = nested_samples[indices]
+    else:
+        raise ValueError(
+            f"Unknown method of drawing posterior samples: {method}"
+        )
+
+    if return_indices:
+        return samples, indices
+    else:
+        return samples
