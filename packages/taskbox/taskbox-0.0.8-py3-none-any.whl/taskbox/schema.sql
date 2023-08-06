24bda92d7dabcec2d00fd575b6608c925d1058ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:50:22 +0000

    Add package taskbox/0.0.8/taskbox-0.0.8-py3-none-any.whl

---
+++ packages/taskbox/taskbox-0.0.8-py3-none-any.whl/taskbox/schema.sql
@@ -0,0 +1,36 @@
+-- Initialize the database.
+-- Drop any existing data and create empty tables.
+
+DROP VIEW IF EXISTS tasks_v;
+DROP TABLE IF EXISTS devices;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE devices (
+	id INTEGER PRIMARY KEY,
+	name TEXT NOT NULL UNIQUE,
+	description TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE users (
+	id INTEGER PRIMARY KEY,
+	role TEXT,
+	username TEXT NOT NULL UNIQUE,
+	password TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE tasks (
+	id INTEGER PRIMARY KEY,
+	name TEXT NOT NULL,
+	command TEXT NOT NULL,
+	device_id INTEGER NOT NULL,
+	FOREIGN KEY(device_id) REFERENCES devices(id)
+);
+
+CREATE VIEW tasks_v AS SELECT
+	devices.name AS device_name,
+	devices.description AS device_description,
+	tasks.id as task_id,
+	tasks.name AS task_name
+FROM devices
+INNER JOIN tasks ON tasks.device_id = devices.id;
