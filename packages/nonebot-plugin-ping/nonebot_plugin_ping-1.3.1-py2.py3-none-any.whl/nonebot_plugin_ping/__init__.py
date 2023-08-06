9e476a57e4072bfdda564568fcd73d669dc6efc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:03 +0000

    Add package nonebot-plugin-ping/1.3.1/nonebot_plugin_ping-1.3.1-py2.py3-none-any.whl

---
+++ packages/nonebot-plugin-ping/nonebot_plugin_ping-1.3.1-py2.py3-none-any.whl/nonebot_plugin_ping/__init__.py
@@ -0,0 +1,46 @@
+from nonebot.plugin.on import on_command
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot.params import CommandArg
+from httpx import AsyncClient
+
+
+
+# PING网址
+ping = on_command('ping', aliases={'Ping'}, priority=60, block=True)
+@ping.handle()
+async def _(msg: Message = CommandArg()):
+    url = msg.extract_plain_text().strip()
+    api = f'https://api.gmit.vip/Api/Ping?format=json&ip={url}'
+    message = await api_ping(api)
+    await ping.finish(message)
+
+async def api_ping(api):
+    async with AsyncClient() as client:
+            res = (await client.get(api)).json()
+            if res["code"] == 200:
+                url = (res["data"]["host"])
+                ip = (res["data"]["ip"])
+                max = (res["data"]["ping_max"])
+                min = (res["data"]["ping_min"])
+                avg = (res["data"]["ping_avg"])
+                place = (res["data"]["location"])
+                res = f"域名: {url}\nIP: {ip}\n最大延迟: {max}\n最小延迟: {min}\n平均延迟: {avg}\n服务器归属地: {place}"
+                return res
+            elif res["code"] == 201:
+                res = (res["data"])
+                return res
+            else:
+                return "寄"
+
+
+
+# 二维码生成
+qrcode = on_command('qrcode', aliases={'二维码', '二维码生成'}, priority=60, block=True)
+@qrcode.handle()
+async def _(msg: Message = CommandArg()):
+    url = msg.extract_plain_text().strip()
+    api = f'https://api.gmit.vip/Api/QrCode?text={url}'
+    await qrcode.finish(MessageSegment.image(file=api))
+
+
+
