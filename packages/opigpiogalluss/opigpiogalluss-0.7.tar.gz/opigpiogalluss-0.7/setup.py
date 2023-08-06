246a2277b35f995af51a6d7ac2412c18947439bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:17 +0000

    Add package opigpiogalluss/0.7/opigpiogalluss-0.7.tar.gz

---
+++ packages/opigpiogalluss/opigpiogalluss-0.7.tar.gz/opigpiogalluss-0.7/setup.py
@@ -0,0 +1,10 @@
+from io import open
+from setuptools import setup, find_packages
+
+setup(name='opigpiogalluss',
+      version='0.7',
+      author='Gallus',
+      author_email='bkmz_repvby@mail.ru',
+      description='opigpiogalluss',
+      packages=['opigpiogalluss']
+      )
