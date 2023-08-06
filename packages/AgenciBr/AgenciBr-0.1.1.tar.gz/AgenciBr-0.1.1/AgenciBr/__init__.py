bec744d44bb284e2f9839a6a682932432e4e7c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:36 +0000

    Add package AgenciBr/0.1.1/AgenciBr-0.1.1.tar.gz

---
+++ packages/AgenciBr/AgenciBr-0.1.1.tar.gz/AgenciBr-0.1.1/AgenciBr/__init__.py
@@ -0,0 +1,12 @@
+'''
+To understand the index:
+https://csis.myclimateservice.eu/node/3527
+https://www.climdex.org/learn/indices/#index-TX10p
+http://etccdi.pacificclimate.org/list_27_indices.shtml
+
+'''
+
+from AgenciBr.Ana import Ana as Ana
+from AgenciBr.Inemet import Inemet as Inemet
+from AgenciBr.Ideam import Ideam as Ideam
+
