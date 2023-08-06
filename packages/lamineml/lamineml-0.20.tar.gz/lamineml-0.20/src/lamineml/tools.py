0d7698487f875323591260a70195f5406fdcdba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:52 +0000

    Add package lamineml/0.20/lamineml-0.20.tar.gz

---
+++ packages/lamineml/lamineml-0.20.tar.gz/lamineml-0.20/src/lamineml/tools.py
@@ -0,0 +1,23 @@
+from jnius import autoclass
+from plyer.platforms.android import activity
+from kivy.app import App
+class main:
+ def open_browser(self,url,*args):
+    Intent = autoclass('android.content.Intent')
+    Uri = autoclass('android.net.Uri')
+    intent = Intent()
+    intent.setAction(Intent.ACTION_VIEW)
+    intent.setData(Uri.parse(url))
+    activity.startActivity(intent)
+ def open_app(self,p,*args):
+
+    s=activity.getPackageManager().getLaunchIntentForPackage(p)
+    activity.startActivity(s)
+ def open_setting(self,*args):
+     intent=autoclass("android.content.Intent")
+     settings = autoclass("android.provider.Settings")
+     activity.startActivity(intent(settings.ACTION_LOCATION_SOURCE_SETTINGS))
+
+
+
+
