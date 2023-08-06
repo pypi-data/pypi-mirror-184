dbc2cae5393e69aff4875c32938de1c49b0bd849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:19 +0000

    Add package NanSwap/0.3/NanSwap-0.3.tar.gz

---
+++ packages/NanSwap/NanSwap-0.3.tar.gz/NanSwap-0.3/NanSwap/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='NanSwap',
+      version='0.1',
+      description='The NanSwap Python API',
+      url='https://github.com/MyEcoria/NanSwap_module',
+      author='MyEcoria',
+      author_email='contact@myecoria.com',
+      license='MIT',
+      packages=['nanswap.py'])
---
+++ packages/NanSwap/NanSwap-0.3.tar.gz/NanSwap-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='NanSwap',
+      version='0.3',
+      description='The NanSwap Python API',
+      url='https://github.com/MyEcoria/NanSwap_module',
+      author='MyEcoria',
+      author_email='contact@myecoria.com',
+      license='MIT')
