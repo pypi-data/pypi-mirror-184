6986ef1b144c7de4629452ddaabcb7a37278d0b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:33 +0000

    Add package text2numb/1.1.2/text2numb-1.1.2.tar.gz

---
+++ packages/text2numb/text2numb-1.1.2.tar.gz/text2numb-1.1.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name = 'text2numb',
+	version = '1.1.2',
+	author = 'Pigeon Nation',
+	description = 'Converts text into a numbers and back again. ',
+	long_description = long_description,
+	long_description_content_type="text/markdown",
+	packages = setuptools.find_packages(),
+	license = 'MIT',
+	classifiers=[
+			"Programming Language :: Python :: 3",
+			"License :: OSI Approved :: MIT License",
+			"Operating System :: OS Independent",
+			"Topic :: Other/Nonlisted Topic"
+	]
+)
