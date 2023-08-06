173601a8ab60b1156c6dcde36bf267f7e592c22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:12 +0000

    Add package invenio-app-rdm/11.0.0.dev13/invenio_app_rdm-11.0.0.dev13-py2.py3-none-any.whl

---
+++ packages/invenio-app-rdm/invenio_app_rdm-11.0.0.dev13-py2.py3-none-any.whl/invenio_app_rdm/fixtures/__init__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 CERN.
+#
+# Invenio App RDM is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Static pages."""
+
+from pathlib import Path
+
+from invenio_app_rdm.fixtures.oai_sets import OAICustomSets
+from invenio_app_rdm.fixtures.pages import StaticPages
+
+
+class Pages:
+    """Pages engine."""
+
+    def run(self, force=False):
+        """Run the pages loading."""
+        dir_ = Path(__file__).parent
+
+        StaticPages(
+            [Path("./app_data"), dir_ / "data"],
+            "pages.yaml",
+            [Path("./app_data/pages"), dir_ / "pages"],
+            force,
+        ).load()
+
+
+class FixturesEngine:
+    """Basic fixtures engine."""
+
+    def run(self):
+        """Run the fixtures loading."""
+        dir_ = Path(__file__).parent
+
+        OAICustomSets(
+            [Path("./app_data"), dir_ / "data"],
+            "oai_sets.yaml",
+        ).load()
+
+        StaticPages(
+            [Path("./app_data"), dir_ / "data"],
+            "pages.yaml",
+            [Path("./app_data/pages"), dir_ / "pages"],
+            force=True,
+        ).load()
