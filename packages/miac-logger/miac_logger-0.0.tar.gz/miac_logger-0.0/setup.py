267a8902024b11554b704405b31de8b2487e4056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:24 +0000

    Add package miac-logger/0.0/miac_logger-0.0.tar.gz

---
+++ packages/miac-logger/miac_logger-0.0.tar.gz/miac_logger-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='miac_logger',
+      version='0.0',
+      description='Simple logger',
+      packages=['miac_logger'],
+      author_email='arhdomain@gmail.com',
+      zip_safe=False)
