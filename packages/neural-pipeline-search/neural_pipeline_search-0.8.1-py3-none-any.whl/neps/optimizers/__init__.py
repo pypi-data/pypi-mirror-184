20293b1ba80bffe1dfd0ed7202a0ea4c0398814a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:16 +0000

    Add package neural-pipeline-search/0.8.1/neural_pipeline_search-0.8.1-py3-none-any.whl

---
+++ packages/neural-pipeline-search/neural_pipeline_search-0.8.1-py3-none-any.whl/neps/optimizers/__init__.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+from typing import Callable
+
+from .base_optimizer import BaseOptimizer
+from .bayesian_optimization.cost_cooling import CostCooling
+from .bayesian_optimization.mf_tpe import MultiFidelityPriorWeightedTreeParzenEstimator
+from .bayesian_optimization.optimizer import BayesianOptimization
+from .grid_search.optimizer import GridSearch
+from .multi_fidelity.hyperband import Hyperband, HyperbandCustomDefault
+from .multi_fidelity.successive_halving import (
+    AsynchronousSuccessiveHalving,
+    AsynchronousSuccessiveHalvingWithPriors,
+    SuccessiveHalving,
+    SuccessiveHalvingWithPriors,
+)
+from .multi_fidelity_prior.async_priorband import PriorBandAsha, PriorBandAshaHB
+from .multi_fidelity_prior.priorband import (
+    PriorBand,
+    PriorBandCustom,
+    PriorBandDyna,
+    PriorBandHypothesis,
+)
+from .multi_fidelity_prior.raceband import RaceBand
+
+## custom algorithms
+from .multi_fidelity_prior.v1 import OurOptimizerV1, OurOptimizerV1_2, OurOptimizerV1_3
+from .multi_fidelity_prior.v2 import OurOptimizerV2, OurOptimizerV2_2, OurOptimizerV2_3
+from .multi_fidelity_prior.v3 import OurOptimizerV3, OurOptimizerV3_2
+from .multi_fidelity_prior.v4 import (
+    OurOptimizerV4_ASHA,
+    OurOptimizerV4_ASHA_HB,
+    OurOptimizerV4_HB,
+    OurOptimizerV4_SH,
+    OurOptimizerV4_V3_2,
+)
+from .multi_fidelity_prior.v5 import (
+    OurOptimizerV5,
+    OurOptimizerV5_2,
+    OurOptimizerV5_2_V4,
+    OurOptimizerV5_3,
+    OurOptimizerV5_V4,
+)
+from .multi_fidelity_prior.v6 import (
+    OurOptimizerV6,
+    OurOptimizerV6_V5,
+    OurOptimizerV6_V5_2,
+    OurOptimizerV6_V5_3,
+)
+from .random_search.optimizer import RandomSearch
+from .regularized_evolution.optimizer import RegularizedEvolution
+
+SearcherMapping: dict[str, Callable] = {
+    "bayesian_optimization": BayesianOptimization,
+    # "mf_bayesian_optimization": BayesianOptimizationMultiFidelity,
+    "cost_cooling_bayesian_optimization": CostCooling,
+    "random_search": RandomSearch,
+    "cost_cooling": CostCooling,
+    "regularized_evolution": RegularizedEvolution,
+    "grid_search": GridSearch,
+    "successive_halving": SuccessiveHalving,
+    "successive_halving_prior": SuccessiveHalvingWithPriors,
+    "asha": AsynchronousSuccessiveHalving,
+    "hyperband": Hyperband,
+    "asha_prior": AsynchronousSuccessiveHalvingWithPriors,
+    "multifidelity_tpe": MultiFidelityPriorWeightedTreeParzenEstimator,
+    "raceband": RaceBand,
+    "hyperband_custom_default": HyperbandCustomDefault,
+    "priorband": PriorBand,
+}
---
+++ packages/neural-pipeline-search/neural_pipeline_search-0.8.1-py3-none-any.whl/neps/optimizers/bayesian_optimization/acquisition_functions/prior_weighted.py
@@ -0,0 +1,58 @@
+from typing import Iterable, Union
+
+import numpy as np
+import torch
+
+from .base_acquisition import BaseAcquisition
+
+
+class DecayingPriorWeightedAcquisition(BaseAcquisition):
+    def __init__(
+        self,
+        base_acquisition,
+        pibo_beta=10,
+        log: bool = False,
+    ):  # pylint: disable=super-init-not-called
+        self.pibo_beta = pibo_beta
+        self.base_acquisition = base_acquisition
+        self.log = log
+
+    def eval(
+        self,
+        x: Iterable,
+        **base_acquisition_kwargs,
+    ) -> Union[np.ndarray, torch.Tensor, float]:
+        super().__init__()
+        acquisition = self.base_acquisition(x, **base_acquisition_kwargs)
+
+        if self.base_acquisition.surrogate_model.x[0].has_fidelity:
+            decay_t = np.sum(
+                [
+                    _x.fidelity.value == _x.fidelity.upper
+                    for _x in self.base_acquisition.surrogate_model.x
+                ]
+            )
+        else:
+            decay_t = len(self.base_acquisition.surrogate_model.x)
+
+        if self.log:
+            min_acq_val = abs(min(acquisition)) if min(acquisition) < 0 else 0
+        for i, candidate in enumerate(x):
+            prior_weight = candidate.compute_prior(log=self.log)
+            if prior_weight != 1.0:
+                if self.log:
+                    # for log -> the smaller the prior_weight,
+                    # the more unlikely it is from the prior
+                    # also shift acquisition values to avoid negativ values
+                    acquisition[i] = (
+                        np.log(acquisition[i] + min_acq_val + 1e-12)
+                        + (self.pibo_beta / decay_t) * prior_weight
+                    )
+                else:
+                    acquisition[i] *= np.power(
+                        prior_weight + 1e-12, self.pibo_beta / decay_t
+                    )
+        return acquisition
+
+    def set_state(self, surrogate_model, **kwargs):
+        self.base_acquisition.set_state(surrogate_model, **kwargs)
