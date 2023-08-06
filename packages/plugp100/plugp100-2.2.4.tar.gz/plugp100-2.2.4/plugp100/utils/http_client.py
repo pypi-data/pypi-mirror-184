e772c34e1ab16878889ab211a47c544fa441d1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:45 +0000

    Add package plugp100/2.2.4/plugp100-2.2.4.tar.gz

---
+++ packages/plugp100/plugp100-2.2.4.tar.gz/plugp100-2.2.4/plugp100/utils/http_client.py
@@ -0,0 +1,33 @@
+from typing import Any
+
+import aiohttp
+
+
+class AsyncHttp:
+
+    def __init__(self, session: aiohttp.ClientSession):
+        self.session = session
+        self.session.connector._force_close = True
+
+    async def async_make_post(self, url, json: Any) -> aiohttp.ClientResponse:
+        async with self.session.post(url, json=json, headers={"Content-Type": "application/json"}) as response:
+            return await self._force_read_release(response)
+
+    async def async_make_post_cookie(self, url, json, cookie) -> aiohttp.ClientResponse:
+        self.session.cookie_jar.clear()
+        async with self.session.post(
+                url,
+                json=json,
+                cookies=cookie,
+                headers={
+                    "Content-Type": "application/json",
+                    "requestByApp": "true",
+                    "Accept": "application/json"
+                }
+        ) as response:
+            return await self._force_read_release(response)
+
+    async def _force_read_release(self, response):
+        await response.read()
+        await response.release()
+        return response
