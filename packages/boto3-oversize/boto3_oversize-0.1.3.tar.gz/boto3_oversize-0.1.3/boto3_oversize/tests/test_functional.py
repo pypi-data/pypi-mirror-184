909ee0dcc82451b040b83ebcc19f66609a4f3df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:19 +0000

    Add package boto3-oversize/0.1.3/boto3_oversize-0.1.3.tar.gz

---
+++ packages/boto3-oversize/boto3_oversize-0.1.3.tar.gz/boto3_oversize-0.1.3/boto3_oversize/tests/test_functional.py
@@ -0,0 +1,144 @@
+import json
+from typing import Generator
+from unittest.mock import MagicMock, patch
+
+import boto3
+import pytest
+from botocore.exceptions import ClientError
+from moto import mock_s3, mock_sns, mock_sqs  # type: ignore[import]
+from mypy_boto3_s3.service_resource import Bucket
+from mypy_boto3_sns.service_resource import Topic
+from mypy_boto3_sqs.service_resource import Queue
+
+import boto3_oversize
+
+
+@pytest.fixture
+def bucket() -> Generator[Bucket, None, None]:
+    mock_s3().start()
+    s3_client = boto3.resource('s3')
+    bucket: Bucket = s3_client.create_bucket(Bucket='oversize-payload-bucket')
+    yield bucket
+    mock_s3().stop()
+
+
+@pytest.fixture
+def topic(bucket: Bucket) -> Generator[Topic, None, None]:
+    mock_sns().start()
+    sns_client = boto3_oversize.resource('sns')
+    topic: Topic = sns_client.create_topic(Name='large-payload-test')
+    yield topic
+    mock_sns().stop()
+
+
+@pytest.fixture
+def queue(topic: Topic) -> Generator[Queue, None, None]:
+    mock_sqs().start()
+    sqs_client = boto3_oversize.resource('sqs')
+    queue: Queue = sqs_client.create_queue(QueueName='large-payload-test')
+    topic.subscribe(Protocol='sqs', Endpoint=queue.attributes['QueueArn'])
+    yield queue
+    mock_sqs().stop()
+
+
+@patch('boto3_oversize.oversize.getenv', return_value='oversize-payload-bucket')
+def test_resource_oversized_publish(getenv: MagicMock,
+                                    bucket: Bucket, topic: Topic, queue: Queue) -> None:
+    payload = '0' * boto3_oversize.oversize.MAX_PAYLOAD_SIZE
+    topic.publish(Message=payload)
+
+    messages = queue.receive_messages()
+    assert json.loads(messages[0].body).get('Message') == payload
+    assert len(list(bucket.objects.all())) == 1
+
+
+@patch('boto3_oversize.oversize.getenv', return_value='oversize-payload-bucket')
+def test_resource_oversized_publish_raw(getenv: MagicMock,
+                                        bucket: Bucket, topic: Topic, queue: Queue) -> None:
+    next(iter(topic.subscriptions.all())).set_attributes(AttributeName='RawMessageDelivery',
+                                                         AttributeValue='true')
+
+    payload = '0' * boto3_oversize.oversize.MAX_PAYLOAD_SIZE
+    topic.publish(Message=payload)
+
+    messages = queue.receive_messages()
+    assert messages[0].body == payload
+    assert len(list(bucket.objects.all())) == 1
+
+
+@patch('boto3_oversize.oversize.getenv', return_value='oversize-payload-bucket')
+def test_resource_oversized_publish_multi(getenv: MagicMock,
+                                          bucket: Bucket, topic: Topic, queue: Queue) -> None:
+    payloads = [
+        '0' * boto3_oversize.oversize.MAX_PAYLOAD_SIZE,
+        '1' * boto3_oversize.oversize.MAX_PAYLOAD_SIZE,
+        '2' * boto3_oversize.oversize.MAX_PAYLOAD_SIZE,
+    ]
+    for payload in payloads:
+        topic.publish(Message=payload)
+
+    messages = queue.receive_messages(MaxNumberOfMessages=10)
+    assert json.loads(messages[0].body).get('Message') == payloads[0]
+    assert json.loads(messages[1].body).get('Message') == payloads[1]
+    assert json.loads(messages[2].body).get('Message') == payloads[2]
+    assert len(list(bucket.objects.all())) == 3
+
+
+@patch('boto3_oversize.oversize.getenv', return_value=None)
+def test_resource_oversized_publish_notset(getenv: MagicMock,
+                                           bucket: Bucket, topic: Topic, queue: Queue) -> None:
+    with pytest.raises(ClientError):
+        payload = '0' * boto3_oversize.oversize.MAX_PAYLOAD_SIZE
+        topic.publish(Message=payload)
+
+
+@patch('boto3_oversize.oversize.getenv', return_value='oversize-payload-bucket')
+def test_resource_undersized_publish(getenv: MagicMock,
+                                     bucket: Bucket, topic: Topic, queue: Queue) -> None:
+    payload = '0' * int(boto3_oversize.oversize.MAX_PAYLOAD_SIZE / 2)
+    topic.publish(Message=payload)
+
+    messages = queue.receive_messages()
+    assert json.loads(messages[0].body).get('Message') == payload
+    assert len(list(bucket.objects.all())) == 0
+
+
+@patch('boto3_oversize.oversize.getenv', return_value='oversize-payload-bucket')
+def test_resource_undersized_publish_raw(getenv: MagicMock,
+                                         bucket: Bucket, topic: Topic, queue: Queue) -> None:
+    next(iter(topic.subscriptions.all())).set_attributes(AttributeName='RawMessageDelivery',
+                                                         AttributeValue='true')
+
+    payload = '0' * int(boto3_oversize.oversize.MAX_PAYLOAD_SIZE / 2)
+    topic.publish(Message=payload)
+
+    messages = queue.receive_messages()
+    assert messages[0].body == payload
+    assert len(list(bucket.objects.all())) == 0
+
+
+@patch('boto3_oversize.oversize.getenv', return_value='oversize-payload-bucket')
+def test_resource_undersized_publish_raw_json(getenv: MagicMock,
+                                              bucket: Bucket, topic: Topic, queue: Queue) -> None:
+    next(iter(topic.subscriptions.all())).set_attributes(AttributeName='RawMessageDelivery',
+                                                         AttributeValue='true')
+
+    payload = '{"foo": "bar"}'
+    topic.publish(Message=payload)
+
+    messages = queue.receive_messages()
+    assert messages[0].body == payload
+    assert len(list(bucket.objects.all())) == 0
+
+
+@patch('boto3_oversize.oversize.getenv', return_value='oversize-payload-bucket')
+def test_client_oversized_publish(getenv: MagicMock,
+                                  bucket: Bucket, topic: Topic, queue: Queue) -> None:
+    payload = '0' * boto3_oversize.oversize.MAX_PAYLOAD_SIZE
+    sns_client = boto3_oversize.client('sns')
+    sns_client.publish(TopicArn=topic.arn, Message=payload)
+
+    sqs_client = boto3_oversize.client('sqs')
+    response = sqs_client.receive_message(QueueUrl=queue.url)
+    assert json.loads(response['Messages'][0]['Body']).get('Message') == payload
+    assert len(list(bucket.objects.all())) == 1
---
+++ packages/boto3-oversize/boto3_oversize-0.1.3.tar.gz/boto3_oversize-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['boto3_oversize', 'boto3_oversize.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3-stubs[s3,sns,sqs]>=1.20.30', 'boto3>=1.20.30']
+
+setup_kwargs = {
+    'name': 'boto3-oversize',
+    'version': '0.1.3',
+    'description': 'Transparently stores oversize SNS messages in S3 and retrieves them when receiving messages using SQS.',
+    'long_description': "boto3-oversize\n==============\n\nMessages published using Amazon SNS have a [maximum size of 256KiB](https://aws.amazon.com/about-aws/whats-new/2013/06/18/amazon-sqs-announces-256KB-large-payloads/),\nwhich can be a limitation for certain use cases. AWS provides an\n[extended client library for Java](https://aws.amazon.com/about-aws/whats-new/2020/08/amazon-sns-launches-client-library-supporting-message-payloads-of-up-to-2-gb/)\nthat transparently uploads messages exceeding this threshold to S3 and restores them when the\nmessages are received. This Python package provides the same functionality for\n[boto3](https://aws.amazon.com/sdk-for-python/).\n\n## Installation\n\n1. Create an Amazon S3 bucket that will store message payloads that exceed the maximum size.\n   * While the Java library deletes payloads from S3 when they are retrieved, this is not\n     appropriate when there are multiple subscribers to the topic. Instead, apply a\n     [S3 lifecycle configuration](https://docs.aws.amazon.com/AmazonS3/latest/userguide/object-lifecycle-mgmt.html)\n     to expire the message payloads after a reasonable length of time, e.g., 14 days.\n2. Install this package, e.g., `pip install boto3-oversize`.\n3. Define the `OVERSIZE_PAYLOAD_BUCKET_NAME` environment variable as the name of the bucket you created;\n   ensure your AWS access credentials have permission to call `PutObject` and `GetObject` in the\n   root of the bucket.\n\n## Usage\n\nThe library provides replacement implementations of the core boto3 entry points that transparently\napply the necessary changes to the SNS and SQS clients, both for the low-level client and service\nresource. Simply reference `boto3_oversize` instead of `boto3`.\n\n### Low-level client example\n\n```python\nimport boto3_oversize\n\nsns_client = boto3_oversize.client('sns')\nresponse = sns_client.create_topic(Name='example-large-payload-topic')\nsns_client.publish(TopicArn=response['TopicArn'], Message='example-message')\n```\n\n### Service resource example\n\n```python\nimport boto3_oversize\n\nsqs_client = boto3_oversize.resource('sqs')\nqueue = sqs_client.create_queue(QueueName='example-large-payload-queue')\nmessages = queue.receive_messages()\n```\n\n## Implementation\n\nCalls to publish messages are intercepted and the message body sized check against the limit,\nreduced by a small percentage to consider SNS message envelope overhead if raw message delivery is\nnot enabled. If the message exceeds this threshold, it is uploaded to an S3 bucket and the SNS\nmessage replaced with the object ARN.\n\nWhen receiving messages, the SQS client checks if the entire message body appears to be an S3 object\nARN. If it is, the object is retrieved from S3 and returned to the caller as the message body.\n",
+    'author': 'roberthl',
+    'author_email': 'roberthl@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
