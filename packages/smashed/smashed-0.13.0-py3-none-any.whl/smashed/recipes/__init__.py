123c94351b50afe0aa93f2afa10df5b7054c23d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:29 +0000

    Add package smashed/0.13.0/smashed-0.13.0-py3-none-any.whl

---
+++ packages/smashed/smashed-0.13.0-py3-none-any.whl/smashed/recipes/__init__.py
@@ -0,0 +1,8 @@
+from .collators import CollatorRecipe, SlowCollatorRecipe
+from .prompting import PromptingRecipe
+
+__all__ = [
+    "CollatorRecipe",
+    "PromptingRecipe",
+    "SlowCollatorRecipe",
+]
