a1ecdc5040187cd11f03224518189275e56841c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:19 +0000

    Add package jnunes-account/0.1.1.2/jnunes-account-0.1.1.2.tar.gz

---
+++ packages/jnunes-account/jnunes-account-0.1.1.2.tar.gz/jnunes-account-0.1.1.2/account/python/static_messages.py
@@ -0,0 +1,11 @@
+# User Registration
+user_registration_success = 'Conta criada com sucesso'
+user_registration_invalid_form = 'O formulário contém informações inválidas.'
+
+# Restore passoword
+user_restore_password_send_email = 'Instruções para redefinição de senha enviados para o e-mail informado'
+user_restore_password_success = 'Senha alterada com sucesso.'
+
+# Error messages
+invalid_login = "Entre com um usuário e senha corretos."
+inactive = "Sua conta está desativada."
