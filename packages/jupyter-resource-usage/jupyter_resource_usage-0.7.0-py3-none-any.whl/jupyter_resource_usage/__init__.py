87776b2cb7e44a2153fdff4621bb4e6dff3d36b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:54 +0000

    Add package jupyter-resource-usage/0.7.0/jupyter_resource_usage-0.7.0-py3-none-any.whl

---
+++ packages/jupyter-resource-usage/jupyter_resource_usage-0.7.0-py3-none-any.whl/jupyter_resource_usage/__init__.py
@@ -0,0 +1,39 @@
+import json
+from pathlib import Path
+
+from ._version import __version__
+from .server_extension import load_jupyter_server_extension
+
+HERE = Path(__file__).parent.resolve()
+
+data = json.loads((HERE / "labextension" / "package.json").read_text())
+
+
+def _jupyter_labextension_paths():
+    return [{"src": "labextension", "dest": data["name"]}]
+
+
+def _jupyter_server_extension_points():
+    """
+    Set up the server extension for collecting metrics
+    """
+    return [{"module": "jupyter_resource_usage"}]
+
+
+def _jupyter_nbextension_paths():
+    """
+    Set up the notebook extension for displaying metrics
+    """
+    return [
+        {
+            "section": "notebook",
+            "dest": "jupyter_resource_usage",
+            "src": "static",
+            "require": "jupyter_resource_usage/main",
+        }
+    ]
+
+
+# For backward compatibility
+_load_jupyter_server_extension = load_jupyter_server_extension
+_jupyter_server_extension_paths = _jupyter_server_extension_points
