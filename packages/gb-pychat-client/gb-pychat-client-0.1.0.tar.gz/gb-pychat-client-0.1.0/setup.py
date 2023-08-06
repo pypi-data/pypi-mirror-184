b6e808aa97aac3a314b68e312922b20138dd5b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:31:30 +0000

    Add package gb-pychat-client/0.1.0/gb-pychat-client-0.1.0.tar.gz

---
+++ packages/gb-pychat-client/gb-pychat-client-0.1.0.tar.gz/gb-pychat-client-0.1.0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['client', 'client.gui']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Jinja2>=3.1.2,<4.0.0',
+ 'PyQt6>=6.4.0,<7.0.0',
+ 'SQLAlchemy>=1.4.44,<2.0.0',
+ 'gb-pychat-common @ ../common']
+
+entry_points = \
+{'console_scripts': ['client-gui = client.run_client:run']}
+
+setup_kwargs = {
+    'name': 'gb-pychat-client',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'DashViolin',
+    'author_email': 'mymail@srv.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
