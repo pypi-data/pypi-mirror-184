e70bb6eb18464b68236abdcf543e203a88d7468a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:21 +0000

    Add package supervisely/6.68.92/supervisely-6.68.92.tar.gz

---
+++ packages/supervisely/supervisely-6.68.92.tar.gz/supervisely-6.68.92/supervisely/app/widgets/project_thumbnail/project_thumbnail.py
@@ -0,0 +1,31 @@
+from supervisely.app import DataJson
+from supervisely.app.widgets import Widget
+from supervisely.api.project_api import ProjectInfo
+from supervisely.project.project import Project
+
+
+class ProjectThumbnail(Widget):
+    def __init__(self, info: ProjectInfo, widget_id: str = None):
+        self._info = info
+        self._description = f"{self._info.items_count} {self._info.type} in project"
+        self._url = Project.get_url(self._info.id)
+        super().__init__(widget_id=widget_id, file_path=__file__)
+
+    def get_json_data(self):
+        return {
+            "id": self._info.id,
+            "name": self._info.name,
+            "description": self._description,
+            "url": self._url,
+            "image_preview_url": self._info.image_preview_url,
+        }
+
+    def get_json_state(self):
+        return None
+
+    def set(self, info: ProjectInfo):
+        self._info = info
+        self._description = f"{self._info.items_count} {self._info.type} in project"
+        self._url = Project.get_url(self._info.id)
+        self.update_data()
+        DataJson().send_changes()
