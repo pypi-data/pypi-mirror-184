ede0141945bcbf5981605e4dc21d7803a376ac8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:19 +0000

    Add package iam-distributions/0.1/iam_distributions-0.1.tar.gz

---
+++ packages/iam-distributions/iam_distributions-0.1.tar.gz/iam_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='iam_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['iam_distributions'],
+      a_name = 'Agbaji Marcus Ifeanyi',
+      a_email = "agbajimarcusifeanyi@gmail.com",
+      zip_safe=False)
