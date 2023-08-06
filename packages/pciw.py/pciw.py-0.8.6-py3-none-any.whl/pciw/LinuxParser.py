c413a1c4f7efe1964d0ead4c013ffc328c96fb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:53 +0000

    Add package pciw.py/0.8.6/pciw.py-0.8.6-py3-none-any.whl

---
+++ packages/pciw.py/pciw.py-0.8.6-py3-none-any.whl/pciw/LinuxParser.py
@@ -0,0 +1,26 @@
+import cpuinfo
+from typing import Any, Dict
+from . import Converter
+
+# ! Функции парсинга
+def get_cpu() -> Dict[str, Any]:
+    info = cpuinfo.get_cpu_info()
+    return {
+        "name": info["brand_raw"],
+        "model": info["model"],
+        "family": info["family"],
+        "stepping": info["stepping"],
+        "architecture": info["arch"],
+        "bits": int(info["bits"]),
+        "frequency": round(info["hz_actual"][0] / 1e9, 1),
+        "cores_count": info["count"],
+        "cache": {
+            "l2_size": Converter.linux_bytes(
+                Converter.exists_key("l2_cache_size", info)[1]
+            ),
+            "l3_size": Converter.linux_bytes(
+                Converter.exists_key("l3_cache_size", info)[1]
+            )
+        },
+        "flags": [i.upper() for i in info["flags"]]
+    }
---
+++ packages/pciw.py/pciw.py-0.8.6-py3-none-any.whl/pciw/__main__.py
@@ -0,0 +1,40 @@
+import pciw
+from rich.console import Console
+
+# * Иницализация
+console = Console()
+
+# * Проверка
+console.print(f"[yellow]Location of this file[/] [red]->[/] [green]{__file__}[/]")
+console.rule("CPU INFO")
+console.print(pciw.get_cpu_info())
+
+try:
+    console.rule("CPU STATUS TEST")
+    console.print(pciw.get_cpu_status())
+except:
+    console.print_exception()
+
+console.rule("RAM INFO")
+console.print(pciw.get_ram_info())
+
+console.rule("GPU INFO")
+console.print(pciw.get_gpu_info())
+
+console.rule("MONITORS INFO")
+console.print(pciw.get_monitors_info())
+
+console.rule("MOTHERBOARD INFO")
+console.print(pciw.get_motherboard_info())
+
+console.rule("BIOS INFO")
+console.print(pciw.get_bios_info())
+
+console.rule("NVIDIA VIDEOCARDS AND STATUS")
+try:
+    console.print(pciw.get_ngpu_info())
+except pciw.NvidiaSMIError:
+    console.print("[red]Error[/]: [green]The NVIDIA driver is not installed![/]")
+
+console.rule("SOUND DEVICES")
+console.print(pciw.get_sound_device_info())
