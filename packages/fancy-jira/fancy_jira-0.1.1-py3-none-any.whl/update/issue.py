9874eaa3cb1d3362496c93e518c3f0b028359890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:18 +0000

    Add package fancy-jira/0.1.1/fancy_jira-0.1.1-py3-none-any.whl

---
+++ packages/fancy-jira/fancy_jira-0.1.1-py3-none-any.whl/update/issue.py
@@ -0,0 +1,20 @@
+import click
+import requests
+from jiractl_shared_functions import get_config
+
+
+@click.command()
+@click.argument('issue_id')
+@click.option('--story-points', '-s', help="Sets the story points for the jira issue")
+@click.pass_context
+def issue(ctx, issue_id, story_points):
+    config = get_config()
+    body = {'fields':{}}
+
+    if story_points:
+        body['fields']["customfield_10002"] = float(story_points)
+
+    request = requests.put(config['api_endpoint'] + '/latest/issue/' + issue_id, json=body, headers=config['headers'])
+
+    request.raise_for_status()
+
