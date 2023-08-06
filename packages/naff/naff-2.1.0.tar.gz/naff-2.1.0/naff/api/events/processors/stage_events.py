c72d88d94d0bdb489427511043a485b0fecd8bcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:32 +0000

    Add package naff/2.1.0/naff-2.1.0.tar.gz

---
+++ packages/naff/naff-2.1.0.tar.gz/naff-2.1.0/naff/api/events/processors/stage_events.py
@@ -0,0 +1,24 @@
+from typing import TYPE_CHECKING
+
+import naff.api.events as events
+from naff.models import StageInstance
+from ._template import EventMixinTemplate, Processor
+
+if TYPE_CHECKING:
+    from naff.api.events import RawGatewayEvent
+
+__all__ = ("StageEvents",)
+
+
+class StageEvents(EventMixinTemplate):
+    @Processor.define()
+    async def _on_raw_stage_instance_create(self, event: "RawGatewayEvent") -> None:
+        self.dispatch(events.StageInstanceCreate(StageInstance.from_dict(event.data, self)))  # type: ignore
+
+    @Processor.define()
+    async def _on_raw_stage_instance_update(self, event: "RawGatewayEvent") -> None:
+        self.dispatch(events.StageInstanceUpdate(StageInstance.from_dict(event.data, self)))  # type: ignore
+
+    @Processor.define()
+    async def _on_raw_stage_instance_delete(self, event: "RawGatewayEvent") -> None:
+        self.dispatch(events.StageInstanceDelete(StageInstance.from_dict(event.data, self)))  # type: ignore
---
+++ packages/naff/naff-2.1.0.tar.gz/naff-2.1.0/setup.py
@@ -0,0 +1,68 @@
+import itertools
+from pathlib import Path
+
+import tomli
+from setuptools import find_packages, setup
+
+with open("pyproject.toml", "rb") as f:
+    pyproject = tomli.load(f)
+
+extras_require = {
+    "voice": ["PyNaCl>=1.5.0,<1.6"],
+    "speedup": ["aiodns", "orjson", "Brotli"],
+    "sentry": ["sentry-sdk"],
+    "jurigged": ["jurigged"],
+}
+extras_require["all"] = list(itertools.chain.from_iterable(extras_require.values()))
+extras_require["docs"] = extras_require["all"] + [
+    "mkdocs-autorefs",
+    "mkdocs-awesome-pages-plugin",
+    "mkdocs-material",
+    "mkdocstrings-python",
+    "mkdocs-minify-plugin",
+    "mkdocs-git-committers-plugin-2",
+    "mkdocs-git-revision-date-localized-plugin",
+]
+extras_require["tests"] = [
+    "pytest",
+    "pytest-recording",
+    "pytest-asyncio",
+    "pytest-cov",
+    "python-dotenv",
+    "typeguard",
+]
+
+setup(
+    name="naff",
+    description=pyproject["tool"]["poetry"]["description"],
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="LordOfPolls",
+    author_email="naff@lordofpolls.com",
+    url="https://github.com/Discord-Snake-Pit/NAFF",
+    version=pyproject["tool"]["poetry"]["version"],
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.10",
+    install_requires=(Path(__file__).parent / "requirements.txt").read_text().splitlines(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: AsyncIO",
+        "Framework :: aiohttp",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Documentation",
+        "Typing :: Typed",
+    ],
+    project_urls={
+        "Discord": "https://discord.gg/hpfNhH8BsY",
+        "Documentation": "https://naff-docs.readthedocs.io/en/latest/",
+    },
+    extras_require=extras_require,
+)
