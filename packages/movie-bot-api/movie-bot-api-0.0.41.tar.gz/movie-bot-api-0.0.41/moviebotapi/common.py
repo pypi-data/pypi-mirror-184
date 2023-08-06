95353f4588e62a567ef32437a5ddcc2d4c774767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:03 +0000

    Add package movie-bot-api/0.0.41/movie-bot-api-0.0.41.tar.gz

---
+++ packages/movie-bot-api/movie-bot-api-0.0.41.tar.gz/movie-bot-api-0.0.41/moviebotapi/common.py
@@ -0,0 +1,36 @@
+from typing import Optional, Dict
+
+from moviebotapi import Session
+
+
+class CommonApi:
+    def __init__(self, session: Session):
+        self._session: Session = session
+
+    def restart_app(self, secs: Optional[int] = 3):
+        self._session.get('common.restart_app', {
+            'secs': secs
+        })
+
+    def get_cache(self, namespace: str, key: str) -> Dict:
+        return self._session.get('common.get_cache', {
+            'namespace': namespace,
+            'key': key
+        })
+
+    def set_cache(self, namespace: str, key: str, data: Dict):
+        self._session.post('common.set_cache', {
+            'namespace': namespace,
+            'key': key,
+            'data': data
+        })
+
+    def get_image_text(self, b64_img: str):
+        return self._session.post('common.get_image_text', {
+            'b64_image': b64_img
+        })
+
+    def get_cache_image_filepath(self, img_url: str) -> str:
+        return self._session.get('common.get_cache_image_filepath', {
+            'url': img_url
+        })
