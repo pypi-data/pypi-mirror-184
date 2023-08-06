1c9f891e46e24fb7b3c22f2983166a7b7bfa34dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:49 +0000

    Add package numericalAnalysisTool/0.1.2/numericalAnalysisTool-0.1.2.tar.gz

---
+++ packages/numericalAnalysisTool/numericalAnalysisTool-0.1.2.tar.gz/numericalAnalysisTool-0.1.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='numericalAnalysisTool',
+      version='0.1.2',
+      description='Numerical anlysis tool',
+      author='Clément Dutriez',
+      author_email='clement.dutriez@u-pem.fr',
+      url='https://github.com/CafeKrem/pyNumericalAnlysis',
+      packages=['numericalAnalysis'],
+      long_description=open('README.rst').read(),
+     )
