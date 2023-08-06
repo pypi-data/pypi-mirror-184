b49da7f2d043d68d12eeb886ad3394394736a03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:27 +0000

    Add package oneflow/0.9.0/oneflow-0.9.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/oneflow/oneflow-0.9.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/oneflow/include/glog-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ci-user/manylinux-cache-dir/release/cu117/build/_deps/glog-src
+# Build directory: /home/ci-user/manylinux-cache-dir/release/cu117/build/_deps/glog-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+set(CTEST_RESOURCE_SPEC_FILE "/home/ci-user/runners/release/_work/oneflow/oneflow/ci/test/resource-spec/2x-rtx-2080.json")
