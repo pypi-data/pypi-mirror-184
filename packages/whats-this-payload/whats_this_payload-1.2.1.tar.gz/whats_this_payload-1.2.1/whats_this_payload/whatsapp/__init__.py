e2a912d7ab20f08739d4a8527a3037822888c6b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:48 +0000

    Add package whats-this-payload/1.2.1/whats_this_payload-1.2.1.tar.gz

---
+++ packages/whats-this-payload/whats_this_payload-1.2.1.tar.gz/whats_this_payload-1.2.1/whats_this_payload/whatsapp/__init__.py
@@ -0,0 +1,7 @@
+from whats_this_payload.whatsapp.identifier import WhatsappIdentifier
+from whats_this_payload.whatsapp.enums import PayloadType
+
+__all__ = [
+    "WhatsappIdentifier",
+    "PayloadType",
+]
