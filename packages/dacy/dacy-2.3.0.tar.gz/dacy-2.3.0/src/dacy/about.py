376fc22d9a6540b74f252187ff6e17e749944cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:14 +0000

    Add package dacy/2.3.0/dacy-2.3.0.tar.gz

---
+++ packages/dacy/dacy-2.3.0.tar.gz/dacy-2.3.0/src/dacy/about.py
@@ -0,0 +1,5 @@
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution("dacy").version
+__title__ = "dacy"
+__download_url__ = "https://github.com/centre-for-humanities-computing/DaCy"
