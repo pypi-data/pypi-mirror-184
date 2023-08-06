34dcc118b4b8233c497350b509019b8293c43ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:51:31 +0000

    Add package perlib/0.3.4/perlib-0.3.4.tar.gz

---
+++ packages/perlib/perlib-0.3.4.tar.gz/perlib-0.3.4/perlib/__init__.py
@@ -0,0 +1,7 @@
+from .analysis import *
+from .core import *
+from .core.metrics import *
+from .preprocessing._utils import *
+from .preprocessing import *
+from .piplines import *
+from .forecaster import *
