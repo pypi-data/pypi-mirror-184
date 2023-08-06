79acbc86c57a921a407738d951012b6fbce7c753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:34 +0000

    Add package astro-pics/1.1.1/astro_pics-1.1.1.tar.gz

---
+++ packages/astro-pics/astro_pics-1.1.1.tar.gz/astro_pics-1.1.1/src/astro_pics/__init__.py
@@ -0,0 +1,9 @@
+import subprocess
+subprocess.run(["pip", "install", "astropy", "-q"])
+subprocess.run(["pip", "install", "requests", "-q"])
+subprocess.run(['pip', 'install', 'pyquery', "-q"])
+subprocess.run(["pip", "install", "pillow", "-q"])
+subprocess.run(["pip", "install", "matplotlib", "-q"])
+subprocess.run(["pip", "install", "numpy", "-q"])
+subprocess.run(["pip", "install", "scipy", "-q"])
+subprocess.run(["pip", "install", "socket", "-q"])
