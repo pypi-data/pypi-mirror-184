a66f8b1b9c35ca74626ff7872df2633b7517f4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:45 +0000

    Add package Ctse/0.1/Ctse-0.1.tar.gz

---
+++ packages/Ctse/Ctse-0.1.tar.gz/Ctse-0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Ctse',
+      version='0.1',
+      description='For help with progress bar ',
+      long_description='For help with progress bar. You can print in colors to :)',
+      author='Geoloup Team',
+      author_email='franckiebbb@gmail.com',
+      packages=['pyHelper'],
+     )
