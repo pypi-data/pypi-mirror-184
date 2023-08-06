493e312ced8cfbbafb5d1a0ac00d13a53b3be204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:53 +0000

    Add package openmtpk/0.4.95/openmtpk-0.4.95-cp310-cp310-manylinux1_x86_64.whl

---
+++ packages/openmtpk/openmtpk-0.4.95-cp310-cp310-manylinux1_x86_64.whl/openmtpk/setup.py
@@ -0,0 +1,22 @@
+# Execution of CPP->Python wrapping with Swig
+import os
+from distutils.core import setup, Extension
+
+src_files = Extension('_openmtpk',
+                      sources=['openmtpk/openmtpk_wrap.cxx',
+                               '../../modules/arithmetic/arith.cpp',
+                               '../../modules/number_theory/primes.cpp',
+                               '../../modules/number_theory/rc4.cpp',
+                               '../../modules/number_theory/cipher.cpp'],
+                      extra_compile_args=['-std=c++17'],
+                      )
+
+setup(name='openmtpk',
+      author='Akiel Aries',
+      description='openMTPK Python API with SWIG',
+      ext_modules = [src_files],
+      py_modules = ['openmtpk'],
+    )
+
+#os.system('mv *.so openmtpk/')
+
