92d22b292b160490323d9e2d039a43d09bde7433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:56 +0000

    Add package biblescrapeway/0.2.1/biblescrapeway-0.2.1.tar.gz

---
+++ packages/biblescrapeway/biblescrapeway-0.2.1.tar.gz/biblescrapeway-0.2.1/biblescrapeway/cli.py
@@ -0,0 +1,39 @@
+import click
+import json
+
+from pathlib import Path
+
+from .query import query
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+def _format_line(obj, _format):
+    if _format == 'refstr':
+        return obj.to_string()
+    if _format == 'str':
+        return '`{}` ({})'.format(obj.text,obj.version)
+
+def _formatter(obj_list, _format):
+    # Handle json
+    if _format == 'json':
+        return json.dumps([v.to_dict() for v in obj_list],indent=4)
+
+    # Handle everything else
+    return "\n".join([_format_line(obj,_format) for obj in obj_list])
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option('--version', '-v', default='ESV', help='Bible version to query, default is `ESV`')
+@click.option('--format', '-f', '_format', default='refstr', type=click.Choice(['refstr','str','json']),
+        help='Specify output format, default is `refstr`')
+@click.option('--cache/--no-cache',  default=False, 
+        help='Look up verses saved in a local cache first, and save new queries locally')
+@click.argument('reference_string')
+def scrape(version, reference_string, _format, cache):
+    """Scrape bible verses
+
+    REFERENCE_STRING a (comma delimited) list of references, e.g. `John3.16` or `1Peter3.1-5` or `Gen1,2`
+    """
+    
+    verses = query( reference_string, version, cache )
+    click.echo(_formatter(verses, _format))
+
