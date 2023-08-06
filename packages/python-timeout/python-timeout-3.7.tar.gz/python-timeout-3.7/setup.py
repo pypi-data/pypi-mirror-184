819bcccd0ac1cd6130371a623e7c7bcf3002f036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:49 +0000

    Add package python-timeout/3.7/python-timeout-3.7.tar.gz

---
+++ packages/python-timeout/python-timeout-3.7.tar.gz/python-timeout-3.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'python-timeout',
+    packages = ['timeout'],
+    version='3.7',
+    description = 'Random timeout between minimum and maximum values',
+    url = 'https://github.com/xjxckk/python-timeout/',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+    )
