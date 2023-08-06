3dbfcbc67880c96e478e9d2e55f3fdb7edfe850b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:45 +0000

    Add package example-robot-data/4.0.3.2/example_robot_data-4.0.3.2-0-pp38-pypy38_pp73-manylinux_2_17_x86_64.whl

---
+++ packages/example-robot-data/example_robot_data-4.0.3.2-0-pp38-pypy38_pp73-manylinux_2_17_x86_64.whl/cmeel.prefix/include/example-robot-data/path.hpp
@@ -0,0 +1,6 @@
+#ifndef EXAMPLE_ROBOT_DATA_PATH_HPP
+#define EXAMPLE_ROBOT_DATA_PATH_HPP
+
+#define EXAMPLE_ROBOT_DATA_MODEL_DIR "/tmp/cmeel-c5h_mynt/pfx/cmeel.prefix/share/example-robot-data/robots"
+
+#endif
