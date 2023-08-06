9524c7bc5702bcc974c6894c2bbe05d7ac24a75c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:39 +0000

    Add package searchlauncher/0.2.0/searchlauncher-0.2.0.tar.gz

---
+++ packages/searchlauncher/searchlauncher-0.2.0.tar.gz/searchlauncher-0.2.0/README.md
@@ -0,0 +1,81 @@
+# searchlauncher
+
+Search multiple websites at once using your default browser.
+
+The search (launcher) can be triggered from anywhere as it's listening for a global keypress.
+
+### Installation
+
+```bash
+pip install searchlauncher
+```
+
+#### Requirements
+
+Python `>=3.10` with [`tkinter`](https://docs.python.org/3/library/tkinter.html) installed.
+
+### Running in background
+
+```bash
+searchlauncher
+```
+
+to start as a daemon waiting for a [configurable](#settings-and-supported-websites) keypress (e.g., `Ctrl + Shift + E`).
+
+Then type your query and press `Enter` to submit or `Esc` to close the window.
+
+This will open the search for each website in a new tab.
+
+Searching different website groups can be triggered with different hotkeys.
+
+### Searching from console
+
+Instead of running in background, you can use the CLI run a one-off search.
+
+To open a search for all [configured websites](#settings-and-supported-websites):
+
+```shell
+searchlauncher search "an item I'm looking for"
+```
+
+You can also select a [custom search group](#settings-and-supported-websites):
+
+```shell
+searchlauncher search "and now for something completely different" -g en
+```
+
+### Settings and supported websites
+
+To open the config file location, you can run
+
+```shell
+searchlauncher config
+```
+
+after installing.
+
+See the available [`default websites and groups`](src/searchlauncher/settings/default.toml).
+
+You can easily modify this file to add and modify target websites as well as their groups.
+
+You can also customise the default `shortcut` hotkey as well as shortcuts for all groups.
+
+## TODO
+
+- [x] CLI to search for a single item
+- [x] GUI (popup on hotkey)
+- Configurability
+    - [x] Customise the hotkey(s)
+    - [x] Customise search sites
+    - [x] Customisable search groups
+    - [ ] Add and select different browsers
+
+## Development
+
+### Setup
+
+1. [Install poetry](https://python-poetry.org/docs/#installation) and `cd` to this folder.
+
+2. `poetry install`
+
+3. `poetry run pre-commit install`
---
+++ packages/searchlauncher/searchlauncher-0.2.0.tar.gz/searchlauncher-0.2.0/src/searchlauncher/cli.py
@@ -0,0 +1,77 @@
+import sys
+
+if sys.version_info >= (3, 11):
+    from enum import StrEnum
+else:
+    from backports.strenum import StrEnum  # type: ignore
+from typing import Generator
+
+import typer
+from loguru import logger
+
+from searchlauncher import open_in_browser, start_gui
+from searchlauncher.settings import DEFAULT_SETTINGS_PATH, load_settings
+
+cli = typer.Typer()
+
+settings = load_settings()
+Group = StrEnum("Group", list(settings.groups.keys()))  # type: ignore
+
+
+def complete_groups(incomplete: str) -> Generator[tuple[str, str], None, None]:
+    for group_name, group in settings.groups.items():
+        if group_name.startswith(incomplete):
+            yield group_name, ", ".join(group.sites)
+
+
+@cli.command()
+def search(
+    query: str,
+    group: Group = typer.Option(  # type: ignore
+        None,
+        "--group",
+        "-g",
+        autocompletion=complete_groups,
+        case_sensitive=False,
+    ),
+) -> None:
+    """
+    Directly open search for the given query using all sites or the specified group.
+
+    Searches all sites unless a group is specified with "-g" or "--group" (see "searchlauncher config").
+    """
+    open_in_browser(query, group_name=group)
+
+
+@cli.command()
+def config() -> None:
+    """
+    Open the config directory.
+
+    You can modify the "toml" file to modify keyboard shortcuts, sites and groups.
+    """
+    settings_dir = str(DEFAULT_SETTINGS_PATH)
+    logger.info(f"Opening settings directory: {settings_dir}")
+    typer.launch(settings_dir, locate=True)
+
+
+@cli.callback(invoke_without_command=True)
+def run(
+    ctx: typer.Context,
+) -> None:
+    """
+    Run search launcher in background, waiting for shortcuts.
+
+    See the "Commands" section below for more.
+    """
+
+    # This makes running just "searchlauncher" without any further commands run the main loop
+    # https://github.com/tiangolo/typer/issues/18
+    if ctx.invoked_subcommand is not None:
+        # If there's any command, we don't run the main loop
+        return
+    start_gui()
+
+
+if __name__ == "__main__":
+    cli()
