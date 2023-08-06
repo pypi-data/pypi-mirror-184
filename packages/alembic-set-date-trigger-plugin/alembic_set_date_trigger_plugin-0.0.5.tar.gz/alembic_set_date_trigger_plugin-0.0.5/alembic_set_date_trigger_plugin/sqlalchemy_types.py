52323ed152ec80914ff0ff17c955d0f390879e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:40 +0000

    Add package alembic-set-date-trigger-plugin/0.0.5/alembic_set_date_trigger_plugin-0.0.5.tar.gz

---
+++ packages/alembic-set-date-trigger-plugin/alembic_set_date_trigger_plugin-0.0.5.tar.gz/alembic_set_date_trigger_plugin-0.0.5/alembic_set_date_trigger_plugin/sqlalchemy_types.py
@@ -0,0 +1,21 @@
+from typing import Union, Tuple
+
+from sqlalchemy import DateTime
+
+
+class TriggerOnEnum(str):
+    update = "update"
+    insert = "insert"
+
+
+def DateTimeWithSetDateTrigger(
+    trigger_on: Union[TriggerOnEnum, Tuple[TriggerOnEnum]], datetime_processor=DateTime, *args, **kwargs
+):
+    """SQLAlchemy Datetime type with support for the creation of a Postgresql trigger to update the date"""
+
+    class DateTimeWithSetDateTriggerClass(datetime_processor):
+        def __init__(self, trigger_on: Union[TriggerOnEnum, Tuple[TriggerOnEnum]], *args, **kwargs):
+            self.trigger_on = trigger_on
+            super().__init__(*args, **kwargs)
+
+    return DateTimeWithSetDateTriggerClass(trigger_on=trigger_on, *args, **kwargs)
