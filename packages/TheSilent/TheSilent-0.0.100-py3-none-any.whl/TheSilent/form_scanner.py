9e50e7f1232f5273da48ed07b38f3767f60d389e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:14 +0000

    Add package TheSilent/0.0.100/TheSilent-0.0.100-py3-none-any.whl

---
+++ packages/TheSilent/TheSilent-0.0.100-py3-none-any.whl/TheSilent/form_scanner.py
@@ -0,0 +1,49 @@
+import re
+import requests
+
+red = "\033[1;31m"
+
+#create html sessions object
+web_session = requests.Session()
+
+#fake user agent
+user_agent = {"User-Agent" : "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
+
+#increased security
+requests.packages.urllib3.disable_warnings()
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ":HIGH:!DH:!aNULL"
+
+#increased security
+try:
+    requests.packages.urllib3.contrib.pyopenssl.util.ssl_.DEFAULT_CIPHERS += ":HIGH:!DH:!aNULL"
+
+except AttributeError:
+    pass
+
+#scans for forms
+def form_scanner(url, secure = True, parse = "forms"):
+    if secure == True:
+        my_secure = "https://"
+
+    if secure == False:
+        my_secure = "http://"
+
+    url = my_secure + url
+
+    try:
+        result = web_session.get(url, verify = False, headers = user_agent, timeout = (5, 30)).text
+
+        try:
+            if parse == "forms":
+                forms = re.findall("<form[\n\w\S\s]+?form>", result)
+                return forms
+
+            if parse == "input":
+                input_form = re.findall("<input.+?>", result)
+                return input_form
+
+        except UnicodeDecodeError:
+            return []
+
+    except:
+        return []
