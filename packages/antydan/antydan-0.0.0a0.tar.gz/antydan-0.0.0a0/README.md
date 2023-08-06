0e2c56b6b2c019f314eed7e659a059a632d4252d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:53 +0000

    Add package antydan/0.0.0a0/antydan-0.0.0a0.tar.gz

---
+++ packages/antydan/antydan-0.0.0a0.tar.gz/antydan-0.0.0a0/README.md
@@ -0,0 +1,7 @@
+# antydan
+
+Prompt types for your generative AIs.
+
+[antydan](https://en.wiktionary.org/wiki/antydan) is Swedish for "hint".
+
+Coming soon!
---
+++ packages/antydan/antydan-0.0.0a0.tar.gz/antydan-0.0.0a0/antydan/types.py
@@ -0,0 +1,4 @@
+# TODO: remember facts - dates, times, places, people, things
+# TODO: examples of success criteria for a prompt
+# TODO: field names to table structure
+# TODO: name and description
