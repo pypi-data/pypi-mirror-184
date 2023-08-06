a0d5c03ecd6ed03cc9141a3fbc31d1a2631d47b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:08:58 +0000

    Add package jiahaofour/0.0.1/jiahaofour-0.0.1.tar.gz

---
+++ packages/jiahaofour/jiahaofour-0.0.1.tar.gz/jiahaofour-0.0.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/12/27 17:36
+# @Author : jh
+# @File : setup.py
+
+
+from setuptools import setup, find_packages
+
+setup(name='jiahaofour',
+      py_modules='label_main',
+      version='0.0.1',
+      description='use for test',
+      author='jh',
+      author_email='jhdyxa@163.com',
+      requires=['ahocorasick'],
+      packages=find_packages(),     # 系统自动从当前目录开始找包
+      # 如果有的文件不用打包，则只能指定需要打包的文件
+      # packages=['代码1','代码2','__init__']   # 指定目录中需要打包的py文件，注意，不需要写后缀
+      # license=''    # 支持的开源协议
+      
+       )
