414a070df53b14d744a7432bb593f543767904fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:05 +0000

    Add package dune.gdt/2022.5.3/dune.gdt-2022.5.3-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/dune.gdt/dune.gdt-2022.5.3-cp310-cp310-manylinux_2_28_x86_64.whl/dune/gdt/_version.py
@@ -0,0 +1,18 @@
+# ~~~
+# This file is part of the dune-xt project:
+#   https://github.com/dune-community/dune-xt
+# Copyright 2009-2020 dune-xt developers and contributors. All rights reserved.
+# License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+#      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+#          with "runtime exception" (http://www.dune-project.org/license.html)
+# Authors:
+#   Felix Schindler (2014, 2016 - 2017, 2019)
+#   René Fritze     (2012, 2015 - 2016, 2018 - 2019)
+#   Tim Keil        (2019)
+#   Tobias Leibner  (2019 - 2020)
+# ~~~
+
+# this mirrors the definition in config.h.cmake
+__version__ = "2022.5.3"
+# this is the full git describe
+__git_revision__ = "2022.5.3"
