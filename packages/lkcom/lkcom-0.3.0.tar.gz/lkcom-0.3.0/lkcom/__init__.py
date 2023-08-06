1f482e13b593e4340b072a8789e8faf4b1c784ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:09 +0000

    Add package lkcom/0.3.0/lkcom-0.3.0.tar.gz

---
+++ packages/lkcom/lkcom-0.3.0.tar.gz/lkcom-0.3.0/lkcom/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""lkcom - a Python library of useful routines.
+
+Copyright 2015-2022 Lukas Kontenis
+Contact: dse.ssd@gmail.com
+"""
+
+# flake8: noqa
+
+__version__ = "0.3.0"
