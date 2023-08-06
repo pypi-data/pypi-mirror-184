e9836818b8a1d3c55d7ba6b28d24c72f501ead6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:49 +0000

    Add package ProSad/1.0.0/ProSad-1.0.0-py3-none-any.whl

---
+++ packages/ProSad/ProSad-1.0.0-py3-none-any.whl/ProSad/How.py
@@ -0,0 +1,27 @@
+#Copyright (C) 2022 X CODER
+from time import sleep
+
+class color:
+      red = '\033[91m'
+      green = '\033[92m'
+      blue = '\033[94m'
+      yellow = '\033[93m'
+      magenta = '\033[95m'
+      cyan = '\033[96m'
+      white = '\033[97m'
+      bold = '\033[1m'
+      underline = '\033[4m'
+      black='\033[30m'
+
+class chup:
+      x_coder = f"""{color.bold}
+{color.white} [✧✧✦]  ༒   {color.yellow} 𝑃𝑅𝑂𝑆𝐴𝐷  {color.red} 𝐕𝐄𝐑𝐒𝐈𝐎𝐍  {color.cyan}𝟷.0.0
+{color.bold}
+{color.white} [✧✧✦] ༒   {color.yellow} 𝑃𝑅𝑂𝑆𝐴𝐷   {color.red} 𝙲𝙾𝙿𝚈𝚁𝙸𝙶𝙷𝚃 (𝑪) {color.cyan} ProSad
+
+{color.white} [✧✧✦] ༒   {color.yellow} 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁  {color.red} 𝐑𝐔𝐁𝐈𝐊𝐀 {color.cyan} : {color.blue} 𝑃𝑅𝑂| {color.green}𝑆𝐴𝐷  
+
+{color.white}╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾
+"""
+      print(x_coder)
+      
