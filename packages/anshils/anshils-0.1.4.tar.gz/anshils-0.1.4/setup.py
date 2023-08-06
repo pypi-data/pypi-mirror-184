e537dd9a2289ca5d01406af800ef39a92cd5a55b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:49 +0000

    Add package anshils/0.1.4/anshils-0.1.4.tar.gz

---
+++ packages/anshils/anshils-0.1.4.tar.gz/anshils-0.1.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='anshils',
+      version='0.1.4',
+      description='Gaussian and Binomial distributions',
+      packages=['anshils'],
+      author_email='i89041056398@gmail.com',
+      zip_safe=True)
