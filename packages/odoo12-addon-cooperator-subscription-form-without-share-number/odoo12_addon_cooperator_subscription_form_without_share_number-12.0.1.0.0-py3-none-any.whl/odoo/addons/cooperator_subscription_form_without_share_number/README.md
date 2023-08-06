ac179e198cc6d951fe4455786c228b5e5bd7712c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:39 +0000

    Add package odoo12-addon-cooperator-subscription-form-without-share-number/12.0.1.0.0/odoo12_addon_cooperator_subscription_form_without_share_number-12.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo12-addon-cooperator-subscription-form-without-share-number/odoo12_addon_cooperator_subscription_form_without_share_number-12.0.1.0.0-py3-none-any.whl/odoo/addons/cooperator_subscription_form_without_share_number/README.md
@@ -0,0 +1,11 @@
+## Become cooperator form without share number
+
+This module removes the share number field from the cooperator subscription form.
+
+It depends on the module [easy_my_coop_website](https://pypi.org/project/odoo12-addon-easy-my-coop-website/).
+
+The source code can be found [here](https://git.coopdevs.org/coopdevs/odoo/odoo-addons/odoo-addons).
+
+#### Authors
+
+* Coopdevs
---
+++ packages/odoo12-addon-cooperator-subscription-form-without-share-number/odoo12_addon_cooperator_subscription_form_without_share_number-12.0.1.0.0-py3-none-any.whl/odoo/addons/cooperator_subscription_form_without_share_number/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021-Coopdevs Treball SCCL (<https://coopdevs.org>)
+# - konykon - <kon.tsagari@coopdevs.org>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Cooperator subscription form without share number",
+    "version": "12.0.1.0.0",
+    "depends": ["easy_my_coop_website"],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Project Management",
+    "website": "https://coopdevs.org",
+    "description": "Remove field share number from cooperator subscription form.",
+    "license": "AGPL-3",
+    "summary": "Remove field share number from cooperator subscription form.",
+    "data": ["views/subscription_template.xml"],
+    "installable": True,
+}
