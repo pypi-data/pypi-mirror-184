a9e328683d0649ea1201212c8776a1197f8299fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:46 +0000

    Add package chain-app-client-sdk/0.0.2.9/chain_app_client_sdk-0.0.2.9-py3-none-any.whl

---
+++ packages/chain-app-client-sdk/chain_app_client_sdk-0.0.2.9-py3-none-any.whl/chain_app_client_sdk/apis/path_to_api.py
@@ -0,0 +1,23 @@
+import typing_extensions
+
+from chain_app_client_sdk.paths import PathValues
+from chain_app_client_sdk.apis.paths.auth_signup import AuthSignup
+from chain_app_client_sdk.apis.paths.auth_signin import AuthSignin
+from chain_app_client_sdk.apis.paths.main_pool_get_pool_size_history import MainPoolGetPoolSizeHistory
+
+PathToApi = typing_extensions.TypedDict(
+    'PathToApi',
+    {
+        PathValues.AUTH_SIGNUP: AuthSignup,
+        PathValues.AUTH_SIGNIN: AuthSignin,
+        PathValues.MAIN_POOL_GET_POOL_SIZE_HISTORY: MainPoolGetPoolSizeHistory,
+    }
+)
+
+path_to_api = PathToApi(
+    {
+        PathValues.AUTH_SIGNUP: AuthSignup,
+        PathValues.AUTH_SIGNIN: AuthSignin,
+        PathValues.MAIN_POOL_GET_POOL_SIZE_HISTORY: MainPoolGetPoolSizeHistory,
+    }
+)
