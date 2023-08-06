2662dfcf7ed87eb50371d22edf660e57017710f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:32 +0000

    Add package Flask-BigApp-Edge/0.1/Flask-BigApp-Edge-0.1.tar.gz

---
+++ packages/Flask-BigApp-Edge/Flask-BigApp-Edge-0.1.tar.gz/Flask-BigApp-Edge-0.1/README.md
@@ -0,0 +1,18 @@
+![](https://raw.githubusercontent.com/CheeseCake87/Flask-BigApp/master/app/theme/static/img/Flask-BigApp-Logo.png)
+
+# Flask-BigApp-Edge
+
+`pip install Flask-BigApp-Edge`
+
+![Tests](https://github.com/CheeseCake87/Flask-BigApp/actions/workflows/tests.yml/badge.svg)
+
+## WARNING
+
+### This is the edge build of Flask-BigApp, this built may be very unstable, or not work at all. Version numbers will change a lot!
+
+**What is Flask-BigApp?**
+
+Flask-BigApp's main purpose is to help simplify the importing of blueprints, routes and models.
+It has a few extra features built in to help with theming, securing pages and password authentication.
+
+[Go to Wiki](https://github.com/CheeseCake87/Flask-BigApp/wiki)
---
+++ packages/Flask-BigApp-Edge/Flask-BigApp-Edge-0.1.tar.gz/Flask-BigApp-Edge-0.1/setup.py
@@ -0,0 +1,43 @@
+import pathlib
+from setuptools import setup, find_packages
+
+readme = pathlib.Path(pathlib.Path.cwd() / "README.md").read_text()
+
+
+
+setup(
+    name='Flask-BigApp-Edge',
+    version=f'0.1',
+    url='https://github.com/CheeseCake87/Flask-BigApp/tree/edge',
+    license='GNU General Public License v2 or later (GPLv2+)',
+    author='David Carmichael',
+    author_email='carmichaelits@gmail.com',
+    description='This is the edge build of Flask-BigApp, this built '
+                'may be very unstable, or not work at all. Version numbers will change a lot!',
+    long_description=f'{readme}',
+    long_description_content_type='text/markdown',
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment', 'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: OS Independent', 'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Natural Language :: English',
+    ],
+    install_requires=[
+        'Flask',
+        'Flask-SQLAlchemy',
+        'toml',
+    ],
+    zip_safe=False,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'flask-bigapp-edge = flask_bigapp_edge_cli:cli',
+        ]
+    }
+)
