d4cc52214e979b02562e64960f3e3ade4ded6e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:00 +0000

    Add package sxxxs/0.2.12/sxxxs-0.2.12.tar.gz

---
+++ packages/sxxxs/sxxxs-0.2.12.tar.gz/sxxxs-0.2.12/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sxxxs', 'sxxxs..sc']
+
+package_data = \
+{'': ['*'],
+ 'sxxxs': ['.idea/*', '.idea/inspectionProfiles/*'],
+ 'sxxxs..sc': ['web/*', 'web/assets/*']}
+
+entry_points = \
+{'console_scripts': ['aaa = sxxxs.main:main']}
+
+setup_kwargs = {
+    'name': 'sxxxs',
+    'version': '0.2.12',
+    'description': '',
+    'long_description': '',
+    'author': 'sss',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
