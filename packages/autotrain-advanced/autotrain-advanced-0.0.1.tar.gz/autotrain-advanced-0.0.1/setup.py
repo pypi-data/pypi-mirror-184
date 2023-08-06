d4d67c7237e39a4ffbb139a3f439d7e5187b3019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:50 +0000

    Add package autotrain-advanced/0.0.1/autotrain-advanced-0.0.1.tar.gz

---
+++ packages/autotrain-advanced/autotrain-advanced-0.0.1.tar.gz/autotrain-advanced-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+
+if __name__ == "__main__":
+    setup(
+        name="autotrain-advanced",
+        version="0.0.1",
+        description="autotrain",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="Abhishek Thakur",
+        url="https://github.com/abhishekkrthakur/autotrain",
+        license="Apache License",
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=["torch>=1.10.0"],
+        platforms=["linux", "unix"],
+        python_requires=">=3.8",
+    )
