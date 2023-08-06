55685d1bb2a9e9928599e47766d563c413b0b835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:23:53 +0000

    Add package graphbin/1.6.1/graphbin-1.6.1.tar.gz

---
+++ packages/graphbin/graphbin-1.6.1.tar.gz/graphbin-1.6.1/src/graphbin/utils/graphbin_Func.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+__author__ = "Vijini Mallawaarachchi"
+__copyright__ = "Copyright 2019-2022, GraphBin Project"
+__credits__ = ["Vijini Mallawaarachchi", "Anuradha Wickramarachchi", "Yu Lin"]
+__license__ = "BSD-3"
+__version__ = "1.6.1"
+__maintainer__ = "Vijini Mallawaarachchi"
+__email__ = "vijini.mallawaarachchi@anu.edu.au"
+__status__ = "Production"
+
+
+def getClosestLabelledVertices(graph, node, binned_contigs):
+    # Remove labels of ambiguous vertices
+    # -------------------------------------
+
+    queu_l = [graph.neighbors(node, mode="ALL")]
+    visited_l = [node]
+    labelled = []
+
+    while len(queu_l) > 0:
+        active_level = queu_l.pop(0)
+        is_finish = False
+        visited_l += active_level
+
+        for n in active_level:
+            if n in binned_contigs:
+                is_finish = True
+                labelled.append(n)
+        if is_finish:
+            return labelled
+        else:
+            temp = []
+            for n in active_level:
+                temp += graph.neighbors(n, mode="ALL")
+                temp = list(set(temp))
+            temp2 = []
+
+            for n in temp:
+                if n not in visited_l:
+                    temp2.append(n)
+            if len(temp2) > 0:
+                queu_l.append(temp2)
+    return labelled
