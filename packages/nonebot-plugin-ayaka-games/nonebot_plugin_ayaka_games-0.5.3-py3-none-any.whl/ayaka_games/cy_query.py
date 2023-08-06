46a26252eb85192c004c68891cbd2986771db2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:28 +0000

    Add package nonebot-plugin-ayaka-games/0.5.3/nonebot_plugin_ayaka_games-0.5.3-py3-none-any.whl

---
+++ packages/nonebot-plugin-ayaka-games/nonebot_plugin_ayaka_games-0.5.3-py3-none-any.whl/ayaka_games/cy_query.py
@@ -0,0 +1,27 @@
+'''
+    成语查询
+'''
+from ayaka import AyakaBox, AyakaConfig, slow_load_config, Field
+from .data import load_data
+
+box = AyakaBox("成语查询")
+box.help = '''
+有效提高群文学氛围
+'''
+
+
+@slow_load_config
+class Config(AyakaConfig):
+    __config_name__ = box.name
+    words: dict = Field(default_factory=lambda: load_data("chengyu.json"))
+
+
+@box.on_cmd(cmds=["查询成语", "成语查询"])
+async def handle():
+    config = Config()
+    word = box.arg.extract_plain_text()
+    search_dict = config.words
+    if word in search_dict:
+        await box.send(search_dict[word])
+    else:
+        await box.send("没有找到相关信息")
