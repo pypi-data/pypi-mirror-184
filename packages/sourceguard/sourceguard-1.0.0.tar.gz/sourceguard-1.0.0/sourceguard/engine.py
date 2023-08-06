b703faf6fcbe4880f79ada92410631be09782393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:50 +0000

    Add package sourceguard/1.0.0/sourceguard-1.0.0.tar.gz

---
+++ packages/sourceguard/sourceguard-1.0.0.tar.gz/sourceguard-1.0.0/sourceguard/engine.py
@@ -0,0 +1,70 @@
+import re
+from pathlib import Path
+from typing import Dict
+from typing import Generator
+from typing import Iterator
+
+from sourceguard.git import FileDiff
+from sourceguard.banrule import BanRule
+
+ExtensionPattern = str
+BanRulesMap = Dict[ExtensionPattern, Iterator[BanRule]]
+
+
+class Validator:
+
+    def __init__(self, banrules: Iterator[BanRule]):
+        self.pattern_lookup = {
+            banrule.pattern: banrule
+            for banrule in banrules
+        }
+        combined_patterns = "|".join(banrule.pattern for banrule in banrules)
+        self.regex = re.compile(
+            combined_patterns) if combined_patterns else None
+
+    def validate(self, file_diff: FileDiff) -> Generator[BanRule, None, None]:
+
+        def file_not_in_excluded_path(banrule: BanRule) -> bool:
+            if not banrule.excluded_paths:
+                return True
+
+            return any(
+                Path(excluded_path).match(file_diff.filepath)
+                for excluded_path in banrule.excluded_paths)
+
+        if not self.regex:
+            return
+
+        matches = (self.regex.match(diff_line.strip())
+                   for diff_line in file_diff.diff_lines)
+
+        yield from filter(file_not_in_excluded_path,
+                          (self.pattern_lookup[match.group(0)]
+                           for match in matches if match is not None))
+
+
+class ValidationEngine:
+
+    def __init__(self, validators: Dict[ExtensionPattern, Validator]):
+        self.validators = validators
+
+    def validate(self, file_diff: FileDiff) -> Generator[BanRule, None, None]:
+        filepath = Path(file_diff.filepath)
+        extension_pattern = next(
+            filter(filepath.match,
+                   (extension_pattern
+                    for extension_pattern in self.validators.keys())), "")
+        if not extension_pattern:
+            return
+
+        validator = self.validators[extension_pattern]
+        yield from ((file_diff.filepath, failed_rule.description)
+                    for failed_rule in validator.validate(file_diff))
+
+
+def get_change_validation_engine(banrules_map: BanRulesMap):
+    validators = {
+        extension_pattern: Validator(rules)
+        for extension_pattern, rules in banrules_map.items()
+    }
+    return ValidationEngine(validators)
---
+++ packages/sourceguard/sourceguard-1.0.0.tar.gz/sourceguard-1.0.0/sourceguard/main.py
@@ -0,0 +1,73 @@
+import itertools
+import textwrap
+from importlib.machinery import SourceFileLoader
+from pathlib import Path
+from typing import List
+from typing import Tuple
+
+from sourceguard.engine import BanRulesMap
+from sourceguard.engine import get_change_validation_engine
+from sourceguard.git import get_project_root_dir
+from sourceguard.git import get_changed_files_diffs
+from sourceguard.git import get_diff_output
+
+Filename = str
+BanRuleDescription = str
+ValidationOutput = Tuple[Filename, BanRuleDescription]
+
+
+def run(diff_output: str, banrules_map: BanRulesMap) -> List[ValidationOutput]:
+    validation_engine = get_change_validation_engine(banrules_map)
+    failed_diff_output = itertools.chain.from_iterable(
+        validation_engine.validate(file_diff)
+        for file_diff in get_changed_files_diffs(diff_output))
+
+    return list(failed_diff_output)
+
+
+def create_template_toplevel_banned_file(ban_info_path: Path) -> None:
+    if ban_info_path.exists():
+        return
+
+    with open(ban_info_path, "w+") as fh:
+        fh.write(
+            textwrap.dedent('''
+                from sourceguard.banrule import BanRule
+
+                """
+                Example
+
+                BanRule(
+                   "os.path.join",
+                   "Please use pathlib.Path instead"
+                   )
+                """
+                PYTHON_BANRULES = [
+                ]
+
+                # maps extension regex to banrules
+                BANRULES_MAP = {
+                    "*.py" : PYTHON_BANRULES
+                }
+                ''').strip())
+
+
+def main() -> None:
+    root_dir = get_project_root_dir()
+    diff_output = get_diff_output(root_dir)
+
+    if not (ban_info_path := Path(root_dir).absolute() / ".banned").exists():
+        create_template_toplevel_banned_file(ban_info_path)
+
+    banrules = SourceFileLoader("banned", str(ban_info_path)).load_module()
+
+    if not (failed_diff_output := run(diff_output, banrules.BANRULES_MAP)):
+        return
+
+    for filepath, failure_description in failed_diff_output:
+        print("There's a banned code fragment in {filepath}: "
+              "\n\t{failure_description}")
+
+
+if __name__ == "__main__":
+    main()
