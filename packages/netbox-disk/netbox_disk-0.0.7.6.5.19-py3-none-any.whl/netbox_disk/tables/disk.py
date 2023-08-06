e078d82a87c60e6a1a69608eb214604858eed1ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:35 +0000

    Add package netbox-disk/0.0.7.6.5.19/netbox_disk-0.0.7.6.5.19-py3-none-any.whl

---
+++ packages/netbox-disk/netbox_disk-0.0.7.6.5.19-py3-none-any.whl/netbox_disk/tables/disk.py
@@ -0,0 +1,69 @@
+import django_tables2 as tables
+
+from netbox.tables import (
+    NetBoxTable,
+    ChoiceFieldColumn,
+    ToggleColumn,
+    TagColumn,
+    ActionsColumn,
+)
+
+from netbox_disk.models import Disk
+
+
+class DiskBaseTable(NetBoxTable):
+    """Base class for tables displaying Disks"""
+
+    vg_name = tables.Column(
+        linkify=True,
+    )
+    lv_name = tables.Column()
+    size = tables.Column()
+    path = tables.Column()
+
+
+class DiskTable(DiskBaseTable):
+    """Table for displaying Disk objects."""
+
+    pk = ToggleColumn()
+    cluster = tables.Column(
+        linkify=True
+    )
+
+    class Meta(NetBoxTable.Meta):
+        model = Disk
+        fields = (
+            "pk",
+            "vg_name",
+            "lv_name",
+            "size",
+            "path",
+            "cluster",
+            "description",
+        )
+        default_columns = (
+            "vg_name",
+            "lv_name",
+            "size",
+            "path",
+        )
+
+
+class RelatedDiskTable(DiskBaseTable):
+    actions = ActionsColumn(actions=())
+
+    class Meta(NetBoxTable.Meta):
+        model = Disk
+        fields = (
+            "vg_name",
+            "lv_name",
+            "size",
+            "path",
+            "cluster",
+        )
+        default_columns = (
+            "vg_name",
+            "lv_name",
+            "size",
+            "path",
+        )
