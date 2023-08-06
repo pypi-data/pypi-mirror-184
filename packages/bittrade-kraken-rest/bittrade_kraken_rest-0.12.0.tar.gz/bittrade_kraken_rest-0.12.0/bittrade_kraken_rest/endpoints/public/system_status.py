ec2efed920a237e9e6878dd67ea0e667b01fe085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:12 +0000

    Add package bittrade-kraken-rest/0.12.0/bittrade_kraken_rest-0.12.0.tar.gz

---
+++ packages/bittrade-kraken-rest/bittrade_kraken_rest-0.12.0.tar.gz/bittrade_kraken_rest-0.12.0/bittrade_kraken_rest/endpoints/public/system_status.py
@@ -0,0 +1,28 @@
+from bittrade_kraken_rest.connection.observable import send_public
+from bittrade_kraken_rest.connection.result import map_to_result
+from typing import Literal
+from pydantic.dataclasses import dataclass
+from reactivex import Observable
+from datetime import datetime
+
+Status = Literal["online", "maintenance", "cancel_only", "post_only"]
+
+
+@dataclass
+class GetSystemStatusResult:
+    status: Status
+    timestamp: datetime
+
+
+def get_system_status_response():
+    return send_public(url="/0/public/SystemStatus")
+
+
+def get_system_status() -> Observable[GetSystemStatusResult]:
+    return get_system_status_response().pipe(map_to_result(GetSystemStatusResult))
+
+
+__all__ = [
+    "get_system_status",
+    "GetSystemStatusResult",
+]
