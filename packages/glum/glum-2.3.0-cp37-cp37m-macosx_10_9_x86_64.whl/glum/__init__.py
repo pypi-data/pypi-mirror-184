477cdcf6e6c57c8b97f97dac94494628510183b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:29:30 +0000

    Add package glum/2.3.0/glum-2.3.0-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/glum/glum-2.3.0-cp37-cp37m-macosx_10_9_x86_64.whl/glum/__init__.py
@@ -0,0 +1,39 @@
+import pkg_resources
+
+from ._distribution import (
+    BinomialDistribution,
+    ExponentialDispersionModel,
+    GammaDistribution,
+    GeneralizedHyperbolicSecant,
+    InverseGaussianDistribution,
+    NormalDistribution,
+    PoissonDistribution,
+    TweedieDistribution,
+)
+from ._glm import GeneralizedLinearRegressor, get_family, get_link
+from ._glm_cv import GeneralizedLinearRegressorCV
+from ._link import IdentityLink, Link, LogLink, TweedieLink
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except Exception:
+    __version__ = "unknown"
+
+__all__ = [
+    "BinomialDistribution",
+    "ExponentialDispersionModel",
+    "GammaDistribution",
+    "GeneralizedHyperbolicSecant",
+    "InverseGaussianDistribution",
+    "NormalDistribution",
+    "PoissonDistribution",
+    "TweedieDistribution",
+    "IdentityLink",
+    "Link",
+    "LogLink",
+    "TweedieLink",
+    "GeneralizedLinearRegressor",
+    "GeneralizedLinearRegressorCV",
+    "get_family",
+    "get_link",
+]
