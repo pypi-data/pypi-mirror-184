33186a942419e5a8dbc6b7f4c3724e5c2502e39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:15 +0000

    Add package autotrain-advanced/0.4.0/autotrain-advanced-0.4.0.tar.gz

---
+++ packages/autotrain-advanced/autotrain-advanced-0.4.0.tar.gz/autotrain-advanced-0.4.0/src/autotrain/tasks.py
@@ -0,0 +1,11 @@
+TASKS = {
+    "binary_classification": 1,
+    "multi_class_classification": 2,
+    "entity_extraction": 4,
+    "extractive_question_answering": 5,
+    "summarization": 8,
+    "single_column_regression": 10,
+    "speech_recognition": 11,
+}
+
+DATASETS_TASKS = ["text-classification", "question-answering-extractive"]
