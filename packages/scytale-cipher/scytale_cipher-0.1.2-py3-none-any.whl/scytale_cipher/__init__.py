489442129bf8cb2d9a429cd595ed249f47299dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:32 +0000

    Add package scytale-cipher/0.1.2/scytale_cipher-0.1.2-py3-none-any.whl

---
+++ packages/scytale-cipher/scytale_cipher-0.1.2-py3-none-any.whl/scytale_cipher/__init__.py
@@ -0,0 +1,9 @@
+"""
+Scytale Cipher
+======================
+
+Encode or decode a string with the scytale cipher
+"""
+from .scytale import scytale_init, scytale_process, main_menu
+
+__all__ = ["scytale_init", "scytale_process", "main_menu"]
