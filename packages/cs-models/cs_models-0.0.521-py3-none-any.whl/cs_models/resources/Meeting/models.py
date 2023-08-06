69ed8a1215a57f95a9e5705485872776ae3eabf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:59 +0000

    Add package cs-models/0.0.521/cs_models-0.0.521-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.521-py3-none-any.whl/cs_models/resources/Meeting/models.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    Boolean,
+)
+
+from ...database import Base
+
+
+class MeetingModel(Base):
+    __tablename__ = "meetings"
+
+    id = Column(Integer, primary_key=True)
+    meeting_name = Column(String(191), nullable=False, index=True)
+    start_date = Column(DateTime, nullable=False)
+    end_date = Column(DateTime, nullable=False)
+    from_website = Column(Boolean, nullable=True)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
