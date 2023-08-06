672c0ee4126146a21b18d2e0523f30eb027bb511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:46 +0000

    Add package aivtu/2/aivtu-2-py3-none-any.whl

---
+++ packages/aivtu/aivtu-2-py3-none-any.whl/aivtu/main.py
@@ -0,0 +1,28 @@
+def p1():
+    with open('astar.py','r') as f:
+        print(f.read())
+
+def p2():
+    with open('aostar.py','r') as f:
+        print(f.read())
+def p3():
+    with open('ce.py','r') as f:
+        print(f.read())
+def p4():
+    with open('id3.py','r') as f:
+        print(f.read())
+def p5():
+    with open('knn.py','r') as f:
+        print(f.read())
+def p6():
+    with open('ann.py','r') as f:
+        print(f.read())
+def p7():
+    with open('nb.py','r') as f:
+        print(f.read())
+def p8():
+    with open('em.py','r') as f:
+        print(f.read())
+def p9():
+    with open('reg.py','r') as f:
+        print(f.read())
