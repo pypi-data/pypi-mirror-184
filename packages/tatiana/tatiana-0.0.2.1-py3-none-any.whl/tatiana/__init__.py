8a578f1fcca6a969af8a8feb164db00c1e2d492b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:10 +0000

    Add package tatiana/0.0.2.1/tatiana-0.0.2.1-py3-none-any.whl

---
+++ packages/tatiana/tatiana-0.0.2.1-py3-none-any.whl/tatiana/__init__.py
@@ -0,0 +1,9 @@
+"""
+.. include:: ../README.md
+"""
+from .world import World
+from .entity import Entity
+from . import systems
+from . import components
+from . import structures
+from . import typenames
