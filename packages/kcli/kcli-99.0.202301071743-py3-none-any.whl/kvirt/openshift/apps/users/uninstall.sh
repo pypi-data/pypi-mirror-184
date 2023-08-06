4b08935c7bed3a434278968f37da26356769318d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:49:01 +0000

    Add package kcli/99.0.202301071743/kcli-99.0.202301071743-py3-none-any.whl

---
+++ packages/kcli/kcli-99.0.202301071743-py3-none-any.whl/kvirt/openshift/apps/users/uninstall.sh
@@ -0,0 +1,7 @@
+NAMESPACE={{ namespace|default("clusters") if hypershift|default(False) else 'openshift-config' }}
+{% if hypershift|default(False) %}
+oc patch hc -n $NAMESPACE $CLUSTER -p='[{"op": "remove", "path": "/spec/identityProviders"}]' --type=json
+{% else %}
+oc patch oauth cluster -p='[{"op": "remove", "path": "/spec/identityProviders"}]' --type=json
+{% endif %}
+oc delete secret htpass-secret -n $NAMESPACE
