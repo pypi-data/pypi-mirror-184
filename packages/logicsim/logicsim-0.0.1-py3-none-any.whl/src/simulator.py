fda9438ba2e9e76c8a6d803e52c6d5ec57e8a24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:44 +0000

    Add package logicsim/0.0.1/logicsim-0.0.1-py3-none-any.whl

---
+++ packages/logicsim/logicsim-0.0.1-py3-none-any.whl/src/simulator.py
@@ -0,0 +1,34 @@
+import gateinterface
+import gateobjcreate
+
+class Simulator():
+    def __init__(self,  line: tuple) -> None:
+        self.line = line
+        self.gates = [gateobjcreate.GateObject(gate) for gate in self.line]
+    
+    def finder(self, target: str) -> object:
+        for gate in self.gates:
+            if target == gate.out[0]:
+                return gate
+
+    # simulates lines individually
+    def single(self) -> list:
+        for gate in self.gates:
+            if gate.in1.isalpha():
+                tar1 = gate.in1
+                out1 = self.finder(tar1)
+                g = gateinterface.Gates(out1)
+                gate.in1 = g.find_output()
+
+            if gate.in2.isalpha():
+                tar2 = gate.in2
+                out2 = self.finder(tar2)
+                g = gateinterface.Gates(out2)
+                gate.in2 = g.find_output()
+            
+            g = gateinterface.Gates(gate)
+            gate.out[1] = g.find_output()
+
+            #print(gate.type, gate.in1, gate.in2, gate.out)
+
+        return self.gates
