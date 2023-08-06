7844c2ba204a5710c2b67be718cfb7c21fe85b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:36 +0000

    Add package cypilot/0.0.1/cypilot-0.0.1.tar.gz

---
+++ packages/cypilot/cypilot-0.0.1.tar.gz/cypilot-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cypilot',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.netcys.com',
+    author='jft7',
+    author_email='jf@netcys.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
