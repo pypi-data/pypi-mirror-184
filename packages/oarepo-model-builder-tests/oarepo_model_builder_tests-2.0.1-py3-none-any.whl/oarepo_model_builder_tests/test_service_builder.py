84bff1b1017a74e17ce6275a4b69f3d444a2dbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:31:24 +0000

    Add package oarepo-model-builder-tests/2.0.1/oarepo_model_builder_tests-2.0.1-py3-none-any.whl

---
+++ packages/oarepo-model-builder-tests/oarepo_model_builder_tests-2.0.1-py3-none-any.whl/oarepo_model_builder_tests/test_service_builder.py
@@ -0,0 +1,15 @@
+from oarepo_model_builder.invenio.invenio_base import InvenioBaseClassPythonBuilder
+
+
+class TestServiceBuilder(InvenioBaseClassPythonBuilder):
+    TYPE = "test_service"
+    template = "test_service"
+    MODULE = "tests.test_service"
+
+    def finish(self, **extra_kwargs):
+        python_path = self.module_to_path(self.MODULE)
+        self.process_template(
+            python_path,
+            self.template,
+            **extra_kwargs,
+        )
