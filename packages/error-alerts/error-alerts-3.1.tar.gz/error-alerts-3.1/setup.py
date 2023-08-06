03ecb0ab2c651ae82f1411ab2a51a875bd4058f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:12 +0000

    Add package error-alerts/3.1/error-alerts-3.1.tar.gz

---
+++ packages/error-alerts/error-alerts-3.1.tar.gz/error-alerts-3.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'error-alerts',
+    packages = ['error_alerts'],
+    version = '3.1',
+    description = 'Error alerts via Telegram',
+    url = 'https://github.com/xjxckk/error-alerts/',
+    download_url = 'https://github.com/xjxckk/error-alerts/archive/refs/tags/v2.tar.gz',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['python-telegram-bot']
+    )
