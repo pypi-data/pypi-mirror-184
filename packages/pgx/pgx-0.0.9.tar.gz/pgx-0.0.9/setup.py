5aa84522f7d893576cac7e5e71cda5d3d1a4091f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:43 +0000

    Add package pgx/0.0.9/pgx-0.0.9.tar.gz

---
+++ packages/pgx/pgx-0.0.9.tar.gz/pgx-0.0.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pgx",
+    version="0.0.9",
+    long_description_content_type="text/markdown",
+    description="",
+    url="",
+    author="Sotetsu KOYAMADA",
+    author_email="sotetsu.koyamada@gmail.com",
+    keywords="",
+    packages=find_packages(),
+    install_requires=["jax", "flax", "svgwrite"],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+    ],
+)
