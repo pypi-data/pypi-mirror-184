5c2feb3ee2ab5b14c78f36a14ac5768dd9070861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:03 +0000

    Add package snow-pal/0.1.2/snow-pal-0.1.2.tar.gz

---
+++ packages/snow-pal/snow-pal-0.1.2.tar.gz/snow-pal-0.1.2/test.py
@@ -0,0 +1,9 @@
+import snowpal
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+teton = snowpal.palettes('tetons-at-twilight')
+print(teton)
+
+#sns.palplot(['#599BFF','#FFD400','#7C8485', '#2A5315', '#C6D1D8'])
+#plt.show()
