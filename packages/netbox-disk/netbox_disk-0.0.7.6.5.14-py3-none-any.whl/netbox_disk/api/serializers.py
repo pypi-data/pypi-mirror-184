c134a807f0e0c0df64abbc7ef0b04afaa3729b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:20 +0000

    Add package netbox-disk/0.0.7.6.5.14/netbox_disk-0.0.7.6.5.14-py3-none-any.whl

---
+++ packages/netbox-disk/netbox_disk-0.0.7.6.5.14-py3-none-any.whl/netbox_disk/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from netbox_disk.models import Disk
+
+
+class DiskSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Disk
+        fields = (
+            "id",
+            "vg_name",
+            "lv_name",
+            "size",
+            "path",
+            "description",
+            "created",
+            "last_updated",
+            "custom_fields",
+        )
