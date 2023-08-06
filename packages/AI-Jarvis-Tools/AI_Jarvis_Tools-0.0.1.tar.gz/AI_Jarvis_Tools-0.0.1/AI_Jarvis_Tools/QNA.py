fdf045a763f444be6cdec5ad9e2bdc76355cb666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:49 +0000

    Add package AI-Jarvis-Tools/0.0.1/AI_Jarvis_Tools-0.0.1.tar.gz

---
+++ packages/AI-Jarvis-Tools/AI_Jarvis_Tools-0.0.1.tar.gz/AI_Jarvis_Tools-0.0.1/AI_Jarvis_Tools/QNA.py
@@ -0,0 +1,68 @@
+Intro = '''
+Application Name:- Jarvis
+Developer Name:- RISHABH-SAHIL 
+'''
+Intro = '''
+Application Name:- Jarvis
+Developer Name:- RISHABH-SAHIL 
+'''
+
+# ----------------------Modules Name----------------------
+import openai
+from dotenv import load_dotenv
+
+def Questions_Answers(question,chat_log=None):
+    fileopen = open("Data\\Api.txt","r")
+    API = fileopen.read()
+    fileopen.close()
+    openai.api_key = API
+    load_dotenv()
+    completion = openai.Completion()
+
+    try:
+        FileLog = open("DataBase\\qna_log.txt","r")
+        chat_log_template = FileLog.read()
+        FileLog.close()
+
+        if chat_log is None:
+            chat_log = chat_log_template
+        
+        prompt = f'{chat_log} Question : {question}\nAnswer : '
+        response = completion.create(
+            model = "text-davinci-002",
+            prompt=prompt,
+            temperature = 0,
+            max_tokens = 100,
+            top_p = 1,
+            frequency_penalty = 0,
+            presence_penalty = 0)
+        answer = response.choices[0].text.strip()
+        chat_log_template_update = chat_log_template + f"\nQuestion : {question} \nAnswer : {answer}"
+        FileLog = open("DataBase\\qna_log.txt","w")
+        FileLog.write(chat_log_template_update)
+        FileLog.close()
+        return answer
+    except:
+        import os
+        # Create a folder
+        os.makedirs("DataBase")
+        os.makedirs("Data")
+        # Open a file
+        f = open("DataBase\\qna_log.txt", "w")
+        first='''Answer: Jarvis: My Name is Jarvis'''
+        # Write to the file
+        f.write(str(first))
+
+        # Close the file
+        f.close()
+        f = open("Data\\Api.txt", "w")
+        # Write to the file
+        f.write('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')
+
+        # Close the file
+        f.close()
+
+# while True:
+#     query = input(">> ")
+#     replay = Questions_Answers(query)
+#     print(replay)
