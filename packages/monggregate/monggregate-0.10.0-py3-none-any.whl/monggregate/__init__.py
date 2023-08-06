9860a403d2286ce28d0ccc8d01e5d1d88e8e1c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:03 +0000

    Add package monggregate/0.10.0/monggregate-0.10.0-py3-none-any.whl

---
+++ packages/monggregate/monggregate-0.10.0-py3-none-any.whl/monggregate/__init__.py
@@ -0,0 +1,11 @@
+"""App Package"""
+
+from monggregate.pipeline import Pipeline
+from monggregate.expressions.expressions import Expression
+
+# IMPORTANT : Please think about updated the pyproject.toml file when changing the version here.
+__version__ = "0.10.0"
+__author__ = "Vianney Mixtur"
+__contact__ = "vianney.mixtur@outlook.fr"
+__copyright__ = "Copyright © 2022 Vianney Mixtur"
+__license__ = "MIT"
