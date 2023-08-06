4923d0f077b01eb7b89381600c6a4877e4e50612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:44 +0000

    Add package langchain/0.0.57/langchain-0.0.57-py3-none-any.whl

---
+++ packages/langchain/langchain-0.0.57-py3-none-any.whl/langchain/schema.py
@@ -0,0 +1,36 @@
+"""Common schema objects."""
+
+from typing import List, NamedTuple, Optional
+
+
+class AgentAction(NamedTuple):
+    """Agent's action to take."""
+
+    tool: str
+    tool_input: str
+    log: str
+
+
+class AgentFinish(NamedTuple):
+    """Agent's return value."""
+
+    return_values: dict
+    log: str
+
+
+class Generation(NamedTuple):
+    """Output of a single generation."""
+
+    text: str
+    """Generated text output."""
+    # TODO: add log probs
+
+
+class LLMResult(NamedTuple):
+    """Class that contains all relevant information for an LLM Result."""
+
+    generations: List[List[Generation]]
+    """List of the things generated. This is List[List[]] because
+    each input could have multiple generations."""
+    llm_output: Optional[dict] = None
+    """For arbitrary LLM provider specific output."""
