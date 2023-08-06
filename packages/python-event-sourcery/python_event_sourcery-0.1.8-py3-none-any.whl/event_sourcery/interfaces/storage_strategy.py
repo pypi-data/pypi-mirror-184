0aa24d04be51d9151f79b98cbffb4ac5affe40c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:54:42 +0000

    Add package python-event-sourcery/0.1.8/python_event_sourcery-0.1.8-py3-none-any.whl

---
+++ packages/python-event-sourcery/python_event_sourcery-0.1.8-py3-none-any.whl/event_sourcery/interfaces/storage_strategy.py
@@ -0,0 +1,42 @@
+import abc
+from typing import Callable, Iterator
+
+from event_sourcery.dto import RawEvent
+from event_sourcery.types.stream_id import StreamId
+from event_sourcery.versioning import Versioning
+
+
+class StorageStrategy(abc.ABC):
+    @abc.abstractmethod
+    def fetch_events(
+        self, stream_id: StreamId, start: int | None = None, stop: int | None = None
+    ) -> list[RawEvent]:
+        pass
+
+    @abc.abstractmethod
+    def iter(self, *stream_ids: StreamId) -> Iterator[RawEvent]:
+        pass
+
+    @abc.abstractmethod
+    def insert_events(self, events: list[RawEvent]) -> None:
+        pass
+
+    @abc.abstractmethod
+    def save_snapshot(self, snapshot: RawEvent) -> None:
+        pass
+
+    @abc.abstractmethod
+    def ensure_stream(
+        self,
+        stream_id: StreamId,
+        versioning: Versioning,
+    ) -> None:
+        pass
+
+    @abc.abstractmethod
+    def delete_stream(self, stream_id: StreamId) -> None:
+        pass
+
+    @abc.abstractmethod
+    def run_after_commit(self, callback: Callable[[], None]) -> None:
+        pass
---
+++ packages/python-event-sourcery/python_event_sourcery-0.1.8-py3-none-any.whl/event_sourcery_sqlalchemy/models.py
@@ -0,0 +1,61 @@
+from typing import Type
+
+from sqlalchemy import BigInteger, Column, DateTime, Index, Integer, String
+from sqlalchemy.ext.declarative import instrument_declarative
+
+from event_sourcery_sqlalchemy.guid import GUID
+from event_sourcery_sqlalchemy.jsonb import JSONB
+
+
+def configure_models(base: Type) -> None:
+    for model_cls in (Stream, Event, Snapshot, OutboxEntry):
+        instrument_declarative(model_cls, {}, base.metadata)
+
+
+class Stream:
+    __tablename__ = "event_sourcery_streams"
+
+    uuid = Column(GUID(), primary_key=True)
+    version = Column(BigInteger(), nullable=True)
+
+
+class Event:
+    __tablename__ = "event_sourcery_events"
+    __table_args__ = (
+        Index(
+            "ix_events_stream_id_version",
+            "stream_id",
+            "version",
+            unique=True,
+        ),
+    )
+
+    id = Column(BigInteger().with_variant(Integer(), "sqlite"), primary_key=True)
+    version = Column(Integer(), nullable=True)
+    uuid = Column(GUID(), index=True, unique=True)
+    stream_id = Column(GUID(), nullable=False, index=True)
+    name = Column(String(200), nullable=False)
+    data = Column(JSONB(), nullable=False)
+    event_context = Column(JSONB(), nullable=False)
+    created_at = Column(DateTime(), nullable=False, index=True)
+
+
+class Snapshot:
+    __tablename__ = "event_sourcery_snapshots"
+
+    uuid = Column(GUID, primary_key=True)
+    version = Column(Integer(), nullable=False)
+    stream_id = Column(GUID(), nullable=False, index=True)
+    name = Column(String(50), nullable=False)
+    data = Column(JSONB(), nullable=False)
+    event_context = Column(JSONB(), nullable=False)
+    created_at = Column(DateTime(), nullable=False)
+
+
+class OutboxEntry:
+    __tablename__ = "event_sourcery_outbox_entries"
+
+    id = Column(BigInteger().with_variant(Integer(), "sqlite"), primary_key=True)
+    created_at = Column(DateTime(), nullable=False, index=True)
+    data = Column(JSONB(), nullable=False)
+    tries_left = Column(Integer(), nullable=False, server_default="3")
