0a6fd9434c2455fc0449fa66f95b8b89556429f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:50 +0000

    Add package mathkeyboardengine/0.1.0a5/mathkeyboardengine-0.1.0a5.tar.gz

---
+++ packages/mathkeyboardengine/mathkeyboardengine-0.1.0a5.tar.gz/mathkeyboardengine-0.1.0a5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+root_dir = path.abspath(path.dirname(__file__))
+with open(path.join(root_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="mathkeyboardengine",
+    version="0.1.0-alpha.5",
+    description="MathKeyboardEngine provides the logic for a highly customizable virtual math keyboard. It is intended for use together with any LaTeX typesetting library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MathKeyboardEngine/MathKeyboardEngine.Python",
+    author="MathKeyboardEngine",
+    author_email="symbolinker@gmail.com",
+    license="ISC",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    packages=['mathkeyboardengine'],
+    package_dir={'mathkeyboardengine':'src'}
+)
