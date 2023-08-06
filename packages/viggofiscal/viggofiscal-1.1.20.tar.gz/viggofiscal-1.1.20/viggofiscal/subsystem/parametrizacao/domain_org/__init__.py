e1a24d93a56d39f914fe1bed5d04dc409384a3d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:31 +0000

    Add package viggofiscal/1.1.20/viggofiscal-1.1.20.tar.gz

---
+++ packages/viggofiscal/viggofiscal-1.1.20.tar.gz/viggofiscal-1.1.20/viggofiscal/subsystem/parametrizacao/domain_org/__init__.py
@@ -0,0 +1,4 @@
+from viggocore.common import subsystem
+from viggofiscal.subsystem.parametrizacao.domain_org import resource
+
+subsystem = subsystem.Subsystem(resource=resource.DomainOrg)
---
+++ packages/viggofiscal/viggofiscal-1.1.20.tar.gz/viggofiscal-1.1.20/viggofiscal/subsystem/parametrizacao/tipo_operacao/__init__.py
@@ -0,0 +1,4 @@
+from viggocore.common import subsystem
+from viggofiscal.subsystem.parametrizacao.tipo_operacao import resource
+
+subsystem = subsystem.Subsystem(resource=resource.TipoOperacao)
