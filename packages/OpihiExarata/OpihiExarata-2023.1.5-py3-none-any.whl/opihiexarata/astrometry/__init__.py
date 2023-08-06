759ee285de8ea6653cdff80fbd7badff7a82c389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:40 +0000

    Add package OpihiExarata/2023.1.5/OpihiExarata-2023.1.5-py3-none-any.whl

---
+++ packages/OpihiExarata/OpihiExarata-2023.1.5-py3-none-any.whl/opihiexarata/astrometry/__init__.py
@@ -0,0 +1,5 @@
+# The solver itself.
+from opihiexarata.astrometry.solution import AstrometricSolution
+
+# The astrometric solver engines.
+from opihiexarata.astrometry.webclient import AstrometryNetWebAPIEngine
