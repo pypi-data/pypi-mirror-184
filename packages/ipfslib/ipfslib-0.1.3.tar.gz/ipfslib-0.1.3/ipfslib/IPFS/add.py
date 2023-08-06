4acf511ae1f432401291ac80ca14f47d6e5e2e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:55 +0000

    Add package ipfslib/0.1.3/ipfslib-0.1.3.tar.gz

---
+++ packages/ipfslib/ipfslib-0.1.3.tar.gz/ipfslib-0.1.3/ipfslib/IPFS/add.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+# Adds file to ipfs
+def add(api, filepath):
+    files = {
+        'file': open(filepath, 'r'),
+    }
+    response = requests.post('http://{endpoint}/api/v0/add'.format(endpoint=api.endpoint), files=files)
+    raw_json = response.text
+    try:
+        return json.loads(raw_json)["Hash"]
+    except KeyError:
+        raise Exception(response['Message'])
