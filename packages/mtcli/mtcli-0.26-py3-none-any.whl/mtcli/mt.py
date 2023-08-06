fba468879a07e4fd5b27e0be78544baf81a4268f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:19 +0000

    Add package mtcli/0.26/mtcli-0.26-py3-none-any.whl

---
+++ packages/mtcli/mtcli-0.26-py3-none-any.whl/mtcli/mt.py
@@ -0,0 +1,33 @@
+# mtcli
+# Copyright 2023 Valmir França da Silva
+# http://github.com/vfranca
+from mtcli import conf
+from mtcli import __version__
+import click
+
+# Importa os comandos
+from mtcli.bars import bars
+from mtcli.mm import mm
+from mtcli.rm import rm
+from mtcli.set import set
+
+
+# Cria o grupo de comandos mt
+@click.group(invoke_without_command=True)
+@click.option("--version", is_flag=True, help="Exibe a versao")
+def mt(version):
+    """Converte graficos do MetaTrader 5 para texto."""
+    if version:
+        click.echo("mtcli %s" % __version__)
+        return 0
+
+
+# Adiciona os comandos do mt
+mt.add_command(bars)
+mt.add_command(mm)
+mt.add_command(rm)
+mt.add_command(set)
+
+
+if __name__ == "__main__":
+    mt()
