f1c411e5d9a0c5135fe2dc71331b5a250be8dbbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:08 +0000

    Add package aivtu/3/aivtu-3.tar.gz

---
+++ packages/aivtu/aivtu-3.tar.gz/aivtu-3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+VERSION = '3'
+DESCRIPTION = 'AI/ML programs'
+LONG_DESCRIPTION = 'VTU lab programs.'
+
+# Setting up
+setup(
+    name="aivtu",
+    version=VERSION,
+    author="Sanjay R",
+    author_email="<sanjay66r@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['vtu'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
