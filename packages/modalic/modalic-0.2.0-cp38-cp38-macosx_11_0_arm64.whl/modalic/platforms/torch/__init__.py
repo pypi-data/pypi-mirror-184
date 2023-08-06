31ec2cf5dd682a42286322b4d86181f8973f17bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:50 +0000

    Add package modalic/0.2.0/modalic-0.2.0-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/modalic/modalic-0.2.0-cp38-cp38-macosx_11_0_arm64.whl/modalic/platforms/torch/__init__.py
@@ -0,0 +1,13 @@
+from .misc import (
+    deserialize_torch_model,
+    get_torch_model_dtype,
+    get_torch_model_shape,
+    serialize_torch_model,
+)
+
+__all__ = [
+    "deserialize_torch_model",
+    "get_torch_model_dtype",
+    "get_torch_model_shape",
+    "serialize_torch_model",
+]
