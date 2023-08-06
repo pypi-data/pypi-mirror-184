9898e956a2e7ba460e5b71565ddbfc159524ab74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:21 +0000

    Add package pyasd/0.0.1/pyasd-0.0.1.tar.gz

---
+++ packages/pyasd/pyasd-0.0.1.tar.gz/pyasd-0.0.1/asd/__init__.py
@@ -0,0 +1,6 @@
+__built_time__="built at Thu Jan  5 23:28:14 2023 CST"
+__name__='pyasd'
+__version__='1.0.0'
+__author__='Shunhong Zhang'
+__email__='szhang2@ustc.edu.cn'
+__url__='hppts://to_be_posted'
