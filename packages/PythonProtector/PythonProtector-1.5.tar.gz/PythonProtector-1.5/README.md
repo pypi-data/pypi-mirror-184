2a18d045d712ae4278141ad11bb10125bd5e46a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:48 +0000

    Add package PythonProtector/1.5/PythonProtector-1.5.tar.gz

---
+++ packages/PythonProtector/PythonProtector-1.5.tar.gz/PythonProtector-1.5/README.md
@@ -0,0 +1,26 @@
+# PythonProtector
+
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+
+A modern, easy to use and feature-rich way to protect your Python Programs.
+
+## Installation
+
+**Python 3.11 or higher is required**
+
+Install The PyPi Version:
+
+```sh
+py -3 -m pip install -U pyprotector
+```
+
+You may also install the development version:
+```sh
+pip install git+https://github.com/xFGhoul/PythonProtector.git
+```
+
+## Usage
+
+Check out the [examples](https://github.com/xFGhoul/PythonProtector/blob/dev/examples/)
+
+Made by `ghoul#0002` and `Marci#4399`
---
+++ packages/PythonProtector/PythonProtector-1.5.tar.gz/PythonProtector-1.5/setup.py
@@ -0,0 +1,53 @@
+"""
+    ____          ____                __               __
+   / __ \\ __  __ / __ \\ _____ ____   / /_ ___   _____ / /_
+  / /_/ // / / // /_/ // ___// __ \\ / __// _ \\ / ___// __/
+ / ____// /_/ // ____// /   / /_/ // /_ /  __// /__ / /_
+/_/     \\__, //_/    /_/    \\____/ \\__/ \\___/ \\___/ \\__/
+       /____/
+
+Made With ❤️ By Ghoul & Marci
+"""
+
+from distutils.core import setup
+
+with open("README.md") as readme_file:
+    README = readme_file.read()
+
+with open("HISTORY.md") as history_file:
+    HISTORY = history_file.read()
+
+setup(
+    name="PythonProtector",
+    packages=["pyprotector"],
+    version="1.5",
+    license="MIT",
+    description="Library for protecting your python files",
+    author="Ghoul & Marci",
+    url="https://github.com/xFGhoul/PythonProtecttor",
+    long_description_content_type="text/markdown",
+    long_description=README + "\n\n" + HISTORY,
+    keywords=["keyauth", "protection", "protect", "obfuscate", "obfuscation", "WMI", "windows"],
+    install_requires=[
+        "humanize",
+        "loguru",
+        "discord-webhook",
+        "py-cpuinfo",
+        "command_runner",
+        "psutil",
+        "httpx",
+        "WMI",
+        "pywin32",
+        "numpy",
+        "pyautogui",
+        "opencv-python",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
