66a8da6ed357f1dbdb8cbfb26bd32f63093038a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:16 +0000

    Add package colemen-silent-engine/0.0.0/colemen_silent_engine-0.0.0.tar.gz

---
+++ packages/colemen-silent-engine/colemen_silent_engine-0.0.0.tar.gz/colemen_silent_engine-0.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import build_utils as _bu
+PY_MODULES = _bu.list_py_modules()
+
+VERSION = '0.0.0'
+DESCRIPTION = 'colemen_silentEngine'
+LONG_DESCRIPTION = 'colemen_silentEngine'
+
+# Setting up
+setup(
+    name="colemen_silent_engine",
+    version=VERSION,
+    author="Colemen Atwood",
+    author_email="<atwoodcolemen@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    py_modules=['main'] + PY_MODULES,
+    # add any additional packages that
+    # need to be installed along with your package. Eg: 'caer'
+    install_requires=[
+        'colemen_utils',
+    ],
+
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
