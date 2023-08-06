128026d319636a1e3656e97a41234f0a3eef8dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:00 +0000

    Add package recap-core/0.2.0/recap_core-0.2.0-py3-none-any.whl

---
+++ packages/recap-core/recap_core-0.2.0-py3-none-any.whl/recap/plugins/catalogs/recap.py
@@ -0,0 +1,67 @@
+import httpx
+from .abstract import AbstractCatalog
+from contextlib import contextmanager
+from pathlib import PurePosixPath
+from recap.plugins.commands.serve import DEFAULT_URL
+from typing import Any, List, Generator
+
+
+class RecapCatalog(AbstractCatalog):
+    def __init__(
+        self,
+        client: httpx.Client,
+    ):
+        self.client = client
+
+    def touch(
+        self,
+        path: PurePosixPath,
+    ):
+        self.client.put(str(path))
+
+    def write(
+        self,
+        path: PurePosixPath,
+        type: str,
+        metadata: Any,
+    ):
+        params = {'type': type} if type else None
+        self.client.put(str(path), params=params, json=metadata)
+
+    def rm(
+        self,
+        path: PurePosixPath,
+        type: str | None = None,
+    ):
+        params = {'type': type} if type else None
+        self.client.delete(str(path), params=params)
+
+    def ls(
+        self,
+        path: PurePosixPath,
+    ) -> List[str] | None:
+        return self.client.get(str(path)).json()
+
+    def read(
+        self,
+        path: PurePosixPath,
+    ) -> dict[str, Any] | None:
+        return self.client.get(str(path), params={'read': True}).json()
+
+    def search(
+        self,
+        query: str,
+    ) -> List[dict[str, Any]]:
+        return self.client.get('/search', params={'query': query}).json()
+
+    @staticmethod
+    def openable(url: str) -> bool:
+        from urllib.parse import urlparse
+        return urlparse(url).scheme.split('+')[0] == 'file'
+
+    @staticmethod
+    @contextmanager
+    def open(**config) -> Generator['RecapCatalog', None, None]:
+        url = config.get('url', DEFAULT_URL)
+        with httpx.Client(base_url=url) as client:
+            yield RecapCatalog(client)
