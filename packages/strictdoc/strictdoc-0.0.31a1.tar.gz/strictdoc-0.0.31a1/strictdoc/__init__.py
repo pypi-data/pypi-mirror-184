fad9fa3aab3d6f14907fd7a787c6312ebd3cd587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:35 +0000

    Add package strictdoc/0.0.31a1/strictdoc-0.0.31a1.tar.gz

---
+++ packages/strictdoc/strictdoc-0.0.31a1.tar.gz/strictdoc-0.0.31a1/strictdoc/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+__version__ = "0.0.31-alpha.1"
+
+STRICTDOC_ROOT_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..")
+)
+assert os.path.isabs(STRICTDOC_ROOT_PATH), f"{STRICTDOC_ROOT_PATH}"
