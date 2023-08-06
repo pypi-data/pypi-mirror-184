34baa95384b4b3dee9b493a2f98e45b22a0361fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:36 +0000

    Add package graphene-protector/0.7.0/graphene_protector-0.7.0.tar.gz

---
+++ packages/graphene-protector/graphene_protector-0.7.0.tar.gz/graphene_protector-0.7.0/graphene_protector/base.py
@@ -0,0 +1,314 @@
+import re
+import sys
+from dataclasses import dataclass, fields, replace
+from functools import wraps
+from typing import List, Union
+
+from graphql.error import GraphQLError
+from graphql.execution import ExecutionResult
+from graphql.language import (
+    DefinitionNode,
+    FragmentSpreadNode,
+    OperationDefinitionNode,
+    parse,
+)
+from graphql.validation import ValidationRule, validate
+
+
+def get_optype(schema, definition):
+    operation_type = definition.operation.name.title()
+    return schema.get_type(operation_type)
+
+
+# Adapted from this response in Stackoverflow
+# http://stackoverflow.com/a/19053800/1072990
+def to_camel_case(snake_str):
+    components = snake_str.split("_")
+    # We capitalize the first letter of each component except the first one
+    # with the 'capitalize' method and join them together.
+    return components[0] + "".join(
+        x.capitalize() if x else "_" for x in components[1:]
+    )
+
+
+# From this response in Stackoverflow
+# http://stackoverflow.com/a/1176023/1072990
+def to_snake_case(name):
+    s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
+    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
+
+
+class MISSING:
+    """custom MISSING sentinel for merge logic"""
+
+
+_deco_options = {}
+if sys.version_info >= (3, 10):
+    _deco_options["slots"] = True
+
+if sys.version_info >= (3, 11):
+    _deco_options["weakref_slot"] = True
+
+
+@dataclass(**_deco_options)
+class Limits:
+    depth: Union[int, None, MISSING] = MISSING
+    selections: Union[int, None, MISSING] = MISSING
+    complexity: Union[int, None, MISSING] = MISSING
+
+    def __call__(self, field):
+        setattr(field, "_graphene_protector_limits", self)
+        return field
+
+
+_missing_limits = Limits()
+DEFAULT_LIMITS = Limits(depth=20, selections=None, complexity=100)
+
+
+class ResourceLimitReached(GraphQLError):
+    pass
+
+
+class DepthLimitReached(ResourceLimitReached):
+    pass
+
+
+class SelectionsLimitReached(ResourceLimitReached):
+    pass
+
+
+class ComplexityLimitReached(ResourceLimitReached):
+    pass
+
+
+def merge_limits(old_limits, new_limits):
+    _limits = {}
+    for field in fields(new_limits):
+        value = getattr(new_limits, field.name)
+        if value != MISSING:
+            _limits[field.name] = value
+    return replace(old_limits, **_limits)
+
+
+def limits_for_field(field, old_limits, **kwargs):
+    # retrieve optional limitation attributes defined for the current
+    # operation
+    effective_limits = getattr(
+        field,
+        "_graphene_protector_limits",
+        _missing_limits,
+    )
+    return merge_limits(old_limits, effective_limits)
+
+
+def check_resource_usage(
+    schema,
+    node,
+    validation_context,
+    limits,
+    on_error,
+    auto_snakecase=False,
+    get_limits_for_field=limits_for_field,
+    level=0,
+):
+    # level 0: starts on query level. Every query is level 1
+    selections = 0
+    max_depth = level
+    assert (
+        limits.depth is not MISSING
+    ), "missing should be already resolved here"
+    if limits.depth and max_depth > limits.depth:
+        on_error(
+            DepthLimitReached(
+                "Query is too deep",
+            )
+        )
+    for field_orig in node.selection_set.selections:
+        fieldname = field_orig.name.value
+        # ignore introspection queries
+        if fieldname.startswith("__"):
+            continue
+        if auto_snakecase and not hasattr(schema, fieldname):
+            fieldname = to_snake_case(fieldname)
+        if isinstance(field_orig, FragmentSpreadNode):
+            field = validation_context.getFragment(field_orig.name.value)
+        else:
+            field = field_orig
+        if field.selection_set:
+            try:
+                schema_field = getattr(schema, fieldname)
+            except AttributeError:
+                schema_field = schema
+                if hasattr(schema_field, "of_type"):
+                    schema_field = schema_field.of_type
+
+                if hasattr(schema_field, "fields"):
+                    schema_field = schema_field.fields[fieldname]
+            sub_limits = get_limits_for_field(
+                schema_field, limits, parent=schema, fieldname=fieldname
+            )
+            new_depth, local_selections = check_resource_usage(
+                schema_field.type,
+                field,
+                validation_context,
+                sub_limits,
+                on_error=on_error,
+                auto_snakecase=auto_snakecase,
+                get_limits_for_field=get_limits_for_field,
+                level=level + 1,
+            )
+            # called per query, selection
+            if (
+                sub_limits.complexity
+                and (new_depth - level) * local_selections
+                > sub_limits.complexity
+            ):
+                on_error(ComplexityLimitReached("Query is too complex", node))
+            # ignore selection_set fields because we have depth for that
+            selections += local_selections
+            if new_depth > max_depth:
+                max_depth = new_depth
+        else:
+            selections += 1
+
+        if limits.selections and selections > limits.selections:
+            on_error(SelectionsLimitReached("Query selects too much", node))
+    return max_depth, selections
+
+
+class LimitsValidationRule(ValidationRule):
+    default_limits = None
+    # TODO: find out if auto_camelcase is set
+    # But no priority as this code works also
+    auto_snakecase = True
+
+    def __init__(self, context):
+        super().__init__(context)
+        schema = self.context.schema
+        # if not set use schema to get defaults or set in case no limits
+        # are found to DEFAULT:LIMITS
+        if not self.default_limits:
+            self.default_limits = getattr(
+                schema, "get_default_limits", lambda: DEFAULT_LIMITS
+            )()
+
+    def enter(self, node, key, parent, path, ancestors):
+        if parent is not None:
+            return None
+        schema = self.context.schema
+
+        document: List[DefinitionNode] = self.context.document
+        for definition in document.definitions:
+            if not isinstance(definition, OperationDefinitionNode):
+                continue
+            maintype = get_optype(schema, definition)
+            get_limits_for_field = limits_for_field
+            if hasattr(maintype, "graphene_type"):
+                maintype = maintype.graphene_type
+            if hasattr(schema, "_strawberry_schema"):
+
+                def get_limits_for_field(
+                    field, old_limits, parent, fieldname, **kwargs
+                ):
+                    try:
+                        name = parent.name
+                    except AttributeError:
+                        name = parent.of_type.name
+                    definition = (
+                        schema._strawberry_schema.schema_converter.type_map[
+                            name
+                        ]
+                    ).definition
+                    nfield = definition.get_field(fieldname)
+                    return limits_for_field(nfield, old_limits)
+
+            check_resource_usage(
+                maintype,
+                definition,
+                self.context,
+                self.default_limits,
+                self.report_error,
+                get_limits_for_field=get_limits_for_field,
+                auto_snakecase=self.auto_snakecase,
+            )
+
+    if not hasattr(ValidationRule, "report_error"):
+
+        def report_error(self, error):
+            self.context.report_error(error)
+
+
+_rules = [LimitsValidationRule]
+
+
+def _decorate_limits_helper(superself, args, kwargs):
+    check_limits = kwargs.pop("check_limits", True)
+    if check_limits and (kwargs.get("query") or len(args)):
+        try:
+            query = kwargs.get("query", args[0])
+        except IndexError:
+            pass
+        if query:
+            try:
+                document_ast = parse(query)
+            except GraphQLError as error:
+                return ExecutionResult(data=None, errors=[error])
+            if hasattr(superself, "graphql_schema"):
+                schema = getattr(superself, "graphql_schema")
+            elif hasattr(superself, "_schema"):
+                schema = getattr(superself, "_schema")
+            else:
+                schema = superself
+            schema.get_default_limits = superself.get_default_limits
+
+            return validate(
+                schema,
+                document_ast,
+                _rules,
+            )
+    return []
+
+
+def decorate_limits(fn):
+    @wraps(fn)
+    def wrapper(superself, *args, **kwargs):
+        validation_errors = _decorate_limits_helper(superself, args, kwargs)
+        if validation_errors:
+            return ExecutionResult(errors=validation_errors)
+        return fn(superself, *args, **kwargs)
+
+    return wrapper
+
+
+def decorate_limits_async(fn):
+    @wraps(fn)
+    async def wrapper(superself, *args, **kwargs):
+        validation_errors = _decorate_limits_helper(superself, args, kwargs)
+        if validation_errors:
+            return ExecutionResult(errors=validation_errors)
+        return await fn(superself, *args, **kwargs)
+
+    return wrapper
+
+
+class SchemaMixin:
+    default_limits = None
+
+    def __init_subclass__(cls, **kwargs):
+        if hasattr(cls, "execute_sync"):
+            cls.execute_sync = decorate_limits(cls.execute_sync)
+            if hasattr(cls, "execute"):
+                cls.execute = decorate_limits_async(cls.execute)
+        else:
+            if hasattr(cls, "execute"):
+                cls.execute = decorate_limits(cls.execute)
+            if hasattr(cls, "execute_async"):
+                cls.execute_async = decorate_limits_async(cls.execute_async)
+        if hasattr(cls, "subscribe"):
+            cls.subscribe = decorate_limits_async(cls.subscribe)
+
+    def get_default_limits(self):
+        return merge_limits(
+            DEFAULT_LIMITS,
+            self.default_limits,
+        )
---
+++ packages/graphene-protector/graphene_protector-0.7.0.tar.gz/graphene_protector-0.7.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['graphene_protector', 'graphene_protector.django']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['graphql-core>=3']
+
+extras_require = \
+{'optional': ['graphene>=3',
+              'graphene-django>=3',
+              'strawberry-graphql>=0.92',
+              'strawberry-graphql-django']}
+
+setup_kwargs = {
+    'name': 'graphene-protector',
+    'version': '0.7.0',
+    'description': 'Protects graphene, graphql or strawberry against malicious queries',
+    'long_description': 'None',
+    'author': 'alex',
+    'author_email': 'devkral@web.de',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/devkral/graphene-protector',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.8,<4',
+}
+
+
+setup(**setup_kwargs)
