64199659b677b460d1a4325531f135595171669b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:12 +0000

    Add package quantalpy/0.1.0/quantalpy-0.1.0-py3-none-any.whl

---
+++ packages/quantalpy/quantalpy-0.1.0-py3-none-any.whl/quantalpy/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.1.0"
+
+from quantalpy.qpu import QPU
+from quantalpy.unary_qubit_gate import UnaryQubitGate, Hadamard, PhaseShift
+from quantalpy.binary_qubit_gate import BinaryQubitGate, ControlledQubitGate
+from quantalpy.circuit import Circuit
+from quantalpy.qft import qft
---
+++ packages/quantalpy/quantalpy-0.1.0-py3-none-any.whl/quantalpy/unary_qubit_gate.py
@@ -0,0 +1,26 @@
+import numpy as np
+import numpy.typing as npt
+
+from quantalpy.runnable import Runnable
+from quantalpy.qpu import QPU
+
+
+class UnaryQubitGate(Runnable):
+    def __init__(self, matrix: npt.ArrayLike, index: int) -> None:
+        self.matrix = np.asarray(matrix, dtype=complex)
+        self.index = index
+
+    def run(self, qpu: QPU) -> None:
+        qpu.apply_unary_qubit_operator(index=self.index, matrix=self.matrix)
+
+
+class Hadamard(UnaryQubitGate):
+    def __init__(self, index: int) -> None:
+        had = np.array([[1, 1], [1, -1]]) / np.sqrt(2)
+        super().__init__(matrix=had, index=index)
+
+
+class PhaseShift(UnaryQubitGate):
+    def __init__(self, phi: float, index: int) -> None:
+        had = np.array([[1, 0], [0, np.exp(1j * phi)]])
+        super().__init__(matrix=had, index=index)
