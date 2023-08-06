ac2db37626a24863ec2c03e688f2d4d997fb807b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:25 +0000

    Add package kfsdutils/0.0.24/kfsdutils-0.0.24.tar.gz

---
+++ packages/kfsdutils/kfsdutils-0.0.24.tar.gz/kfsdutils-0.0.24/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='kfsdutils',
+      version='0.0.24',
+      description='Sample Pkg',
+      long_description='Sample Pkg',
+      long_description_content_type="text/markdown",
+      author='nathangokul',
+      author_email="nathangokul111@gmail.com",
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[
+            'netifaces',
+            'PyYAML'
+      ]
+)
