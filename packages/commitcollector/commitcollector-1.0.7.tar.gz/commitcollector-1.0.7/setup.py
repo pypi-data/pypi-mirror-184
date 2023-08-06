012b4b179ce4ee70e23ffa7adfdc38eb3efecb57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:41 +0000

    Add package commitcollector/1.0.7/commitcollector-1.0.7.tar.gz

---
+++ packages/commitcollector/commitcollector-1.0.7.tar.gz/commitcollector-1.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='commitcollector',
+    version='1.0.7',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    py_modules=['commitcollector'],
+    entry_points={
+        'console_scripts': [
+            'commitcollector = commitcollector:main'
+        ]
+    })
