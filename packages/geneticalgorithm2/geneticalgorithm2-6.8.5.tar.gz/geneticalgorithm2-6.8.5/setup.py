362b20c2d6f245f42f66f3a4dd00d693f0dc94db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:35 +0000

    Add package geneticalgorithm2/6.8.5/geneticalgorithm2-6.8.5.tar.gz

---
+++ packages/geneticalgorithm2/geneticalgorithm2-6.8.5.tar.gz/geneticalgorithm2-6.8.5/setup.py
@@ -0,0 +1,38 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="geneticalgorithm2", 
+    version="6.8.5",
+    author="Demetry Pascal",
+    author_email="qtckpuhdsa@gmail.com",
+    maintainer='Demetry Pascal',
+    description="Supported highly optimized and flexible genetic algorithm package for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PasaOpasen/geneticalgorithm2",
+    license='MIT',
+    keywords=['solve', 'optimization', 'problem', 'genetic', 'algorithm', 'GA', 'easy', 'fast', 'genetic-algorithm', 'combinatorial', 'mixed', 'evolutionary'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'typing_extensions',
+        'OppOpPopInit>=2.0.1'
+    ]
+    
+    )
+
+
+
+
+
