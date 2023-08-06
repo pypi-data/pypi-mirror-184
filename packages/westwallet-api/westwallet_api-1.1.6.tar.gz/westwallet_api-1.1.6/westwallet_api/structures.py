149db0456c384902594f61d694492f437439ee0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:33 +0000

    Add package westwallet-api/1.1.6/westwallet_api-1.1.6.tar.gz

---
+++ packages/westwallet-api/westwallet_api-1.1.6.tar.gz/westwallet_api-1.1.6/westwallet_api/structures.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class Balance:
+    balance: str
+    currency: str
+
+
+@dataclass
+class Transaction:
+    id: int
+    amount: str
+    address: str
+    dest_tag: str
+    currency: str
+    status: str
+    blockchain_hash: str
+    fee: str
+    type: Optional[str] = "send"
+    blockchain_confirmations: Optional[int] = 0
+
+
+@dataclass
+class GenerateAddressResponse:
+    address: str
+    dest_tag: str
+    currency: str
+    label: str
