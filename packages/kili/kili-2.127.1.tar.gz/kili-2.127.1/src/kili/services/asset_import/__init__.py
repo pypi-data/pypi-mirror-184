e803a302adc1c12030bda5c0bd2cc3ed524ec171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:19 +0000

    Add package kili/2.127.1/kili-2.127.1.tar.gz

---
+++ packages/kili/kili-2.127.1.tar.gz/kili-2.127.1/src/kili/services/asset_import/__init__.py
@@ -0,0 +1,48 @@
+"""Service for importing objects into kili"""
+
+from typing import Dict, List
+
+from kili.authentication import KiliAuth
+from kili.constants import NO_ACCESS_RIGHT
+from kili.queries.project import QueriesProject
+from kili.services.asset_import.image import ImageDataImporter
+from kili.services.asset_import.pdf import PdfDataImporter
+from kili.services.asset_import.text import TextDataImporter
+from kili.services.asset_import.video import VideoDataImporter
+
+from .base import LoggerParams, ProcessingParams, ProjectParams
+
+importer_by_type = {
+    "PDF": PdfDataImporter,
+    "IMAGE": ImageDataImporter,
+    "TEXT": TextDataImporter,
+    "VIDEO": VideoDataImporter,
+    "VIDEO_LEGACY": VideoDataImporter,
+}
+
+
+def import_assets(
+    auth: KiliAuth,
+    project_id: str,
+    assets: List[Dict],
+    raise_error=True,
+    disable_tqdm=False,
+):
+    """
+    import the selected assets into the specified project
+    """
+    kili = QueriesProject(auth)
+    projects = list(kili.projects(project_id, disable_tqdm=True))
+    assert len(projects) == 1, NO_ACCESS_RIGHT
+    input_type = projects[0]["inputType"]
+
+    project_params = ProjectParams(project_id=project_id, input_type=input_type)
+    processing_params = ProcessingParams(raise_error=raise_error)
+    logger_params = LoggerParams(disable_tqdm=disable_tqdm)
+    importer_params = (auth, project_params, processing_params, logger_params)
+
+    if input_type not in importer_by_type:
+        raise NotImplementedError(f"There is no imported for the input type: {input_type}")
+    asset_importer = importer_by_type[input_type](*importer_params)
+    result = asset_importer.import_assets(assets=assets)  # type:ignore X
+    return result
