3fcc576314f68678871cc7697a52b9a12ec9db66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:44 +0000

    Add package ct-util-tools/0.1.0/ct_util_tools-0.1.0.tar.gz

---
+++ packages/ct-util-tools/ct_util_tools-0.1.0.tar.gz/ct_util_tools-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ct_util']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.24.1,<2.0.0', 'pandas>=1.5.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'ct-util-tools',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Chris Tran',
+    'author_email': 'chris.l.tran.2016@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
