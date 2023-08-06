21fb1f3c20903bb9913c2fffc08e4ca416fbb9b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:29:09 +0000

    Add package anantmishra/1.0/anantmishra-1.0.tar.gz

---
+++ packages/anantmishra/anantmishra-1.0.tar.gz/anantmishra-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='anantmishra',
+    version='1.0',
+    py_modules=['anant'],
+    install_requires=[],
+    author='Anant Mishra',
+    author_email='gioneemaxuser@gmail.com',
+    description='A simple package for doing calculations',
+)
