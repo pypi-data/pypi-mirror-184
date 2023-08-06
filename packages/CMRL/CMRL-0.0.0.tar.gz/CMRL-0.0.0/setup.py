200e8b442e82a5687c6c8a2387c8843448ade098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:21 +0000

    Add package CMRL/0.0.0/CMRL-0.0.0.tar.gz

---
+++ packages/CMRL/CMRL-0.0.0.tar.gz/CMRL-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='CMRL',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='Rodrigo de Lazcano Perez-Vicente',
+    author_email='r.l.p.v96@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
+
