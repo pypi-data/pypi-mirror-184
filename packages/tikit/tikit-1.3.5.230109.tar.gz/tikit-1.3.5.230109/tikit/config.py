4698ecce8a182a9663188f13df9461ba9c04b129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:11 +0000

    Add package tikit/1.3.5.230109/tikit-1.3.5.230109.tar.gz

---
+++ packages/tikit/tikit-1.3.5.230109.tar.gz/tikit-1.3.5.230109/tikit/config.py
@@ -0,0 +1,31 @@
+EMR_SITE_TEMPLATE = {
+    "hive-site.xml": {
+        "hive.metastore.uris": None,
+        "hive.metastore.warehouse.dir": None,
+        "hive.metastore.kerberos.principal": None,
+        "hive.metastore.sasl.enabled": None,
+        "hive.server2.authentication": None,
+    },
+    "core-site.xml": {
+        "hadoop.security.authentication": None,
+        "fs.cosn.impl": None,
+        "fs.cosn.local_block_size": None,
+        "fs.cosn.userinfo.secretId": None,
+        "fs.cosn.upload.buffer.size": None,
+        "fs.cosn.userinfo.secretKey": None,
+        "fs.cosn.block.size": None,
+        "fs.cosn.read.ahead.block.size": None,
+        "fs.cosn.emrv2.instance.provider.enabled": None,
+        "fs.cosn.userinfo.region": None,
+        "fs.cos.local_block_size": None,
+        "fs.cos.userinfo.useCDN": None,
+        "fs.cos.userinfo.region": None,
+        "fs.cos.buffer.dir": None,
+        "fs.cos.userinfo.appid": None,
+    },
+    "yarn-site.xml": {
+        "yarn.nodemanager.principal": None,
+        "yarn.resourcemanager.principal": None,
+        "yarn.timeline-service.principal": None,
+    }
+}
