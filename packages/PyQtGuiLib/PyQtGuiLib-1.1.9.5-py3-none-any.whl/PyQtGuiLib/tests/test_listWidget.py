e9f3a59e78434730098b8cf60504e3bc744b1afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:55 +0000

    Add package PyQtGuiLib/1.1.9.5/PyQtGuiLib-1.1.9.5-py3-none-any.whl

---
+++ packages/PyQtGuiLib/PyQtGuiLib-1.1.9.5-py3-none-any.whl/PyQtGuiLib/tests/test_listWidget.py
@@ -0,0 +1,41 @@
+from PyQtGuiLib.header import (
+    PYQT_VERSIONS,
+    sys,
+    QApplication,
+    QWidget,
+    QThread,
+    Signal,
+    QColor,
+    QSlider,
+    QListWidgetItem
+)
+
+from PyQtGuiLib.core import ListWidget
+from PyQtGuiLib.styles import ButtonStyle
+
+
+class TestListWidget(QWidget):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.resize(800,600)
+
+        self.listw = ListWidget(self)
+        self.listw.resize(200,300)
+        self.listw.move(100,100)
+        # self.listw.itemDoubleClicked.connect(lambda :print("asd"))
+
+        for i in range(5):
+            widget = QWidget()
+            widget.setFixedHeight(30)
+            widget.setStyleSheet(ButtonStyle.randomStyle())
+            self.listw.addWidget(widget)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = TestListWidget()
+    win.show()
+
+    if PYQT_VERSIONS in ["PyQt6", "PySide6"]:
+        sys.exit(app.exec())
+    else:
+        sys.exit(app.exec_())
