11841c1117ef04703ca8a6091c3cf4e4d6c6d96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:00 +0000

    Add package mofid-parsivar/0.2.4/mofid-parsivar-0.2.4.tar.gz

---
+++ packages/mofid-parsivar/mofid-parsivar-0.2.4.tar.gz/mofid-parsivar-0.2.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='mofid-parsivar',
+      packages=['parsivar'],
+      version='0.2.4',
+      description='Python library for preprocessing Persian text.',
+      author='ICT',
+      author_email='',
+      url='',
+      download_url='',
+      install_requires=['nltk==3.5'],
+      python_requires=">=3.10",
+      classifiers=[],
+      include_package_data=True
+      )
