57d396a103b335a0eeecf18c041b773c72b31d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:29:20 +0000

    Add package pr-stats/0.0.3/pr_stats-0.0.3.tar.gz

---
+++ packages/pr-stats/pr_stats-0.0.3.tar.gz/pr_stats-0.0.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name='pr_stats',
+    version='0.0.3',
+    author="Kazi Amit Hasan",
+    author_email="kaziamithasan89@gmail.com",
+    description="This module brings different stats about pull requests using GitHub API.",
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AmitHasanShuvo/PR-stats",
+    keywords=['stats','pull requests','github'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
