ddff8e249bf7ea8f9572d9a3992a3613d860dad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:05 +0000

    Add package cppython-core/0.6.1.dev30/cppython-core-0.6.1.dev30.tar.gz

---
+++ packages/cppython-core/cppython-core-0.6.1.dev30.tar.gz/cppython-core-0.6.1.dev30/cppython_core/plugin_schema/generator.py
@@ -0,0 +1,40 @@
+"""Generator data plugin definitions"""
+from abc import abstractmethod
+from typing import Any, Generic, TypeVar
+
+from pydantic import Field
+from pydantic.types import DirectoryPath
+
+from cppython_core.schema import DataPlugin, PluginGroupData, SyncDataT
+
+
+class GeneratorGroupData(PluginGroupData):
+    """Base class for the configuration data that is set by the project for the generator"""
+
+    root_directory: DirectoryPath = Field(description="The directory where the pyproject.toml lives")
+
+
+class Generator(DataPlugin[GeneratorGroupData], Generic[SyncDataT]):
+    """Abstract type to be inherited by CPPython Generator plugins"""
+
+    @staticmethod
+    @abstractmethod
+    def sync_data_type() -> type[SyncDataT]:
+        """The sync data type to use for data transfer
+
+        Returns:
+            The data type
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def sync(self, sync_data: list[SyncDataT]) -> None:
+        """Synchronizes generator files and state with the providers input
+
+        Args:
+            sync_data: List of information gathered from providers
+        """
+        raise NotImplementedError()
+
+
+GeneratorT = TypeVar("GeneratorT", bound=Generator[Any])
