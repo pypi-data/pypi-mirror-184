eae305ddc40f284a59a9577d67ab4949c35be3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:19 +0000

    Add package ogarantia-streamlit-card/0.0.2/ogarantia_streamlit_card-0.0.2-py3-none-any.whl

---
+++ packages/ogarantia-streamlit-card/ogarantia_streamlit_card-0.0.2-py3-none-any.whl/ogarantia_streamlit_card/frontend/build/precache-manifest.71d4704b12d9470ef28318c05dc865f0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "93ce1d698bc885238021",
+    "url": "./static/js/main.3864f6db.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f8bb4ec0d3ae75a429d2",
+    "url": "./static/js/2.8229b76d.chunk.js"
+  },
+  {
+    "revision": "762efad5beec937fe8bd71c6bab02181",
+    "url": "./index.html"
+  }
+];
