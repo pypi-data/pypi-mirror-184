924dc21d901a44a93166f5af1fd858f1d8519ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:26 +0000

    Add package thunno/1.2.0/thunno-1.2.0.tar.gz

---
+++ packages/thunno/thunno-1.2.0.tar.gz/thunno-1.2.0/thunno/main.py
@@ -0,0 +1,58 @@
+from thunno import flags
+from thunno import run
+import sys
+
+
+THUNNO_VERSION = "1.2.0"
+
+
+def execute(code, inputs, flags_list):
+    inputs, stack = flags.handle_input_flags(inputs, flags_list)
+    stack, vars, _ = run.run(code, inputs, stack)
+    flags.handle_output_flags(stack, vars, flags_list)
+
+
+def from_filename(filename, flags_list):
+    sys.stderr.write('Thunno, v' + THUNNO_VERSION)
+    try:
+        with open(filename) as f:
+            execute(f.read(), sys.stdin.read(), ''.join(flags_list))
+    except Exception as E:
+        sys.stderr.write('An error occurred -' + repr(E))
+
+
+def not_from_filename():
+    print('Thunno Interpreter (v' + THUNNO_VERSION + ')')
+    print('\nFlags:')
+    flags_list = input()
+    code = ''
+    print('\nHeader:')
+    inp = input()
+    while inp:
+        code += inp + '\n'
+        inp = input()
+    print('\nCode:')
+    inp = input()
+    while inp:
+        code += inp + '\n'
+        inp = input()
+    print('\nFooter:')
+    inp = input()
+    while inp:
+        code += inp + '\n'
+        inp = input()
+    inputs = ''
+    print('\nInput:')
+    inp = input()
+    while inp:
+        inputs += inp + '\n'
+        inp = input()
+    print('\nOutput:')
+    execute(code, inputs, flags_list)
+
+def cmdline():
+    args = sys.argv[1:]
+    if not args:
+        not_from_filename()
+    else:
+        from_filename(args[0], args[1:])
