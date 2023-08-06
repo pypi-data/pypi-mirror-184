992bbe9dc974ad41ce6c2d1a3bf41cdcf010fbaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:09 +0000

    Add package python-incidentio-client/0.25.0/python_incidentio_client-0.25.0-py3-none-any.whl

---
+++ packages/python-incidentio-client/python_incidentio_client-0.25.0-py3-none-any.whl/incident_io_client/models/incident_attachments_v1_create_request_body_resource_resource_type.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class IncidentAttachmentsV1CreateRequestBodyResourceResourceType(str, Enum):
+    PAGER_DUTY_INCIDENT = "pager_duty_incident"
+    GITHUB_PULL_REQUEST = "github_pull_request"
+    SENTRY_ISSUE = "sentry_issue"
+    STATUSPAGE_INCIDENT = "statuspage_incident"
+    ZENDESK_TICKET = "zendesk_ticket"
+
+    def __str__(self) -> str:
+        return str(self.value)
