4f29236a5c6c67d7aedb866a59512ab24e4d68f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:55 +0000

    Add package gaussian-binomial-dists-fp/0.1/gaussian_binomial_dists_fp-0.1.tar.gz

---
+++ packages/gaussian-binomial-dists-fp/gaussian_binomial_dists_fp-0.1.tar.gz/gaussian_binomial_dists_fp-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_dists_fp',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      author = 'Florian Perras',
+      author_email = 'Flo92-Perras@web.de',
+      packages=['gaussian_binomial_dists_fp'],
+      zip_safe=False)
