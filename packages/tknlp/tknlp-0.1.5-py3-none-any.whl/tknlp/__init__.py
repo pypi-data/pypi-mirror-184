1b7f01997e2847d813318f4900dc03d8ba796f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:23 +0000

    Add package tknlp/0.1.5/tknlp-0.1.5-py3-none-any.whl

---
+++ packages/tknlp/tknlp-0.1.5-py3-none-any.whl/tknlp/__init__.py
@@ -0,0 +1,9 @@
+from .utils import one_time, find_dotenv, get_dir, ojoin, oexists
+from .cli import Args
+from .contribute import color_str, pickle_dump, pickle_load, get_num_gpu
+from dotenv import load_dotenv
+
+@one_time
+def load_env():
+    load_dotenv(find_dotenv(get_dir()))
+load_env()
