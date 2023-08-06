db90ca989d1aded054ca4daca76a1fc85356bd61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:42 +0000

    Add package mathkeyboardengine/0.1.0a12/mathkeyboardengine-0.1.0a12.tar.gz

---
+++ packages/mathkeyboardengine/mathkeyboardengine-0.1.0a12.tar.gz/mathkeyboardengine-0.1.0a12/mathkeyboardengine/encapsulateAllPartsOfNumberWithDigitsLeftOfIndex.py
@@ -0,0 +1,14 @@
+from typing import List
+from .TreeNode import TreeNode
+from .PartOfNumberWithDigits import PartOfNumberWithDigits
+from .Placeholder import Placeholder
+
+def encapsulateAllPartsOfNumberWithDigitsLeftOfIndex(exclusiveRightIndex: int, siblingNodes: List[TreeNode], toPlaceholder: Placeholder) -> None:
+  for i in range(exclusiveRightIndex - 1, -1, -1):
+    siblingNode = siblingNodes[i]
+    if isinstance(siblingNode, PartOfNumberWithDigits):
+      siblingNodes.remove(siblingNode)
+      toPlaceholder.nodes.insert(0, siblingNode)
+      siblingNode.parentPlaceholder = toPlaceholder
+    else:
+      break
---
+++ packages/mathkeyboardengine/mathkeyboardengine-0.1.0a12.tar.gz/mathkeyboardengine-0.1.0a12/setup.py
@@ -0,0 +1,58 @@
+import shutil
+from setuptools import setup
+from codecs import open
+from os import path
+import glob
+import os
+collected_modules = []
+temp_dist = 'mathkeyboardengine'
+if os.path.isdir(temp_dist):
+    shutil.rmtree(temp_dist)
+os.mkdir(temp_dist)
+for file in glob.glob('src/**/*.py', recursive=True):
+    fileName = os.path.basename(file)
+    if (fileName != '__init__.py'):
+        with open(file, encoding='utf-8') as srcFile:
+            collected_modules.append(fileName.split('.')[0])
+            with open(path.join(temp_dist, fileName), "x", encoding='utf-8') as distFile:
+                lines = srcFile.read().splitlines()
+                for line in lines:
+                    if (line.startswith('from src')):
+                        modules = line.split(' import ')[-1].split(', ')
+                        for module in modules:
+                            distFile.write('from .' + module + ' import ' + module + '\n')
+                    else:
+                        distFile.write(line + '\n')
+
+with open(path.join(temp_dist, '__init__.py'), 'x') as init:
+    init.writelines([('from .' + m + ' import ' + m + '\n') for m in collected_modules])
+
+root_dir = path.abspath(path.dirname(__file__))
+with open(path.join(root_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="mathkeyboardengine",
+    version="0.1.0-alpha.12",
+    description="MathKeyboardEngine provides the logic for a highly customizable virtual math keyboard. It is intended for use together with any LaTeX typesetting library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MathKeyboardEngine/MathKeyboardEngine.Python",
+    author="MathKeyboardEngine",
+    author_email="symbolinker@gmail.com",
+    license="ISC",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent"
+    ],
+    packages=["mathkeyboardengine"]
+)
