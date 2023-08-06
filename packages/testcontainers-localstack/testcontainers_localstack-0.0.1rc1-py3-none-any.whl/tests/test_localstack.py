73871ca8b7f2233f9a07a10f85c9667c0a693e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:41 +0000

    Add package testcontainers-localstack/0.0.1rc1/testcontainers_localstack-0.0.1rc1-py3-none-any.whl

---
+++ packages/testcontainers-localstack/testcontainers_localstack-0.0.1rc1-py3-none-any.whl/tests/test_localstack.py
@@ -0,0 +1,15 @@
+import json
+import urllib
+
+from testcontainers.localstack import LocalStackContainer
+
+
+def test_docker_run_localstack():
+    with LocalStackContainer() as localstack:
+        resp = urllib.request.urlopen('{}/health'.format(localstack.get_url()))
+        services = json.loads(resp.read().decode())['services']
+
+        # Check that all services are running
+        assert all(value == 'running' for value in services.values())
+        # Check that some of the services keys
+        assert all(test_service in services.keys() for test_service in ['dynamodb', 'sns', 'sqs'])
