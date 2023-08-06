44c1338a4218e372b1df709ee12d57b0b780a769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:12 +0000

    Add package nonebot-plugin-ayaka-test/0.0.1/nonebot_plugin_ayaka_test-0.0.1.tar.gz

---
+++ packages/nonebot-plugin-ayaka-test/nonebot_plugin_ayaka_test-0.0.1.tar.gz/nonebot_plugin_ayaka_test-0.0.1/__init__.py
@@ -0,0 +1,3 @@
+from . import _cmd as __cmd
+from . import api as __api
+from .clean import clean_port
---
+++ packages/nonebot-plugin-ayaka-test/nonebot_plugin_ayaka_test-0.0.1.tar.gz/nonebot_plugin_ayaka_test-0.0.1/setup.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': '..'}
+
+packages = \
+['ayaka_test', 'ayaka_test.old']
+
+package_data = \
+{'': ['*'],
+ 'ayaka_test': ['.git/*',
+                '.git/hooks/*',
+                '.git/info/*',
+                '.git/logs/*',
+                '.git/logs/refs/heads/*',
+                '.git/logs/refs/remotes/origin/*',
+                '.git/objects/05/*',
+                '.git/objects/06/*',
+                '.git/objects/08/*',
+                '.git/objects/0f/*',
+                '.git/objects/10/*',
+                '.git/objects/18/*',
+                '.git/objects/21/*',
+                '.git/objects/29/*',
+                '.git/objects/2e/*',
+                '.git/objects/30/*',
+                '.git/objects/36/*',
+                '.git/objects/3b/*',
+                '.git/objects/41/*',
+                '.git/objects/46/*',
+                '.git/objects/4a/*',
+                '.git/objects/4b/*',
+                '.git/objects/52/*',
+                '.git/objects/59/*',
+                '.git/objects/66/*',
+                '.git/objects/6a/*',
+                '.git/objects/6c/*',
+                '.git/objects/9b/*',
+                '.git/objects/9d/*',
+                '.git/objects/9f/*',
+                '.git/objects/ac/*',
+                '.git/objects/af/*',
+                '.git/objects/b4/*',
+                '.git/objects/bf/*',
+                '.git/objects/cd/*',
+                '.git/objects/d0/*',
+                '.git/objects/db/*',
+                '.git/objects/e3/*',
+                '.git/objects/e9/*',
+                '.git/objects/eb/*',
+                '.git/objects/ed/*',
+                '.git/objects/f6/*',
+                '.git/objects/f9/*',
+                '.git/refs/heads/*',
+                '.git/refs/remotes/origin/*',
+                'dist/*']}
+
+install_requires = \
+['nonebot2>=2.0.0b5,<3.0.0']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-ayaka-test',
+    'version': '0.0.1',
+    'description': 'ayaka_test',
+    'long_description': '<div align="center">\n\n# ayaka_test - 0.0.1\n\n</div>\n\n## 文档\n\nhttps://bridgel.github.io/ayaka_doc/latest/develop/test\n',
+    'author': 'Su',
+    'author_email': 'wxlxy316@163.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/bridgeL/nonebot-plugin-ayaka-test',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
