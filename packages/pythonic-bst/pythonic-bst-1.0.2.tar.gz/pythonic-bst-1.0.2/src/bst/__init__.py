42443019dc033bc2aa4539eb9ef7c385d80baf51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:15 +0000

    Add package pythonic-bst/1.0.2/pythonic-bst-1.0.2.tar.gz

---
+++ packages/pythonic-bst/pythonic-bst-1.0.2.tar.gz/pythonic-bst-1.0.2/src/bst/__init__.py
@@ -0,0 +1,11 @@
+# ___  _   _ ___ _  _ ____ _  _ _ ____    ___  ____ ___
+# |__]  \_/   |  |__| |  | |\ | | |       |__] [__   |
+# |      |    |  |  | |__| | \| | |___    |__] ___]  |
+# <=<=<<https://github.com/squillero/pythonic-bst>>=>=>
+
+# Copyright 2022 Giovanni Squillero.
+# SPDX-License-Identifier: 0BSD
+
+__version__ = '1.0.2'
+
+from .core import BST
