6a2a2cbab68b76a528d826461f35c0057729571a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:28 +0000

    Add package nonebot-plugin-ayaka/1.0.2/nonebot_plugin_ayaka-1.0.2-py3-none-any.whl

---
+++ packages/nonebot-plugin-ayaka/nonebot_plugin_ayaka-1.0.2-py3-none-any.whl/ayaka/__init__.py
@@ -0,0 +1,16 @@
+'''当前版本 1.0.2
+
+https://bridgel.github.io/ayaka_doc/1.0.2/
+'''
+from .init import DONT_USE_ME as __DONT_USE_ME
+        
+# ---- ayaka box ----
+from .box import AyakaBox
+from .config import AyakaConfig
+from .helpers import Timer, get_user, do_nothing, singleton, run_in_startup, slow_load_config, safe_open_file, load_data_from_file
+
+# ---- 未来将被sqlmodel取代 ----
+from .orm import AyakaDB, AyakaGroupDB, AyakaUserDB
+
+# ---- 懒人包 ----
+from .lazy import *
