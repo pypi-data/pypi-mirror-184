9db15098a005872dd43cd36af9a4821075123d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:03 +0000

    Add package tensor-theorem-prover/0.12.0/tensor_theorem_prover-0.12.0-py3-none-any.whl

---
+++ packages/tensor-theorem-prover/tensor_theorem_prover-0.12.0-py3-none-any.whl/tensor_theorem_prover/prover/ProofStats.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+
+@dataclass
+class ProofStats:
+    """Stats on how complex a proof was to compute"""
+
+    attempted_unifications: int = 0
+    successful_unifications: int = 0
+    similarity_comparisons: int = 0
+    similarity_cache_hits: int = 0
+    attempted_resolutions: int = 0
+    successful_resolutions: int = 0
+    max_resolvent_width_seen: int = 0
+    max_depth_seen: int = 0
+    discarded_proofs: int = 0
+    resolvent_checks: int = 0
+    resolvent_check_hits: int = 0
