308d1c30cf68fc42388a20beb0fc3f79b5da5ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:07 +0000

    Add package django-xss-protector/1.0.3/django-xss-protector-1.0.3.tar.gz

---
+++ packages/django-xss-protector/django-xss-protector-1.0.3.tar.gz/django-xss-protector-1.0.3/src/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="django-xss-protector",  # should be unique in pypi packages
+    version="1.0.3",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    # these packages will be excluded
+    packages=setuptools.find_packages(exclude=["tests", "data", "test"])
+)
