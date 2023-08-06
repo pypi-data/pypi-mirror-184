26fccc15b6716b1b0e3b5602c1d93d3bb197b2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:49:27 +0000

    Add package colorful-terminal/0.0.3/colorful_terminal-0.0.3.tar.gz

---
+++ packages/colorful-terminal/colorful_terminal-0.0.3.tar.gz/colorful_terminal-0.0.3/colorful_terminal/__init__.py
@@ -0,0 +1,15 @@
+from .definitions import (
+    Fore,
+    Back,
+    Style,
+    TermAct,
+    colored_print,
+    demo_print,
+    foreground_demo,
+    background_demo,
+    style_demo,
+    rgb_demo,
+    rainbow_demo,
+)
+
+__version__ = "0.0.3"
