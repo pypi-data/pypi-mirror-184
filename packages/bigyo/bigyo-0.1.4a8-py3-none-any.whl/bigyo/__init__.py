173c20a054ad3d191392f8e4be65c3dbdeeca319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:12 +0000

    Add package bigyo/0.1.4a8/bigyo-0.1.4a8-py3-none-any.whl

---
+++ packages/bigyo/bigyo-0.1.4a8-py3-none-any.whl/bigyo/__init__.py
@@ -0,0 +1,18 @@
+"""
+bigyo
+=====
+
+Bigyo (비교(KR): comparison) is Python library for side-by-side diff in terminal.
+
+use difflib as its engine.
+
+Supports beautiful output with multi-width or zero-width character.
+
+:date: 2022-12-21
+:Author: LegenDUST
+:Version: 0.1.4a8
+"""
+
+from bigyo._bigyo import Bigyo
+
+__all__ = ['Bigyo']
