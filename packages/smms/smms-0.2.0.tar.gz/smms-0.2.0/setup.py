a815d5aa4bf3e17cb4294e88d5c7c44fec7aa311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:34 +0000

    Add package smms/0.2.0/smms-0.2.0.tar.gz

---
+++ packages/smms/smms-0.2.0.tar.gz/smms-0.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='smms',
+    version='0.2.0',
+    packages=['smms'],
+    url='https://github.com/CNSeniorious000/SMMS4py',
+    license='MIT License',
+    author='Muspi Merol',
+    author_email='wjj123345567@iCloud.com',
+    description='unofficial python SDK for SM.MS',
+    long_description=open("readme.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    install_requires=["httpx[http2]", "orjson", "pydantic"]
+)
---
+++ packages/smms/smms-0.2.0.tar.gz/smms-0.2.0/smms/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.2.0"
+
+from .user import User
+
+__all__ = ["User"]
