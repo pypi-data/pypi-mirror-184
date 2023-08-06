40f8e98193c30a2fb7a0a858981470d2d5b2fae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:34 +0000

    Add package curcheck/0.0/curcheck-0.0.tar.gz

---
+++ packages/curcheck/curcheck-0.0.tar.gz/curcheck-0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='curcheck',
+    version='0.0',
+    description='Library for parsing SPA and MPA sites',
+    packages=['curcheck'],
+    author_email='Khamdbulat@yandex.ru',
+    zip_safe=False
+)
