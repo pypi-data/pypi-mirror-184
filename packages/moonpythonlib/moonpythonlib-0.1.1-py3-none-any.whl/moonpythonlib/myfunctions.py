51582f1e271a68de7982daf35b84528a9b8e9584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:03 +0000

    Add package moonpythonlib/0.1.1/moonpythonlib-0.1.1-py3-none-any.whl

---
+++ packages/moonpythonlib/moonpythonlib-0.1.1-py3-none-any.whl/moonpythonlib/myfunctions.py
@@ -0,0 +1,7 @@
+from math import radians, cos, sin, asin, sqrt
+
+print("ImporteD!")
+
+def haversine(lon1: float, lat1: float, lon2: float, lat2: float) -> float:
+    print("Evil haversine!")
+    return 1
