e724da31c02eb2fc07c9335e3e75f4e30c779bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:44 +0000

    Add package airflow-provider-hex/0.1.8/airflow-provider-hex-0.1.8.tar.gz

---
+++ packages/airflow-provider-hex/airflow-provider-hex-0.1.8.tar.gz/airflow-provider-hex-0.1.8/airflow_provider_hex/types.py
@@ -0,0 +1,26 @@
+from typing_extensions import TypedDict
+
+RunResponse = TypedDict(
+    "RunResponse",
+    {
+        "projectId": str,
+        "runId": str,
+        "runUrl": str,
+        "runStatusUrl": str,
+        "traceId": str,
+    },
+)
+
+StatusResponse = TypedDict(
+    "StatusResponse",
+    {
+        "projectId": str,
+        "runId": str,
+        "runUrl": str,
+        "status": str,
+        "startTime": str,
+        "endTime": str,
+        "elapsedTime": float,
+        "traceId": str,
+    },
+)
