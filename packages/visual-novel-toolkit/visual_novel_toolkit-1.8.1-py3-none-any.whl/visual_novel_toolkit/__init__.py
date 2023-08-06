93d89dabb800842fa949de5bd48a2080438acb47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:26 +0000

    Add package visual-novel-toolkit/1.8.1/visual_novel_toolkit-1.8.1-py3-none-any.whl

---
+++ packages/visual-novel-toolkit/visual_novel_toolkit-1.8.1-py3-none-any.whl/visual_novel_toolkit/__init__.py
@@ -0,0 +1,7 @@
+"""A set of useful tools to improve DX of visual novel games."""
+from visual_novel_toolkit.cli import cli
+from visual_novel_toolkit.types import Report
+from visual_novel_toolkit.types import YASpeller
+
+
+__all__ = ("cli", "Report", "YASpeller")
