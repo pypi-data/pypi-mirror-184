e5b24dc12e0b533eedc5fd9c1b4b3edf4b7b50c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:46 +0000

    Add package PyFunceble-dev/4.2.0a5/PyFunceble_dev-4.2.0a5-py3-none-any.whl

---
+++ packages/PyFunceble-dev/PyFunceble_dev-4.2.0a5-py3-none-any.whl/PyFunceble/converter/url2netloc.py
@@ -0,0 +1,119 @@
+"""
+The tool to check the availability or syntax of domain, IP or URL.
+
+::
+
+
+    ██████╗ ██╗   ██╗███████╗██╗   ██╗███╗   ██╗ ██████╗███████╗██████╗ ██╗     ███████╗
+    ██╔══██╗╚██╗ ██╔╝██╔════╝██║   ██║████╗  ██║██╔════╝██╔════╝██╔══██╗██║     ██╔════╝
+    ██████╔╝ ╚████╔╝ █████╗  ██║   ██║██╔██╗ ██║██║     █████╗  ██████╔╝██║     █████╗
+    ██╔═══╝   ╚██╔╝  ██╔══╝  ██║   ██║██║╚██╗██║██║     ██╔══╝  ██╔══██╗██║     ██╔══╝
+    ██║        ██║   ██║     ╚██████╔╝██║ ╚████║╚██████╗███████╗██████╔╝███████╗███████╗
+    ╚═╝        ╚═╝   ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚══════╝╚═════╝ ╚══════╝╚══════╝
+
+Provides a way to convert/extract the network location of a given URL.
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+Special thanks:
+    https://pyfunceble.github.io/#/special-thanks
+
+Contributors:
+    https://pyfunceble.github.io/#/contributors
+
+Project link:
+    https://github.com/funilrys/PyFunceble
+
+Project documentation:
+    https://pyfunceble.readthedocs.io/en/dev/
+
+Project homepage:
+    https://pyfunceble.github.io/
+
+License:
+::
+
+
+    Copyright 2017, 2018, 2019, 2020, 2022 Nissar Chababy
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+import urllib.parse
+from typing import Any, Optional
+
+from PyFunceble.converter.base import ConverterBase
+
+
+class Url2Netloc(ConverterBase):
+    """
+    Provides the interface for the conversion/extration of the network location
+    of a given URL.
+    """
+
+    parsed_url: Optional[urllib.parse.ParseResult] = None
+    """
+    Expose the parsed URL.
+    """
+
+    @ConverterBase.data_to_convert.setter
+    def data_to_convert(self, value: Any) -> None:
+        """
+        Overrites the default behavior.
+
+        :raise TypeError:
+            When the given data to convert is not :py:class:`str`
+        :raise ValueError:
+            When the given data to convert is empty.
+        """
+
+        if not isinstance(value, str):
+            raise TypeError(f"<value> should be {str}, {type(value)} given.")
+
+        if not value:
+            raise ValueError("<value> should not be empty.")
+
+        # pylint: disable=no-member
+        super(Url2Netloc, self.__class__).data_to_convert.fset(self, value)
+
+    def parse_url(self) -> "Url2Netloc":
+        """
+        Parses the URL.
+        """
+
+        if self.data_to_convert:
+            self.parsed_url = urllib.parse.urlparse(self.data_to_convert)
+        return self
+
+    def get_converted(self) -> str:
+        """
+        Provides the converted data (after conversion)
+        """
+
+        self.parse_url()
+
+        if not self.parsed_url.netloc and self.parsed_url.path:
+            netloc = self.parsed_url.path
+        elif self.parsed_url.netloc:
+            netloc = self.parsed_url.netloc
+        else:  # pragma: no cover ## Safety
+            netloc = self.data_to_convert
+
+        if "//" in netloc:
+            netloc = netloc[netloc.find("//") + 2 :]
+
+        if "/" in netloc:
+            netloc = netloc[: netloc.find("/")]
+
+        return netloc
