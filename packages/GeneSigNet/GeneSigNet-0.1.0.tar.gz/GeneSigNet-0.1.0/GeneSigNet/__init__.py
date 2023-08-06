75f773ed9de77b0b8ce70c2a87b2dfa433e813ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:32 +0000

    Add package GeneSigNet/0.1.0/GeneSigNet-0.1.0.tar.gz

---
+++ packages/GeneSigNet/GeneSigNet-0.1.0.tar.gz/GeneSigNet-0.1.0/GeneSigNet/__init__.py
@@ -0,0 +1,1 @@
+from .GeneSigNet import WeightMatrix
---
+++ packages/GeneSigNet/GeneSigNet-0.1.0.tar.gz/GeneSigNet-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='GeneSigNet', 
+version='0.1.0', 
+description = 'Construct Gene-Signature Network',
+url='https://github.com/ncbi/GeneSigNet',
+author='Bayarbaatar Amgalan',
+author_email = 'bayarbaatar.amgalan@nih.gov',
+packages=['GeneSigNet'],
+zip_safe=False)
