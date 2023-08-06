6e128d5bcd95d028b0dc5b45a99a04c6855073d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:52 +0000

    Add package pywapor/3.2.2/pywapor-3.2.2.tar.gz

---
+++ packages/pywapor/pywapor-3.2.2.tar.gz/pywapor-3.2.2/pywapor/collect/product/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Laust Færch
+Module: Collect/PROBAV
+
+Description:
+This module preprocesses Landsat(7+8) data downloaded externaly
+
+"""
+from pywapor.collect.product import Landsat, CHIRPS, ERA5, GEOS5, GLOBCOVER, MERRA2, MODIS, PROBAV, SRTM, STATICS, SENTINEL2, SENTINEL3, VIIRSL1, COPERNICUS
+
+__all__ = ["Landsat", "CHIRPS", "ERA5", "GEOS5", "GLOBCOVER", "MERRA2", "MODIS", "PROBAV", "SRTM", "STATICS", "SENTINEL2", "SENTINEL3", "VIIRSL1", "COPERNICUS"]
+
+__version__ = '0.1'
