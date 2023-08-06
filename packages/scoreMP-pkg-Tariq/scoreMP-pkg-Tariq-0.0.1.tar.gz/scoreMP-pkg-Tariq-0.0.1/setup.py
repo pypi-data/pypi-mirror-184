b896189c8dca57296bbe163aa19c965b10d9ab2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:02 +0000

    Add package scoreMP-pkg-Tariq/0.0.1/scoreMP-pkg-Tariq-0.0.1.tar.gz

---
+++ packages/scoreMP-pkg-Tariq/scoreMP-pkg-Tariq-0.0.1.tar.gz/scoreMP-pkg-Tariq-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="scoreMP-pkg-Tariq",
+    version="0.0.1",
+    author="Tariq_azd",
+    author_email="tarikazdoud00@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+    "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+)
