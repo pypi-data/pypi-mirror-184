6dbe995af3015bcadcd290569bed515b41789ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:12 +0000

    Add package shiboken6/6.4.2/shiboken6-6.4.2-pp39-pypy39_pp73-manylinux_2_28_x86_64.whl

---
+++ packages/shiboken6/shiboken6-6.4.2-pp39-pypy39_pp73-manylinux_2_28_x86_64.whl/shiboken6/_config.py
@@ -0,0 +1,11 @@
+shiboken_library_soversion = str(6.4)
+
+version = "6.4.2"
+version_info = (6, 4, 2, "", "")
+
+__build_date__ = '2023-01-04T12:05:06+00:00'
+
+
+
+
+__setup_py_package_version__ = '6.4.2'
