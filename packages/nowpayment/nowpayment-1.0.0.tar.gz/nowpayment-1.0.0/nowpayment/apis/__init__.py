c4f330dc7af8cea1f22b9bd7bc40c0e4b3305186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:25 +0000

    Add package nowpayment/1.0.0/nowpayment-1.0.0.tar.gz

---
+++ packages/nowpayment/nowpayment-1.0.0.tar.gz/nowpayment-1.0.0/nowpayment/apis/__init__.py
@@ -0,0 +1,38 @@
+import requests
+
+
+class BaseAPI:
+    """Base API class.
+
+    Args:
+        api_key (str): API key.
+    """
+
+    def __init__(self, api_key: str, jwt_token: str = None):
+        self.api_key = api_key
+        self.jwt_token = jwt_token
+
+    def _request(self, method: str, url: str, **kwargs):
+        """Make request to API.
+
+        Args:
+            method (str): HTTP method.
+            url (str): URL of API.
+            **kwargs: Keyword arguments.
+
+        Returns:
+            dict: Response of API.
+        """
+        headers: dict = {
+            'Content-Type': 'application/json',
+            'x-api-key': self.api_key
+        }
+        if self.jwt_token:
+            headers['Authorization'] = self.jwt_token
+
+        return requests.request(
+            method,
+            url,
+            headers=headers,
+            **kwargs
+        ).json()
