1c43c7e3bde8e3b6dedc2a6ce9ed3b96ca658baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:47 +0000

    Add package db2qthelp/0.2/db2qthelp-0.2-py3-none-any.whl

---
+++ packages/db2qthelp/db2qthelp-0.2-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,15 @@
+# ===================================================================
+# db2qthelp - a DocBook book to QtHelp project converter.
+# Version 0.2.
+#
+# Tests module initialisation
+#
+# (c) Daniel Krajzewicz 2022-2023
+# - daniel@krajzewicz.de
+# - http://www.krajzewicz.de
+# - https://github.com/dkrajzew/db2qthelp
+# - http://www.krajzewicz.de/blog/db2qthelp.php
+# 
+# Available under the BSD license.
+# ===================================================================
+
