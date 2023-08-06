a33421bc2f64fb2a367524c411a38ea93d6c74ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:41 +0000

    Add package pil-plus/0.1/pil_plus-0.1.tar.gz

---
+++ packages/pil-plus/pil_plus-0.1.tar.gz/pil_plus-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pil_plus',
+    version='0.1',
+    license='APACHE-2.0',
+    author="Fahad Maqsood Qazi",
+    author_email='qazifahadmaqsood@gmail.com',
+    py_modules=['pil_plus'],
+    url='https://github.com/fahadmaqsood/pil_plus',
+    keywords=['PIL', 'image processing', 'PIL wrapper', 'easy PIL', 'pillow'],
+    install_requires=[
+          'opencv-python',
+          'pillow',
+          'matplotlib',
+          'numpy',
+          'backgroundremover'
+      ],
+)
