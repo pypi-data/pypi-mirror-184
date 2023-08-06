8055fe67b5440c2a990bd10085ae7b8ee5182739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:00 +0000

    Add package caddo-file-parser/0.20.1/caddo_file_parser-0.20.1-py3-none-any.whl

---
+++ packages/caddo-file-parser/caddo_file_parser-0.20.1-py3-none-any.whl/caddo_file_parser/settings/settings_loader.py
@@ -0,0 +1,20 @@
+from caddo_file_parser.settings.settings import Settings
+
+
+class SettingsLoader:
+    def load_settings_object(self, settings_file):
+        settings_data: Settings = Settings()
+        settings_data.data_source_path = settings_file['data_source']['path']
+        settings_data.data_source_separator = settings_file['data_source']['separator']
+        settings_data.data_source_x_cols = settings_file['data_source']['x_columns_names']
+        settings_data.data_source_y_cols = settings_file['data_source']['y_columns_names']
+        settings_data.extraction_data_module = settings_file['extraction_data']['file_name_without_extension']
+        settings_data.percent_of_training_data = settings_file['model']['percent_of_training_data']
+        settings_data.number_of_folds = settings_file['model']['number_of_folds']
+        settings_data.runs = settings_file['model']['runs']
+        settings_data.output_file_name = settings_file['output']['file_name']
+        settings_data.output_separator = settings_file['output']['separator']
+        settings_data.folding_method = settings_file['folding']['method']
+        settings_data.seeds_path = settings_file['folding']['seeds']['path']
+        settings_data.seeds = settings_file['folding']['seeds']['write_directly']
+        return settings_data
