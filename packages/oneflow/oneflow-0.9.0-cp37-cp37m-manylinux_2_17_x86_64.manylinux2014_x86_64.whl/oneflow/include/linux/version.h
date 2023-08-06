e6efb5a0fbed560228b97c4e197d25c3c4545081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:12 +0000

    Add package oneflow/0.9.0/oneflow-0.9.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/oneflow/oneflow-0.9.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/oneflow/include/linux/version.h
@@ -0,0 +1,8 @@
+#define LINUX_VERSION_CODE 199168
+#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
+#define RHEL_MAJOR 7
+#define RHEL_MINOR 9
+#define RHEL_RELEASE_VERSION(a,b) (((a) << 8) + (b))
+#define RHEL_RELEASE_CODE 1801
+#define RHEL_RELEASE "1160.81.1"
+#define KERNEL_HEADERS_CHECKSUM "42432c26a896b0eb1b573f853dbb1d3660133094"
