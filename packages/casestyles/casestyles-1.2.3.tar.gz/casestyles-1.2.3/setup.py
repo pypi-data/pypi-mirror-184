005cb62b0e5aa6438123a34cc4c898053f70ee57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:48:56 +0000

    Add package casestyles/1.2.3/casestyles-1.2.3.tar.gz

---
+++ packages/casestyles/casestyles-1.2.3.tar.gz/casestyles-1.2.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().split('\n')
+
+setuptools.setup(
+	name="casestyles",
+	version="1.2.3",
+	author="Alexey-Chebotarev",
+	description="A package for converting case styles to each other",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/Chebotarev-Alexey/CaseStyles",
+	packages=setuptools.find_packages(),
+	install_requires=requirements,
+	classifiers=[
+        "Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
