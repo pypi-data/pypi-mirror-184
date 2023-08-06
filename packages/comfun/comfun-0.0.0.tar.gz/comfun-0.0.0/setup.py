328f752a6f10e87ea91a15f057ee1b2f4d85a40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:16 +0000

    Add package comfun/0.0.0/comfun-0.0.0.tar.gz

---
+++ packages/comfun/comfun-0.0.0.tar.gz/comfun-0.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='comfun',
+    version='',
+    packages=[
+        'comfun',
+    ],
+    url='',
+    license='',
+    author='Roland Pfeiffer',
+    author_email='',
+    description='COMmonly used FUNctions',
+    install_requires=[
+        "opencv-python",
+        "PyPDF2",
+        "wget",
+        "setuptools",
+    ]
+)
