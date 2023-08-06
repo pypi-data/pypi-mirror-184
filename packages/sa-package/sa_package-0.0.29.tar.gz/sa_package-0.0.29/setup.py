1fdcfa6be82d045e00699b01222f46ee4fb05460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:52 +0000

    Add package sa-package/0.0.29/sa_package-0.0.29.tar.gz

---
+++ packages/sa-package/sa_package-0.0.29.tar.gz/sa_package-0.0.29/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sa_package",
+    version="0.0.29",
+    
+    url="https://github.com/tmddk2709/sa_package",
+    author="Seunga Shin",
+    author_email="seungashin9275@gmail.com",
+
+    packages=find_packages(),
+
+    install_requires=[
+        "bs4",
+        "pandas",
+        "gspread",
+        "oauth2client",
+        "google-api-python-client",
+        "google-auth-httplib2",
+        "google-auth-oauthlib",
+        "selenium",
+        "webdriver-manager",
+        "packaging",
+        "pymysql"
+    ]
+)
