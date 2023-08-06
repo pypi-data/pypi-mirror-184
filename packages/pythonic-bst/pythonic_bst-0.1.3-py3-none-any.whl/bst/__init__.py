a2e051e84c9a13d4f85d8705add58bf1dae78fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:37 +0000

    Add package pythonic-bst/0.1.3/pythonic_bst-0.1.3-py3-none-any.whl

---
+++ packages/pythonic-bst/pythonic_bst-0.1.3-py3-none-any.whl/bst/__init__.py
@@ -0,0 +1,11 @@
+# ___  _   _ ___ _  _ ____ _  _ _ ____    ___  ____ ___
+# |__]  \_/   |  |__| |  | |\ | | |       |__] [__   |
+# |      |    |  |  | |__| | \| | |___    |__] ___]  |
+# <=<=<<https://github.com/squillero/pythonic-bst>>=>=>
+
+# Copyright 2022 Giovanni Squillero.
+# SPDX-License-Identifier: 0BSD
+
+__version__ = '0.1.3'
+
+from .core import BST
