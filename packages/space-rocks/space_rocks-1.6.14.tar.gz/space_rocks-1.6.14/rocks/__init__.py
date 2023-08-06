9898ba2b744bede0ed9c1ce88cec2e27ea5dab1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:34 +0000

    Add package space-rocks/1.6.14/space_rocks-1.6.14.tar.gz

---
+++ packages/space-rocks/space_rocks-1.6.14.tar.gz/space_rocks-1.6.14/rocks/__init__.py
@@ -0,0 +1,27 @@
+"""For space rocks."""
+
+import inspect
+from pathlib import Path
+
+from . import config
+
+# Welcome to rocks
+__version__ = "1.6.14"
+
+# Only define user API if rocks is not called via command line
+context = inspect.stack()[-1].code_context
+if context is None or "rocks.cli" not in context[0]:
+
+    # Expose API to user
+    from .core import Rock
+    from .core import rocks_ as rocks
+    from .resolve import identify
+
+    # Alias id to identify
+    id = identify
+
+# Ensure the asteroid name-number index exists
+if not config.PATH_INDEX.is_dir():
+    from . import index
+
+    index._ensure_index_exists()
