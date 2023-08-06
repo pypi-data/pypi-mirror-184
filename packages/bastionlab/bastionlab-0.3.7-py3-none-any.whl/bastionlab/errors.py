090b6ff9c20588fc88bdfb62b0388e4e885ff9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:49 +0000

    Add package bastionlab/0.3.7/bastionlab-0.3.7-py3-none-any.whl

---
+++ packages/bastionlab/bastionlab-0.3.7-py3-none-any.whl/bastionlab/errors.py
@@ -0,0 +1,74 @@
+import grpc  # type: ignore [import]
+from grpc._channel import _InactiveRpcError, _MultiThreadedRendezvous  # type: ignore [import]
+from dataclasses import dataclass
+from typing import Callable, TypeVar, Union
+
+
+T = TypeVar("T")
+
+
+@dataclass
+class GRPCException(Exception):
+    """
+    A custom exception class for wrapping gRPC errors. This allows for better error
+    handling and display.
+
+    Args:
+        err (Union[grpc._channel._InactiveRpcError, grpc._channel._MultiThreadedRendezvous]):
+            The gRPC error that was caught and wrapped by this exception.
+    """
+
+    err: Union[grpc._channel._InactiveRpcError, grpc._channel._MultiThreadedRendezvous]
+
+    @property
+    def code(self) -> grpc.StatusCode:
+        """
+        Get the status code of the gRPC error.
+
+        Returns:
+            The status code of the gRPC error.
+        """
+        return self.err._state.code
+
+    def __str__(self):
+        if self.code == grpc.StatusCode.NOT_FOUND:
+            prefix = "Remote resource not found"
+        elif self.code == grpc.StatusCode.INVALID_ARGUMENT:
+            prefix = "Invalid argument passed to server"
+        elif self.code == grpc.StatusCode.OUT_OF_RANGE:
+            prefix = "Not yet available (or out-of-range) resource"
+        elif self.code == grpc.StatusCode.INTERNAL:
+            prefix = "Internal server error"
+        elif self.code == grpc.StatusCode.CANCELLED:
+            prefix = f"Cancelled gRPC call"
+        elif self.code == grpc.StatusCode.UNAVAILABLE:
+            prefix = "Connection to the gRPC server failed"
+        elif self.code == grpc.StatusCode.UNIMPLEMENTED:
+            prefix = "Incompatible client/server versions"
+        elif self.code == grpc.StatusCode.FAILED_PRECONDITION:
+            prefix = "Attestation is not available. Running in Simulation Mode"
+        else:
+            prefix = f"Received gRPC error"
+
+        return f"{prefix}: code={self.code} message={self.err.details()}"
+
+    @staticmethod
+    def map_error(f: Callable[[], T]) -> T:
+        """
+        Map gRPC errors to `GRPCException` exceptions.
+
+        Args:
+            f: The function to call and map errors from.
+
+        Returns:
+            The result of calling `f`, if no errors were raised.
+
+        Raises:
+            GRPCException: if `f` raised a gRPC error.
+        """
+        try:
+            return f()
+        except _InactiveRpcError as e:
+            raise GRPCException(e)
+        except _MultiThreadedRendezvous as e:
+            raise GRPCException(e)
