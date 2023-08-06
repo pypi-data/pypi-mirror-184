b79a722fbd34ad564ece42ae44040a2cb9464aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:54 +0000

    Add package esycord/1.0.1/esycord-1.0.1.tar.gz

---
+++ packages/esycord/esycord-1.0.1.tar.gz/esycord-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='esycord',
+    version='1.0.1',
+    license='MIT',
+    author="EgogorGames",
+    author_email='egogorgames@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/EgogorGames/EasyCord',
+    keywords='esycord',
+    install_requires=[
+          'scikit-learn',
+      ],
+
+)
