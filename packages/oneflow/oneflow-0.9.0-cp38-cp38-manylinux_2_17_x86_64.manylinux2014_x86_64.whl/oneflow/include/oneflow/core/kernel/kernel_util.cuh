fc23633da90f6b01215f4fa1870f25c069170c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:06 +0000

    Add package oneflow/0.9.0/oneflow-0.9.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/oneflow/oneflow-0.9.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/oneflow/include/oneflow/core/kernel/kernel_util.cuh
@@ -0,0 +1,102 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#ifndef ONEFLOW_CORE_KERNEL_KERNEL_UTIL_CUH_
+#define ONEFLOW_CORE_KERNEL_KERNEL_UTIL_CUH_
+#include "oneflow/core/cuda/atomic.cuh"
+#include "oneflow/core/device/cuda_pseudo_half.h"
+#include "oneflow/core/common/data_type.h"
+#include "oneflow/core/device/cuda_util.h"
+
+namespace oneflow {
+
+template<typename T, typename std::enable_if<IsFloating<T>::value>::type* = nullptr>
+OF_DEVICE_FUNC T MaxWithLogThreshold(T x) {
+  const T threshold = 1e-20;
+  return x > threshold ? x : threshold;
+}
+
+template<typename T, typename std::enable_if<IsIntegral<T>::value>::type* = nullptr>
+OF_DEVICE_FUNC T MaxWithLogThreshold(T x) {
+  return x;
+}
+
+#if defined(__CUDACC__)
+__device__ __forceinline__ half MaxWithLogThreshold(half x) {
+  half threshold = hexp2(__float2half(-14.0));
+  if (__hgt(x, threshold)) { return x; }
+  return threshold;
+}
+#endif
+
+template<typename T>
+OF_DEVICE_FUNC T SafeLog(T x) {
+  return logf(MaxWithLogThreshold(x));
+}
+
+#if defined(__CUDACC__)
+__device__ __forceinline__ half SafeLog(half x) { return hlog(MaxWithLogThreshold(x)); }
+#endif
+
+// FastAtomicAdd is referenced from
+// https://github.com/pytorch/pytorch/blob/396c3b1d88d7624938a2bb0b287f2a19f1e89bb4/aten/src/ATen/native/cuda/KernelUtils.cuh#L29
+#if defined(__CUDACC__)
+template<typename T, typename IndexT,
+         typename std::enable_if<std::is_same<half, T>::value>::type* = nullptr>
+__device__ __forceinline__ void fastSpecializedAtomicAdd(T* base, IndexT offset,
+                                                         const IndexT length, T value) {
+#if ((defined(CUDA_VERSION) && (CUDA_VERSION < 10000)) \
+     || (defined(__CUDA_ARCH__) && (__CUDA_ARCH__ < 700)))
+  cuda::atomic::Add(reinterpret_cast<half*>(base) + offset, static_cast<half>(value));
+#else
+  // Accounts for the chance base falls on an odd 16 bit alignment (ie, not 32 bit aligned)
+  __half* target_addr = reinterpret_cast<__half*>(base + offset);
+  bool low_byte = (reinterpret_cast<std::uintptr_t>(target_addr) % sizeof(__half2) == 0);
+
+  if (low_byte && offset < (length - 1)) {
+    __half2 value2;
+    value2.x = value;
+    value2.y = __float2half_rz(0);
+    cuda::atomic::Add(reinterpret_cast<__half2*>(target_addr), value2);
+
+  } else if (!low_byte && offset > 0) {
+    __half2 value2;
+    value2.x = __float2half_rz(0);
+    value2.y = value;
+    cuda::atomic::Add(reinterpret_cast<__half2*>(target_addr - 1), value2);
+
+  } else {
+    cuda::atomic::Add(reinterpret_cast<__half*>(base) + offset, static_cast<__half>(value));
+  }
+#endif
+}
+
+template<typename T, typename IndexT,
+         typename std::enable_if<!std::is_same<half, T>::value>::type* = nullptr>
+__device__ __forceinline__ void fastSpecializedAtomicAdd(T* base, IndexT offset,
+                                                         const IndexT length, T value) {
+  cuda::atomic::Add(base + offset, value);
+}
+
+template<class T, class IndexT>
+__device__ __forceinline__ void FastAtomicAdd(T* base, IndexT offset, const IndexT length,
+                                              T value) {
+  fastSpecializedAtomicAdd(base, offset, length, value);
+}
+#endif
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_KERNEL_KERNEL_UTIL_CUH_
