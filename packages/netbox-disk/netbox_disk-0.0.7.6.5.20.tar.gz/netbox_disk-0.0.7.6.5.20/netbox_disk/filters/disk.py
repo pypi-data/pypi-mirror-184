962f487f0d5e0a50d7e9134e83e11303bbd8a14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:38 +0000

    Add package netbox-disk/0.0.7.6.5.20/netbox_disk-0.0.7.6.5.20.tar.gz

---
+++ packages/netbox-disk/netbox_disk-0.0.7.6.5.20.tar.gz/netbox_disk-0.0.7.6.5.20/netbox_disk/filters/disk.py
@@ -0,0 +1,32 @@
+import django_filters
+from django.db.models import Q
+
+from netbox.filtersets import NetBoxModelFilterSet
+
+from netbox_disk.models import Disk
+from virtualization.models import Cluster
+
+
+class DiskFilter(NetBoxModelFilterSet):
+    """Filter capabilities for Disk instances."""
+    cluster = django_filters.ModelMultipleChoiceFilter(
+        field_name='cluster__name',
+        queryset=Cluster.objects.all(),
+        to_field_name='name',
+        label='Cluster',
+    )
+
+    class Meta:
+        model = Disk
+        fields = ("vg_name", "lv_name", "size", "path", "cluster")
+
+    def search(self, queryset, name, value):
+        """Perform the filtered search."""
+        if not value.strip():
+            return queryset
+        qs_filter = (
+            Q(vg_name__icontains=value)
+            | Q(lv_name__icontains=value)
+            | Q(size__icontains=value)
+        )
+        return queryset.filter(qs_filter)
