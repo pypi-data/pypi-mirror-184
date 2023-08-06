39a137a2dbce812093cb07830828070710c581e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:21 +0000

    Add package pytubedata/1.0.2/pytubedata-1.0.2.tar.gz

---
+++ packages/pytubedata/pytubedata-1.0.2.tar.gz/pytubedata-1.0.2/setup.py
@@ -0,0 +1,31 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+with open ("README.md", "r") as rf:
+    long_description= rf.read()
+
+setup(
+    name="pytubedata",
+    version="1.0.2",
+    description="A simple wrapper for YouTube Data API v3 written in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/professor91/pytubedata",
+    author="Keshav Saini",
+    author_email="keshavandteam@gmail.com",
+    packages=["Youtube"],
+    keywords=["youtubedata", "youtubedataapi", "pytubedata", "pytubedata.py", "googleapi"],
+    install_requires=[
+        "requests"
+        ],
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent"
+    ],
+    # python_requires="~=3.5"
+)
