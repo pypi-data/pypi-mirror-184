3535d4f568120d3a4d5bac4988f1ab48e68ba2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:04 +0000

    Add package fritzbox-tray/1.0.30/fritzbox_tray-1.0.30.tar.gz

---
+++ packages/fritzbox-tray/fritzbox_tray-1.0.30.tar.gz/fritzbox_tray-1.0.30/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['ft']
+install_requires = \
+['Pillow>=9.4.0,<10.0.0',
+ 'aiohttp>=3.8.1,<4.0.0',
+ 'certifi>=2022.12.7,<2023.0.0',
+ 'charset-normalizer>=2.1.1,<3.0.0',
+ 'idna>=3.4,<4.0',
+ 'pystray>=0.19.4,<0.20.0',
+ 'requests>=2.28.1,<3.0.0',
+ 'six>=1.16.0,<2.0.0',
+ 'urllib3>=1.26.13,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['fritzbox-tray = fritzbox_tray.__main__:main']}
+
+setup_kwargs = {
+    'name': 'fritzbox-tray',
+    'version': '1.0.30',
+    'description': 'The description of the package',
+    'long_description': 'None',
+    'author': 'Andreas Violaris',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/aviolaris/fritzbox-tray',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
