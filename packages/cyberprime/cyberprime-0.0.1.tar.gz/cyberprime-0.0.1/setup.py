7e0bef8aabe664fe730bf8ed8ba7570f7cb15a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:23 +0000

    Add package cyberprime/0.0.1/cyberprime-0.0.1.tar.gz

---
+++ packages/cyberprime/cyberprime-0.0.1.tar.gz/cyberprime-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'test buiding packages'
+LONG_DESCRIPTION = 'A package that allows to ___.'
+
+# Setting up
+setup(
+    name="cyberprime",
+    version=VERSION,
+    author="Blank",
+    author_email="<blank@dineonwine.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(
+        where='src/cyberprime',
+        include='utils'
+    ),
+    install_requires=['numpy', 'matplotlib'],
+    keywords=['python', 'useless'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
