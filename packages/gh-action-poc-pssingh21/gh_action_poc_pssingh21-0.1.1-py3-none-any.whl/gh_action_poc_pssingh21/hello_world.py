ac83238b9dfd0f03b4300570f744b4c1f5a6c069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:21 +0000

    Add package gh-action-poc-pssingh21/0.1.1/gh_action_poc_pssingh21-0.1.1-py3-none-any.whl

---
+++ packages/gh-action-poc-pssingh21/gh_action_poc_pssingh21-0.1.1-py3-none-any.whl/gh_action_poc_pssingh21/hello_world.py
@@ -0,0 +1,12 @@
+import sys
+import datetime
+import os
+
+def sayHello():
+    print('Hello World')
+
+print('Hello ' + sys.argv[1])
+time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    print(f'time={time}', file=fh)
