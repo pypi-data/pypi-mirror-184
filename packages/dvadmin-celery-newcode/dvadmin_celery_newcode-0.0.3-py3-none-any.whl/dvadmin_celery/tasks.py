5ce59e6a1a25017fd81311b6c91a78e03773b3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:35 +0000

    Add package dvadmin-celery-newcode/0.0.3/dvadmin_celery_newcode-0.0.3-py3-none-any.whl

---
+++ packages/dvadmin-celery-newcode/dvadmin_celery_newcode-0.0.3-py3-none-any.whl/dvadmin_celery/tasks.py
@@ -0,0 +1,11 @@
+'''
+Description:
+Version:
+Author: fanji.meng
+Date: 2023-01-06 14:09:43
+LastEditors: fanji.meng
+LastEditTime: 2023-01-06 14:12:25
+'''
+# -*- coding: utf-8 -*-
+
+from application.celery import app
