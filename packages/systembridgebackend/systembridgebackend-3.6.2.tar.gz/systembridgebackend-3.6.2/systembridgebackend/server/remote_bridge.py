9f9203fc9449c45f943f84d3aaed890a637ed622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:02 +0000

    Add package systembridgebackend/3.6.2/systembridgebackend-3.6.2.tar.gz

---
+++ packages/systembridgebackend/systembridgebackend-3.6.2.tar.gz/systembridgebackend-3.6.2/systembridgebackend/server/remote_bridge.py
@@ -0,0 +1,10 @@
+"""System Bridge: Server Handler - Remote Bridge"""
+from systembridgeshared.database import Database
+from systembridgeshared.models.database_data_remote_bridge import RemoteBridge
+
+
+def get_remote_bridges(
+    database: Database,
+) -> list[RemoteBridge]:
+    """Get all remote bridges."""
+    return database.get_data(RemoteBridge)
