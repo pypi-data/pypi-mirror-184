0cb5063a8423975b936ffd4c69236097b7eafbca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:29 +0000

    Add package b2-utils/0.1.4/b2-utils-0.1.4.tar.gz

---
+++ packages/b2-utils/b2-utils-0.1.4.tar.gz/b2-utils-0.1.4/src/b2_utils/serializers/_serializers.py
@@ -0,0 +1,62 @@
+from rest_framework import serializers
+
+from b2_utils.models import Address, City, Phone
+
+__all__ = [
+    "PrimaryKeyRelatedFieldWithSerializer",
+    "PhoneSerializer",
+    "CitySerializer",
+    "AddressSerializer",
+]
+
+
+class PrimaryKeyRelatedFieldWithSerializer(serializers.PrimaryKeyRelatedField):
+    def __init__(self, representation_serializer, **kwargs):
+        self.representation_serializer = representation_serializer
+        super().__init__(**kwargs)
+
+    def to_representation(self, value):
+        if callable(value):
+            return self.representation_serializer(value.all(), many=True).data
+
+        instance = self.queryset.get(pk=value.pk)
+
+        return self.representation_serializer(instance).data
+
+
+class PhoneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Phone
+        fields = ["id", "country_code", "area_code", "number", "created", "modified"]
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = [
+            "id",
+            "name",
+            "state",
+            "created",
+            "modified",
+        ]
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    city = PrimaryKeyRelatedFieldWithSerializer(
+        CitySerializer, queryset=City.objects.all()
+    )
+
+    class Meta:
+        model = Address
+        fields = [
+            "id",
+            "city",
+            "street",
+            "number",
+            "additional_info",
+            "district",
+            "zip_code",
+            "created",
+            "modified",
+        ]
