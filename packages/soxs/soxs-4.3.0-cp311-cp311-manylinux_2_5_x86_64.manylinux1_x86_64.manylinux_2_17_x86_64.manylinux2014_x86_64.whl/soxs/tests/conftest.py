77145ad4bda535ac970bbbc1929fe50f0aa04635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:49 +0000

    Add package soxs/4.3.0/soxs-4.3.0-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/soxs/soxs-4.3.0-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/soxs/tests/conftest.py
@@ -0,0 +1,35 @@
+import os
+
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--answer_dir", help="Directory where answers are stored.")
+    parser.addoption(
+        "--answer_store",
+        action="store_true",
+        help="Generate new answers, but don't test.",
+    )
+
+
+@pytest.fixture()
+def answer_store(request):
+    return request.config.getoption("--answer_store")
+
+
+@pytest.fixture()
+def answer_dir(request):
+    ad = request.config.getoption("--answer_dir")
+    if ad is None:
+        pytest.skip("No answer directory")
+    ad = os.path.abspath(ad)
+    if not os.path.exists(ad):
+        os.makedirs(ad)
+    return ad
+
+
+@pytest.fixture
+def info_with_some_attr(info):
+    if not hasattr(info, "some_attr"):
+        pytest.skip("Missing attribute 'some_attr'")
+    yield info
