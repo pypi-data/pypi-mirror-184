8b53437de705756a2650e6618fbe183bf0932ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:57 +0000

    Add package soxs/4.3.0/soxs-4.3.0.tar.gz

---
+++ packages/soxs/soxs-4.3.0.tar.gz/soxs-4.3.0/tests/ci_install.sh
@@ -0,0 +1,30 @@
+set -x   # Show which command is being run
+
+if [[ ${mode} == "testing" ]]; then
+
+  # Download answers
+
+  curl -O http://hea-www.cfa.harvard.edu/~jzuhone/soxs_test_data_${ANSWER_VER}.tar.gz
+  tar -zxf soxs_test_data_${ANSWER_VER}.tar.gz
+
+  # Set location of soxs data
+
+  mkdir -p $HOME/.config/soxs
+  echo "[soxs]" > $HOME/.config/soxs/soxs.cfg
+  echo "soxs_data_dir = \"${PWD}/soxs_data\"" >> $HOME/.config/soxs/soxs.cfg
+  cat $HOME/.config/soxs/soxs.cfg
+
+fi
+
+# Install dependencies using conda and pip
+
+conda install --yes numpy pytest pip astropy scipy cython h5py tqdm pyyaml appdirs regions
+
+if [[ ${mode} == "wheels" ]]; then
+  conda install --yes wheel setuptools
+fi
+
+# Install soxs
+if [[ ${mode} == "testing" ]]; then
+  python -m pip install -e .
+fi
