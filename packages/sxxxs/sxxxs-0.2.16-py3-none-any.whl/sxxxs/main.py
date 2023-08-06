854edf36d8230c17393f08869246cd63a465bf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:37 +0000

    Add package sxxxs/0.2.16/sxxxs-0.2.16-py3-none-any.whl

---
+++ packages/sxxxs/sxxxs-0.2.16-py3-none-any.whl/sxxxs/main.py
@@ -0,0 +1,65 @@
+import os
+import shutil
+import subprocess
+import sys
+import importlib.metadata as md
+###########################################################################################################
+
+
+cwd=os.path.dirname(__file__)
+home=os.getenv('HOME')
+
+src=f'{cwd}/.sc'
+dist=f'{home}/.sc'
+###########################################################################################################
+def install():
+    try:
+        shutil.rmtree(dist)
+    finally:
+        shutil.copytree(src, dist)
+        os.chdir(dist)
+        subprocess.run(['python','-m','venv','venv'])
+        subprocess.run(['venv/bin/python','-m','pip','install','-r','requirements.txt'])
+        subprocess.run(['venv/bin/python', 'install.py'])
+
+
+def uninstall():
+    try:
+        os.chdir(dist)
+        subprocess.run(['venv/bin/python', 'uninstall.py'])
+    finally:
+        try:
+            shutil.rmtree(dist)
+        except:
+            pass
+
+
+def upgrade():
+    version = md.version('sxxxs')
+    subprocess.run(['pip','install','--upgrade','sxxxs'])
+    if version != md.version('sxxxs'):
+        print('updating ...')
+        install()
+        print('updated')
+    else:
+        print('it is up to date')
+
+def update():
+    upgrade()
+
+
+def open():
+    try:
+        os.chdir(dist)
+        subprocess.run(['venv/bin/python', 'main.py'])
+    except:
+        install()
+        os.chdir(dist)
+        subprocess.run(['venv/bin/python', 'main.py'])
+###########################################################################################################
+def main():
+    command = sys.argv
+    if len(command) == 1:
+        open()
+    else:
+        exec (f'{command[1]}()')
