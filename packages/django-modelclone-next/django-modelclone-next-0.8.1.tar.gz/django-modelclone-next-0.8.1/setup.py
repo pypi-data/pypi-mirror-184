6e5d546be2cd9aba35630734dc4af0c4fcb86537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:17 +0000

    Add package django-modelclone-next/0.8.1/django-modelclone-next-0.8.1.tar.gz

---
+++ packages/django-modelclone-next/django-modelclone-next-0.8.1.tar.gz/django-modelclone-next-0.8.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from os.path import dirname, join
+
+setup(
+    name = "django-modelclone-next",
+    version = "0.8.1",
+    description = "Django application that allows users to clone a model in Admin",
+    long_description='file: README.md',
+    long_description_content_type='text/markdown',
+    url = "https://github.com/xuhcc/django-modelclone",
+    packages = [
+        'modelclone',
+    ],
+    package_data = {
+        'modelclone': ['templates/modelclone/*'],
+    },
+    author = "xuhcc",
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    license='MIT',
+)
