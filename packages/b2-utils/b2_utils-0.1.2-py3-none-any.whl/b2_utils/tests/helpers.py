b25d2850e508861c788cdb76ec63ff67add92ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:15 +0000

    Add package b2-utils/0.1.2/b2_utils-0.1.2-py3-none-any.whl

---
+++ packages/b2-utils/b2_utils-0.1.2-py3-none-any.whl/b2_utils/tests/helpers.py
@@ -0,0 +1,25 @@
+from model_bakery import baker
+from validate_docbr import CNPJ, CPF
+
+from b2_utils.models import Address, City, Phone
+
+
+def sample_city(**kwargs):
+    return baker.make(City, **kwargs)
+
+
+def sample_address(**kwargs):
+    kwargs["city"] = kwargs.get("city", sample_city)
+    return baker.make(Address, **kwargs)
+
+
+def sample_phone(**kwargs):
+    return baker.make(Phone, **kwargs)
+
+
+def sample_cpf():
+    return CPF().generate()
+
+
+def sample_cnpj():
+    return CNPJ().generate()
