eadd6ecd43e60cf4225e80f039a8c7bf5f81a9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:29 +0000

    Add package pkginfo/1.9.4/pkginfo-1.9.4.tar.gz

---
+++ packages/pkginfo/pkginfo-1.9.4.tar.gz/pkginfo-1.9.4/setup.py
@@ -0,0 +1,55 @@
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    extras = {}
+else:
+    extras = {
+        'test_suite': 'pkginfo.tests',
+        'zip_safe': False,
+        'extras_require': {
+            'testing': ['pytest', 'pytest-cov'],
+        },
+    }
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+setup(
+    name='pkginfo',
+    version='1.9.4',
+    description='Query metadatdata from sdists / bdists / installed packages.',
+    platforms=['Unix', 'Windows'],
+    long_description='\n\n'.join([README, CHANGES]),
+    keywords='distribution sdist installed metadata',
+    url='https://code.launchpad.net/~tseaver/pkginfo/trunk',
+    author='Tres Seaver, Agendaless Consulting',
+    author_email='tseaver@agendaless.com',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Software Distribution',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pkginfo = pkginfo.commandline:main',
+        ]
+    },
+    packages=['pkginfo', 'pkginfo.tests'],
+    package_data={'pkginfo': ['py.typed', '*.pyi']},
+    python_requires='>=3.6',
+    **extras
+)
