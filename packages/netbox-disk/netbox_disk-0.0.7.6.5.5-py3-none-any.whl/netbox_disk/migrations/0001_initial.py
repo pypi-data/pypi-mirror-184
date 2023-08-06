aa6cd25d7b443dd3c30ebebd9f2f770ea604c13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:32 +0000

    Add package netbox-disk/0.0.7.6.5.5/netbox_disk-0.0.7.6.5.5-py3-none-any.whl

---
+++ packages/netbox-disk/netbox_disk-0.0.7.6.5.5-py3-none-any.whl/netbox_disk/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+from django.db import migrations, models
+import utilities.json
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    operations = [
+        migrations.CreateModel(
+            name="Disk",
+            fields=[
+                ("created", models.DateField(auto_now_add=True, null=True)),
+                ("last_updated", models.DateTimeField(auto_now=True, null=True)),
+                (
+                    "custom_field_data",
+                    models.JSONField(
+                        blank=True,
+                        default=dict,
+                        encoder=utilities.json.CustomFieldJSONEncoder
+                    ),
+                ),
+                ("id", models.BigAutoField(primary_key=True, serialize=False)),
+                ("vg_name", models.CharField(max_length=255)),
+                ("lv_name", models.CharField(max_length=1000)),
+                ("size", models.PositiveIntegerField()),
+                ("description", models.CharField(max_length=1000)),
+            ],
+            options={
+                "ordering": ("lv_name", "id"),
+            },
+        ),
+    ]
